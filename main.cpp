ae73251e79a362c1cbfa8dd1ca949e83d58d1010
Mati <swichu91@gmail.com>
2019-05-06 13:34:01 +0200

    Added core/gui/utils submodules

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <memory>
+#include "SystemManager/SystemManager.hpp"
+
+
+
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    auto sysmgr = std::make_shared<core::SystemManager>(20000,5000);
+
+    sysmgr->StartSystem();
+
+
+    cpp_freertos::Thread::StartScheduler();
+
     return 0;
 }
