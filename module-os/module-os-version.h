077581d5b2407cf53e5d02a7452bad75de3a0b03
Robert Borzecki <robert.borzecki@mudita.com>
2019-05-21 23:55:42 +0200

    Made linux compilation working.

---
+++ module-os/module-os-version.h
@@ -0,0 +1,9 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// the configured options and settings for PureOS
+#define MODULE_OS "1.0"
+#define GIT_BRANCH "master"
+#define GIT_COMMIT_HASH "f3828cc"
+
+#endif
