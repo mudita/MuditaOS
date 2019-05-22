ef6a816c1c1201191f5ee6bef1e741789feb2a08
Mati <swichu91@gmail.com>
2019-05-22 13:45:30 +0200

    Added module-db + unit tests

---
+++ module-db/targets/Target_Cross.cmake
@@ -0,0 +1,9 @@
+set(BOARD_SOURCES
+        ${CMAKE_CURRENT_SOURCE_DIR}/board/cross/sqlitevfs.cpp
+
+        CACHE INTERNAL ""
+        )
+
+set(BOARD_DIR_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/board/cross CACHE INTERNAL "")
+
+
---
+++ module-db/tests/unittest.cpp
@@ -0,0 +1,29 @@
+/*
+ * @file unittests.cpp
+ * @author Mateusz Piesta (mateusz.piesta@mudita.com)
+ * @date 22.05.19
+ * @brief
+ * @copyright Copyright (C) 2019 mudita.com
+ * @details
+ */
+
+#include "vfs.hpp"
+
+#include "catch.hpp"
+
+#include <stdint.h>
+#include <algorithm>
+
+
+#include <iostream>
+
+
+class vfs vfs;
+
+
+TEST_CASE( "Test case 1" ) {
+
+        vfs.Init();
+
+
+}
