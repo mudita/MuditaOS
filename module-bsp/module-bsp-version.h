31ed37aba4e6dac820f0e02bca9f81517161889a
Mati <swichu91@gmail.com>
2019-05-22 07:56:54 +0200

    Removed submodule's version files

--- module-bsp/module-bsp-version.h
+++
@@ -1,9 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-// the configured options and settings for PureOS
-#define MODULE_BSP "1.0"
-#define GIT_BRANCH "master"
-#define GIT_COMMIT_HASH "f3828cc"
-
-#endif
