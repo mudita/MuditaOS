f42c41dbdf553dde3b24ee26f3e5f3e1b7da3f2e
Mati <swichu91@gmail.com>
2019-05-22 11:53:46 +0200

    Reorganized bsp/os file structure

--- module-os/targets/Target_RT1051.cmake
+++ module-os/targets/Target_RT1051.cmake
@@ -2,8 +2,6 @@ set(BOARD_SOURCES
         ${CMAKE_CURRENT_SOURCE_DIR}/board/rt1051/port.c
         ${CMAKE_CURRENT_SOURCE_DIR}/board/rt1051/fsl_tickless_gpt.c
         ${CMAKE_CURRENT_SOURCE_DIR}/board/rt1051/fsl_tickless_systick.c
-        ${CMAKE_CURRENT_SOURCE_DIR}/board/rt1051/system_MIMXRT1051.c
-
 
         CACHE INTERNAL ""
         )
