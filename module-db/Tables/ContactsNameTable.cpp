3e717e2b0118b0674099a92218b035114381f996
Mati <swichu91@gmail.com>
2019-05-28 13:58:16 +0200

    Added ContactsNameTable

---
+++ module-db/Tables/ContactsNameTable.cpp
@@ -0,0 +1,129 @@
+
+/*
+ * @file ContactsName.cpp
+ * @author Mateusz Piesta (mateusz.piesta@mudita.com)
+ * @date 28.05.19
+ * @brief
+ * @copyright Copyright (C) 2019 mudita.com
+ * @details
+ */
+
+
+#include "ContactsNameTable.hpp"
+
+
+ContactsNameTable::ContactsNameTable(Database *db):db(db) {
+
+}
+
+ContactsNameTable::~ContactsNameTable() {
+
+}
+
+bool ContactsNameTable::Create() {
+    return db->Execute(createTableQuery);
+}
+
+bool ContactsNameTable::Add(ContactsNameTableRow entry) {
+    return db->Execute(
+            "insert into contact_name (contact_id, name_primary, name_alternative) VALUES (%lu, '%s', '%s');",
+            entry.contactID,
+            entry.namePrimary.c_str(),
+            entry.nameAlternative.c_str()
+    );
+}
+
+bool ContactsNameTable::RemoveByID(uint32_t id) {
+    return db->Execute("DELETE FROM contact_name where _id = %u;", id);
+}
+
+bool ContactsNameTable::Update(ContactsNameTableRow entry) {
+    return db->Execute(
+            "UPDATE contact_name SET contact_id = %lu, name_primary = '%s' ,name_alternative = '%s' WHERE _id=%lu;",
+            entry.contactID,
+            entry.namePrimary.c_str(),
+            entry.nameAlternative.c_str(),
+            entry.ID
+    );
+}
+
+ContactsNameTableRow ContactsNameTable::GetByID(uint32_t id) {
+    auto retQuery = db->Query("SELECT * FROM contact_name WHERE _id= %lu;", id);
+
+    if (retQuery->GetRowCount() == 0) {
+        return ContactsNameTableRow();
+    }
+
+    return ContactsNameTableRow{(*retQuery)[0].GetUInt32(),  // ID
+                            (*retQuery)[1].GetUInt32(),    // contactID
+                            (*retQuery)[2].GetString(),    // namePrimary
+                            (*retQuery)[3].GetString(),    // nameAlternative
+    };
+}
+
+std::vector<ContactsNameTableRow> ContactsNameTable::GetLimitOffset(uint32_t offset, uint32_t limit) {
+    auto retQuery = db->Query("SELECT * from contact_name ORDER BY name_primary LIMIT %lu OFFSET %lu;",
+                              limit,
+                              offset);
+
+    if (retQuery->GetRowCount() == 0) {
+        return std::vector<ContactsNameTableRow>();
+    }
+
+    std::vector<ContactsNameTableRow> ret;
+
+    do {
+        ret.push_back(ContactsNameTableRow{(*retQuery)[0].GetUInt32(),  // ID
+                                           (*retQuery)[1].GetUInt32(),    // contactID
+                                           (*retQuery)[2].GetString(),    // namePrimary
+                                           (*retQuery)[3].GetString(),    // nameAlternative
+        });
+    } while (retQuery->NextRow());
+
+    return ret;
+}
+
+std::vector<ContactsNameTableRow> ContactsNameTable::GetLimitOffsetByFieldID(uint32_t offset, uint32_t limit,
+                                                                             const char *field, uint32_t id) {
+    auto retQuery = db->Query("SELECT * from contact_name WHERE %s=%lu ORDER BY name_primary LIMIT %lu OFFSET %lu;",
+                              field,
+                              id,
+                              limit,
+                              offset);
+
+    if ((retQuery == nullptr) || (retQuery->GetRowCount() == 0)) {
+        return std::vector<ContactsNameTableRow>();
+    }
+
+    std::vector<ContactsNameTableRow> ret;
+
+    do {
+        ret.push_back(ContactsNameTableRow{(*retQuery)[0].GetUInt32(),  // ID
+                                           (*retQuery)[1].GetUInt32(),    // contactID
+                                           (*retQuery)[2].GetString(),    // namePrimary
+                                           (*retQuery)[3].GetString(),    // nameAlternative
+        });
+    } while (retQuery->NextRow());
+
+    return ret;
+}
+
+uint32_t ContactsNameTable::GetCount() {
+    auto queryRet = db->Query("SELECT COUNT(*) FROM contact_name;");
+
+    if (queryRet->GetRowCount() == 0) {
+        return 0;
+    }
+
+    return uint32_t{(*queryRet)[0].GetUInt32()};
+}
+
+uint32_t ContactsNameTable::GetCountByFieldID(const char *field, uint32_t id) {
+    auto queryRet = db->Query("SELECT COUNT(*) FROM contact_name WHERE %s=%lu;",field,id);
+
+    if ((queryRet == nullptr) || (queryRet->GetRowCount() == 0)) {
+        return 0;
+    }
+
+    return uint32_t{(*queryRet)[0].GetUInt32()};
+}
---
+++ module-db/tests/ContactsNameTable_tests.cpp
@@ -0,0 +1,106 @@
+
+/*
+ * @file ContactsNameTable_tests.cpp
+ * @author Mateusz Piesta (mateusz.piesta@mudita.com)
+ * @date 28.05.19
+ * @brief
+ * @copyright Copyright (C) 2019 mudita.com
+ * @details
+ */
+
+
+#include "vfs.hpp"
+
+#include "catch.hpp"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+
+#include <iostream>
+
+#include "../Database/Database.hpp"
+#include "../Databases/ContactsDB.hpp"
+
+#include "Tables/ContactsTable.hpp"
+
+TEST_CASE("Contacts Name Table tests")
+{
+    Database::Initialize();
+
+    vfs.remove(ContactsDB::GetDBName());
+
+    ContactsDB contactsdb;
+
+    ContactsNameTableRow testRow1 = {.ID=0,
+            .contactID=0,
+            .namePrimary="Mateusz",
+            .nameAlternative="Pati"
+    };
+
+    // Add 4 elements into table
+    REQUIRE(contactsdb.name.Add(testRow1));
+    REQUIRE(contactsdb.name.Add(testRow1));
+    REQUIRE(contactsdb.name.Add(testRow1));
+    REQUIRE(contactsdb.name.Add(testRow1));
+
+    // Table should have 4 elements
+    REQUIRE(contactsdb.name.GetCount() == 4);
+
+    // Update existing element in table
+    testRow1.ID = 4;
+    testRow1.nameAlternative = "Pateusz";
+    REQUIRE(contactsdb.name.Update(testRow1));
+
+    // Get table row using valid ID & check if it was updated
+    auto sms = contactsdb.name.GetByID(4);
+    REQUIRE(sms.nameAlternative == testRow1.nameAlternative);
+
+    // Get table row using invalid ID(should return empty contactsdb.nameRow)
+    auto smsFailed = contactsdb.name.GetByID(100);
+    REQUIRE(smsFailed.nameAlternative  == "");
+
+    // Get table rows using valid offset/limit parameters
+    auto retOffsetLimit = contactsdb.name.GetLimitOffset(0,4);
+    REQUIRE(retOffsetLimit.size() == 4);
+
+    // Get table rows using valid offset/limit parameters and specific field's ID
+    REQUIRE(contactsdb.name.GetLimitOffsetByFieldID(0,4,"contact_id", 0).size() == 4);
+
+    // Get table rows using valid offset/limit parameters and specific field's ID
+    REQUIRE(contactsdb.name.GetLimitOffsetByFieldID(0,4,"contact_id", 100).size() == 0);
+
+    // Get table rows using invalid limit parameters(should return 4 elements instead of 100)
+    auto retOffsetLimitBigger = contactsdb.name.GetLimitOffset(0,100);
+    REQUIRE(retOffsetLimitBigger.size() == 4);
+
+    // Get table rows using invalid offset/limit parameters(should return empty object)
+    auto retOffsetLimitFailed = contactsdb.name.GetLimitOffset(5,4);
+    REQUIRE(retOffsetLimitFailed.size() == 0);
+
+    // Get count of elements by field's ID
+    REQUIRE(contactsdb.name.GetCountByFieldID("contact_id",0) == 4);
+
+    // Get count of elements by invalid field's ID
+    REQUIRE(contactsdb.name.GetCountByFieldID("invalid_field",0) == 0);
+
+
+    REQUIRE(contactsdb.name.RemoveByID(2));
+
+    // Table should have now 3 elements
+    REQUIRE(contactsdb.name.GetCount() == 3);
+
+    // Remove non existing element
+    REQUIRE(contactsdb.name.RemoveByID(100));
+
+    // Remove all elements from table
+    REQUIRE(contactsdb.name.RemoveByID(1));
+    REQUIRE(contactsdb.name.RemoveByID(3));
+    REQUIRE(contactsdb.name.RemoveByID(4));
+
+    // Table should be empty now
+    REQUIRE(contactsdb.name.GetCount() == 0);
+
+    Database::Deinitialize();
+}
