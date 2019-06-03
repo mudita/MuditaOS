2782c6b3ea18261355be41d68294f6dd793879b5
Mati <swichu91@gmail.com>
2019-06-03 09:09:22 +0200

    Added i18 module

---
+++ module-utils/i18/i18.cpp
@@ -0,0 +1,22 @@
+/*
+ *  @file i18.cpp
+ *  @author Mateusz Piesta (mateusz.piesta@mudita.com)
+ *  @date 29 mar 2019
+ *  @brief I18 implementation
+ *  @copyright Copyright (C) 2019 mudita.com
+ *  @details
+ */
+
+#include "i18.hpp"
+
+
+namespace utils
+{
+
+i18 localize;
+
+}
+
+
+
+
