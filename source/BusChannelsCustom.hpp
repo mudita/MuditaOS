71a5539e8cc059a5821cfed82074cb5c3dc54155
Mati <swichu91@gmail.com>
2019-07-08 10:17:11 +0200

    Fixed BusChannelsCustom.hpp compilation problems

---
+++ source/BusChannelsCustom.hpp
@@ -0,0 +1,12 @@
+//
+// Created by mati on 28.04.19.
+//
+
+#ifndef MODULE_CORE_BUSCHANNELSCUSTOM_HPP
+#define MODULE_CORE_BUSCHANNELSCUSTOM_HPP
+
+// Custom Bus channels
+TestCustomBusChannel,
+Test2CustomBusChannel
+
+#endif //MODULE_CORE_BUSCHANNELSCUSTOM_HPP
