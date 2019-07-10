c646ae8c51213426203dc228858748adb440f0be
Kuba <jakub.kleczkowski@mudita.com>
2019-07-10 14:47:26 +0200

    Battery charger linux driver cleaned.

--- module-bsp/board/linux/battery-charger/battery_charger.cpp
+++ module-bsp/board/linux/battery-charger/battery_charger.cpp
@@ -18,15 +18,6 @@
 #define BSP_TOP_CONTROLLER_I2C_ADDR                         (0xCC >> 1)
 
 
-
-
-static const uint16_t chargerRegs [][2] = {
-		{static_cast<uint16_t>(bsp::batteryChargerRegisters::RepCap_REG) , 2000},
-		{0,0}
-};
-
-static const uint32_t secondsToTick = 2;
-
 static xQueueHandle qHandleIrq = NULL;
 static TaskHandle_t battery_worker_handle = NULL;
 
@@ -50,7 +41,7 @@ namespace bsp{
 		qHandleIrq = NULL;
 		vTaskDelete(battery_worker_handle);
 	}
-	void battery_getData(uint8_t& levelPercent)
+	void battery_getBatteryLevel(uint8_t& levelPercent)
 	{
 			levelPercent = battLevel;
 	}
@@ -59,70 +50,16 @@ namespace bsp{
 	{
 		status = plugged;
 	}
-	int battery_fuelGaugeWrite(bsp::batteryChargerRegisters registerAddress, uint16_t value)
+
+
+	void battery_ClearAllIRQs(void)
 	{
 
-	//    bsp_i2c_inst_t* i2c = (bsp_i2c_inst_t*)BOARD_GetI2CInstance();
-	//    return (int)bsp_i2c_Send(i2c, BSP_FUEL_GAUGE_I2C_ADDR, registerAddress, (uint8_t*)&value, sizeof(uint16_t));
 	}
 
-	int battery_fuelGaugeRead(bsp::batteryChargerRegisters registerAddress, uint16_t* value)
+	void battery_clearFuelGuageIRQ(void)
 	{
-		uint32_t i = 0;
-		while(chargerRegs[i][0] != 0)
-		{
-			if( chargerRegs[i][0] == static_cast<uint16_t>(registerAddress) )
-			{
-				*value = chargerRegs[i][1];
-				break;
-			}
-			i++;
-		}
-		return 0;
-	//    if (value == NULL)
-	//    {
-	//        return -1;
-	//    }
-	//
-	//    bsp_i2c_inst_t* i2c = (bsp_i2c_inst_t*)BOARD_GetI2CInstance();
-	//    return (int)bsp_i2c_Receive(i2c, BSP_FUEL_GAUGE_I2C_ADDR, registerAddress, (uint8_t*)value, sizeof(uint16_t));
-	}
 
-	int battery_chargerWrite(bsp::batteryChargerRegisters registerAddress, uint16_t value)
-	{
-	//    bsp_i2c_inst_t* i2c = (bsp_i2c_inst_t*)BOARD_GetI2CInstance();
-	//
-	//    return (int)bsp_i2c_Send(i2c, BSP_BATTERY_CHARGER_I2C_ADDR, registerAddress, (uint8_t*)&value, sizeof(uint16_t));
-	}
-
-	int battery_chargerRead(bsp::batteryChargerRegisters registerAddress, uint16_t* value)
-	{
-	//    if (value == NULL)
-	//    {
-	//        return -1;
-	//    }
-	//
-	//    bsp_i2c_inst_t* i2c = (bsp_i2c_inst_t*)BOARD_GetI2CInstance();
-	//    return (int)bsp_i2c_Receive(i2c, BSP_BATTERY_CHARGER_I2C_ADDR, registerAddress, (uint8_t*)value, sizeof(uint16_t));
-	}
-
-
-	int battery_chargerTopControllerWrite(bsp::batteryChargerRegisters registerAddress, uint16_t value)
-	{
-	//    bsp_i2c_inst_t* i2c = (bsp_i2c_inst_t*)BOARD_GetI2CInstance();
-	//
-	//    return (int)bsp_i2c_Send(i2c, BSP_TOP_CONTROLLER_I2C_ADDR, registerAddress, (uint8_t*)&value, sizeof(uint16_t));
-	}
-
-	int battery_chargerTopControllerRead(bsp::batteryChargerRegisters registerAddress, uint16_t* value)
-	{
-	//    if (value == NULL)
-	//    {
-	//        return -1;
-	//    }
-	//
-	//    bsp_i2c_inst_t* i2c = (bsp_i2c_inst_t*)BOARD_GetI2CInstance();
-	//    return (int)bsp_i2c_Receive(i2c, BSP_TOP_CONTROLLER_I2C_ADDR, registerAddress, (uint8_t*)value, sizeof(uint16_t));
 	}
 
 	static void battery_worker(void *pvp)
@@ -174,44 +111,5 @@ namespace bsp{
 	}
 }
 
-/*BaseType_t BSP_BatteryChargerINOKB_IRQHandler()
-{
-//	return 0;
-}
 
-BaseType_t BSP_BatteryChargerWCINOKB_IRQHandler()
-{
-//	return 0;
-}
-
-BaseType_t BSP_BatteryChargerINTB_IRQHandler()
-{
-//	return 0;
-}*/
-
-/*
- * *****************************************************************************************************************************************************
- * *                                                                                                                                                   *
- * *                                                         STATIC FUNCTIONS                                                                          *
- * *                                                                                                                                                   *
- * *****************************************************************************************************************************************************
- */
-
-/*	static void s_BSP_BatteryChargerIrqPinsInit()
-	{
-		gpio_pin_config_t pinConfig;
-
-		pinConfig.direction         = kGPIO_DigitalInput;
-		pinConfig.interruptMode     = kGPIO_IntRisingOrFallingEdge;
-		pinConfig.outputLogic       = 0;
-
-		GPIO_PinInit(BOARD_BATTERY_CHARGER_INOKB_GPIO,      BOARD_BATTERY_CHARGER_INOKB_PIN,    &pinConfig);
-		GPIO_PinInit(BOARD_BATTERY_CHARGER_WCINOKB_GPIO,    BOARD_BATTERY_CHARGER_WCINOKB_PIN,  &pinConfig);
-		GPIO_PinInit(BOARD_BATTERY_CHARGER_INTB_GPIO,       BOARD_BATTERY_CHARGER_INTB_PIN,     &pinConfig);
-
-		GPIO_PortEnableInterrupts(BOARD_BATTERY_CHARGER_INOKB_GPIO, 1U << BOARD_BATTERY_CHARGER_INOKB_PIN);
-		GPIO_PortEnableInterrupts(BOARD_BATTERY_CHARGER_WCINOKB_GPIO, 1U << BOARD_BATTERY_CHARGER_WCINOKB_PIN);
-		GPIO_PortEnableInterrupts(BOARD_BATTERY_CHARGER_INTB_GPIO, 1U << BOARD_BATTERY_CHARGER_INTB_PIN);
-
-	}*/
 
