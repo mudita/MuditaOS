b0f4de0412ca49961d890c5a0b5145f5d81a8f7f
Mateusz <swichu91@gmail.com>
2019-07-15 16:15:22 +0200

    Fixed warnings

--- module-cellular/Modem/CommunicationMuxChannel.cpp
+++ module-cellular/Modem/CommunicationMuxChannel.cpp
@@ -39,7 +39,7 @@ int CommunicationMuxChannel::ParseInputData(uint8_t* data, size_t size) {
         responseBuffer.erase();
         LOG_INFO("Received unneeded data");
     }
-
+    return 1;
 }
 
 
