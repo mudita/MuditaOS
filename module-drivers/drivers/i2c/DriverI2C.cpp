7dc1172a4bd10992f7f65a5536cfd1f04eabc743
Mateusz <swichu91@gmail.com>
2019-08-08 12:15:31 +0200

    WiP: cleaning

--- module-drivers/drivers/i2c/DriverI2C.cpp
+++ module-drivers/drivers/i2c/DriverI2C.cpp
@@ -11,13 +11,27 @@
 
 #include "DriverI2C.hpp"
 
+#if defined(TARGET_RT1051)
+#include "board/rt1051/drivers/RT1051DriverI2C.hpp"
+#elif defined(TARGET_Linux)
+
+//#include ""
+#else
+#error "Unsupported target"
+#endif
+
 
 //TODO:M.P this is only unfinished template, I will continue work on this during power manager development
 std::shared_ptr<DriverI2C> DriverI2C::Create(const DriverParams &params) {
+
+    std::shared_ptr<DriverI2C> inst = instance.lock();
+
+    if(!inst){
+
+    }
 #if defined(TARGET_RT1051)
 
 #elif defined(TARGET_Linux)
-    //inst = std::make_unique<bsp::LinuxAudiocodec>(callback );
 #else
                 #error "Unsupported target"
 #endif
