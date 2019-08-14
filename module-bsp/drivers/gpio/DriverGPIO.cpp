0bc858e1fbed30c3684c68f6a4fdaf8e985c291d
Mateusz <swichu91@gmail.com>
2019-08-14 15:03:09 +0200

    WiP: added GPIO template

---
+++ module-bsp/drivers/gpio/DriverGPIO.cpp
@@ -0,0 +1,12 @@
+/*
+ *  @file DriverGPIO.cpp
+ *  @author Mateusz Piesta (mateusz.piesta@mudita.com)
+ *  @date 14.08.19
+ *  @brief  
+ *  @copyright Copyright (C) 2019 mudita.com
+ *  @details
+ */
+
+
+
+#include "DriverGPIO.hpp"
---
+++ module-bsp/drivers/gpio/DriverGPIO.hpp
@@ -0,0 +1,21 @@
+/*
+ *  @file DriverGPIO.hpp
+ *  @author Mateusz Piesta (mateusz.piesta@mudita.com)
+ *  @date 14.08.19
+ *  @brief  
+ *  @copyright Copyright (C) 2019 mudita.com
+ *  @details
+ */
+
+
+
+#ifndef PUREPHONE_DRIVERGPIO_HPP
+#define PUREPHONE_DRIVERGPIO_HPP
+
+
+class DriverGPIO {
+
+};
+
+
+#endif //PUREPHONE_DRIVERGPIO_HPP
