10d6ee6f60dfda19a57e619c50db98fd5c16a532
pholat <pholat@gmail.com>
2019-09-27 14:22:44 +0200

    Tests: Make test compile again

--- module-db/tests/ContactsTable_tests.cpp
+++ module-db/tests/ContactsTable_tests.cpp
@@ -38,7 +38,6 @@ TEST_CASE("Contacts Table tests")
             .numbersID="0 1 2 3 4",
             .ringID=0,
             .addressIDs="5 6 7 8",
-            .type=ContactType ::PHONE,
             .isOnWhitelist=true,
             .isOnBlacklist=true,
             .isOnFavourites=false,
