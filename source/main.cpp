f356690545292d11c21a2f52f68de8a7fe8c40c1
Mateusz <swichu91@gmail.com>
2019-10-24 14:37:37 +0200

    Test code

--- source/main.cpp
+++ source/main.cpp
@@ -49,7 +49,7 @@ class BlinkyService : public sys::Service {
 public:
     BlinkyService(const std::string &name)
             : sys::Service(name) {
-        timer_id = CreateTimer(5000, true);
+        timer_id = CreateTimer(100, true);
         ReloadTimer(timer_id);
     }
 
@@ -90,7 +90,23 @@ public:
 
     // Invoked when timer ticked
     void TickHandler(uint32_t id) override {
-#if 1 // M.P: left here on purpose
+
+        static bool laststate = false;
+
+        if(!laststate){
+            LOG_FATAL("Entering low power..");
+            LPM_EnterLowPowerIdle();
+            GPIO_PinWrite(GPIO1,1,0);
+            laststate = true;
+        }
+        else{
+            LOG_FATAL("Entering full speed..");
+            LPM_EnterFullSpeed();
+            GPIO_PinWrite(GPIO1,1,1);
+            laststate = false;
+        }
+
+#if 0 // M.P: left here on purpose
         LOG_DEBUG("Blinky service tick!");
 
         stopTimer(timer_id);
@@ -119,25 +135,81 @@ public:
 
 #include "board/rt1051/common/clock_config.h"
 
+#define     PAD_CONFIG_SLEW_RATE_SLOW          (0 << 0)
+#define     PAD_CONFIG_SLEW_RATE_FAST          (1 << 0)
+
+#define     PAD_CONFIG_DRIVER_DISABLED         (0 << 3)
+#define     PAD_CONFIG_DRIVER_STRENGTH_LVL_1   (1 << 3)
+#define     PAD_CONFIG_DRIVER_STRENGTH_LVL_2   (2 << 3)
+#define     PAD_CONFIG_DRIVER_STRENGTH_LVL_3   (3 << 3)
+#define     PAD_CONFIG_DRIVER_STRENGTH_LVL_4   (4 << 3)
+#define     PAD_CONFIG_DRIVER_STRENGTH_LVL_5   (5 << 3)
+#define     PAD_CONFIG_DRIVER_STRENGTH_LVL_6   (6 << 3)
+#define     PAD_CONFIG_DRIVER_STRENGTH_LVL_7   (7 << 3)
+
+#define     PAD_CONFIG_SPEED_SLOW_50MHz        (0 << 6)
+#define     PAD_CONFIG_SPEED_MEDIUM_1_100MHz   (1 << 6)
+#define     PAD_CONFIG_SPEED_MEDIUM_2_100MHz   (2 << 6)
+#define     PAD_CONFIG_SPEED_MAX_200MHz        (3 << 6)
+
+#define     PAD_CONFIG_OPEN_DRAIN_DISABLED     (0 << 11)
+#define     PAD_CONFIG_OPEN_DRAIN_ENABLED      (1 << 11)
+
+#define     PAD_CONFIG_PULL_KEEPER_DISABLED    (0 << 12)
+#define     PAD_CONFIG_PULL_KEEPER_ENABLED     (1 << 12)
+
+#define     PAD_CONFIG_SELECT_KEEPER           (0 << 13)
+#define     PAD_CONFIG_SELECT_PULL             (1 << 13)
+
+#define     PAD_CONFIG_PULL_DOWN_100kOhm       (0 << 14)
+#define     PAD_CONFIG_PULL_UP_47kOhm          (1 << 14)
+#define     PAD_CONFIG_PULL_UP_100kOhm         (2 << 14)
+#define     PAD_CONFIG_PULL_UP_22kOhm          (3 << 14)
+
+#define     PAD_CONFIG_HYSTERESIS_DISABLED     (0 << 16)
+#define     PAD_CONFIG_HYSTERESIS_ENABLED      (1 << 16)
+
 int main() {
 
     bsp::BoardInit();
 
     LOG_PRINTF("Launching PurePhone..\n");
 
-/*
-    while(1){
-        LPM_EnterFullSpeed();
-        LPM_EnterLowPowerIdle();
-    }
-*/
 
-    LPM_EnterFullSpeed();
+
+    IOMUXC_SetPinMux(
+            IOMUXC_GPIO_AD_B0_01_GPIO1_IO01,
+            0U);
+
+    IOMUXC_SetPinConfig(
+            IOMUXC_GPIO_AD_B0_01_GPIO1_IO01,
+            PAD_CONFIG_SLEW_RATE_SLOW |
+            PAD_CONFIG_DRIVER_STRENGTH_LVL_1 |
+            PAD_CONFIG_PULL_KEEPER_ENABLED |
+            PAD_CONFIG_SELECT_PULL |
+            PAD_CONFIG_PULL_UP_100kOhm |
+            PAD_CONFIG_HYSTERESIS_DISABLED);
+
+/*    while(1){
+        uint32_t cnt = 0;
+        LPM_EnterFullSpeed();
+
+        while(cnt++ < 1000000){
+            asm("nop");
+        }
+        LPM_EnterLowPowerIdle();
+        cnt = 0;
+        while(cnt++ < 1000000){
+            asm("nop");
+        }
+    }
+
+*//*    LPM_EnterFullSpeed();
     LPM_EnterLowPowerIdle();
 
-    while(1);
+    while(1);*/
 
-#if 0
+#if 1
     auto sysmgr = std::make_shared<sys::SystemManager>(5000);
 
     sysmgr->StartSystem([sysmgr]()->int{
@@ -146,10 +218,10 @@ int main() {
 
         bool ret = false;
 
-        ret |= sys::SystemManager::CreateService(std::make_shared<EventManager>("EventManager"), sysmgr.get());
-        ret |= sys::SystemManager::CreateService(std::make_shared<ServiceDB>(), sysmgr.get());
+       // ret |= sys::SystemManager::CreateService(std::make_shared<EventManager>("EventManager"), sysmgr.get());
+       // ret |= sys::SystemManager::CreateService(std::make_shared<ServiceDB>(), sysmgr.get());
         ret |= sys::SystemManager::CreateService(std::make_shared<BlinkyService>("Blinky"), sysmgr.get());
-        ret |= sys::SystemManager::CreateService(std::make_shared<ServiceCellular>(), sysmgr.get());
+/*        ret |= sys::SystemManager::CreateService(std::make_shared<ServiceCellular>(), sysmgr.get());
         ret |= sys::SystemManager::CreateService(std::make_shared<ServiceAudio>(), sysmgr.get());
         ret |= sys::SystemManager::CreateService(std::make_shared<ServiceBluetooth>(),sysmgr.get());
 
@@ -165,7 +237,7 @@ int main() {
 
 		//start application manager
         ret |= sysmgr->CreateService(std::make_shared<sapm::ApplicationManager>("ApplicationManager", sysmgr.get(), applications),
-                                     sysmgr.get());
+                                     sysmgr.get());*/
 
         if (ret) {
             return 0;
