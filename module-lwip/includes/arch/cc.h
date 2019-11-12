108084f18c76504eafc47cd4eafa26570929d86d
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2019-11-12 13:55:09 +0100

    [EDG-2135] lwip added for bluetooth PAN

--- module-lwip/includes/arch/cc.h
+++ module-lwip/includes/arch/cc.h
@@ -1,1 +1,4 @@
 #pragma once
+
+#define LWIP_DECLARE_MEMORY_ALIGNED(variable_name, size) u8_t variable_name[LWIP_MEM_ALIGN_BUFFER(size)] \
+__attribute__((section(".sdram"))) __attribute__((aligned(4)));
