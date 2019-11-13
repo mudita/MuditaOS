4ddc6c2cba2d00a7e174e89c29c37b3d8c861923
rkubiak01 <roman.kubiak@gmail.com>
2019-11-13 17:03:33 +0100

    UTILS: added trim and is_number

--- module-utils/Utils.hpp
+++ module-utils/Utils.hpp
@@ -7,6 +7,8 @@
 namespace utils
 {
 
+static const std::string WHITESPACE = " \n\r\t\f\v";
+
 template <typename Out> void split(const std::string &s, char delim, Out result)
 {
     std::stringstream ss(s);
@@ -24,12 +26,26 @@ static inline std::vector<std::string> split(const std::string &s, char delim)
     return elems;
 }
 
+static inline bool is_number(const std::string &s)
+{
+    return !s.empty() && std::find_if(s.begin(), s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
+}
+
+static inline std::string ltrim(const std::string &s)
+{
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+
+static inline std::string rtrim(const std::string &s)
+{
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+
 static inline std::string trim(const std::string &s)
 {
-    auto wsfront = std::find_if_not(s.begin(), s.end(), [](int c) { return std::isspace(c); });
-    return std::string(wsfront, std::find_if_not(s.rbegin(), std::string::const_reverse_iterator(wsfront), [](int c) {
-                                    return std::isspace(c);
-                                }).base());
+    return rtrim(ltrim(s));
 }
 
 } // namespace utils
