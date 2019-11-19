e2a4f5d7eb1061c513ea5d92ffcb3de13dbab023
Alek <aleksander.rudnik@mudita.com>
2019-11-19 23:45:51 +0100

    EGD-2117 put all layout constraints in one file

--- module-apps/application-calllog/windows/CallLogDetailsWindow.cpp
+++ module-apps/application-calllog/windows/CallLogDetailsWindow.cpp
@@ -29,6 +29,7 @@
 #include <Style.hpp>
 
 using namespace calllog;
+using namespace callLogStyle::detailsWindow;
 
 namespace gui {
 
@@ -70,95 +71,6 @@ Label * CallLogDetailsWindow::decorateData(Label* label){
 	return label;
 }
 
-// layout constants
-constexpr uint32_t default_x = style::window::default_left_margin;
-constexpr uint32_t default_w = style::window_width - style::window::default_left_margin - style::window::default_right_margin;
-namespace information
-{
-namespace label
-{
-constexpr uint32_t x = default_x;
-constexpr uint32_t y = 111;
-constexpr uint32_t w = default_w;
-} // namespace label
-namespace number
-{
-constexpr uint32_t x = default_x;
-constexpr uint32_t y = 174;
-constexpr uint32_t w = default_w;
-} // namespace number
-namespace imgs
-{
-constexpr uint32_t y = 162;
-constexpr uint32_t w = 55;
-constexpr uint32_t h = 55;
-namespace call
-{
-constexpr uint32_t x = 317;
-}
-namespace sms
-{
-constexpr uint32_t x = 389;
-}
-} // namespace imgs
-} // namespace information
-namespace type
-{
-namespace label
-{
-constexpr uint32_t x = default_x;
-constexpr uint32_t y = 222;
-constexpr uint32_t w = style::window_width / 2 - x;
-} // namespace label
-namespace img
-{
-const uint32_t x = default_x;
-const uint32_t y = 285;
-} // namespace img
-namespace data
-{
-constexpr uint32_t x = 70;
-constexpr uint32_t y = 285;
-constexpr uint32_t w = style::window_width / 2 - x;
-} // namespace data
-} // namespace type
-namespace duration
-{
-namespace label
-{
-constexpr uint32_t x = 281;
-constexpr uint32_t y = 222;
-constexpr uint32_t w = style::window_width - x - style::window::default_right_margin;
-} // namespace label
-namespace data
-{
-constexpr uint32_t x = 281;
-constexpr uint32_t y = 285;
-constexpr uint32_t w = style::window_width / 2 - style::window::default_right_margin;
-} // namespace data
-} // namespace duration
-namespace date
-{
-namespace label
-{
-constexpr uint32_t x = default_x;
-constexpr uint32_t y = 333;
-constexpr uint32_t w = default_w;
-} // namespace label
-namespace dataDay
-{
-constexpr uint32_t x = default_x;
-constexpr uint32_t y = 396;
-constexpr uint32_t w = default_w;
-} // namespace dataDay
-namespace dataDate
-{
-constexpr uint32_t x = default_x;
-constexpr uint32_t y = dataDay::y + style::window::label::small_h;
-constexpr uint32_t w = default_w;
-} // namespace dataDate
-} // namespace date
-
 void CallLogDetailsWindow::buildInterface() {
 	AppWindow::buildInterface();
 
@@ -169,7 +81,7 @@ void CallLogDetailsWindow::buildInterface() {
 	topBar->setActive(TopBar::Elements::TIME, true );
 
     // NOTE: height of all labels is set using decorators
-	
+
     // Information
     informationLabel =
         decorateLabel(new gui::Label(this, information::label::x, information::label::y, information::label::w, 0, utils::localize.get("common_information")));
