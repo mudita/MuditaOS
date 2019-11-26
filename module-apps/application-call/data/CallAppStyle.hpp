bcf51fcc6ee34ece2858deceda30f4120adcf155
Alek-Mudita <54846206+Alek-Mudita@users.noreply.github.com>
2019-11-26 09:50:35 +0100

    Egd 516 enter number window (#89)
    * EGD-516 fixed layout in enterwindow.
    Gently refactor the code.
    Minor enhancments.
    
    * EGD-516 removed reinterpret casts from enterwindow
    
    * EGD-516 added new contact icon
    
    * EGD-516 added missing font and new Icon widget.
    
    * EGD-516 fixed icon label alignment
    
    * EGD-516 minor icon changes
    
    * EGD-516 add CallAppStyle
    
    * EGD-516 added icon styl to callappstyle

---
+++ module-apps/application-call/data/CallAppStyle.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <Style.hpp>
+
+namespace callAppStyle
+{
+// ICON
+namespace icon
+{
+constexpr uint32_t w = 100;
+constexpr uint32_t h = 100;
+namespace label
+{
+constexpr uint32_t x = 9;
+constexpr uint32_t y = 58;
+constexpr uint32_t w = 82;
+constexpr uint32_t h = 20;
+} // namespace label
+namespace img
+{
+constexpr uint32_t x = 34;
+constexpr uint32_t y = 15;
+constexpr uint32_t w = 32;
+constexpr uint32_t h = 32;
+} // namespace img
+} // namespace icon
+
+// ENTER NUMBER WINDOW
+namespace enterNumberWindow
+{
+namespace numberLabel
+{
+constexpr uint32_t x = 60;
+constexpr uint32_t y = 157;
+constexpr uint32_t w = style::window_width - 2 * x;
+constexpr uint32_t h = 66;
+constexpr uint32_t borderW = 1;
+} // namespace numberLabel
+namespace newContactIcon
+{
+constexpr uint32_t x = 190;
+constexpr uint32_t y = 411;
+} // namespace newContactIcon
+} // namespace enterNumberWindow
+} // namespace callAppStyle
---
+++ module-apps/application-call/widgets/Icon.cpp
@@ -0,0 +1,23 @@
+#include "Icon.hpp"
+#include "../data/CallAppStyle.hpp"
+#include <log/log.hpp>
+
+using namespace callAppStyle;
+namespace gui
+{
+
+Icon::Icon(Item *parent, const uint32_t &x, const uint32_t &y, const std::string &imageName, const std::string &labelStr)
+    : Rect(parent, x, y, icon::w, icon::h)
+{
+    setEdges( RectangleEdgeFlags::GUI_RECT_EDGE_BOTTOM | RectangleEdgeFlags::GUI_RECT_EDGE_TOP );
+	setPenFocusWidth(style::window::default_border_focucs_w);
+	setPenWidth(style::window::default_border_no_focus_w);
+
+    img = new gui::Image(this, icon::img::x, icon::img::y, icon::img::w, icon::img::h, imageName);
+    label = new gui::Label(this, icon::label::x, icon::label::y, icon::label::w, icon::label::h, labelStr);
+    label->setEdges(RectangleEdgeFlags::GUI_RECT_EDGE_NO_EDGES);
+    label->setAlignement(gui::Alignment(gui::Alignment::ALIGN_HORIZONTAL_CENTER, gui::Alignment::ALIGN_VERTICAL_CENTER));
+    label->setFont(style::window::font::verysmall);
+}
+
+} // namespace gui
