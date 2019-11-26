ceefd7abb2c24928d98c1e35d82045c64736858d
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2019-11-26 14:02:08 +0100

    [EGD-2335] Added missing file

---
+++ module-utils/common_data/RawKey.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdint>
+#include <bsp/keyboard/key_codes.hpp>
+
+/// default application timer trigger
+const inline uint32_t key_timer_ms = 200;
+/// default time key press will be counted as press again
+const inline uint32_t key_time_cycle_ms = 500;
+/// default long press time
+const inline uint32_t key_time_longpress_ms = 1000;
+
+struct RawKey
+{
+    enum class State
+    {
+        Undefined,
+        Pressed,
+        Released,
+    } state = State::Undefined;
+    bsp::KeyCodes key_code = bsp::KeyCodes::Undefined;
+    int time_press = 0;
+    int time_release = 0;
+
+    /// set all values to zero, set new key_code and state
+    void reset(bsp::KeyCodes key_code = bsp::KeyCodes::Undefined, State state = State::Undefined)
+    {
+        this->key_code = key_code;
+        this->state = state;
+        time_press = 0;
+        time_release = 0;
+    }
+};
