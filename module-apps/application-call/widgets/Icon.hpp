3768914f02da1a7e669f18bc2674bc3ae227eccb
Alek-Mudita <54846206+Alek-Mudita@users.noreply.github.com>
2019-11-26 09:50:35 +0100

    Egd 516 enter number window (#89)
    * EGD-516 fixed layout in enterwindow.
    Gently refactor the code.
    Minor enhancments.
    
    * EGD-516 removed reinterpret casts from enterwindow
    
    * EGD-516 added new contact icon
    
    * EGD-516 added missing font and new Icon widget.
    
    * EGD-516 fixed icon label alignment
    
    * EGD-516 minor icon changes
    
    * EGD-516 add CallAppStyle
    
    * EGD-516 added icon styl to callappstyle

---
+++ module-apps/application-call/widgets/Icon.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Image.hpp"
+#include "Label.hpp"
+
+namespace gui
+{
+
+// Icon widget with custom image and label
+class Icon : public Rect
+{
+  protected:
+    Image *img = nullptr;
+    Label *label = nullptr;
+
+  public:
+    Icon() = delete;
+    Icon(Item *parent, const uint32_t &x, const uint32_t &y, const std::string &imageName, const std::string &labelStr);
+    virtual ~Icon() = default;
+};
+} // namespace gui
