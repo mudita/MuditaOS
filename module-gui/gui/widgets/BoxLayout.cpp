d04ddc279ae1585c7cc75761cec757b040d7780f
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2019-12-24 12:09:46 +0100

    [BoxLayout] commonized HBox, VBox elements

--- module-gui/gui/widgets/BoxLayout.cpp
+++ module-gui/gui/widgets/BoxLayout.cpp
@@ -45,6 +45,121 @@ std::list<DrawCommand*> BoxLayout::buildDrawList() {
 	return Rect::buildDrawList();
 }
 
+template <BoxLayout::BoxAxis axis> void BoxLayout::resizeItems()
+{
+    if (children.empty())
+    {
+        // LOG_ERROR("Resize empty box!");
+        return;
+    }
+
+    uint32_t freeSpace = axis == BoxAxis::Y ? widgetArea.h : widgetArea.w;
+    int16_t offsetInAxis = 0;
+    uint16_t maxInAxis;
+    uint16_t movedInAxis;
+
+    // accessor lambdas, depending on if it's in Y or X axiss we want to access in x or Y
+    auto setPosInAxis = [=](Item *it) {
+        if (axis == BoxAxis::Y)
+        {
+            it->widgetArea.y = offsetInAxis;
+        }
+        else
+        {
+            it->widgetArea.x = offsetInAxis;
+        }
+    };
+
+    auto setSizeInAxis = [=](Item *it) {
+        auto oldbox = it->widgetArea;
+        if (axis == BoxAxis::Y)
+        {
+            it->widgetArea.h = movedInAxis;
+        }
+        else
+        {
+            it->widgetArea.w = movedInAxis;
+        }
+        auto newbox = it->widgetArea;
+        it->onDimensionChanged(oldbox, newbox);
+    };
+
+    auto inAxisPos = [=](Item *it) -> uint16_t & { return (axis == BoxAxis::Y) ? (it->widgetArea.h) : (it->widgetArea.w); };
+
+    auto getMaxInAxis = [=](Item *it) { return (axis == BoxAxis::Y) ? (it->getMaxHeight()) : (it->getMaxWidth()); };
+
+    // create list of elements to be modified. Each element is placed in the BoxElement structure;
+    // width of all elements are set to 0
+    std::list<BoxElement> boxElements;
+    for (Item *item : children)
+    {
+        boxElements.push_back(BoxElement(item));
+        inAxisPos(item) = 0;
+    }
+
+    uint32_t elementsToChange = children.size();
+    while (freeSpace > 0)
+    {
+        offsetInAxis = 0;
+        uint16_t expandValue = freeSpace / elementsToChange;
+
+        // for case when freeSpace is greater than 0 but smaller than elementsToChange
+        // expand value is set to 1
+        if (expandValue == 0)
+            expandValue = 1;
+
+        std::list<BoxElement>::iterator i = boxElements.begin();
+
+        while ((i != boxElements.end()) && (freeSpace > 0))
+        {
+            // element was already updated in previous run and no further modification is possible or required
+            if (i->noUpdate)
+            {
+                setPosInAxis(i->item);
+                offsetInAxis += inAxisPos(i->item);
+                ++i;
+                continue;
+            }
+            else if (inAxisPos(i->item) == getMaxInAxis(i->item))
+            {
+                movedInAxis = inAxisPos(i->item);
+                offsetInAxis += movedInAxis;
+                freeSpace -= movedInAxis;
+                setPosInAxis(i->item);
+                setSizeInAxis(i->item);
+                i->noUpdate = true;
+                elementsToChange--;
+            }
+            else
+            {
+                maxInAxis = getMaxInAxis(i->item);
+                movedInAxis = inAxisPos(i->item) + expandValue;
+
+                if (movedInAxis > maxInAxis)
+                {
+                    movedInAxis = maxInAxis;
+                    elementsToChange--;
+                    i->noUpdate = true;
+                }
+
+                freeSpace -= (movedInAxis - inAxisPos(i->item));
+                setPosInAxis(i->item);
+                setSizeInAxis(i->item);
+
+                offsetInAxis += movedInAxis;
+            }
+
+            ++i;
+        }
+
+        if (elementsToChange <= 0)
+        {
+            break;
+        }
+    }
+
+    Rect::updateDrawArea();
+}
 
 HBox::HBox() : BoxLayout() {
 	type = ItemType::HBOX;
@@ -55,89 +170,8 @@ HBox::HBox( Item* parent, const uint32_t& x, const uint32_t& y, const uint32_t& 
 }
 
 void HBox::resizeItems() {
-
-	if( children.empty())
-		return;
-
-	uint32_t freeSpace = widgetArea.w;
-	int16_t newX = 0;
-	int16_t newY = 0;
-	uint16_t newH = widgetArea.h;
-	uint16_t maxWidthValue;
-	uint16_t newW;
-
-	//create list of elements to be modified. Each element is placed in the BoxElement structure;
-	//width of all elements are set to 0
-	std::list<BoxElement> boxElements;
-	for( Item* item : children ) {
-		boxElements.push_back( BoxElement(item) );
-		item->widgetArea.w = 0;
-	}
-
-	int32_t elementsToChange = children.size();
-	while( freeSpace > 0 ) {
-		newX = 0;
-
-		uint16_t expandValue = freeSpace / elementsToChange;
-
-		//for case when freeSpace is greater than 0 but smaller than elementsToChange
-		//expand value is set to 1
-		if( expandValue == 0 )
-			expandValue = 1;
-
-		std::list<BoxElement>::iterator i = boxElements.begin();
-
-		while ((i != boxElements.end()) && (freeSpace > 0))
-		{
-			//element was already updated in previous run and no further modification is possible or required
-            newY = (*i).item->widgetArea.y;
-			if( (*i).noUpdate ) {
-				(*i).item->setPosition( newX, newY );
-				newX += (*i).item->widgetArea.w;
-				++i;
-				continue;
-			}
-			else if( (*i).item->widgetArea.w == (*i).item->getMaxWidth())
-			{
-				newH = (*i).item->widgetArea.h;
-				newW = (*i).item->widgetArea.w;
-				newX += newW;
-
-				freeSpace -= newW;
-
-				(*i).item->setPosition( newX, newY );
-				(*i).item->setSize( newW, newH );
-				(*i).noUpdate = true;
-				elementsToChange--;
-			}
-			else
-			{
-				maxWidthValue = (*i).item->getMaxWidth();
-				newW = (*i).item->widgetArea.w + expandValue;
-
-				if( newW > maxWidthValue ) {
-					newW = maxWidthValue;
-					elementsToChange--;
-					(*i).noUpdate = true;
-				}
-
-				freeSpace -= (newW - (*i).item->widgetArea.w);
-				(*i).item->setPosition( newX, newY );
-				(*i).item->setSize( newW, newH );
-
-				newX += newW;
-			}
-
-			++i;
-		}
-
-		if(elementsToChange <= 0 )
-			break;
-	}
-
-	Rect::updateDrawArea();
+    BoxLayout::resizeItems<BoxLayout::BoxAxis::X>();
 }
-
 
 VBox::VBox() : BoxLayout() {
 	type = ItemType::VBOX;
@@ -148,88 +182,7 @@ VBox::VBox( Item* parent, const uint32_t& x, const uint32_t& y, const uint32_t& 
 }
 
 void VBox::resizeItems() {
-
-	if( children.empty())
-		return;
-
-	uint32_t freeSpace = widgetArea.h;
-	int16_t newX = 0;
-	int16_t newY = 0;
-	uint16_t newH;
-	uint16_t maxHeightValue;
-	uint16_t newW = widgetArea.w;
-
-	//create list of elements to be modified. Each element is placed in the BoxElement structure;
-	//width of all elements are set to 0
-	std::list<BoxElement> boxElements;
-	for( Item* item : children ) {
-		boxElements.push_back( BoxElement(item) );
-		item->widgetArea.h = 0;
-	}
-
-	uint32_t elementsToChange = children.size();
-	while( freeSpace > 0 ) {
-		newY = 0;
-		if( freeSpace < elementsToChange ) {
-			break;
-		}
-		uint16_t expandValue = freeSpace / elementsToChange;
-
-		//for case when freeSpace is greater than 0 but smaller than elementsToChange
-		//expand value is set to 1
-		if( expandValue == 0 )
-			expandValue = 1;
-
-		std::list<BoxElement>::iterator i = boxElements.begin();
-
-		while ((i != boxElements.end()) && (freeSpace > 0))
-		{
-            newX = (*i).item->widgetArea.x;
-			//element was already updated in previous run and no further modification is possible or required
-			if( (*i).noUpdate ) {
-				(*i).item->setPosition( newX, newY );
-				newY += (*i).item->widgetArea.h;
-				++i;
-				continue;
-			}
-			else if( (*i).item->widgetArea.h == (*i).item->getMaxHeight())
-			{
-				newH = (*i).item->widgetArea.h;
-				newW = (*i).item->widgetArea.w;
-				newY += newH;
-
-				freeSpace -= newH;
-
-				(*i).item->setPosition( newX, newY );
-				(*i).item->setSize( newW, newH );
-				(*i).noUpdate = true;
-				elementsToChange--;
-			}
-			else
-			{
-				maxHeightValue = (*i).item->getMaxHeight();
-				newH = (*i).item->widgetArea.h + expandValue;
-
-				if( newH > maxHeightValue ) {
-					newH = maxHeightValue;
-					elementsToChange--;
-					(*i).noUpdate = true;
-				}
-
-				freeSpace -= (newH - (*i).item->widgetArea.h);
-				(*i).item->setPosition( newX, newY );
-				(*i).item->setSize( newW, newH );
-
-				newY += newH;
-			}
-
-			++i;
-		}
-
-		if(elementsToChange == 0 )
-			break;
-	}
-
-	Rect::updateDrawArea();
+    BoxLayout::resizeItems<BoxLayout::BoxAxis::Y>();
 }
+
 } /* namespace gui */
