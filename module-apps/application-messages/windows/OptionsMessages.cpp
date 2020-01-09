38622db127ef7012b23f54c247c8acfc439f6ee1
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-01-09 14:26:27 +0100

    [EGD-2475] Options looks fixups

--- module-apps/application-messages/windows/OptionsMessages.cpp
+++ module-apps/application-messages/windows/OptionsMessages.cpp
@@ -5,41 +5,44 @@
 
 #include <BoxLayout.hpp>
 
-gui::HBox *newCombo(app::ApplicationMessages *app, const UTF8 &text)
-{
-    auto dummy = ContactRecord();
+using namespace style::window;
 
-    auto box = new gui::HBox(nullptr, 0, 0, style::window_width, style::window::label::big_h);
+gui::HBox *newCombo(app::ApplicationMessages *app, const ContactRecord &contact)
+{
+    const uint32_t wlabel = 64;
+    const uint32_t offset = default_left_margin;
+
+    auto box = new gui::HBox(nullptr, offset, 0, style::window_width - 2 * offset, label::big_h);
     box->setPenWidth(0);
     box->setPenFocusWidth(0);
-    auto l = new gui::Label(box, 0, 0, 300, style::window::label::big_h);
-    style::window::decorate(l);
-    l->setText(text);
+    auto l = new gui::Label(box, 0, 0, style::window_width - 3 * wlabel - 2 * offset - 1, label::big_h);
+    decorate(l);
+    l->setText(contact.primaryName);
     l->activeItem = false;
 
     auto p = [=](const UTF8 &icon, auto foo) {
-        auto phone = new gui::Label(box, 0, 0, 59, 50);
-        style::window::decorate(phone);
+        auto phone = new gui::Label(box, 0, 0, wlabel, label::big_h);
+        decorate(phone);
         // does nothing for image :|
         phone->setAlignement({gui::Alignment::ALIGN_HORIZONTAL_CENTER | gui::Alignment::ALIGN_VERTICAL_CENTER});
-        new gui::Image(phone, 10, 10, 50, style::window::label::big_h, icon);
+        new gui::Image(phone, 12, 12, 50, label::big_h, icon);
         phone->activatedCallback = foo;
     };
 
     p("phonebook_phone_ringing", [=](gui::Item &) -> bool {
-        LOG_INFO("Call: %s", text.c_str());
-        return call(app, dummy);
+        LOG_INFO("Call: %s", contact.primaryName);
+        return call(app, contact);
     });
 
     p("mail", [=](gui::Item &) -> bool {
-        LOG_INFO("SMS to: %s", text.c_str());
-        sms(app, dummy);
+        LOG_INFO("SMS to: %s", contact.primaryName);
+        sms(app, contact);
         return true;
     });
 
     p("cross", [=](gui::Item &) -> bool {
-        LOG_INFO("Add contact: %s", text.c_str());
-        addContact(app, dummy);
+        LOG_INFO("Add contact: %s", contact.primaryName);
+        addContact(app, contact);
         return true;
     });
 
@@ -49,15 +52,23 @@ gui::HBox *newCombo(app::ApplicationMessages *app, const UTF8 &text)
 
 gui::Item *placeholder(const UTF8 &text)
 {
-    auto ret = new gui::Label(nullptr, 0, 20, style::window_height - 20, style::window::label::small_h, text);
+    auto ret = new gui::Rect(nullptr, default_left_margin, 0, style::window_width - default_left_margin - default_right_margin, label::small_h);
+    auto t = new gui::Label(ret, 0, 0, 0, 0, text);
+
+    auto width = t->getTextNeedSpace() + 20;
+    t->setSize(width, label::small_h);
+    t->setPenWidth(0);
+
+    auto r = new gui::Rect(ret, width, ret->getHeight() / 2, ret->getWidth() - width - 1, 2);
+    r->setPenWidth(1);
     ret->setPenWidth(0);
     ret->activeItem = false;
     return ret;
 }
 
-std::list<gui::Item *> smsWindowOptions(app::ApplicationMessages *app, const SMSRecord *record)
+std::list<gui::Item *> smsWindowOptions(app::ApplicationMessages *app, const SMSRecord &record)
 {
-    auto contact = ContactRecord();
+    ContactRecord contact = DBServiceAPI::ContactGetByID(app, record.contactID)->front();
     return {
         gui::newOptionLabel(callOption(app, contact, true)), gui::newOptionLabel(contactDetails(app, contact)),
 
@@ -65,6 +76,6 @@ std::list<gui::Item *> smsWindowOptions(app::ApplicationMessages *app, const SMS
         gui::newOptionLabel({UTF8(" <STUB> ") + UTF8(utils::localize.get("sms_forvard_message")), [=](gui::Item &item) { return false; }}),
         gui::newOptionLabel({UTF8(" <STUB> ") + UTF8(utils::localize.get("sms_copy")), [=](gui::Item &item) { return false; }}),
         gui::newOptionLabel({UTF8(" <STUB> ") + UTF8(utils::localize.get("sms_delete_message")), [=](gui::Item &item) { return false; }}),
-        placeholder(utils::localize.get("sms_from_this_sms")), newCombo(app, "+123 123 123") // contact.numbers[0].numberE164)
+        placeholder(utils::localize.get("sms_from_this_sms")), newCombo(app, contact) // contact.numbers[0].numberE164)
     };
 }
