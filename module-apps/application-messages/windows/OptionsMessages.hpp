ae794cbb79019a00bba18fa41a3f8c710ec4a1cd
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-01-10 16:47:43 +0100

    [EGD-2475] Added Common options window widget to use anywhere needed

---
+++ module-apps/application-messages/windows/OptionsMessages.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../ApplicationMessages.hpp"
+#include "Interface/SMSRecord.hpp"
+#include "OptionWindow.hpp"
+
+namespace gui
+{
+    namespace name
+    {
+        namespace window
+        {
+            inline const std::string messages_options = "options_mess";
+        };
+    }; // namespace name
+};     // namespace gui
+
+std::list<gui::Item *> smsWindowOptions(app::ApplicationMessages *appl, const SMSRecord *record);
