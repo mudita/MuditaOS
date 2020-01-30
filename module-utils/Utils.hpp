a813da2a5862a6f14efaf19aac66f51d8ea7151e
Alek-Mudita <54846206+Alek-Mudita@users.noreply.github.com>
2020-01-30 10:17:35 +0100

    [EGD-2423] added numbertostring (#153)
    * [EGD-2423] added numbertostring
    
    * [EGD-2423] renamed to to_string

--- module-utils/Utils.hpp
+++ module-utils/Utils.hpp
@@ -6,46 +6,51 @@
 
 namespace utils
 {
+    static const std::string WHITESPACE = " \n\r\t\f\v";
 
-static const std::string WHITESPACE = " \n\r\t\f\v";
-
-template <typename Out> void split(const std::string &s, char delim, Out result)
-{
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim))
+    template <typename Out> void split(const std::string &s, char delim, Out result)
     {
-        *(result++) = item;
+        std::stringstream ss(s);
+        std::string item;
+        while (std::getline(ss, item, delim))
+        {
+            *(result++) = item;
+        }
     }
-}
 
-static inline std::vector<std::string> split(const std::string &s, char delim)
-{
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
+    static inline std::vector<std::string> split(const std::string &s, char delim)
+    {
+        std::vector<std::string> elems;
+        split(s, delim, std::back_inserter(elems));
+        return elems;
+    }
 
-static inline bool is_number(const std::string &s)
-{
-    return !s.empty() && std::find_if(s.begin(), s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
-}
+    static inline bool is_number(const std::string &s)
+    {
+        return !s.empty() && std::find_if(s.begin(), s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
+    }
 
-static inline std::string ltrim(const std::string &s)
-{
-    size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == std::string::npos) ? "" : s.substr(start);
-}
+    static inline std::string ltrim(const std::string &s)
+    {
+        size_t start = s.find_first_not_of(WHITESPACE);
+        return (start == std::string::npos) ? "" : s.substr(start);
+    }
 
-static inline std::string rtrim(const std::string &s)
-{
-    size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
-}
+    static inline std::string rtrim(const std::string &s)
+    {
+        size_t end = s.find_last_not_of(WHITESPACE);
+        return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+    }
 
-static inline std::string trim(const std::string &s)
-{
-    return rtrim(ltrim(s));
-}
+    static inline std::string trim(const std::string &s)
+    {
+        return rtrim(ltrim(s));
+    }
 
+    template <typename T> std::string to_string(T Number)
+    {
+        std::ostringstream ss;
+        ss << Number;
+        return ss.str();
+    }
 } // namespace utils
