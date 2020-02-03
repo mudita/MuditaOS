bb121db4693147df1ed758e9bdc6609664b007d7
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-02-03 10:47:07 +0100

    [EGD-2532] HW timer tested, call made, seems fine

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 make -C build -j 4 && \
-~/gcc-arm-none-eabi-8-2019-q3-update/bin/arm-none-eabi-gdb build/PurePhone.elf -x .gdbinit-1051
+~/gcc-arm-none-eabi-8-2019-q3-update/bin/arm-none-eabi-gdb-py build/PurePhone.elf -x .gdbinit-1051
