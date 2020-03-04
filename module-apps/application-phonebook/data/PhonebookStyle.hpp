168c6657de147d7f26014ec682e10e1c68576f9b
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2020-03-04 15:19:13 +0100

    [EGD-2936] Minor improvements in PhonebookStyle

--- module-apps/application-phonebook/data/PhonebookStyle.hpp
+++ module-apps/application-phonebook/data/PhonebookStyle.hpp
@@ -16,24 +16,21 @@ namespace search {
     namespace horizontalBox {
         constexpr uint32_t x = style::window::default_left_margin;
         constexpr uint32_t y = 153;
-        constexpr uint32_t w =
-                style::window_width - style::window::default_left_margin - style::window::default_right_margin;
+        constexpr uint32_t w = default_w;
         constexpr uint32_t h = 33;
         constexpr uint32_t penWidth = 2;
-        namespace inputField {
-            constexpr uint32_t x = 1;
-            constexpr uint32_t y = 1;
-            constexpr uint32_t w =
-                    style::window_width - style::window::default_left_margin - style::window::default_right_margin - 32;
-            constexpr uint32_t h = 33;
-        } // namespace inputField
         namespace searchTop {
-            constexpr uint32_t x =
-                    style::window_width - style::window::default_left_margin - style::window::default_right_margin;
+            constexpr uint32_t x = default_w;
             constexpr uint32_t y = 1;
             constexpr uint32_t w = 32;
             constexpr uint32_t h = 32;
         } // namespace searchTop
+        namespace inputField {
+            constexpr uint32_t x = 1;
+            constexpr uint32_t y = 1;
+            constexpr uint32_t w = default_w - searchTop::w;
+            constexpr uint32_t h = 33;
+        } // namespace inputField
     } // namespace horizontalBox
     namespace searchResultList {
         constexpr uint32_t x = 11;
