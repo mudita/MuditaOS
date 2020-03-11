789e7ec106cc38e9063d695d3b6b2043bd8d3bc3
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2020-03-11 09:56:43 +0100

    Add paths ignored by style checking

--- config/format-config.sh
+++ config/format-config.sh
@@ -3,7 +3,7 @@
 # ignore file for clang-format autoformating
 
 # set this variable in your shell if you wish to disable autoformatting on commit for time being
-# DISABLE_AUTO_FORMATTING=1
+DISABLE_AUTO_FORMATTING=1
 
 # set this variable to get more verbose output
 VERBOSE=1
@@ -11,5 +11,20 @@ VERBOSE=1
 # ignore_paths for formatter - these are regex matched with filenames to be formated
 # if you don't wish to format i.e one file - just pass whole path to this file from git root directory
 declare ignore_paths=(
+    './module-os/board/'
     './module-bsp/bsp/'
+    './module-vfs/board/cross/freeRTOS_FAT/'
+    './module-db/Database/sqlite3.h'
+    './module-db/Database/sqlite3.c'
+    './module-db/Database/config.h'
+    './module-bluetooth/lib/btstack/'
+    './module-lwip/lib/lwip/'
+    './module-utils/tinyfsm/'
+    './module-utils/microtar/'
+    './module-utils/json/'
+    './module-utils/log/segger/'
+    './build'
+    './.*/lib/'
+    './.*/catch.hpp'
+
 )
