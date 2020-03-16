38606825151e6b42f54ca4740319338e32138e4d
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2020-03-16 14:53:30 +0100

    [EGD-2932] Add PhonebookMAinWindow styles to PhonebookStyle.hpp

--- module-apps/application-phonebook/data/PhonebookStyle.hpp
+++ module-apps/application-phonebook/data/PhonebookStyle.hpp
@@ -3,10 +3,48 @@
 
 namespace phonebookStyle
 {
-    namespace search
-    {
-        constexpr uint32_t default_x = style::window::default_left_margin;
-        constexpr uint32_t default_w =
+namespace mainWindow {
+    constexpr uint32_t default_x = style::window::default_left_margin;
+    constexpr uint32_t default_w =
+            style::window_width - style::window::default_left_margin - style::window::default_right_margin;
+    namespace leftArrowImage {
+        constexpr uint32_t x = default_x;
+        constexpr uint32_t y = 62;
+        constexpr uint32_t w = 11;
+        constexpr uint32_t h = 13;
+    } // namespace leftArrowImage
+    namespace rightArrowImage {
+        constexpr uint32_t x = style::window_width - style::window::default_left_margin - 11;
+        constexpr uint32_t y = 62;
+        constexpr uint32_t w = 11;
+        constexpr uint32_t h = 13;
+    } // namespace rightArrowImage
+    namespace newContactImage {
+        constexpr uint32_t x = style::window::default_left_margin + 20;
+        constexpr uint32_t y = 55;
+        constexpr uint32_t w = 24;
+        constexpr uint32_t h = 24;
+    } // namespace newContactImage
+    namespace searchImage {
+        constexpr uint32_t x = style::window_width - style::window::default_left_margin - rightArrowImage::w - 8 - 26;
+        constexpr uint32_t y = 55;
+        constexpr uint32_t w = 26;
+        constexpr uint32_t h = 26;
+    } // namespace searchImage
+    namespace contactsList {
+        constexpr uint32_t x = style::window::default_left_margin;
+        constexpr uint32_t y = 105 + 6;
+        constexpr uint32_t w = style::window_width - style::window::default_left_margin - style::window::default_right_margin;
+        constexpr uint32_t h = style::window_height - y - 50;
+        constexpr uint32_t maxElements = 7;
+        constexpr uint32_t pageSize = 7;
+        constexpr uint32_t penFocusWidth = style::window::default_border_no_focus_w;
+        constexpr uint32_t penWidth = 0;
+    } // namespace contactsList
+} // namespace mainWindow
+namespace search {
+    constexpr uint32_t default_x = style::window::default_left_margin;
+    constexpr uint32_t default_w =
             style::window_width - style::window::default_left_margin - style::window::default_right_margin;
         namespace searchHeader
         {
@@ -43,17 +81,15 @@ namespace phonebookStyle
         constexpr uint32_t default_x = style::window::default_left_margin;
         constexpr uint32_t default_w =
             style::window_width - style::window::default_left_margin - style::window::default_right_margin;
-        namespace searchResultList
-        {
-            constexpr uint32_t x = style::window::default_left_margin;
-            constexpr uint32_t y = 105 + 6;
-            constexpr uint32_t w =
-                style::window_width - style::window::default_left_margin - style::window::default_right_margin;
-            constexpr uint32_t h             = style::window_height - y - 50;
-            constexpr uint32_t maxElements   = 7;
-            constexpr uint32_t pageSize      = 7;
-            constexpr uint32_t penFocusWidth = style::window::default_border_no_focus_w;
-            constexpr uint32_t penWidth      = 0;
-        } // namespace searchResultList
-    }     // namespace searchResults
-} // namespace phonebookStyle
+    namespace searchResultList {
+        constexpr uint32_t x = style::window::default_left_margin;
+        constexpr uint32_t y = 105 + 6;
+        constexpr uint32_t w = style::window_width - style::window::default_left_margin - style::window::default_right_margin;
+        constexpr uint32_t h = style::window_height - y - 50;
+        constexpr uint32_t maxElements = 7;
+        constexpr uint32_t pageSize = 7;
+        constexpr uint32_t penFocusWidth = style::window::default_border_no_focus_w;
+        constexpr uint32_t penWidth = 0;
+    } // namespace searchResultList
+} //namespace searchResults
+} //namespace phonebookStyle
