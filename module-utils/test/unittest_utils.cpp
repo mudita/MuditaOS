3d054ea0317a061e8293fb608ebb427a96239feb
Alek-Mudita <54846206+Alek-Mudita@users.noreply.github.com>
2020-03-23 16:21:17 +0100

    [EGD-2998] fixed UT for Utils. (#253)
    * [EGD-2998] fixed UT for Utils.
    
    * [EGD-2998] clean up
    
    * [EGD-2998] missing cmake file

--- module-utils/test/unittest_utils.cpp
+++ module-utils/test/unittest_utils.cpp
@@ -15,7 +15,7 @@ TEST_CASE("Split tests")
     std::string delimiter = "\r\n";
 
     std::string inputStr1 = "\r\nOK\r\n\r\nNO CARRIER\r\n\r\nFINISHED\r\n";
-    auto ret              = utils::split(inputStr1, delimiter, 0, false);
+    auto ret              = utils::split(inputStr1, delimiter, false);
     REQUIRE(ret.size() == 7);
     REQUIRE(ret[0] == "");
     REQUIRE(ret[1] == "OK");
@@ -25,24 +25,11 @@ TEST_CASE("Split tests")
     REQUIRE(ret[5] == "FINISHED");
     REQUIRE(ret[6] == "");
 
-    inputStr1 = "\r\nOK\r\n\r\nNO CARRIER\r\n\r\nFINISHED\r\n";
     ret       = utils::split(inputStr1, delimiter);
     REQUIRE(ret.size() == 3);
     REQUIRE(ret[0] == "OK");
     REQUIRE(ret[1] == "NO CARRIER");
     REQUIRE(ret[2] == "FINISHED");
-
-    inputStr1 = "\r\nOK\r\n\r\nNO CARRIER\r\n\r\nFINISHED\r\n";
-    ret       = utils::split(inputStr1, delimiter, 2, false);
-    REQUIRE(ret.size() == 2);
-    REQUIRE(ret[0] == "");
-    REQUIRE(ret[1] == "OK");
-
-    inputStr1 = "\r\nOK\r\n\r\nNO CARRIER\r\n\r\nFINISHED\r\n";
-    ret       = utils::split(inputStr1, delimiter, 2);
-    REQUIRE(ret.size() == 2);
-    REQUIRE(ret[0] == "OK");
-    REQUIRE(ret[1] == "NO CARRIER");
 
     ret = utils::split("a-b-c", "-");
     REQUIRE(ret.size() == 3);
@@ -50,7 +37,7 @@ TEST_CASE("Split tests")
     REQUIRE(ret[1] == "b");
     REQUIRE(ret[2] == "c");
 
-    ret = utils::split("a--c", "-", 0, false);
+    ret = utils::split("a--c", "-", false);
     REQUIRE(ret.size() == 3);
     REQUIRE(ret[0] == "a");
     REQUIRE(ret[1] == "");
@@ -61,7 +48,7 @@ TEST_CASE("Split tests")
     REQUIRE(ret[0] == "a");
     REQUIRE(ret[1] == "c");
 
-    ret = utils::split("-b-", "-", 0, false);
+    ret = utils::split("-b-", "-", false);
     REQUIRE(ret.size() == 3);
     REQUIRE(ret[0] == "");
     REQUIRE(ret[1] == "b");
@@ -71,7 +58,7 @@ TEST_CASE("Split tests")
     REQUIRE(ret.size() == 1);
     REQUIRE(ret[0] == "b");
 
-    ret = utils::split("", "-", 0, false);
+    ret = utils::split("", "-", false);
     REQUIRE(ret.size() == 1);
     REQUIRE(ret[0] == "");
 
