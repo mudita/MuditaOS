035e29c407999a581f4d93cab107db2d3aeb9844
Mateusz Grzywacz <mateusz.grzywacz@mudita.com>
2020-03-30 18:01:35 +0200

    [EGD-3047] T4 gsm status pin mux pull-up value

--- module-services/service-evtmgr/WorkerEvent.cpp
+++ module-services/service-evtmgr/WorkerEvent.cpp
@@ -131,7 +131,7 @@ bool WorkerEvent::handleMessage(uint32_t queueID)
         if (notification == bsp::cellular::statusPin) {
             auto GSMstatus = bsp::cellular::status::getStatus();
             LOG_DEBUG("GSM Status pin change: %s",
-                      (GSMstatus == bsp::cellular::status::value::ACTIVE ? "NORMAL START-UP" : "ABNORMAL START-UP"));
+                      (GSMstatus == bsp::cellular::status::value::ACTIVE ? "ACTIVE" : "INACTIVE"));
         }
 
         if (notification == bsp::cellular::trayPin) {
