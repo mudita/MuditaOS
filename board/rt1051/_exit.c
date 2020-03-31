0755e99e1cbab5c7488edc5a98ddce79ed78eb97
alek <aleksander.rudnik@mudita.com>
2020-03-31 13:50:22 +0200

    [EGD-3046] added DEBUG check

--- board/rt1051/_exit.c
+++ board/rt1051/_exit.c
@@ -62,7 +62,9 @@ void __attribute__((weak)) _exit(int code __attribute__((unused)))
     LOG_FATAL("_exit %d", code);
     haltIfDebugging();
     vTaskEndScheduler();
+#ifdef DEBUG
     while(1){};
+#endif
 }
 
 // ----------------------------------------------------------------------------
