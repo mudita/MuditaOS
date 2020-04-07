4227e3f175cad1c3794340d1a41ac912606c8d3c
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-04-07 15:19:47 +0200

    EGD-3055 Cleaned up StartJLinkServer.sh
    * added and tested option to dump `info threads` and threads backtrace via gdb
    * parameters added

--- StartJLinkServer.sh
+++ StartJLinkServer.sh
@@ -1,15 +1,45 @@
 #!/usr/bin/env bash
 
-# if there is no argument - then use Mati startup, else use mine :)
-# mine uses other `-device` because my instalation of tools lost ours (but works fine with gdb)
-# speed should be definetly configurable
-# `mine` is continous (to be used together with `./run.sh`)
-if [[ $# -lt 1 ]]; then
-    JLinkGDBServerCLExe -if SWD -device MCIMXRT1051 \
-        -speed 25000 -jlinkscriptfile "evkbimxrt1050_sdram_init.jlinkscript" -singlerun -strict -ir &
-elif [[ $1 == "continous" ]]; then
-    JLinkGDBServerCLExe -if SWD -device MIMXRT1064xxx5A \
-        -endian little -port 2331 -swoport 2332 -telnetport 2333 -localhostonly 1 \
-        -speed 16000 -jlinkscriptfile "evkbimxrt1050_sdram_init.jlinkscript" -strict -ir &
-fi
+declare -A OPTS=( ['continous']=' -singlerun ' ['verify']='  ' ['speed']=' -speed 25000 ')
 
+help()
+{
+cat << EOF
+For basic run with default values:
+./StartJLinkServer.sh
+
+Params:
+    continous   - run in continous mode
+    verify      - verify data loaded
+    speed       - change speed (please mind 30000 is max by docs)
+EOF
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        "continous")
+            echo "change to continous mode"
+            OPTS[$1]=' ';
+            ;;
+        "verify")
+            OPTS[$1]=' -vf ';
+            ;;
+        "speed")
+            OPTS[$1]=" -speed $2 "
+            shift
+            ;;
+        *)
+            help
+            ;;
+    esac;
+    shift
+done
+
+CMD="JLinkGDBServerCLExe -if SWD -device MCIMXRT1051                     \
+    -jlinkscriptfile evkbimxrt1050_sdram_init.jlinkscript -strict -ir    \
+    -rtos GDBServer/RTOSPlugin_FreeRTOS                                  \
+    ${OPTS[@]}"
+
+echo "RUN: ${CMD}"
+
+eval ${CMD}
