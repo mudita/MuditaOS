b94f2022dc31d9f376918f4c093a465ff3a5ff6a
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-04-08 13:28:00 +0200

    EGD-3056 SystemManager - initial work on delaying reboot
    * updated docs
    * added PreShutdownLoop
    * added c_str() for SystemManager inner state for debug

--- module-sys/SystemManager/SystemManager.hpp
+++ module-sys/SystemManager/SystemManager.hpp
@@ -25,6 +25,7 @@ namespace sys
     enum class SystemManagerMsgType
     {
         CloseSystem,
+        Reboot,
     };
 
     class SystemManagerMsg : public DataMessage
@@ -39,6 +40,13 @@ namespace sys
     class SystemManager : public Service
     {
       public:
+        enum class State
+        {
+            Running,
+            Suspend,
+            Shutdown,
+            Reboot,
+        } state = State::Running;
         SystemManager(TickType_t pingInterval);
 
         ~SystemManager();
@@ -59,8 +67,16 @@ namespace sys
         // Create new service
         static bool CreateService(std::shared_ptr<Service> service, Service *caller, TickType_t timeout = 5000);
 
-        // Destroy existing service
+        /// Destroy existing service
+        /// @note there is no fallback
         static bool DestroyService(const std::string &name, Service *caller, TickType_t timeout = 5000);
+
+        /// Kill service
+        /// @note - this is final, it straight takes service, calls it's close callback and it's gone
+        /// please mind that services & apps not registered in SystemManager cant be killed - these should ba handled by
+        /// parenst (as above in Destroy) ApplicationManager somehow propagates this, but I would call how it's done
+        /// `imperfect`
+        bool kill(std::shared_ptr<Service> const &toKill);
 
       private:
         void TickHandler(uint32_t id) override;
@@ -81,7 +97,20 @@ namespace sys
 
         void Run() override;
 
+        // Sysmgr stores list of all active services but some of them are under control of parent services.
+        // Parent services ought to manage lifetime of child services hence we are sending DestroyRequests only to
+        // parents.
         void CloseSystemHandler();
+
+        void RebootHandler();
+
+        /// loop to handle prior to full system close
+        /// for now for rt1051 we need to
+        /// 1. check if we have power plug in
+        /// 2. if power is in, if we didn't get request for poweron ( this triggers reboot)
+        /// this is needed as we can't just shutdown phone with power in - it would end in phone unable to boot (as
+        /// red key won't work to power it on. For more information follow up with schematics
+        bool PreShutdownLoop();
 
         TickType_t pingInterval;
         uint32_t pingPongTimerID;
@@ -94,5 +123,20 @@ namespace sys
     };
 
 } // namespace sys
+
+inline const char *c_str(sys::SystemManager::State state)
+{
+    switch (state) {
+    case sys::SystemManager::State::Running:
+        return "Running";
+    case sys::SystemManager::State::Suspend:
+        return "Suspend";
+    case sys::SystemManager::State::Shutdown:
+        return "Shutdown";
+    case sys::SystemManager::State::Reboot:
+        return "Reboot";
+    }
+    return "";
+}
 
 #endif /* SYSTEMMANAGER_SYSTEMMANAGER_HPP_ */
