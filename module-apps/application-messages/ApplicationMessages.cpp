64be7e66d11309dd459902aab49b56df959c01cc
alek <aleksander.rudnik@mudita.com>
2020-04-10 14:38:45 +0200

    [EGD-3078] fix style

--- module-apps/application-messages/ApplicationMessages.cpp
+++ module-apps/application-messages/ApplicationMessages.cpp
@@ -131,7 +131,7 @@ namespace app
                     }
                     return this->switchWindow(gui::name::window::main_window);
                 };
-                meta.text = utils::localize.get("app_messages_thread_delete_confirmation");
+                meta.text       = utils::localize.get("app_messages_thread_delete_confirmation");
                 auto contactRec = DBServiceAPI::ContactGetByID(this, record->contactID);
                 auto cont       = !contactRec->empty() ? contactRec->front() : ContactRecord{};
                 meta.title      = cont.getFormattedName();
