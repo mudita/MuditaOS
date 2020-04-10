2c724a799c072d371e5d7cb90299de3d072e9006
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-04-10 08:52:18 +0200

    EGD-3056 Messages debugging print edbug.hpp option added

--- module-utils/log/debug.hpp
+++ module-utils/log/debug.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#define DEBUG_APPLICATION_MANAGEMENT 0
-#define DEBUG_SCOPED_TIMINGS         1
-#define _RT1051_UART_DEBUG           0
-#define DEBUG_MODEM_OUTPUT_RESPONSE  1
+#define DEBUG_APPLICATION_MANAGEMENT 0 /// show verbose logs in ApplicationManager
+#define DEBUG_SCOPED_TIMINGS         1 /// show timings in measured functions
+#define _RT1051_UART_DEBUG           0 /// show full modem uart communication
+#define DEBUG_MODEM_OUTPUT_RESPONSE  1 /// show full modem output
+#define DEBUG_SERVICE_MESSAGES       0 /// show messages prior to handling in service
