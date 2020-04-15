b5de9b84abf8bfce25a0dfaf0930ad3d3e7c001a
alek <aleksander.rudnik@mudita.com>
2020-04-15 13:52:59 +0200

    [EGD-3086] added sending sms from new sms window

--- module-apps/application-messages/windows/NewMessage.cpp
+++ module-apps/application-messages/windows/NewMessage.cpp
@@ -1,16 +1,44 @@
 #include "NewMessage.hpp"
+
 #include "ThreadViewWindow.hpp"
-#include "application-phonebook/ApplicationPhonebook.hpp"
-#include "application-phonebook/windows/PhonebookSearchResults.hpp"
-#include "service-appmgr/ApplicationManager.hpp"
-#include "service-db/api/DBServiceAPI.hpp"
+#include "../ApplicationMessages.hpp"
+#include "../data/SMSdata.hpp"
+
+#include <application-phonebook/ApplicationPhonebook.hpp>
+#include <application-phonebook/windows/PhonebookSearchResults.hpp>
+#include <service-appmgr/ApplicationManager.hpp>
+#include <service-db/api/DBServiceAPI.hpp>
 #include <i18/i18.hpp>
-#include <widgets/BoxLayout.hpp>
-#include <widgets/Text.hpp>
+#include <BoxLayout.hpp>
+#include <Text.hpp>
+
+#include <cassert>
 
 namespace gui
 {
     NewSMS_Window::NewSMS_Window(app::Application *app) : AppWindow(app, name::window::new_sms)
+    {
+        buildInterface();
+    }
+
+    bool NewSMS_Window::onInput(const InputEvent &inputEvent)
+    {
+        return AppWindow::onInput(inputEvent);
+    }
+
+    void NewSMS_Window::onBeforeShow(ShowMode mode, SwitchData *data)
+    {
+        if (auto pdata = dynamic_cast<PhonebookSearchReuqest *>(data)) {
+            LOG_INFO("received search results");
+            recipient->setText(pdata->result->getFormattedName());
+            contact = pdata->result;
+        }
+    }
+
+    void NewSMS_Window::rebuild()
+    {}
+
+    void NewSMS_Window::buildInterface()
     {
         AppWindow::buildInterface();
         bottomBar->setActive(BottomBar::Side::LEFT, true);
@@ -32,32 +60,32 @@ namespace gui
         label->setFont(style::window::font::small);
         label->setAlignement(Alignment(Alignment::ALIGN_HORIZONTAL_LEFT, Alignment::ALIGN_VERTICAL_BOTTOM));
 
-        text = new gui::Text(nullptr, 0, 0, body->getWidth(), 43, "", gui::Text::ExpandMode::EXPAND_NONE);
-        text->setEdges(gui::RectangleEdgeFlags::GUI_RECT_EDGE_BOTTOM);
-        text->setInputMode(new InputMode({InputMode::phone}));
-        text->setPenFocusWidth(style::window::default_border_focucs_w);
-        text->setPenWidth(style::window::messages::sms_border_no_focus);
-        text->setFont(style::window::font::medium);
-        text->setAlignment(Alignment(Alignment::ALIGN_HORIZONTAL_LEFT, Alignment::ALIGN_VERTICAL_BOTTOM));
-        text->activatedCallback = [=](Item &) -> bool {
+        recipient = new gui::Text(nullptr, 0, 0, body->getWidth(), 43, "", gui::Text::ExpandMode::EXPAND_NONE);
+        recipient->setEdges(gui::RectangleEdgeFlags::GUI_RECT_EDGE_BOTTOM);
+        recipient->setInputMode(new InputMode({InputMode::phone}));
+        recipient->setPenFocusWidth(style::window::default_border_focucs_w);
+        recipient->setPenWidth(style::window::messages::sms_border_no_focus);
+        recipient->setFont(style::window::font::medium);
+        recipient->setAlignment(Alignment(Alignment::ALIGN_HORIZONTAL_LEFT, Alignment::ALIGN_VERTICAL_BOTTOM));
+        recipient->activatedCallback = [=](Item &) -> bool {
             std::shared_ptr<std::vector<ContactRecord>> searchResults =
-                DBServiceAPI::ContactSearch(application, text->getText(), text->getText(), text->getText());
+                DBServiceAPI::ContactSearch(application, {}, {}, recipient->getText());
             LOG_INFO("Get contact from another app, contacts matching num: %d",
                      static_cast<int>(searchResults.get()->size()));
             if (searchResults.get()->size() > 0) {
                 std::unique_ptr<PhonebookSearchReuqest> data =
-                    std::make_unique<PhonebookSearchReuqest>(text->getText(), searchResults);
+                    std::make_unique<PhonebookSearchReuqest>(recipient->getText(), searchResults);
                 data->disableAppClose = true;
                 return sapm::ApplicationManager::messageSwitchApplication(
                     application, app::name_phonebook, name::window::search_results, std::move(data));
             }
             return true;
         };
-        text->focusChangedCallback = [=](Item &) -> bool {
+        recipient->focusChangedCallback = [=](Item &) -> bool {
             bottomBar->setText(BottomBar::Side::CENTER, utils::localize.get("common_select"));
             return true;
         };
-        body->addWidget(text);
+        body->addWidget(recipient);
 
         auto labelMessage = new Label(body, 0, 0, body->getWidth(), 44);
         labelMessage->setText(utils::localize.get("app_messages_message"));
@@ -66,17 +94,52 @@ namespace gui
         labelMessage->setFont(style::window::font::small);
         labelMessage->setAlignement(Alignment(Alignment::ALIGN_HORIZONTAL_LEFT, Alignment::ALIGN_VERTICAL_BOTTOM));
 
-        auto message = new gui::Text(nullptr, 0, 0, body->getWidth(), 43, "", gui::Text::ExpandMode::EXPAND_UP);
+        message = new gui::Text(nullptr, 0, 0, body->getWidth(), 43, "", gui::Text::ExpandMode::EXPAND_UP);
         message->setEdges(gui::RectangleEdgeFlags::GUI_RECT_EDGE_BOTTOM);
         message->setInputMode(new InputMode(
             {InputMode::ABC, InputMode::abc, InputMode::digit},
-            [=](const UTF8 &text) {},
+            [=](const UTF8 &) {},
             [=]() { textSelectSpecialCB(); }));
         message->setPenFocusWidth(style::window::default_border_focucs_w);
         message->setPenWidth(style::window::messages::sms_border_no_focus);
         message->setFont(style::window::font::medium);
         message->setAlignment(Alignment(Alignment::ALIGN_HORIZONTAL_LEFT, Alignment::ALIGN_VERTICAL_BOTTOM));
-        message->activatedCallback = [=](Item &) -> bool { return true; };
+        message->activatedCallback = [=](Item &) -> bool {
+            auto app = dynamic_cast<app::ApplicationMessages *>(application);
+            assert(app != nullptr);
+            // if a valid contact was found, choose it. Otherwise, get a raw entered number
+            UTF8 number =
+                (contact && contact->numbers.size() != 0) ? contact->numbers[0].numberE164 : recipient->getText();
+            auto ret = app->sendSms(number, message->getText());
+            if (!ret) {
+                LOG_ERROR("sendSms failed");
+                return false;
+            }
+            uint32_t contactId;
+            if (!contact || contact->numbers.size() == 0) {
+                // once the sms is send, there is assumption that contact exists
+                auto records = DBServiceAPI::ContactGetByPhoneNumber(application, number);
+                if (records->empty()) {
+                    LOG_ERROR("not valid contact for number %s", number.c_str());
+                    return false;
+                }
+                contact = std::make_shared<ContactRecord>(records->operator[](0));
+            }
+
+            contactId = contact->ID;
+
+            auto thread = DBServiceAPI::ThreadGetByContact(application, contactId);
+            if (thread) {
+                application->switchWindow(gui::name::window::thread_view,
+                                          std::make_unique<SMSThreadData>(std::move(thread)));
+            }
+            else {
+                LOG_FATAL("No thread and thread not created!");
+                return false;
+            }
+
+            return true;
+        };
         message->focusChangedCallback = [=](Item &) -> bool {
             bottomBar->setText(BottomBar::Side::CENTER, utils::localize.get("common_send"));
             return true;
@@ -90,25 +153,10 @@ namespace gui
         setFocusItem(body);
     }
 
-    bool NewSMS_Window::onInput(const InputEvent &inputEvent)
-    {
-        return AppWindow::onInput(inputEvent);
-    }
-
-    void NewSMS_Window::onBeforeShow(ShowMode mode, SwitchData *data)
-    {
-        if (auto pdata = dynamic_cast<PhonebookSearchReuqest *>(data)) {
-            LOG_INFO("received search results");
-            text->setText(pdata->result->getFormattedName());
-        }
-    }
-
-    void NewSMS_Window::rebuild()
-    {}
-
-    void NewSMS_Window::buildInterface()
-    {}
-
     void NewSMS_Window::destroyInterface()
-    {}
+    {
+        AppWindow::destroyInterface();
+
+        children.clear();
+    }
 }; // namespace gui
