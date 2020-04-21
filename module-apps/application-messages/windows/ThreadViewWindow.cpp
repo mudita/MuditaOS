67592deb247b3ba869aa75fabc2c25c841eb69b4
alek <aleksander.rudnik@mudita.com>
2020-04-21 14:09:25 +0200

    [EGD-3082] PR fixes

--- module-apps/application-messages/windows/ThreadViewWindow.cpp
+++ module-apps/application-messages/windows/ThreadViewWindow.cpp
@@ -96,7 +96,6 @@ namespace gui
         };
         text->inputCallback = [=](Item &, const InputEvent &event) {
             if (event.state == InputEvent::State::keyReleasedShort && event.keyCode == KeyCode::KEY_LF) {
-                LOG_INFO("New message options");
                 auto app = dynamic_cast<app::ApplicationMessages *>(application);
                 assert(app != nullptr);
                 return app->newMessageOptions();
