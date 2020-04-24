9fde478b56d188fec09376555794e87b93f83f41
rkubiak01 <roman.kubiak@mudita.com>
2020-04-24 10:54:04 +0200

    using smart pointer for buffer management

--- module-vfs/vfs-utils.cpp
+++ module-vfs/vfs-utils.cpp
@@ -2,17 +2,17 @@
 
 unsigned long vfs::computeCRC32(vfs::FILE *file, unsigned long *outCrc32)
 {
-    unsigned char buf[purefs::buffer::crc_buf];
+    std::unique_ptr<unsigned char> buf(new unsigned char[purefs::buffer::crc_buf]);
     size_t bufLen;
 
     *outCrc32 = 0;
 
     while (!::vfs.eof(file)) {
-        bufLen = ::vfs.fread(buf, 1, purefs::buffer::crc_buf, file);
+        bufLen = ::vfs.fread(buf.get(), 1, purefs::buffer::crc_buf, file);
         if (bufLen <= 0)
             break;
 
-        *outCrc32 = Crc32_ComputeBuf(*outCrc32, buf, bufLen);
+        *outCrc32 = Crc32_ComputeBuf(*outCrc32, buf.get(), bufLen);
     }
 
     return 0;
@@ -34,19 +34,20 @@ bool vfs::verifyCRC(const std::string filePath, const unsigned long crc32)
 
 bool vfs::verifyCRC(const fs::path filePath)
 {
-    char crcBuf[purefs::buffer::crc_char_size] = {0};
+    std::unique_ptr<char> crcBuf(new char[purefs::buffer::crc_char_size]);
+
     fs::path crcFilePath(filePath);
     crcFilePath += purefs::extension::crc32;
 
     vfs::FILE *fp = ::vfs.fopen(crcFilePath.c_str(), "r");
     if (fp != NULL) {
-        if (::vfs.fgets(crcBuf, purefs::buffer::crc_char_size, fp) == NULL) {
+        if (::vfs.fgets(crcBuf.get(), purefs::buffer::crc_char_size, fp) == NULL) {
             LOG_ERROR("verifyCRC ff_fgets on %s returned NULL", crcFilePath.c_str());
             return (false);
         }
 
         ::vfs.fclose(fp);
-        const unsigned long crc32Read = strtoull(crcBuf, nullptr, purefs::buffer::crc_radix);
+        const unsigned long crc32Read = strtoull(crcBuf.get(), nullptr, purefs::buffer::crc_radix);
         return (verifyCRC(filePath, crc32Read));
     }
     LOG_ERROR("verifyCRC can't open %s", crcFilePath.c_str());
