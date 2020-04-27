4cc4101f2d7ae9fa99c28adbe6a83042265285e4
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2020-04-27 21:20:37 +0200

    [EGD-2950] Fix minor issues with Add to favourites and blocked icons

--- module-apps/application-phonebook/windows/PhonebookNewContact.hpp
+++ module-apps/application-phonebook/windows/PhonebookNewContact.hpp
@@ -83,12 +83,7 @@ namespace gui
                 noteLabel->setVisible(visible);
                 text[0]->setVisible(visible);
                 text[1]->setVisible(visible);
-                if (visible && favSelected) {
-                    imageTick->setVisible(true);
-                }
-                else {
-                    imageTick->setVisible(false);
-                }
+                imageTick->setVisible(visible && favSelected);
             }
         };
 
