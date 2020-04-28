d3faef1101638f4886b5f87e77a9043e823ace55
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-04-28 16:58:18 +0200

    EGD-2964 application calendar - removed leak, added to menu

--- module-apps/application-calendar/windows/CalendarMainWindow.hpp
+++ module-apps/application-calendar/windows/CalendarMainWindow.hpp
@@ -1,13 +1,4 @@
-/*
- * @file CalendarMainWindow.hpp
- * @author Robert Borzecki (robert.borzecki@mudita.com)
- * @date 3 cze 2019
- * @brief
- * @copyright Copyright (C) 2019 mudita.com
- * @details
- */
-#ifndef MODULE_APPS_APPLICATION_CALENDAR_WINDOWS_CALENDARMAINWINDOW_HPP_
-#define MODULE_APPS_APPLICATION_CALENDAR_WINDOWS_CALENDARMAINWINDOW_HPP_
+#pragma once
 
 #include "windows/AppWindow.hpp"
 #include "Application.hpp"
@@ -20,17 +11,11 @@ namespace app
      */
     class CalendarMainWindow : public gui::AppWindow
     {
-      protected:
-        // labels with names of the days
-        //	gui::Label days;
       public:
         CalendarMainWindow(Application *app, std::string name);
-        virtual ~CalendarMainWindow();
         void rebuild() override;
         void buildInterface() override;
         void destroyInterface() override;
     };
 
 } /* namespace app */
-
-#endif /* MODULE_APPS_APPLICATION_CALENDAR_WINDOWS_CALENDARMAINWINDOW_HPP_ */
