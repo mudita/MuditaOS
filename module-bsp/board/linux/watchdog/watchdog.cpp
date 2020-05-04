61014adb4dfdb39ffd62031919aa486886a9f729
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-05-04 17:55:14 +0200

    Fix cd: removed weaks

---
+++ module-bsp/board/linux/watchdog/watchdog.cpp
@@ -0,0 +1,15 @@
+#include <bsp/watchdog/watchdog.hpp>
+
+namespace bsp::watchdog
+{
+    __attribute__((weak)) void init()
+    {}
+    __attribute__((weak)) void system_reset()
+    {}
+    __attribute__((weak)) void pet()
+    {}
+    __attribute__((weak)) std::string reset_cause()
+    {
+        return {};
+    }
+} // namespace bsp::watchdog
