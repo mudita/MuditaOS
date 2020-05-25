8c26838f138c8818076b3f6463c3b457e88bac36
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2020-05-25 09:37:30 +0200

    [EGD-3226] Add automatic copy of proper .gdbinit to build direcotry

---
+++ config/CopyGdbInit.cmake
@@ -0,0 +1,16 @@
+if ($ENV{JETBRAINS_IDE})
+    set(CLION_IDE True CACHE BOOL "Build under CLion")
+endif ()
+
+if (${PROJECT_TARGET} STREQUAL "TARGET_Linux")
+    set(GDB_INIT_SCRIPT "${CMAKE_SOURCE_DIR}/.gdbinit")
+else()
+    if(CLION_IDE)
+        set(GDB_INIT_SCRIPT "${CMAKE_SOURCE_DIR}/.gdbinit-1051-clion")
+    else()
+        set(GDB_INIT_SCRIPT "${CMAKE_SOURCE_DIR}/.gdbinit-1051")
+    endif()
+endif()
+
+configure_file(${GDB_INIT_SCRIPT} ${CMAKE_BINARY_DIR}/.gdbinit COPYONLY)
+
