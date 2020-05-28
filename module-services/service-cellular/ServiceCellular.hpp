04acd2c1c7d128d5499a174401f0ad635bf344f1
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2020-05-28 22:21:36 +0200

    [EGD-3236] Add new cellular states URCReady and Ready

--- module-services/service-cellular/ServiceCellular.hpp
+++ module-services/service-cellular/ServiceCellular.hpp
@@ -104,6 +104,8 @@ class ServiceCellular : public sys::Service
     bool handle_audio_conf_procedure();
     /// modem on event is used in desktop to follow up sim selection
     bool handle_modem_on();
+    /// URCReady event is set when serwice is ready to handle URC notifications
+    bool handle_URCReady();
     /// check one time modem configuration for sim (hot swap)
     /// if hot swap is not enabled full modem restart is needed (right now at best reboot)
     bool handle_sim_sanity_check();
@@ -116,6 +118,7 @@ class ServiceCellular : public sys::Service
     /// fatal failure handler, if we have power switch - we could handle it here
     /// \note some run state should be added to ignore non system messages now...
     bool handle_fatal_failure();
+    bool handle_ready();
 
     /// @}
 
