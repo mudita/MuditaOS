ad98bec8e88fc02170616c6d2dc20bf3ae7df00d
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2020-06-02 19:17:15 +0200

    [EGD-3232] Move function formatVCard to separate class

---
+++ module-apps/application-phonebook/data/PhonebookUtils.cpp
@@ -0,0 +1,27 @@
+#include "PhonebookUtils.hpp"
+
+
+auto PhonebookUtils::formatVCard(const ContactRecord &contact) -> const std::string
+{
+    const std::string priNumber = (contact.numbers.size() > 0) ? contact.numbers[0].numberE164.c_str() : "";
+    const std::string secNumber = (contact.numbers.size() > 1) ? contact.numbers[1].numberE164.c_str() : "";
+    std::ostringstream vcard;
+    vcard << "BEGIN:VCARD\n"
+          << "VERSION:3.0\n"
+          << "N:" << contact.alternativeName.c_str() << ";" << contact.primaryName.c_str() << ";;;\n"
+          << "FN:" << contact.primaryName.c_str() << " " << contact.alternativeName.c_str() << "\n"
+          << "TEL;TYPE=HOME,VOICE:" << priNumber << "\n";
+
+    if (secNumber.length() > 0) {
+        vcard << "TEL;TYPE=HOME,VOICE:" << secNumber << "\n";
+    }
+
+    vcard << "ADR;TYPE=HOME:;;" << contact.address.c_str() << "\n"
+          << "EMAIL:" << contact.mail.c_str() << "\n"
+          << "END:VCARD";
+
+    LOG_INFO("formatted vcard:");
+    LOG_INFO("%s", vcard.str().c_str());
+
+    return (vcard.str());
+}
