e6cdd96e600ef3db1f3b67c0ddcc0cf1438bbfe3
alek <aleksander.rudnik@mudita.com>
2020-06-04 13:00:54 +0200

    [EGD-3263] Bottomr bar store and restore

--- module-gui/gui/widgets/BottomBar.hpp
+++ module-gui/gui/widgets/BottomBar.hpp
@@ -33,12 +33,15 @@ namespace gui
         };
 
       protected:
-        Label *left;
-        Label *center;
-        Label *right;
+        bool stored = false;
+        UTF8 storedLeft;
+        UTF8 storedCenter;
+        UTF8 storedRight;
+        Label *left   = nullptr;
+        Label *center = nullptr;
+        Label *right  = nullptr;
         gui::Label *prepareLabel(BottomBar::Side side);
         Label *getSide(BottomBar::Side side);
-
       public:
         BottomBar();
         BottomBar(Item *parent, uint32_t x, uint32_t y, uint32_t w, uint32_t h);
@@ -47,6 +50,9 @@ namespace gui
         void setActive(BottomBar::Side side, bool active);
         void setText(BottomBar::Side side, const UTF8 &str, bool active = true);
         UTF8 getText(BottomBar::Side side);
+
+        void store();
+        void restore();
 
         // virtual methods from Item
         bool onDimensionChanged(const BoundingBox &oldDim, const BoundingBox &newDim);
