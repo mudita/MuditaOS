e7c07b8bf272875bf45eb32e72923088e86f921f
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2020-06-15 14:36:43 +0200

    [EGD-3291] Regenerate version info on commit

--- config/Version.cmake
+++ config/Version.cmake
@@ -1,5 +1,7 @@
 # from: https://www.mattkeeter.com/blog/2018-01-06-versioning/
 
+set(VERSION_HEADER "${CMAKE_BINARY_DIR}/source/version.hpp")
+
 execute_process(COMMAND git log --pretty=format:'%h' -n 1
                 OUTPUT_VARIABLE GIT_REV
                 ERROR_QUIET)
@@ -16,12 +18,12 @@ else()
     execute_process(
         COMMAND bash -c "git diff --quiet --exit-code || echo +"
         OUTPUT_VARIABLE GIT_DIFF
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        WORKING_DIRECTORY ${SRC_DIR}
         )
     execute_process(
-        COMMAND git describe --exact-match --tags
+        COMMAND git describe --tags
         OUTPUT_VARIABLE GIT_TAG ERROR_QUIET RESULT_VARIABLE ret
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        WORKING_DIRECTORY ${SRC_DIR}
         )
         if(NOT ret EQUAL "0")
             set(GIT_TAG "none")
@@ -36,14 +38,18 @@ else()
     string(STRIP "${GIT_TAG}" GIT_TAG)
     string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
 endif()
+message("GIT_REV: ${GIT_REV}")
+message("GIT_TAG: ${GIT_TAG}")
 
-string(REGEX MATCH "release-([0-9]*)\.([0-9]*)\.([0-9]*)" VERSION_RAW ${GIT_TAG})
+string(REGEX MATCH "release-([0-9]*).([0-9]*).([0-9]*)" VERSION_RAW ${GIT_TAG})
 
 set(CMAKE_PROJECT_VERSION_MAJOR "${CMAKE_MATCH_1}")
 set(CMAKE_PROJECT_VERSION_MINOR "${CMAKE_MATCH_2}")
 set(CMAKE_PROJECT_VERSION_PATCH "${CMAKE_MATCH_3}")
 
+message("Vession: ${CMAKE_PROJECT_VERSION_MAJOR}.${CMAKE_PROJECT_VERSION_MINOR}.${CMAKE_PROJECT_VERSION_PATCH}")
+
 configure_file(
-    ${CMAKE_CURRENT_SOURCE_DIR}/source/version.hpp.template
+    ${SRC_DIR}/source/version.hpp.template
     ${CMAKE_BINARY_DIR}/source/version.hpp
     )
