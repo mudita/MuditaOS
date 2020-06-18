92e90fec549663beccc4db12ada2ebd13069bf64
Alek Rudnik <54846206+alekrudnik@users.noreply.github.com>
2020-06-18 20:44:25 +0200

    [EGD-3301] - contact add / contact details option (#444)
    * [EGD-3301] - contact add / contact details option
    
    * [EGD-3301] missing change

--- module-apps/application-messages/windows/OptionsWindow.cpp
+++ module-apps/application-messages/windows/OptionsWindow.cpp
@@ -9,26 +9,26 @@ std::list<gui::Option> threadWindowOptions(app::ApplicationMessages *app, const 
 {
 
     ContactRecord contact = record ? DBServiceAPI::ContactGetByID(app, record->contactID)->front() : ContactRecord();
+    std::list<gui::Option> options;
 
-    return {
-        gui::options::call(app, app::CallOperation::ExecuteCall, contact),
-        gui::options::contact(app, app::ContactOperation::Details, contact),
-        gui::Option{utils::localize.get("sms_delete_conversation"),
-                    [=](gui::Item &item) {
-                        LOG_INFO("Removing sms thread!");
-                        return app->removeSMS_thread(record);
-                    }},
+    options.push_back(gui::options::call(app, app::CallOperation::ExecuteCall, contact));
+    auto contactOperation =
+        contact.contactType == ContactType::TEMPORARY ? app::ContactOperation::Add : app::ContactOperation::Details;
+    options.push_back(gui::options::contact(app, contactOperation, contact));
+    // TODO
+    // last sms, all sms? what author had in mind?
+    options.push_back(gui::Option{"<STUB>" + utils::localize.get("sms_mark_unread"), [=](gui::Item &item) {
+                                      LOG_ERROR("TODO: need implementation");
+                                      return true;
+                                  }});
+    options.push_back(gui::Option{utils::localize.get("sms_delete_conversation"), [=](gui::Item &item) {
+                                      LOG_INFO("Removing sms thread!");
+                                      return app->removeSMS_thread(record);
+                                  }});
 
-        // TODO
-        // last sms, all sms? what author had in mind?
-        gui::Option{"<STUB>" + utils::localize.get("sms_mark_unread"),
-                    [=](gui::Item &item) {
-                        LOG_ERROR("TODO: need implementation");
-                        return true;
-                    }},
+    // TODO
+    // shouldn't this be in show contact details actually? it would be much easier too
+    // {utils::localize.get("sms_add_to_contacts"), [=](gui::Item &item) { return true; }},
 
-        // TODO
-        // shouldn't this be in show contact details actually? it would be much easier too
-        // {utils::localize.get("sms_add_to_contacts"), [=](gui::Item &item) { return true; }},
-    };
+    return options;
 };
