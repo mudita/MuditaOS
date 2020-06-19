1d6ae6511269b8cc79884a598276d397a5af5b3a
Roman Kubiak <roman.kubiak@mudita.com>
2020-06-19 16:53:23 +0200

    unit tests added

---
+++ module-services/service-desktop/tests/unittest.cpp
@@ -0,0 +1,29 @@
+#include "catch.hpp"
+#include "vfs.hpp"
+#include <UpdatePureOS.h>
+#include <stdint.h>
+#include <algorithm>
+#include <iostream>
+
+class vfs vfs;
+
+
+TEST_CASE("Test case 1")
+{
+    vfs.Init();
+    UpdatePureOS updateOS(nullptr);
+    
+    updateos::UpdateError err = updateOS.prepareTempDirForUpdate();
+    REQUIRE(err == updateos::UpdateError::NoError);
+
+    updateOS.setUpdateFile("pureos-unittest.tar");
+
+    err = updateOS.unpackUpdate();
+    REQUIRE(err == updateos::UpdateError::NoError);
+
+    err = updateOS.verifyChecksums();
+    REQUIRE(err == updateos::UpdateError::NoError);
+
+    err = updateOS.prepareRoot();
+    REQUIRE(err == updateos::UpdateError::NoError);
+}
