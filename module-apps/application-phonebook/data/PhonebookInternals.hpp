d31675fcbcc3040a970ed25610dd11f9c40a07af
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2020-06-22 18:42:16 +0200

    [EGD-3312] Add enum class for ListItemName

--- module-apps/application-phonebook/data/PhonebookInternals.hpp
+++ module-apps/application-phonebook/data/PhonebookInternals.hpp
@@ -3,4 +3,18 @@
 namespace phonebookInternals
 {
     constexpr uint8_t speedDialMaxValue = 9;
+
+    enum class ListItemName
+    {
+        FirstName,
+        SecondName,
+        Number,
+        OtherNumber,
+        Email,
+        SpeedDialKey,
+        AddToFavourites,
+        AddToICE,
+        Address,
+        Note
+    };
 } // namespace phonebookInternals
