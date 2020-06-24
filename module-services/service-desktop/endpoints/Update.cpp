052cf4e3609d3f7ea8197965555de66ef18b345a
Roman Kubiak <roman.kubiak@mudita.com>
2020-06-24 13:58:10 +0200

    split update into functions

--- module-services/service-desktop/endpoints/Update.cpp
+++ module-services/service-desktop/endpoints/Update.cpp
@@ -2,51 +2,55 @@
 #include "ParserUtils.hpp"
 #include "../ServiceDesktop.hpp"
 
+static sys::ReturnCodes run(uint32_t uuid, json11::Json &body, std::string &responseStr, sys::Service *ownerService)
+{
+    std::string fileName = body["fileName"].string_value();
+
+    json11::Json responseBodyJson = json11::Json::object({{parserutils::json::updateReady, true}});
+
+    json11::Json responsePayloadJson =
+        json11::Json::object({{parserutils::json::endpoint, static_cast<int>(parserutils::Endpoint::update)},
+                              {parserutils::json::status, static_cast<int>(parserutils::http::Code::OK)},
+                              {parserutils::json::uuid, std::to_string(uuid)},
+                              {parserutils::json::body, responseBodyJson}});
+
+    responseStr = EndpointHandler::buildResponseStr(responsePayloadJson.dump().size(), responsePayloadJson.dump());
+
+    auto msg = std::make_shared<sdesktop::UpdateOsMessage>(MessageType::UpdateOS, fileName, uuid);
+    sys::Bus::SendUnicast(msg, service::name::service_desktop, ownerService);
+
+    return sys::ReturnCodes::Success;
+}
+
+static sys::ReturnCodes getUpdates(uint32_t uuid,
+                                   json11::Json &body,
+                                   std::string &responseStr,
+                                   sys::Service *ownerService)
+{
+    json11::Json fileList = vfs.listdir(purefs::dir::os_updates.c_str(), updateos::extension::update, true);
+
+    json11::Json responseBodyJson = json11::Json::object{{parserutils::json::updateFileList, fileList}};
+
+    json11::Json responsePayloadJson =
+        json11::Json::object({{parserutils::json::endpoint, static_cast<int>(parserutils::Endpoint::update)},
+                              {parserutils::json::status, static_cast<int>(parserutils::http::Code::OK)},
+                              {parserutils::json::uuid, std::to_string(uuid)},
+                              {parserutils::json::body, responseBodyJson}});
+
+    responseStr = EndpointHandler::buildResponseStr(responsePayloadJson.dump().size(), responsePayloadJson.dump());
+
+    return sys::ReturnCodes::Success;
+}
+
 sys::ReturnCodes EndpointHandler::update(
     uint8_t httpMethod, uint32_t uuid, json11::Json &body, std::string &responseStr, sys::Service *ownerService)
 {
     if (httpMethod == static_cast<uint8_t>(parserutils::http::Method::post)) {
-        LOG_INFO("update request: ");
-        LOG_INFO("%s", body.dump().c_str());
-        std::string fileName = body["fileName"].string_value();
-
-        LOG_INFO("update request, fileName: %s", fileName.c_str());
-
-        json11::Json responseBodyJson = json11::Json::object({{parserutils::json::updateReady, true}});
-
-        json11::Json responsePayloadJson =
-            json11::Json::object({{parserutils::json::endpoint, static_cast<int>(parserutils::Endpoint::update)},
-                                  {parserutils::json::status, static_cast<int>(parserutils::http::Code::OK)},
-                                  {parserutils::json::uuid, std::to_string(uuid)},
-                                  {parserutils::json::body, responseBodyJson}});
-
-        responseStr = EndpointHandler::buildResponseStr(responsePayloadJson.dump().size(), responsePayloadJson.dump());
-
-        auto msg = std::make_shared<sdesktop::UpdateOsMessage>(MessageType::UpdateOS, fileName, uuid);
-        sys::Bus::SendUnicast(msg, service::name::service_desktop, ownerService);
-
-        return sys::ReturnCodes::Success;
+        return run(uuid, body, responseStr, ownerService);
     }
     else if (httpMethod == static_cast<uint8_t>(parserutils::http::Method::get)) {
-        LOG_INFO("update get request: ");
-        LOG_INFO("%s", body.dump().c_str());
-
-        json11::Json fileList = vfs.listdir(purefs::dir::os_updates.c_str(), updateos::extension::update, true);
-
-        json11::Json responseBodyJson = json11::Json::object{{parserutils::json::updateFileList, fileList}};
-
-        json11::Json responsePayloadJson =
-            json11::Json::object({{parserutils::json::endpoint, static_cast<int>(parserutils::Endpoint::update)},
-                                  {parserutils::json::status, static_cast<int>(parserutils::http::Code::OK)},
-                                  {parserutils::json::uuid, std::to_string(uuid)},
-                                  {parserutils::json::body, responseBodyJson}});
-
-        responseStr = EndpointHandler::buildResponseStr(responsePayloadJson.dump().size(), responsePayloadJson.dump());
-
-        return sys::ReturnCodes::Success;
+        return getUpdates(uuid, body, responseStr, ownerService);
     }
-    else if (httpMethod == static_cast<uint8_t>(parserutils::http::Method::put)) {
-        return sys::ReturnCodes::Failure;
-    }
+
     return sys::ReturnCodes::Failure;
 }
--- module-services/service-desktop/parser/EndpointHandler.hpp
+++ module-services/service-desktop/parser/EndpointHandler.hpp
@@ -11,7 +11,5 @@ class EndpointHandler
         uint8_t httpMethod, uint32_t uuid, json11::Json &body, std::string &responseStr, sys::Service *ownerService);
     sys::ReturnCodes deviceInfo(
         uint8_t httpMethod, uint32_t uuid, json11::Json &body, std::string &responseStr, sys::Service *ownerService);
-
-  private:
-    std::string buildResponseStr(std::size_t responseSize, std::string responsePayloadString);
+    static std::string buildResponseStr(std::size_t responseSize, std::string responsePayloadString);
 };
