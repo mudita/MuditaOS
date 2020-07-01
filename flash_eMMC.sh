12fc435f9f4cf69d98ed36c5ea15849a209fba46
Roman Kubiak <roman.kubiak@mudita.com>
2020-07-01 14:49:39 +0200

    CORE: flash scripts and partition creation script modified for current/previous support. Two partitions are creared .boot.ini is created and used now.

--- flash_eMMC.sh
+++ flash_eMMC.sh
@@ -13,7 +13,7 @@ fi
 
 check_target_rt1051 "$BUILD_PATH"
 
-PURE_DEV=/dev/disk/by-id/usb-NXP_SEMI_NXP_MASS_STORAGE_0123456789ABCDEF-0:0
+PURE_DEV=/dev/disk/by-id/usb-NXP_SEMI_NXP_MASS_STORAGE_0123456789ABCDEF-0:0-part1
 PURE_DISK=`readlink -f $PURE_DEV`
 # check if already mouted
 
@@ -22,22 +22,35 @@ if [ $? -ne 0 ]; then
 	>&2 echo "PurePhone not mounted. Mount or retry with Ozone. https://github.com/muditacom/PurePhone/blob/master/doc/running_on_phone.md#eMMC_upload"
 	exit 2
 fi
+
 PURE_PATH=$(echo $MOUNT_ENTRY | awk -F " " '{print $2}')
 # unespace
-PURE_PATH=$(echo -e $PURE_PATH)
-
+PURE_PATH_CURRENT=$(echo -e $PURE_PATH)/current
+PURE_PATH_ROOT=$(echo -e $PURE_PATH)
 
 echo "PurePhone remove all files"
-rm "$PURE_PATH"/* -r &>/dev/null || echo "PurePhone disk alread empty"
-rm -f $PURE_PATH/.boot.ini*
+rm -rf "$PURE_PATH_ROOT"/* -r &>/dev/null || echo "PurePhone disk alread empty"
+rm -f "$PURE_PATH_ROOT"/.boot.ini
+rm -f "$PURE_PATH_ROOT"/.boot.ini.crc32
+
+echo "Create directories"
+mkdir -p $PURE_PATH_ROOT/$PURE_OS_CURRENT
+mkdir -p $PURE_PATH_ROOT/$PURE_OS_PREVIOUS
+mkdir -p $PURE_PATH_ROOT/$PURE_OS_FACTORY
+
 # sudo sync $PURE_DEV
 
 echo "PurePhone copy build files"
-cp -v $BUILD_PATH/boot.bin "$PURE_PATH"/  # | sed 's/'-\>'/'→'/g'
+cp -v $BUILD_PATH/boot.bin "$PURE_PATH_CURRENT"/  # | sed 's/'-\>'/'→'/g'
 
 for file in $IMAGE_FILES; do
 	echo Copying $file
-	cp -vr $BUILD_PATH/$file "$PURE_PATH"/
+	cp -vr $BUILD_PATH/$file "$PURE_PATH_CURRENT"/
+done
+
+for file in $IMAGE_MOVE_TO_ROOT; do
+	echo "Moving $PURE_PATH_CURRENT/$file to $PURE_PATH_ROOT"
+	mv "$PURE_PATH_CURRENT"/"$file" "$PURE_PATH_ROOT"/
 done
 
 # sudo sync $PURE_DEV # https://unix.stackexchange.com/a/345950
