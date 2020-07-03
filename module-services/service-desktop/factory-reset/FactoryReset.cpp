1158373f9ff27a6108803cbaad0c211465eca79c
Szymon Mroczek <szymon.mroczek@mudita.com>
2020-07-03 15:25:53 +0200

    [EGD-3352] Safety checks added. Min size of factory dir. Maximum legth of soure/target file path. Maximum level of directory recurse depth.

--- module-services/service-desktop/factory-reset/FactoryReset.cpp
+++ module-services/service-desktop/factory-reset/FactoryReset.cpp
@@ -3,9 +3,15 @@
 #include "service-db/includes/DBServiceName.hpp"
 #include "SystemManager/SystemManager.hpp"
 
-static uint32_t resetCounter = 0;
 static int runcount          = 0;
-#define TEST 0
+static const int min_factory_dir_size = 4;
+/* 4 is just an arbitrary value of min number of entries required in the factory reset directory.
+ * Otherwise the factory reset dir might be corrupted.
+ */
+static const int max_recurse_depth = 120;
+/* 120 is just an arbitrary value of max number of recursive calls.
+ * If more then error is assumed, not the real depth of directories."
+ */
 
 void FactoryReset::Run(sys::Service *ownerService)
 {
@@ -14,30 +20,26 @@ void FactoryReset::Run(sys::Service *ownerService)
     }
     runcount = 1;
 
-    resetCounter = 0;
+    LOG_INFO("FactoryReset: restoring factory state started...");
 
-    LOG_INFO("FactoryReset: restoring started");
+    std::vector<vfs::DirectoryEntry> dirlist = vfs.listdir(purefs::dir::os_factory.c_str(), "", true);
 
-    /* TOOO: add check if factory folder is not empty */
-#if TEST
-    std::string maindir = "/sys/main";
-    DeleteDirContent(maindir);
-    CopyDirContent(purefs::dir::os_factory, maindir);
-#else
-    LOG_INFO("FactoryReset: closing ServiceDB...");
-    std::string dbServiceName = service::name::db;
-    sys::SystemManager::DestroyService(dbServiceName, ownerService);
-    DeleteDirContent(purefs::dir::eMMC_disk);
-    CopyDirContent(purefs::dir::os_factory, purefs::dir::eMMC_disk);
-#endif
+    if (dirlist.size() >= min_factory_dir_size) {
+        LOG_INFO("FactoryReset: closing ServiceDB...");
+        std::string dbServiceName = service::name::db;
+        sys::SystemManager::DestroyService(dbServiceName, ownerService);
 
-    LOG_INFO("FactoryReset: restoring finished");
+        DeleteDirContent(purefs::dir::eMMC_disk);
 
-#if TEST
-#else
-    LOG_INFO("DoFactoryReset: factory reset finished, rebooting...");
-    sys::SystemManager::Reboot(ownerService);
-#endif
+        CopyDirContent(purefs::dir::os_factory, purefs::dir::eMMC_disk);
+
+        LOG_INFO("DoFactoryReset: restoring factory state finished, rebooting...");
+        sys::SystemManager::Reboot(ownerService);
+    }
+    else {
+        LOG_ERROR("FactoryReset: restoring factory state aborted");
+        LOG_ERROR("FactoryReset: directory %s seems corrupted.", purefs::dir::os_factory.c_str());
+    }
 }
 
 void FactoryReset::DeleteDirContent(std::string dir)
@@ -65,45 +67,60 @@ void FactoryReset::DeleteDirContent(std::string dir)
     }
 }
 
-/* TODO: add maximum recurse depth limit, is there a limit for the FreeRTOS FAT or Linux? */
 void FactoryReset::CopyDirContent(std::string sourcedir, std::string targetdir)
 {
-    std::vector<vfs::DirectoryEntry> dirlist = vfs.listdir(sourcedir.c_str(), "", true);
+    static int recurseLevel = 0;
 
-    for (auto &direntry : dirlist) {
-        if ((direntry.fileName.compare(".") != 0) && (direntry.fileName.compare("..") != 0) &&
-            (direntry.fileName.compare("...") != 0)) {
-            resetCounter++;
+    recurseLevel++;
 
-            std::string sourcepath = sourcedir;
-            sourcepath += "/";
-            sourcepath += direntry.fileName.c_str();
+    if (recurseLevel < max_recurse_depth) {
+        std::vector<vfs::DirectoryEntry> dirlist = vfs.listdir(sourcedir.c_str(), "", true);
 
-            std::string targetpath = targetdir;
-            targetpath += "/";
-            targetpath += direntry.fileName.c_str();
+        for (auto &direntry : dirlist) {
+            if ((direntry.fileName.compare(".") != 0) && (direntry.fileName.compare("..") != 0) &&
+                (direntry.fileName.compare("...") != 0)) {
 
-            if (direntry.attributes == vfs::FileAttributes::Directory) {
-                if (direntry.fileName.compare(PATH_FACTORY) != 0) {
-                    LOG_INFO("FactoryReset: %lu, restoring dir %s into %s...",
-                             resetCounter,
-                             sourcepath.c_str(),
-                             targetpath.c_str());
+                std::string sourcepath = sourcedir;
+                sourcepath += "/";
+                sourcepath += direntry.fileName.c_str();
 
-                    vfs.mkdir(targetpath.c_str());
+                std::string targetpath = targetdir;
+                targetpath += "/";
+                targetpath += direntry.fileName.c_str();
 
-                    CopyDirContent(sourcepath, targetpath);
+                if ((sourcepath.size() < ffconfigMAX_FILENAME) && (targetpath.size() < ffconfigMAX_FILENAME)) {
+                    if (direntry.attributes == vfs::FileAttributes::Directory) {
+                        if (targetpath.compare(purefs::dir::os_factory.c_str()) != 0) {
+                            LOG_INFO(
+                                "FactoryReset: restoring dir  %s into %s...", sourcepath.c_str(), targetpath.c_str());
+
+                            vfs.mkdir(targetpath.c_str());
+
+                            CopyDirContent(sourcepath, targetpath);
+                        }
+                    }
+                    else {
+                        LOG_INFO("FactoryReset: restoring file %s into %s...", sourcepath.c_str(), targetpath.c_str());
+
+                        CopyFile(sourcepath, targetpath);
+                    }
                 }
-            }
-            else {
-                LOG_INFO("FactoryReset: %lu, restoring file %s into %s...",
-                         resetCounter,
-                         sourcepath.c_str(),
-                         targetpath.c_str());
-                CopyFile(sourcepath, targetpath);
+                else {
+                    LOG_ERROR("FactoryReset: path length (source or target) exceeds system limit of %d",
+                              ffconfigMAX_FILENAME);
+                    LOG_ERROR(
+                        "FactoryReset: skipping restore of dir %s into %s", sourcepath.c_str(), targetpath.c_str());
+                }
             }
         }
     }
+    else {
+        LOG_ERROR("FactoryReset: recurse level %d (too high), error assumed, skipping restore of dir %s",
+                  recurseLevel,
+                  sourcedir.c_str());
+    }
+
+    recurseLevel--;
 }
 
 void FactoryReset::CopyFile(std::string sourcefile, std::string targetfile)
