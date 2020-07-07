1125289398854794a9c0d24c4ac4b3af961eb9d6
Bartek Cichocki <bartosz.cichocki@mudita.com>
2020-07-07 18:06:34 +0200

    [EGD-3379] fixed build flags, suppressed warnings for protobuf, sbini and port.c in module-os

--- module-os/targets/Target_Linux.cmake
+++ module-os/targets/Target_Linux.cmake
@@ -1,3 +1,9 @@
 set(BOARD_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/board/linux/port.c CACHE INTERNAL "")
 
 set(BOARD_DIR_INCLUDES  ${CMAKE_CURRENT_SOURCE_DIR}/board/linux CACHE INTERNAL "")
+
+#Suppress unused variable warning
+set_source_files_properties(${BOARD_SOURCES}
+        PROPERTIES COMPILE_FLAGS
+	"-Wno-unused-but-set-variable"
+)
