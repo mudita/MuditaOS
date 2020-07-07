09bb4dd1663f9d4a6e549bf6abd7d3c368c70cea
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2020-07-07 09:21:33 +0200

    [EGD-3350] Flags icons in top bar in phonebook

---
+++ module-apps/application-phonebook/widgets/ContactFlagsStyle.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Style.hpp>
+
+namespace style
+{
+    namespace widget
+    {
+        namespace ContatFlas
+        {
+            const uint32_t iconsSize   = 32;
+            const uint32_t height      = 74;
+            const uint32_t itemWidth   = 90;
+            const uint32_t itemHeight  = 55;
+            const uint32_t labelHeight = 20;
+        } // namespace ContatFlas
+    }     // namespace widget
+} // namespace style
