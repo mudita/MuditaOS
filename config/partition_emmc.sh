6cde3e04d6bdfc6ea19827eb15fe3e401e9fa97f
Roman Kubiak <roman.kubiak@mudita.com>
2020-07-07 15:57:23 +0200

    some more checks before actual write to device

--- config/partition_emmc.sh
+++ config/partition_emmc.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # set -eo pipefail
+if [ ! -e config/common.sh ]; then
+	echo "No config/common.sh, run this script from git root"
+	exit 1
+fi
+
+if [ ! $(which sfdisk) ]; then
+	echo "No sfdisk, please install"
+	exit 1
+fi
 
 source config/common.sh
 is_root=`id -u`
@@ -18,17 +27,25 @@ else
 fi
 
 pcount=$(sfdisk --dump $dev | grep "start=" | wc -l)
-is_mounted=$(grep $dev /etc/mtab)
-if [ $? != 0 ]; then
+is_mounted=$(grep $dev /etc/mtab | awk '{print $2}')
+if [ "$is_mounted" == "" ]; then
 	echo "$dev is not mounted"
+else
+	echo "$dev is mounted at $is_mounted, please unmount it first"
+	exit 1
 fi
 
 if [ $pcount == 2 ] && [ $use_the_force == 0 ]; then
-	echo "device $dev already has 2 partitions"
+	echo "device $dev already has 2 partitions (use -f to force)"
 	exit 1
 else
 	echo "$dev has $pcount partitions"
 fi
+
+echo "!!! $dev will be wiped, no recovery at this point"
+echo "!!! press ENTER to continue or CTRL+C to cancel"
+echo -ne "?"
+read
 
 sfdisk --wipe always $dev < config/emmc_partition_table.dump
 
