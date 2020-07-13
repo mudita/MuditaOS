bab3b0c29572a7e08470cfbc18b81ce0d1306a4d
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2020-07-13 09:17:58 +0200

    [EGD-3241] Add model for Contact Details

---
+++ module-apps/application-phonebook/models/ContactDetailsModel.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "application-phonebook/data/PhonebookItemData.hpp"
+#include "application-phonebook/widgets/ContactListItem.hpp"
+#include "InternalModel.hpp"
+#include "Application.hpp"
+
+#include <ListItemProvider.hpp>
+
+class ContactDetailsModel : public app::InternalModel<gui::ContactListItem *>, public gui::ListItemProvider
+{
+    app::Application *application = nullptr;
+
+  public:
+    ContactDetailsModel(app::Application *app);
+
+    void clearData();
+    void loadData(std::shared_ptr<ContactRecord> contactRecord);
+    void createData(bool showInformationWidget = false, bool showAddressWidget = false, bool showNoteWidget = false);
+
+    [[nodiscard]] auto getItemCount() const -> int override;
+
+    [[nodiscard]] auto getMinimalItemHeight() const -> unsigned int override;
+
+    auto getItem(gui::Order order) -> gui::ListItem * override;
+
+    void requestRecords(const uint32_t offset, const uint32_t limit) override;
+};
