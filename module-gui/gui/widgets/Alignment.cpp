98b7aa8d48c2f91072956fba3352e809fbb56b77
Przemyslaw Brudny <przemyslaw.brudny@mudita.com>
2020-07-14 18:41:40 +0200

    [EGD-3336] Default values bug fix.

--- module-gui/gui/widgets/Alignment.cpp
+++ module-gui/gui/widgets/Alignment.cpp
@@ -12,20 +12,6 @@ namespace gui
     Alignment::Alignment(Alignment::Vertical valV) : vertical(valV)
     {}
 
-    Alignment &Alignment::operator=(const Alignment &alignment)
-    {
-        if (this == &alignment)
-            return *this;
-
-        if (alignment.horizontal != Alignment::Horizontal::None)
-            this->horizontal = alignment.horizontal;
-
-        if (alignment.vertical != Alignment::Vertical::None)
-            this->vertical = alignment.vertical;
-
-        return *this;
-    }
-
     bool Alignment::operator==(const Alignment &alignment) const
     {
         return !(horizontal != alignment.horizontal || vertical != alignment.vertical);
