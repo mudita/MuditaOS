1fe6e257aeec5a5e29c312b7763b1e8b3d4a71fc
Szymon Mroczek <szymon.mroczek@mudita.com>
2020-07-21 17:26:09 +0200

    Revert "[EGD-3352] Added error handling."
    This reverts commit c556190cd51bd0aa4e915c3d65dff86070893bec.

--- module-services/service-desktop/factory-reset/FactoryReset.cpp
+++ module-services/service-desktop/factory-reset/FactoryReset.cpp
@@ -3,8 +3,7 @@
 #include "service-db/includes/DBServiceName.hpp"
 #include "SystemManager/SystemManager.hpp"
 
-static int runcount                   = 0;
-static int recurseLevel               = 0;
+static int runcount          = 0;
 static const int min_factory_dir_size = 4;
 /* 4 is just an arbitrary value of min number of entries required in the factory reset directory.
  * Otherwise the factory reset dir might be corrupted.
@@ -14,45 +13,36 @@ static const int max_recurse_depth = 120;
  * If more then error is assumed, not the real depth of directories."
  */
 
-bool FactoryReset::Run(sys::Service *ownerService)
+void FactoryReset::Run(sys::Service *ownerService)
 {
     if (runcount != 0) {
-        return false;
+        return;
     }
     runcount = 1;
 
     LOG_INFO("FactoryReset: restoring factory state started...");
 
-    recurseLevel = 0;
-
     std::vector<vfs::DirectoryEntry> dirlist = vfs.listdir(purefs::dir::os_factory.c_str(), "", true);
 
-    if (dirlist.size() < min_factory_dir_size) {
+    if (dirlist.size() >= min_factory_dir_size) {
+        LOG_INFO("FactoryReset: closing ServiceDB...");
+        std::string dbServiceName = service::name::db;
+        sys::SystemManager::DestroyService(dbServiceName, ownerService);
+
+        DeleteDirContent(purefs::dir::eMMC_disk);
+
+        CopyDirContent(purefs::dir::os_factory, purefs::dir::eMMC_disk);
+
+        LOG_INFO("DoFactoryReset: restoring factory state finished, rebooting...");
+        sys::SystemManager::Reboot(ownerService);
+    }
+    else {
         LOG_ERROR("FactoryReset: restoring factory state aborted");
         LOG_ERROR("FactoryReset: directory %s seems corrupted.", purefs::dir::os_factory.c_str());
-        return false;
     }
-
-    LOG_INFO("FactoryReset: closing ServiceDB...");
-    std::string dbServiceName = service::name::db;
-    (void)sys::SystemManager::DestroyService(dbServiceName, ownerService);
-
-    if (DeleteDirContent(purefs::dir::eMMC_disk) != true) {
-        LOG_ERROR("FactoryReset: restoring factory state aborted");
-        return false;
-    }
-
-    if (CopyDirContent(purefs::dir::os_factory, purefs::dir::eMMC_disk) != true) {
-        LOG_ERROR("FactoryReset: restoring factory state aborted");
-        return false;
-    }
-
-    LOG_INFO("DoFactoryReset: restoring factory state finished, rebooting...");
-    sys::SystemManager::Reboot(ownerService);
-    return true;
 }
 
-bool FactoryReset::DeleteDirContent(std::string dir)
+void FactoryReset::DeleteDirContent(std::string dir)
 {
     std::vector<vfs::DirectoryEntry> dirlist = vfs.listdir(dir.c_str(), "", true);
 
@@ -65,29 +55,21 @@ bool FactoryReset::DeleteDirContent(std::string dir)
 
             if (direntry.attributes == vfs::FileAttributes::Directory) {
                 if (direntry.fileName.compare(PATH_FACTORY) != 0) {
-                    LOG_INFO("FactoryReset: recursively deleting dir %s...", delpath.c_str());
-                    if (vfs.deltree(delpath.c_str()) != 0) {
-                        LOG_ERROR("FactoryReset: error deleting dir %s, aborting...", delpath.c_str());
-                        return false;
-                    }
+                    LOG_INFO("FactoryReset: recursively removing dir %s...", delpath.c_str());
+                    vfs.deltree(delpath.c_str());
                 }
             }
             else {
-                LOG_INFO("FactoryReset: deleting file %s...", delpath.c_str());
-                if (vfs.remove(delpath.c_str()) != 0) {
-                    LOG_ERROR("FactoryReset: error deleting file %s, aborting...", delpath.c_str());
-                    return false;
-                }
+                LOG_INFO("FactoryReset: removing file %s...", delpath.c_str());
+                vfs.remove(delpath.c_str());
             }
         }
     }
-
-    return true;
 }
 
-bool FactoryReset::CopyDirContent(std::string sourcedir, std::string targetdir)
+void FactoryReset::CopyDirContent(std::string sourcedir, std::string targetdir)
 {
-    bool ret = true;
+    static int recurseLevel = 0;
 
     recurseLevel++;
 
@@ -112,25 +94,15 @@ bool FactoryReset::CopyDirContent(std::string sourcedir, std::string targetdir)
                             LOG_INFO(
                                 "FactoryReset: restoring dir  %s into %s...", sourcepath.c_str(), targetpath.c_str());
 
-                            if (vfs.mkdir(targetpath.c_str()) != 0) {
-                                LOG_ERROR("FactoryReset: create dir %s failed", targetpath.c_str());
-                                ret = false;
-                                break;
-                            }
+                            vfs.mkdir(targetpath.c_str());
 
-                            if (CopyDirContent(sourcepath, targetpath) != true) {
-                                ret = false;
-                                break;
-                            }
+                            CopyDirContent(sourcepath, targetpath);
                         }
                     }
                     else {
                         LOG_INFO("FactoryReset: restoring file %s into %s...", sourcepath.c_str(), targetpath.c_str());
 
-                        if (CopyFile(sourcepath, targetpath) != true) {
-                            ret = false;
-                            break;
-                        }
+                        CopyFile(sourcepath, targetpath);
                     }
                 }
                 else {
@@ -138,8 +110,6 @@ bool FactoryReset::CopyDirContent(std::string sourcedir, std::string targetdir)
                               ffconfigMAX_FILENAME);
                     LOG_ERROR(
                         "FactoryReset: skipping restore of dir %s into %s", sourcepath.c_str(), targetpath.c_str());
-                    ret = false;
-                    break;
                 }
             }
         }
@@ -148,66 +118,31 @@ bool FactoryReset::CopyDirContent(std::string sourcedir, std::string targetdir)
         LOG_ERROR("FactoryReset: recurse level %d (too high), error assumed, skipping restore of dir %s",
                   recurseLevel,
                   sourcedir.c_str());
-        ret = false;
     }
 
     recurseLevel--;
-
-    return ret;
 }
 
-bool FactoryReset::CopyFile(std::string sourcefile, std::string targetfile)
+void FactoryReset::CopyFile(std::string sourcefile, std::string targetfile)
 {
-    bool ret = true;
-
     vfs::FILE *sf = vfs.fopen(sourcefile.c_str(), "r");
+    vfs::FILE *tf = vfs.fopen(targetfile.c_str(), "w");
 
-    if (sf != nullptr) {
-        vfs::FILE *tf = vfs.fopen(targetfile.c_str(), "w");
+    std::unique_ptr<unsigned char[]> buffer(new unsigned char[purefs::buffer::tar_buf]);
 
-        if (tf != nullptr) {
-            std::unique_ptr<unsigned char[]> buffer(new unsigned char[purefs::buffer::copy_buf]);
+    uint32_t loopcount = (vfs.filelength(sf) / purefs::buffer::tar_buf) + 1u;
+    uint32_t readsize  = purefs::buffer::tar_buf;
 
-            if (buffer.get() != nullptr) {
-                uint32_t loopcount = (vfs.filelength(sf) / purefs::buffer::copy_buf) + 1u;
-                uint32_t readsize  = purefs::buffer::copy_buf;
-
-                for (uint32_t i = 0u; i < loopcount; i++) {
-                    if (i + 1u == loopcount) {
-                        readsize = vfs.filelength(sf) % purefs::buffer::copy_buf;
-                    }
-
-                    if (vfs.fread(buffer.get(), 1, readsize, sf) != readsize) {
-                        LOG_ERROR("FactoryReset: read from sourcefile %s failed", sourcefile.c_str());
-                        ret = false;
-                        break;
-                    }
-
-                    if (vfs.fwrite(buffer.get(), 1, readsize, tf) != readsize) {
-                        LOG_ERROR("FactoryReset: write to targetfile %s failed", targetfile.c_str());
-                        ret = false;
-                        break;
-                    }
-                }
-            }
-            else {
-                LOG_ERROR("FactoryReset: unable to open copy buffer");
-                ret = false;
-            }
-
-            vfs.fclose(tf);
-        }
-        else {
-            LOG_ERROR("FactoryReset: unable to open target file %s", targetfile.c_str());
-            ret = false;
+    for (uint32_t i = 0u; i < loopcount; i++) {
+        if (i + 1u == loopcount) {
+            readsize = vfs.filelength(sf) % purefs::buffer::tar_buf;
         }
 
-        vfs.fclose(sf);
-    }
-    else {
-        LOG_ERROR("FactoryReset: unable to open source file %s", sourcefile.c_str());
-        ret = false;
+        vfs.fread(buffer.get(), 1, readsize, sf);
+
+        vfs.fwrite(buffer.get(), 1, readsize, tf);
     }
 
-    return ret;
+    vfs.fclose(sf);
+    vfs.fclose(tf);
 }
