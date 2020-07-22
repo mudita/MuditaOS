69bcfe5f7306bbe927a63993a7c63c1c835faae3
Przemyslaw Brudny <przemyslaw.brudny@mudita.com>
2020-07-22 18:35:16 +0200

    [EGD-3333] Reworked NumberWithIconsWidget.cpp widget. Added Image Box. Fixed BoxLayout Max resize.

--- module-apps/application-phonebook/widgets/InformationWidget.cpp
+++ module-apps/application-phonebook/widgets/InformationWidget.cpp
@@ -12,11 +12,18 @@ namespace gui
     {
         setMargins(gui::Margins(0, style::margins::very_big, 0, 0));
 
-        vBox = new VBox(this, 0, 0, 0, phonebookStyle::inputLineWithLabelItem::title_label_h);
-        vBox->setEdges(RectangleEdgeFlags::GUI_RECT_EDGE_NO_EDGES);
+        setMinimumSize(phonebookStyle::multiLineTextWithLabelItem::w, phonebookStyle::multiLineTextWithLabelItem::h);
 
-        titleLabel = new Label(vBox, 0, 0, 0, 0, utils::localize.get("app_phonebook_contact_information"));
-        titleLabel->setEdges(RectangleEdgeFlags::GUI_RECT_EDGE_NO_EDGES);
+        vBox = new VBox(this, 0, 0, 0, 0);
+        vBox->setEdges(RectangleEdgeFlags::GUI_RECT_ALL_EDGES);
+
+        titleLabel = new Label(vBox,
+                               0,
+                               0,
+                               phonebookStyle::multiLineTextWithLabelItem::w,
+                               40,
+                               utils::localize.get("app_phonebook_contact_information"));
+        titleLabel->setEdges(RectangleEdgeFlags::GUI_RECT_ALL_EDGES);
         titleLabel->setAlignment(Alignment(gui::Alignment::Horizontal::Left, gui::Alignment::Vertical::Top));
         titleLabel->setFont(style::window::font::small);
         titleLabel->setLineMode(true);
@@ -24,19 +31,36 @@ namespace gui
 
         onLoadCallback = [&](std::shared_ptr<ContactRecord> contact) {
             if (contact->numbers.size() > 0) {
-                primaryNumberHBox = new NumberWithIconsWidget(app, contact->numbers[0].number);
+
+                primaryNumberHBox = new NumberWithIconsWidget(app,
+                                                              contact->numbers[0].number,
+                                                              nullptr,
+                                                              0,
+                                                              0,
+                                                              widgetArea.w,
+                                                              phonebookStyle::multiLineTextWithLabelItem::h);
+                vBox->addWidget(primaryNumberHBox);
             }
             if (contact->numbers.size() > 1) {
-                alternativeNumberHBox = new NumberWithIconsWidget(app, contact->numbers[1].number);
+                alternativeNumberHBox = new NumberWithIconsWidget(app,
+                                                                  contact->numbers[1].number,
+                                                                  this,
+                                                                  0,
+                                                                  0,
+                                                                  phonebookStyle::multiLineTextWithLabelItem::w,
+                                                                  phonebookStyle::multiLineTextWithLabelItem::h);
             }
             if (contact->mail.length() > 0) {
-                emailText = new Text(vBox, 0, 0, 0, phonebookStyle::multiLineTextWithLabelItem::input_text_h);
-                emailText->setEdges(RectangleEdgeFlags::GUI_RECT_EDGE_NO_EDGES);
-                emailText->setAlignment(Alignment(gui::Alignment::Horizontal::Left, gui::Alignment::Vertical::Bottom));
+                emailText = new Text(nullptr, 0, 0, 0, 0);
+                emailText->setMaximumSize(widgetArea.w, phonebookStyle::multiLineTextWithLabelItem::h);
+                emailText->setEdges(RectangleEdgeFlags::GUI_RECT_ALL_EDGES);
+                emailText->setAlignment(Alignment(gui::Alignment::Horizontal::Left, gui::Alignment::Vertical::Center));
                 emailText->setFont(style::window::font::medium);
                 emailText->setPenFocusWidth(style::window::default_border_focus_w);
                 emailText->setPenWidth(style::window::default_border_no_focus_w);
                 emailText->setEditMode(EditMode::BROWSE);
+                vBox->addWidget(emailText);
+                emailText->setText(contact->mail);
             }
         };
 
@@ -45,7 +69,17 @@ namespace gui
             return true;
         };
 
-        setEdges(RectangleEdgeFlags::GUI_RECT_EDGE_NO_EDGES);
+        inputCallback = [&](gui::Item &item, const gui::InputEvent &event) {
+            if (event.state != gui::InputEvent::State::keyReleasedShort) {
+                return false;
+            }
+
+            LOG_INFO("jestem w parencie");
+
+            return vBox->onInput(event);
+        };
+
+        setEdges(RectangleEdgeFlags::GUI_RECT_ALL_EDGES);
     }
 
     auto InformationWidget::onDimensionChanged(const BoundingBox &oldDim, const BoundingBox &newDim) -> bool
