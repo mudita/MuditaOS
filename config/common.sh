0a6244769493b0403783ff998e3774c5edc63ffb
Roman Kubiak <roman.kubiak@mudita.com>
2020-07-24 13:00:32 +0200

    moved version info to common exit on failure on top

--- config/common.sh
+++ config/common.sh
@@ -4,6 +4,12 @@ PURE_PARTITION_RECOVERY="RECOVER"
 PURE_OS_CURRENT="current"
 PURE_OS_PREVIOUS="previous"
 PURE_OS_FACTORY="factory"
+PURE_OS_VERSION_MAJOR=""
+PURE_OS_VERSION_MINOR=""
+PURE_OS_VERSION_PATCH=""
+PURE_OS_GIT_TAG=""
+PURE_US_GIT_REV=""
+PURE_OS_GIT_BRANCH=""
 
 IMAGE_FILES="assets country-codes.db Luts.bin user .boot.ini .boot.ini.crc32"
 IMAGE_MOVE_TO_ROOT="user .boot.ini .boot.ini.crc32"
@@ -21,3 +27,12 @@ check_target_rt1051() {
 check_target_linux() {
     check_any_arch "${@}" '(GNU/Linux)'
 }
+
+PURE_OS_GIT_TAG=`git describe --tags | awk -F'-' '{print $2}'`
+PURE_OS_GIT_REV=`git log --pretty=format:'%h' -n 1`
+PURE_OS_GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+version=( ${PURE_OS_GIT_TAG//./ } )
+PURE_OS_VERSION_MAJOR=${version[0]}
+PURE_OS_VERSION_MINOR=${version[1]}
+PURE_OS_VERSION_PATCH=${version[2]}
