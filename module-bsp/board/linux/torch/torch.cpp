2d714a617a880c3fb4ccc2caf0698eaf6e7ad3d2
Mateusz Grzywacz <mateusz.grzywacz@mudita.com>
2020-07-28 01:58:06 +0200

    [EGD-3449] torch implementation

--- module-bsp/board/linux/torch/torch.cpp
+++ module-bsp/board/linux/torch/torch.cpp
@@ -12,17 +12,41 @@ namespace bsp
 
     namespace torch
     {
+        bool state_simulated = false;
 
         int32_t init(xQueueHandle qHandle)
         {
 
-            qHandleIrq = qHandle;
+            qHandleIrq      = qHandle;
+            state_simulated = false;
             return 1;
+        }
+
+        void deinit()
+        {
+            qHandleIrq = nullptr;
         }
 
         bool isPresent(void)
         {
-            return false;
+            return true;
+        }
+
+        bool turn(bool state)
+        {
+            state_simulated = state;
+            LOG_INFO("Torch is %s", (state == true) ? "ON \U0001f4a1 \U0001f526" : "OFF");
+            return true;
+        }
+
+        bool isOn()
+        {
+            return state_simulated;
+        }
+
+        bool setCurrent(const unsigned short mA)
+        {
+            return true;
         }
 
     } // namespace torch
