0addd20adeca8debdc5e9705ea63652887bb5290
Szymon Mroczek <szymon.mroczek@mudita.com>
2020-07-28 16:03:43 +0200

    [EGD-3099] Return types unified in linux audio modules

--- module-bsp/board/linux/audio/LinuxCellularAudio.cpp
+++ module-bsp/board/linux/audio/LinuxCellularAudio.cpp
@@ -49,7 +49,8 @@ namespace bsp
     {
 
         if (!TryOpenStream(format)) {
-            return paInternalError;
+            LOG_ERROR("PortAudio error: paInternalError");
+            return AudioDevice::RetCode::Failure;
         }
 
         auto err = Pa_StartStream(stream);
