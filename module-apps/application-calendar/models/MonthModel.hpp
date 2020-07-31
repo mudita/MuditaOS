b3d989a730284f461f12229bcc91d3b6f7d3fd29
Tomas Rogala <rogala.tomasz1@gmail.com>
2020-08-01 00:47:53 +0200

    [EGD-3470] rework month model class, add date.h lib, Develop calendar month view model

--- module-apps/application-calendar/models/MonthModel.hpp
+++ module-apps/application-calendar/models/MonthModel.hpp
@@ -6,19 +6,29 @@
 #include <gui/widgets/GridLayout.hpp>
 #include <gui/widgets/Item.hpp>
 #include <Text.hpp>
+#include <application-calendar/date.h>
 #include <map>
 #include <string>
 #include <vector>
 
+using namespace date;
+
 class MonthModel
 {
   public:
-    std::string name;
-    std::vector<DayModel> days;
-    uint32_t numberOfDays = 0;
+    month m;
+    day lastDay;
+    // first week offset
+    uint32_t offset;
+    year y;
 
   public:
-    MonthModel(std::string name, std::vector<DayModel> days);
+    MonthModel(year_month_day ymd);
     MonthModel()          = default;
     virtual ~MonthModel() = default;
+
+    year getYear();
+    month getMonth();
+    day getLastDay();
+    uint32_t getFirstWeekOffset();
 };
