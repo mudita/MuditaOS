a3c9bf353bb61df6824ab4a5e8d75d98371f23f5
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2020-08-05 10:19:54 +0200

    ccache fixup

--- config/CCacheConfig.cmake
+++ config/CCacheConfig.cmake
@@ -1,5 +1,11 @@
 find_program(CCACHE_FOUND ccache)
 if(CCACHE_FOUND)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+    message("ccache found ${CCACHE_FOUND}")
+    set(CMAKE_C_COMPILER_LAUNCHER   "${CCACHE_FOUND}")
+    set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_FOUND}")
 endif(CCACHE_FOUND)
+
+# To show how much time we spend compiling - with ccache it should take close less then 1s per file
+if(COMPILATION_TIMINGS)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CMAKE_COMMAND} -E time")
+endif(COMPILATION_TIMINGS)
