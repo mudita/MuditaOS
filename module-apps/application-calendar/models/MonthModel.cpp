44173fc34f4ed21a579ebafa197573450b0da73d
Tomas Rogala <rogala.tomasz1@gmail.com>
2020-08-05 23:04:11 +0200

    [EGD-3470] Date conversion to string and rework month view model

--- module-apps/application-calendar/models/MonthModel.cpp
+++ module-apps/application-calendar/models/MonthModel.cpp
@@ -3,10 +3,11 @@
 MonthModel::MonthModel(year_month_day ymd)
 {
     this->m                  = ymd.month();
-    this->offset             = weekday{ymd}.c_encoding();
     this->y                  = ymd.year();
     year_month_day_last ymdl = this->y / this->m / last;
-    this->lastDay            = ymdl.day();
+    this->lastDay            = unsigned{ymdl.day()};
+    year_month_day ymdf      = this->y / this->m / 1;
+    this->offset             = weekday{ymdf}.c_encoding();
 }
 
 month MonthModel::getMonth()
@@ -14,12 +15,68 @@ month MonthModel::getMonth()
     return this->m;
 }
 
-day MonthModel::getLastDay()
+uint32_t MonthModel::getLastDay()
 {
     return this->lastDay;
 }
 
 uint32_t MonthModel::getFirstWeekOffset()
 {
-    return this->offset;
+    if (this->offset == 0) {
+        return 7;
+    }
+    else {
+        return this->offset;
+    }
+}
+
+std::string MonthModel::getMonthYearText()
+{
+    int yearUInt        = static_cast<decltype(yearUInt)>(y);
+    std::string yearStr = std::to_string(yearUInt);
+    std::string monthStr;
+    unsigned int monthUInt = unsigned{m};
+
+    switch (monthUInt) {
+    case 1:
+        monthStr = "January";
+        break;
+    case 2:
+        monthStr = "February";
+        break;
+    case 3:
+        monthStr = "March";
+        break;
+    case 4:
+        monthStr = "April";
+        break;
+    case 5:
+        monthStr = "May";
+        break;
+    case 6:
+        monthStr = "June";
+        break;
+    case 7:
+        monthStr = "July";
+        break;
+    case 8:
+        monthStr = "August";
+        break;
+    case 9:
+        monthStr = "September";
+        break;
+    case 10:
+        monthStr = "October";
+        break;
+    case 11:
+        monthStr = "November";
+        break;
+    case 12:
+        monthStr = "December";
+        break;
+    default:
+        monthStr = "";
+    }
+
+    return monthStr + " " + yearStr;
 }
