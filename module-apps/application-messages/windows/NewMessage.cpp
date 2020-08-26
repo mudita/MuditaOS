3581bdfe55c45ef6cbf97e44f6efa963fbdfa8ba
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2020-08-26 21:16:02 +0200

    [EGD-3530] Add forwarding vcard via SMS
    [EGD-3530] Cleanup and minor fixes
    
    [EGD-3530] Fix switching back to previous window and update changelog

--- module-apps/application-messages/windows/NewMessage.cpp
+++ module-apps/application-messages/windows/NewMessage.cpp
@@ -29,6 +29,7 @@ namespace gui
 
     void NewSMS_Window::onBeforeShow(ShowMode mode, SwitchData *data)
     {
+        data->ignoreCurrentWindowOnStack = true;
         if (auto pdata = dynamic_cast<PhonebookSearchReuqest *>(data); pdata != nullptr) {
             LOG_INFO("received search results");
             recipient->setText(pdata->result->getFormattedName());
@@ -47,10 +48,12 @@ namespace gui
             if (!retContact) {
                 LOG_WARN("not valid contact for number %s", number.getEntered().c_str());
                 recipient->setText(number.getFormatted());
+                message->setText(pdata->textData);
                 return;
             }
             contact = std::move(retContact);
             recipient->setText(contact->getFormattedName());
+            message->setText(pdata->textData);
         }
         updateBottomBar();
     }
