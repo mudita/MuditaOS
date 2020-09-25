3e2eab0070eb6266c8c930b552f67365c6fb692b
RobertPiet <robert.pietraszewski@mudita.com>
2020-09-25 08:24:32 +0200

    [EGD-3789] main network window [EGD-3789] OptionSetting intoduced as a base to handle settings windows [EGD-3789] added icons on/off/bt/right arrow [EGD-3789] btn_ok icon used

--- module-apps/windows/OptionWindow.cpp
+++ module-apps/windows/OptionWindow.cpp
@@ -7,12 +7,7 @@
 #include <cassert>
 #include <functional>
 #include <memory>
-
-namespace style::option
-{
-    const gui::Position arrow_position_x = 408;
-    const gui::Position arrow_positon_y  = 24;
-} // namespace style::option
+#include "OptionsStyle.hpp"
 
 namespace gui
 {
