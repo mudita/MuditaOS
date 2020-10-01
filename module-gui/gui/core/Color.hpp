e4b04a9c4a5625895438a3a1d5c108bbea801a5c
Piotr Tanski <piotr.tanski@mudita.com>
2020-10-01 09:08:14 +0200

    [EGD-3661] Drawing circles and arcs functionality added into the Renderer. (#755)
    - Major code cleanup in lines drawing functionality.

--- module-gui/gui/core/Color.hpp
+++ module-gui/gui/core/Color.hpp
@@ -6,25 +6,33 @@ namespace gui
 {
     struct Color
     {
-        constexpr Color(uint8_t intensity = 0, uint8_t alpha = 0) : intensity{intensity}, alpha{alpha} {};
+        constexpr Color() noexcept = default;
+        constexpr Color(std::uint8_t intensity, std::uint8_t alpha) noexcept : intensity{intensity}, alpha{alpha}
+        {}
+
         // this defines color of the pixel.
-        uint8_t intensity = 0;
+        std::uint8_t intensity{0U};
         // defines how transparent is pixel. 0 means solid color and GUI_COLORS_COUNT-1 fully transparent
-        uint8_t alpha = 0;
+        std::uint8_t alpha{0U};
+
+        static constexpr inline std::uint8_t White           = 0x0FU;
+        static constexpr inline std::uint8_t Black           = 0x0U;
+        static constexpr inline std::uint8_t FullTransparent = 0x0FU;
+        static constexpr inline std::uint8_t Opaque          = 0x0U;
     };
 
-    extern Color ColorFullBlack;
-    extern Color ColorFullWhite;
-    extern Color ColorNoColor;
-    extern Color ColorTray;
-
-    inline bool operator==(const Color &r, const Color &l)
+    inline bool operator==(const Color &r, const Color &l) noexcept
     {
         return r.intensity == l.intensity && r.alpha == l.alpha;
     }
 
-    inline bool operator!=(const Color &r, const Color &l)
+    inline bool operator!=(const Color &r, const Color &l) noexcept
     {
-        return !(r == l);
+        return !operator==(r, l);
     }
+
+    static inline constexpr Color ColorFullBlack{Color::Black, Color::Opaque};
+    static inline constexpr Color ColorFullWhite{Color::White, Color::Opaque};
+    static inline constexpr Color ColorNoColor{Color::White, Color::FullTransparent};
+    static inline constexpr Color ColorTray{13, Color::Opaque};
 } // namespace gui
