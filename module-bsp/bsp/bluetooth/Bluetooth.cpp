6e33b01fb20ded008ec323c9b4856721f7a623b0
Bartosz Cichocki <sp2fet@gmail.com>
2020-10-05 12:29:42 +0200

    Revert "[EGD-3641] working key storage in json file"
    This reverts commit 52790f0a5cb33f6c3525066b0253c954c6c86c08.

--- module-bsp/bsp/bluetooth/Bluetooth.cpp
+++ module-bsp/bsp/bluetooth/Bluetooth.cpp
@@ -21,4 +21,17 @@ namespace bsp {
             va_end(args);
         }
     }
+
+    Bluetopia::Bluetopia(unsigned int in_size, unsigned int out_size, int threshold) : BluetoothCommon(in_size,out_size, threshold),rx_thread(0), thandle(NULL)
+    {
+    }
+
+    Bluetopia::~Bluetopia()
+    {
+    }
+
+    Bluetopia *Bluetopia::getInstance()
+    {
+        return Bluetopia::getInstance();
+    }
 };
