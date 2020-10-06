4436e94f876d5335b2fc9274f98c28336e5ae250
Roman Kubiak <roman.kubiak@mudita.com>
2020-10-06 11:49:39 +0200

    style changes

--- module-apps/application-desktop/data/LockPhoneData.hpp
+++ module-apps/application-desktop/data/LockPhoneData.hpp
@@ -51,7 +51,8 @@ namespace gui
     class UpdateSwitchData : public gui::SwitchData
     {
       public:
-        UpdateSwitchData(sdesktop::UpdateOsMessage *messageToCopyFrom) : updateOsMessage(*messageToCopyFrom){}
+        UpdateSwitchData(sdesktop::UpdateOsMessage *messageToCopyFrom) : updateOsMessage(*messageToCopyFrom)
+        {}
         const sdesktop::UpdateOsMessage &getUpdateOsMessage()
         {
             return updateOsMessage;
