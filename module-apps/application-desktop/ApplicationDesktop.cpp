cbf249ef39333dd8313806b2f7362b68c32385ef
Michał Kamoń <70628259+mkamonMdt@users.noreply.github.com>
2020-10-07 14:15:56 +0200

    [EGD-3865] window locked screen (#784)
    * LockedInfoWindow added
    * RichTextParser enabled shortened text formating

--- module-apps/application-desktop/ApplicationDesktop.cpp
+++ module-apps/application-desktop/ApplicationDesktop.cpp
@@ -5,6 +5,7 @@
 #include "windows/MenuWindow.hpp"
 #include "windows/PinLockWindow.hpp"
 #include "windows/PowerOffWindow.hpp"
+#include "windows/LockedInfoWindow.hpp"
 #include "windows/Reboot.hpp"
 
 #include <service-db/api/DBServiceAPI.hpp>
@@ -213,6 +214,7 @@ namespace app
             desktop_pin_lock, new gui::PinLockWindow(this, desktop_pin_lock, lockHandler.lock)));
         windows.insert(std::pair<std::string, gui::AppWindow *>(desktop_menu, new gui::MenuWindow(this)));
         windows.insert(std::pair<std::string, gui::AppWindow *>(desktop_poweroff, new gui::PowerOffWindow(this)));
+        windows.insert(std::pair<std::string, gui::AppWindow *>(desktop_locked, new gui::LockedInfoWindow(this)));
         windows.insert(std::pair<std::string, gui::AppWindow *>(desktop_reboot, new gui::RebootWindow(this)));
     }
 
