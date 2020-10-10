27b418b8aaf48c443681ee35028b224a797c2275
Tomek Sobkowiak <soop3k@gmail.com>
2020-10-10 20:14:54 +0200

    [EDG-4041] Add basic keypad light window

--- module-apps/application-settings-new/windows/KeypadLightWindow.cpp
+++ module-apps/application-settings-new/windows/KeypadLightWindow.cpp
@@ -1,6 +1,7 @@
 #include "KeypadLightWindow.hpp"
 
 #include <application-settings-new/ApplicationSettings.hpp>
+#include <gui/widgets/CheckBoxWithLabel.hpp>
 #include <i18/i18.hpp>
 
 namespace gui
@@ -15,6 +16,33 @@ namespace gui
     {
         BaseSettingsWindow::buildInterface();
         setTitle(utils::localize.get("app_settings_display_keypad_light"));
+
+        auto body =
+            new VBox(nullptr,
+                     0,
+                     title->offset_h() + style::margins::big,
+                     this->getWidth(),
+                     this->getHeight() - style::margins::big - this->title->offset_h() - bottomBar->getHeight());
+        body->setEdges(gui::RectangleEdgeFlags::GUI_RECT_EDGE_NO_EDGES);
+
+        auto toggleBoxes = [this](CheckBoxWithLabel &active) {
+            for (CheckBoxWithLabel *box : boxes) {
+                box->setChecked(false);
+                if (&active == box) {
+                    box->setChecked(!box->isChecked());
+                }
+            }
+        };
+
+        boxes.push_back(new CheckBoxWithLabel(
+            body, 0, 0, utils::localize.get("app_settings_display_keypad_light_on"), toggleBoxes));
+        boxes.push_back(new CheckBoxWithLabel(
+            body, 0, 0, utils::localize.get("app_settings_display_keypad_light_active"), toggleBoxes));
+        boxes.push_back(new CheckBoxWithLabel(
+            body, 0, 0, utils::localize.get("app_settings_display_keypad_light_off"), toggleBoxes));
+
+        addWidget(body);
+        setFocusItem(body);
     }
 
 } // namespace gui
---
+++ module-gui/gui/widgets/CheckBoxWithLabel.cpp
@@ -0,0 +1,47 @@
+#include "CheckBoxWithLabel.hpp"
+
+namespace gui
+{
+    CheckBoxWithLabel::CheckBoxWithLabel(
+        Item *parent, int x, int y, UTF8 description, std::function<void(CheckBoxWithLabel &)> clickCallback)
+    {
+        auto body = new gui::HBox(nullptr, x, y, style::window::default_body_width, style::window::label::big_h);
+        body->setEdges(gui::RectangleEdgeFlags::GUI_RECT_EDGE_NO_EDGES);
+
+        label =
+            new gui::Label(nullptr, 0, 0, style::checkbox::description_width, style::window::label::big_h, description);
+        check = new gui::CheckBox(nullptr,
+                                  style::checkbox::check_x,
+                                  style::checkbox::check_y,
+                                  style::checkbox::check_width,
+                                  style::window::label::big_h);
+
+        label->activatedCallback = [=](Item &item) {
+            setChecked(!check->isChecked());
+            if (clickCallback) {
+                clickCallback(*this);
+            }
+            return true;
+        };
+
+        style::window::decorateOption(label);
+        style::window::decorate(check);
+
+        /*
+         * We need to add widgets after decorate to correctly redraw them
+         */
+        parent->addWidget(body);
+        body->addWidget(label);
+        label->addWidget(check);
+    }
+
+    bool CheckBoxWithLabel::isChecked() const
+    {
+        return check->isChecked();
+    }
+
+    void CheckBoxWithLabel::setChecked(bool state)
+    {
+        check->setImageVisible(state);
+    }
+} // namespace gui
