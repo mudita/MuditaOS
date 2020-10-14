27418a62d46937004734168570f2a84d00681c37
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-10-14 12:55:27 +0200

    [EGD-4075] Use FAT fs in the emulator
    Add script for genrate base phone image

---
+++ generate_fatfs_image.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -xe
+usage() {
+cat << ==usage
+Usage: $(basename $0) [image_dir] [assets_root_dir]
+	image_dir Target disk image
+	asset_root_dir Asset root directory
+==usage
+}
+
+ASSETS_DIR="assets country-codes.db Luts.bin"
+
+if [ $# -ne 2 ]; then
+	echo "Error! Invalid argument count"
+	usage
+	exit -1
+fi
+IMAGE_NAME=$(realpath $1)
+SRC_DATA=$(realpath $2)
+
+if [ ! -d "$SRC_DATA" ]; then
+	echo "Error! asset_root_dir is not a directory"
+	usage
+	exit -1
+fi
+
+truncate -s 16G $IMAGE_NAME
+sfdisk $IMAGE_NAME << ==sfdisk
+label: dos
+label-id: 0x09650eb4
+device: /dev/sda
+unit: sectors
+
+/dev/sda1 : start=        2048, size=    28522496, type=b, bootable
+/dev/sda2 : start=    28524544, size=     2097152, type=b
+==sfdisk
+
+PART1="$IMAGE_NAME@@1048576"
+PART2="$IMAGE_NAME@@14604566528"
+mformat -i "$PART1" -F -T 28522496 -v PUREOS
+mformat -i "$PART2" -F -T 2097152 -v RECOVER
+mmd -i "$PART1" ::/current
+cd "$SRC_DATA"
+for i in $ASSETS_DIR; do
+	mcopy -s -i "$PART1" $i ::/current/
+done
+cd -
+cd $SRC_DATA
+mcopy -s -i "$PART1" user ::
+cd -
