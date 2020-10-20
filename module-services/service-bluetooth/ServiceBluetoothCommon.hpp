d73337494c80a8cc1a46d577494f88b262ba8f07
Hubert Chrzaniuk <hubert.chrzaniuk@mudita.com>
2020-10-20 13:24:51 +0200

    (to be squashed) [EGD-4099] BT set stream

--- module-services/service-bluetooth/ServiceBluetoothCommon.hpp
+++ module-services/service-bluetooth/ServiceBluetoothCommon.hpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
 #pragma once
 
 #include <FreeRTOS.h>
@@ -6,6 +9,6 @@
 class BluetoothStreamData
 {
   public:
-    QueueHandle_t in  = nullptr;
-    QueueHandle_t out = nullptr;
+    QueueHandle_t in    = nullptr;
+    1 QueueHandle_t out = nullptr;
 };
