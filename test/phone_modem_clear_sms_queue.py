a36c26407d9ae9f96946962c95437a83075dda90
Hubert Chrzaniuk <hubert.chrzaniuk@mudita.com>
2020-10-22 08:55:57 +0200

    [EGD-4120] Cellular - new line at the end of the command (#877)

--- test/phone_modem_clear_sms_queue.py
+++ test/phone_modem_clear_sms_queue.py
@@ -4,10 +4,10 @@
 
 from harness.uart import conn, log
 
-conn.gsm("at+cmgl=\"all\"\r")
+conn.gsm("at+cmgl=\"all\"")
 print(conn.read(10))
-conn.gsm("at+cmgd=1,4\r")
-conn.gsm("at+cmgd=0,4\r")
-conn.gsm("at+cmgd=2,4\r")
-conn.gsm("at+cmgl=\"all\"\r")
+conn.gsm("at+cmgd=1,4")
+conn.gsm("at+cmgd=0,4")
+conn.gsm("at+cmgd=2,4")
+conn.gsm("at+cmgl=\"all\"")
 print(conn.read(10))
