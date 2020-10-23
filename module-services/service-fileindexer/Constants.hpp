b0b4694ec9f8718c4e621d62d959731a15f8fa1a
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-10-23 16:13:05 +0200

    [EGD-4030] Initial version of the indexer service

--- module-services/service-fileindexer/Constants.hpp
+++ module-services/service-fileindexer/Constants.hpp
@@ -1,2 +1,10 @@
 // Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+#include <string>
+
+namespace service::name
+{
+    inline constexpr std::string_view file_indexer = "FileIndexer";
+}
