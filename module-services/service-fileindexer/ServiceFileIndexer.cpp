7fda09471ab438aeada3e1e7e7e8ce8238fb53c4
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-10-26 11:59:39 +0100

    [EGD-4181] Pass message from vfs to indexer service

--- module-services/service-fileindexer/ServiceFileIndexer.cpp
+++ module-services/service-fileindexer/ServiceFileIndexer.cpp
@@ -3,37 +3,61 @@
 
 #include <log/log.hpp>
 #include <vfs.hpp>
+#include <filesystem>
 #include "ServiceFileIndexer.hpp"
+#include "FileChangeMessage.hpp"
+#include "Constants.hpp"
+#include <Service/Bus.hpp>
 
 namespace service
 {
 
+    // File indexer service constructor
     ServiceFileIndexer::ServiceFileIndexer(const std::string_view name) : sys::Service(std::string(name))
     {
         LOG_DEBUG("[%s] Initializing", std::string(name).c_str());
     }
-
+    // File indexer service destructor
     ServiceFileIndexer::~ServiceFileIndexer()
     {}
 
+    // When receive notification handler
     sys::Message_t ServiceFileIndexer::DataReceivedHandler(sys::DataMessage *msg, sys::ResponseMessage *resp)
     {
-        LOG_DEBUG("Receive handler data %p resp %p", msg, resp);
-        return nullptr;
+        auto fcm = dynamic_cast<msg::FileChangeMessage *>(msg);
+        if (fcm) {
+            LOG_DEBUG("File notification handler received %s", fcm->newPath().c_str());
+            return std::make_shared<sys::ResponseMessage>();
+        }
+        return std::make_shared<sys::ResponseMessage>(sys::ReturnCodes::Unresolved);
     }
 
+    // Initialize data notification handler
     sys::ReturnCodes ServiceFileIndexer::InitHandler()
     {
-        vfs.registerNotificationHandler([](std::string_view new_path, vfs::FsEvent event, std::string_view old_path) {
-            LOG_DEBUG(
-                "Filesystem change %s %i %s", std::string(new_path).c_str(), int(event), std::string(old_path).c_str());
-        });
+        vfs.registerNotificationHandler(
+            [_this = shared_from_this()](std::string_view new_path, vfs::FsEvent event, std::string_view old_path) {
+                namespace fs       = std::filesystem;
+                const auto new_ext = fs::path(new_path).extension().string();
+                if (new_ext != ".db" && new_ext != ".db-journal" && new_ext != ".wall") {
+                    auto msg = std::make_shared<msg::FileChangeMessage>(new_path, event, old_path);
+                    sys::Bus::SendUnicast(msg, std::string(service::name::file_indexer), _this.get());
+                }
+                else {
+                    LOG_DEBUG("Ignore db file %s", std::string(new_path).c_str());
+                }
+            });
         return sys::ReturnCodes::Success;
     }
+
+    // Deinitialization handler
     sys::ReturnCodes ServiceFileIndexer::DeinitHandler()
     {
+        vfs.registerNotificationHandler(nullptr);
         return sys::ReturnCodes::Success;
     }
+
+    // Power mode switch handler
     sys::ReturnCodes ServiceFileIndexer::SwitchPowerModeHandler(const sys::ServicePowerMode mode)
     {
         LOG_DEBUG("Switch to power Mode %s", c_str(mode));
