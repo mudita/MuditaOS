4d2b393a51294a4866f06edbb4941e27cef11675
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-10-27 11:53:26 +0100

    [EGD-4030] File indexer service list files

--- module-services/service-fileindexer/startupIndexer.cpp
+++ module-services/service-fileindexer/startupIndexer.cpp
@@ -2,28 +2,37 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "startupIndexer.hpp"
+#include "messages/FileChangeMessage.hpp"
 #include <filesystem>
-#include <vfs>
+#include <ff_stdio_listdir_recursive.h>
+#include <vfs.hpp>
+#include <Service/Timer.hpp>
+#include <Service/Bus.hpp>
+#include "Constants.hpp"
 
 namespace service::detail
 {
 
+    namespace fs = std::filesystem;
     namespace
     {
-        const std::vector<std::string_view> allowed_idxs{
-            ".png",
-            ".jpg",
-            ".jpeg",
-            ".bmp",
+        // File extensions indexing allow list
+        const std::vector<std::string_view> allowed_exts{
+            ".txt",
             ".wav",
             ".mp3",
             ".flac",
         };
     }
+
+    startupIndexer::~startupIndexer()
+    {}
+    startupIndexer::startupIndexer()
+    {}
+
     auto startupIndexer::fileShouldBeIndexed(std::string_view path) -> bool
     {
-        namespace fs = std::filesystem;
-        for (const auto &ext : allowed_idxs) {
+        for (const auto &ext : allowed_exts) {
             if (fs::path(path).extension() == ext) {
                 return true;
             }
@@ -33,8 +42,40 @@ namespace service::detail
 
     // Collect startup files when service starts
     auto startupIndexer::collectStartupFiles() -> void
-    {}
+    {
+        using namespace std::string_literals;
+        auto searcher_cb = [](void *ctx, const char *path, bool isDir) {
+            auto _this = reinterpret_cast<startupIndexer *>(ctx);
+            if (!isDir) {
+                for (const auto &ext : allowed_exts) {
+                    if (fs::path(path).extension() == ext) {
+                        _this->mMsgs.emplace_back(std::make_shared<msg::FileChangeMessage>(
+                            path, msg::FileChangeMessage::evt_t::modified, ""s));
+                        LOG_DEBUG("Initial indexing file added %s", path);
+                    }
+                }
+            }
+        };
+
+        ff_stdio_listdir_recursive(purefs::dir::user_disk.c_str(), searcher_cb, this);
+        ff_stdio_listdir_recursive(purefs::dir::os_current.c_str(), searcher_cb, this);
+    }
     // Setup timers for notification
     auto startupIndexer::setupTimers(std::shared_ptr<sys::Service> svc, std::string_view svc_name) -> void
-    {}
+    {
+        if (!mIdxTimer) {
+            mIdxIterator = mMsgs.begin();
+            mIdxTimer    = std::make_unique<sys::Timer>("file_indexing", svc.get(), timer_indexing_time);
+            mIdxTimer->connect([this, svc](sys::Timer &) {
+                if (mIdxIterator != mMsgs.end()) {
+                    sys::Bus::SendUnicast(*mIdxIterator, std::string(service::name::file_indexer), svc.get());
+                    mIdxIterator++;
+                }
+                else {
+                    mIdxTimer->stop();
+                }
+            });
+            mIdxTimer->start();
+        }
+    }
 } // namespace service::detail
