eec4618a54508d6b0addf0f181b38a17b39ef37d
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-10-28 14:33:18 +0100

    [EGD-4030] Text files indexer

---
+++ module-services/service-fileindexer/notesIndexer.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include <log/log.hpp>
+#include "notesIndexer.hpp"
+#include <vfs.hpp>
+#include <cwctype>
+
+namespace service::detail
+{
+
+    notesIndexer::notesIndexer(std::string_view path)
+    {
+        auto file = vfs.fopen(std::string(path).c_str(), "r");
+        if (!file) {
+            LOG_INFO("Unable to open file %s. Ignore...", std::string().c_str());
+            return;
+        }
+        if (!vfs.eof(file)) {
+            auto line = vfs.getline(file, 4096);
+            mLinesCount++;
+            updateLineStats(line);
+        }
+        vfs.fclose(file);
+    }
+    auto notesIndexer::updateLineStats(std::string_view _line) noexcept -> void
+    {
+        UTF8 line(std::string(_line).c_str());
+        bool last_space{};
+        for (std::size_t idx = 0; idx < line.length(); ++idx) {
+            const auto wchar = line[idx];
+            const auto space = std::iswspace(wchar);
+            if (space && !last_space)
+                mWordCount++;
+            if (std::iswprint(wchar) && !space)
+                mCharCount++;
+            last_space = space;
+        }
+    }
+} // namespace service::detail
