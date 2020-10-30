1a6ea1f4b3eb85b3c3018ecf51016cb35d5172cb
Alek Rudnik <54846206+alekrudnik@users.noreply.github.com>
2020-10-30 10:01:09 +0100

    [EGD-4205] mudita 1st audio assets (#940)

--- module-services/service-cellular/ServiceCellular.cpp
+++ module-services/service-cellular/ServiceCellular.cpp
@@ -1304,7 +1304,7 @@ bool ServiceCellular::receiveSMS(std::string messageNumber)
                             this,
                             db::Interface::Name::Notifications,
                             std::make_unique<db::query::notifications::Increment>(NotificationsRecord::Key::Sms));
-                        const std::string ringtone_path = "assets/audio/sms_transformer.wav";
+                        const std::string ringtone_path = "assets/audio/SMS-drum2.mp3";
                         AudioServiceAPI::PlaybackStart(this, audio::PlaybackType::TextMessageRingtone, ringtone_path);
                     }
                     else {
