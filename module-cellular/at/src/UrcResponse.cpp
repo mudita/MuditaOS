58be09d81beec31e560b9bacb3bac0bc89296d83
Hubert Chrzaniuk <hubert.chrzaniuk@mudita.com>
2020-11-04 14:04:14 +0100

    (to be squashed) [EGD-2962] Refactor URC

--- module-cellular/at/src/UrcResponse.cpp
+++ module-cellular/at/src/UrcResponse.cpp
@@ -6,19 +6,6 @@
 
 using namespace at::urc;
 
-/*std::unique_ptr<UrcResponse> UrcResponse::Create(const std::string body)
-{
-    std::string trimmeBody = utils::trim(body);
-    int idx                = 0;
-    for (auto& respBody : responseBodies) {
-        if (respBody.second == trimmeBody) {
-            return std::make_unique<UrcResponse>(respBody.first);
-        }
-        idx++;
-    }
-    return nullptr;
-}*/
-
 UrcResponse::URCResponseType UrcResponse::getURCResponseType() const noexcept
 {
     return type;
