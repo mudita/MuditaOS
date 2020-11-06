b4059c6aa17abd093c35810917add8414105dbdb
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-11-06 17:26:19 +0100

    [EGD-4330] file streams ld preload library
    It is concept library LD_PRELOAD for handle syscalls wrapper to the
    image fat file.

--- Target_Linux.cmake
+++ Target_Linux.cmake
@@ -8,21 +8,25 @@ set(TARGET_COMPILE_FEATURES CACHE INTERNAL "")
 
 set(TARGET_COMPILE_DEFINITIONS CACHE INTERNAL "")
 
+
 add_compile_options(-funsigned-char)
 
 set(TARGET_LIBRARIES
+    iosyscalls
     rt
     pthread
     portaudio
     CACHE INTERNAL "" )
 
+
 option (LINUX_ENABLE_SANITIZER "Enable address sanitizer for Linux" ON)
 if (LINUX_ENABLE_SANITIZER)
-    add_compile_options(-fsanitize=address)
-    add_link_options(-fsanitize=address)
+    add_compile_options(-fsanitize=address -static-libasan)
+    add_link_options(-fsanitize=address -static-libasan)
 endif (LINUX_ENABLE_SANITIZER)
 
 set(CMAKE_STRIP strip CACHE INTERNAL "")
 set(CMAKE_OBJCOPY objcopy CACHE INTERNAL "")
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/sys)
 file(CREATE_LINK ${CMAKE_BINARY_DIR}/user ${CMAKE_BINARY_DIR}/sys/user SYMBOLIC)
+
