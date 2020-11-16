e305b7dc3c59425bbf943501499b6df725155728
Pawel Paprocki <69140352+pawpMudita@users.noreply.github.com>
2020-11-16 16:30:31 +0100

    [EGD-4314] Indexer DB – support for ListView (#996)

--- module-services/service-db/agents/file_indexer/file_indexer.sql
+++ module-services/service-db/agents/file_indexer/file_indexer.sql
@@ -34,24 +34,34 @@ CREATE TABLE IF NOT EXISTS notifications_tab (
     path TEXT NOT NULL,
     service TEXT,
     CONSTRAINT notification_unique
-        UNIQUE(path, service)
+         UNIQUE(path, service)
 );
 
 -- ----------- insert default values ----------------------
 
 INSERT OR REPLACE INTO file_tab (file_id, path, size, mime_type, mtime, directory, file_type) VALUES
     (1, 'mp3/track1.mp3', 456666, 1, 1603929600, 'mp3',12297),
-    (2, 'mp3/track2.mp3', 345354 ,1, 1603929604, 'mp3',12297);
+    (2, 'mp3/track2.mp3', 345354 ,1, 1603929604, 'mp3',12297),
+    (3, 'mp3/track3.mp3', 34534 ,1, 1603929604, 'mp3',12297),
+    (4, 'mp3/track4.mp3', 345354 ,1, 1603929604, 'mp3',12297);
 
 
 INSERT OR REPLACE INTO metadata_tab (file_id, property, value) VALUES
     (1,'artist','Sting'),
     (1,'genre','Rock'),
     (1,'album','Album1'),
-    (2,'artist','Nick Levis'),
-    (2,'genre','Soul'),
-    (2,'album','Album2');
+    (2,'artist','Madonna'),
+    (2,'genre','Rock'),
+    (2,'album','Album2'),
+    (3,'artist','Lady Gaga'),
+    (3,'genre','Rock'),
+    (3,'album','Album3'),
+    (4,'artist','Nick Levis'),
+    (4,'genre','Soul'),
+    (4,'album','Album4');
 
+INSERT OR REPLACE INTO notifications_tab (id, path, service) VALUES
+    (1,'mp3','ServiceDB');
 
 COMMIT;
 
