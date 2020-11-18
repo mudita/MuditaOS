b30927710def15e70857df173e4d3b25560b2707
Radosław Wicik <60609724+rwicik@users.noreply.github.com>
2020-11-18 14:04:00 +0100

    [EGD-3220] Convert service-fota to library (#1038)

--- module-services/service-fota/service-fota/FotaServiceAPI.hpp
+++ module-services/service-fota/service-fota/FotaServiceAPI.hpp
@@ -1,13 +1,13 @@
-// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+﻿// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
 
-#include <string> // for string
 #include <sstream>
-#include <unordered_map> // for unordered_map
+#include <string>
+#include <unordered_map>
+#include <utility>
 #include <vector>
-#include <utility> // for pair
 
 namespace sys
 {
