52f1cc710fab8f41861678dcc31f463196d7ef06
SP2FET <bartosz.cichocki@mudita.com>
2020-11-30 17:06:19 +0100

    [EGD-4578] bluetooth: added interface messages Add Bluetooth interface messages
    [EGD-4578] updated class diagram

---
+++ module-services/service-bluetooth/service-bluetooth/messages/ResponseVisibleDevices.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include <Service/Message.hpp>
+#include "service-bluetooth/BluetoothMessage.hpp"
+#include <string>
+
+namespace message::bluetooth
+{
+    class ResponseVisibleDevices : public BluetoothMessage
+    {
+      public:
+        ResponseVisibleDevices(std::vector<Devicei> devices) : devices(std::move(devices))
+        {}
+        [[nodiscard]] auto getDevices() const -> std::vector<Devicei>
+        {
+            return devices;
+        }
+
+      private:
+        std::vector<Devicei> devices;
+    };
+} // namespace message::bluetooth
