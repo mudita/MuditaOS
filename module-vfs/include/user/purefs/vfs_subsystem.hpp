dc2bc309c2be01b2d42b02fa95605b1aa3f3411e
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-12-01 15:58:17 +0100

    [EGD-4499] Global module VFS

---
+++ module-vfs/include/user/purefs/vfs_subsystem.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+#include <memory>
+#include <purefs/fs/filesystem.hpp>
+#include <purefs/blkdev/disk_manager.hpp>
+
+namespace purefs::subsystem
+{
+    auto initialize() -> std::tuple<std::shared_ptr<blkdev::disk_manager>, std::shared_ptr<fs::filesystem>>;
+    auto disk_mgr() -> std::shared_ptr<blkdev::disk_manager>;
+    auto vfs_core() -> std::shared_ptr<fs::filesystem>;
+} // namespace purefs::subsystem
