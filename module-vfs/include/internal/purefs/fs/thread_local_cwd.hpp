8be15b96e4113903c3501a6dee3fa6108665ff29
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-12-02 09:47:17 +0100

    [EGD-4498] Virtual Filesystem Core Framework (#1074)
    * [EGD-4498] Registering and unregistering VFS
    
    * [EGD-4498] Mount structures added
    
    * [EGD-4498] Mount filesystem completed
    
    * [EGD-4498] Mount / unmount / and find mount impl
    
    * [EGD-4498] Per thread directory TLS
    
    * [EGD-4489] Absolute paths implementation
    
    * [EGD-4498] Git ignore vim
    
    * [EGD-4498] VFS core functions without cwd
    
    * [EGD-4498] Other syscalls
    
    * [EGD-4498] Code review fixes
    
    * [EGD-4498] Code review #2
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>
    
    * [EGD-4498] Code review small fixes vol #3
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>

---
+++ module-vfs/include/internal/purefs/fs/thread_local_cwd.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+#include <string>
+
+namespace purefs::fs::internal
+{
+    auto get_thread_local_cwd_path() noexcept -> std::string_view;
+    auto set_thread_cwd_path(std::string_view path) noexcept -> int;
+    auto cleanup_thread_local_cwd_mem() -> void;
+} // namespace purefs::fs::internal
---
+++ module-vfs/src/purefs/fs/filesystem_syscalls.cpp
@@ -0,0 +1,139 @@
+// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+#include <purefs/fs/filesystem.hpp>
+#include <errno.h>
+#include <log/log.hpp>
+#include <purefs/fs/mount_point.hpp>
+#include <purefs/fs/filesystem_operations.hpp>
+#include <purefs/fs/file_handle.hpp>
+#include <purefs/fs/thread_local_cwd.hpp>
+
+namespace purefs::fs
+{
+    auto filesystem::stat_vfs(std::string_view path, statvfs &stat) const noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::stat_vfs, path, stat);
+    }
+
+    auto filesystem::stat(std::string_view file, struct stat &st) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::stat, file, st);
+    }
+
+    auto filesystem::unlink(std::string_view name) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::unlink, name);
+    }
+
+    auto filesystem::mkdir(std::string_view path, int mode) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::mkdir, path, mode);
+    }
+
+    auto filesystem::ioctl(std::string_view path, int cmd, void *arg) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::ioctl, path, cmd, arg);
+    }
+
+    auto filesystem::utimens(std::string_view path, std::array<timespec, 2> &tv) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::utimens, path, tv);
+    }
+
+    auto filesystem::flock(int fd, int cmd) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::flock, fd, cmd);
+    }
+
+    auto filesystem::isatty(int fd) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::isatty, fd);
+    }
+
+    auto filesystem::chmod(std::string_view path, mode_t mode) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::chmod, path, mode);
+    }
+
+    auto filesystem::close(int fd) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::close, fd);
+    }
+
+    auto filesystem::write(int fd, const char *ptr, size_t len) noexcept -> ssize_t
+    {
+        return invoke_fops(&filesystem_operations::write, fd, ptr, len);
+    }
+
+    auto filesystem::read(int fd, char *ptr, size_t len) noexcept -> ssize_t
+    {
+        return invoke_fops(&filesystem_operations::read, fd, ptr, len);
+    }
+
+    auto filesystem::seek(int fd, off_t pos, int dir) noexcept -> off_t
+    {
+        return invoke_fops(&filesystem_operations::seek, fd, pos, dir);
+    }
+
+    auto filesystem::fstat(int fd, struct stat &st) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::fstat, fd, st);
+    }
+
+    auto filesystem::ftruncate(int fd, off_t len) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::ftruncate, fd, len);
+    }
+
+    auto filesystem::fsync(int fd) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::fsync, fd);
+    }
+
+    auto filesystem::fchmod(int fd, mode_t mode) noexcept -> int
+    {
+        return invoke_fops(&filesystem_operations::fchmod, fd, mode);
+    }
+
+    auto filesystem::symlink(std::string_view existing, std::string_view newlink) noexcept -> int
+    {
+        return invoke_fops_same_mp(&filesystem_operations::symlink, existing, newlink);
+    }
+
+    auto filesystem::link(std::string_view existing, std::string_view newlink) noexcept -> int
+    {
+        return invoke_fops_same_mp(&filesystem_operations::link, existing, newlink);
+    }
+
+    auto filesystem::rename(std::string_view oldname, std::string_view newname) noexcept -> int
+    {
+        return invoke_fops_same_mp(&filesystem_operations::rename, oldname, newname);
+    }
+
+    auto filesystem::open(std::string_view path, int flags, int mode) noexcept -> int
+    {
+        const auto abspath     = absolute_path(path);
+        auto [mountp, pathpos] = find_mount_point(abspath);
+        if (!mountp) {
+            LOG_ERROR("VFS: Unable to find mount point");
+            return -ENOENT;
+        }
+        auto fsops = mountp->fs_ops().lock();
+        if (fsops) {
+            auto fh = fsops->open(mountp, abspath, flags, mode);
+            if (!fh) {
+                LOG_ERROR("VFS: Unable to get fops");
+                return -EBADF;
+            }
+            const auto err = fh->error();
+            if (err) {
+                return err;
+            }
+            return add_filehandle(fh);
+        }
+        else {
+            LOG_ERROR("VFS: Unable to lock fops");
+            return -EIO;
+        }
+    }
+} // namespace purefs::fs
