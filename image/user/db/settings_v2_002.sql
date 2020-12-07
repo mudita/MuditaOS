4fa33eca9e5eb873e9f5cf37e89f2cbc505ff283
RobertPiet <70648461+RobertPiet@users.noreply.github.com>
2020-12-07 12:48:08 +0100

    [EGD-4725] default sim value (#1145)
    * [EGD-4725] fixed lack of sim at startup

--- image/user/db/settings_v2_002.sql
+++ image/user/db/settings_v2_002.sql
@@ -9,7 +9,7 @@ INSERT OR REPLACE INTO settings_tab (path, value) VALUES
     ('system/phone_mode', 'online'),
     ('gs_time_format_12', '0'),
     ('gs_time_date_format', '1'),
-    ('gs_active_sim', '1'),
+    ('gs_active_sim', 'SIM1'),
     ('gs_lock_pass_hash', '3333'),
     ('gs_lock_time', '30000'),
     ('gs_display_language', 'En'),
