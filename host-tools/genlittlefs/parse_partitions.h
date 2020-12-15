52176b059b6af97bf2e2302d871beb31b94082a3
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-12-15 12:37:59 +0100

    [EGD-4760] littlefs host tools (#1161)
    * [EGD-4760] Remove littlefs sumbmodule
    
    * [EGD-4760] littlefs submodule in new location
    
    * [EGD-4760] Littlefs host tools inital build
    
    * [EGD-4760] Find fdisk
    
    * [EGD-4760] LFS Cmake file changed
    
    * [EGD-4760] GenLittlefs work in progress
    
    * [EGD-4760] Parse args in the tool
    
    * [EGD-4760] Inital version of genlittlefs
    
    * [EGD-4760] genlittle fs seams to be working
    
    It is a first working release for genlittle fs
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>
    
    * [EGD-4760] genlittle Speed improvement
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>
    
    * [EGD-4760] genlittlefs verbose flag
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>
    
    * [EGD-4760] genlittlefs fix file support
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>
    
    * [EGD-4760] Check if it is a littlefs
    
    * [EGD-4760] Genlittle fs image
    
    * [EGD-4760] Gen image fixed
    
    * [EGD-4760] Final version of genlittlefs
    
    * [EGD-4760] Rename littlefs rootdir.
    
    * [EGD-4760] Code review round #1 fixes

---
+++ host-tools/genlittlefs/parse_partitions.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+#include <stddef.h>
+
+typedef int part_type_t;
+
+enum part_type_options
+{
+    scan_all_partitions = -1
+};
+
+struct partition
+{
+    off_t start;
+    off_t end;
+    part_type_t type;
+};
+
+struct partition *find_partitions(const char *filename, part_type_t ptype, size_t *nelems)
+    __attribute__((nonnull(1, 3)));
+
+void print_partitions(const struct partition *part, size_t nparts);
