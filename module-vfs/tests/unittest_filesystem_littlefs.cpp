a036b7512107c6cc92de237d22ec1839768eba70
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2020-12-16 17:14:30 +0100

    [EGD-4758] Fix littlefs boundary check
        Currently littlefs boudary is checked in the wrong place
        this commit fix this issue

--- module-vfs/tests/unittest_filesystem_littlefs.cpp
+++ module-vfs/tests/unittest_filesystem_littlefs.cpp
@@ -5,9 +5,41 @@
 #include <purefs/fs/filesystem.hpp>
 #include <purefs/blkdev/disk_manager.hpp>
 #include <purefs/blkdev/disk_image.hpp>
-#include <purefs/fs/drivers/filesystem_vfat.hpp>
+#include <purefs/fs/drivers/filesystem_littlefs.hpp>
 #include <sys/statvfs.h>
 #include <sys/stat.h>
 
-TEST_CASE("Littlefs: Basic tests")
-{}
+namespace
+{
+    constexpr auto disk_image = "lfstest.img";
+}
+
+TEST_CASE("Littlefs: Basic mount and functionality")
+{
+    using namespace purefs;
+    auto dm   = std::make_shared<blkdev::disk_manager>();
+    auto disk = std::make_shared<blkdev::disk_image>(disk_image);
+    REQUIRE(disk);
+    REQUIRE(dm->register_device(disk, "emmc0") == 0);
+    purefs::fs::filesystem fscore(dm);
+    const auto vfs_vfat = std::make_shared<fs::drivers::filesystem_littlefs>();
+    REQUIRE(vfs_vfat->mount_count() == 0);
+    auto ret = fscore.register_filesystem("littlefs", vfs_vfat);
+    REQUIRE(ret == 0);
+    ret = fscore.mount("emmc0part0", "/sys", "littlefs");
+    REQUIRE(ret == 0);
+    REQUIRE(vfs_vfat->mount_count() == 1);
+    REQUIRE(fscore.umount("/ala") == -ENOENT);
+    ret = fscore.mount("emmc0part0", "/sys", "vfat");
+    REQUIRE(ret == -EBUSY);
+    ret = fscore.mount("emmc0part0", "/path", "vfat");
+    REQUIRE(ret == -EBUSY);
+    ret = fscore.umount("/sys");
+    REQUIRE(ret == 0);
+    REQUIRE(vfs_vfat->mount_count() == 0);
+    ret = fscore.mount("emmc0part0", "/path", "littlefs");
+    REQUIRE(ret == 0);
+    REQUIRE(vfs_vfat->mount_count() == 1);
+    ret = fscore.umount("/path");
+    REQUIRE(ret == 0);
+}
