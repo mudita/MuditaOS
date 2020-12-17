60347a231c324aac5f92fcb81a641812f69d8416
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2020-12-17 13:31:13 +0100

    [EGD-4753] Fix logs mocking
    Fix logs mocking by uncommenting mocking code and add missing mock
    function for log printing.

--- test/mock-logs.cpp
+++ test/mock-logs.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
-/*#include <log/log.hpp>
+#include <log/log.hpp>
 #include <cstdarg>
 __attribute__((weak)) void log_Log(
     logger_level level, const char *file, int line, const char *function, const char *fmt, ...)
@@ -9,4 +9,11 @@ __attribute__((weak)) void log_Log(
     va_list args;
     va_start(args, fmt);
     va_end(args);
-}*/
+}
+
+__attribute__((weak)) void log_Printf(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    va_end(args);
+}
