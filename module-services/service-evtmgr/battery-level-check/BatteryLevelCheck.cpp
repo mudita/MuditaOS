181c1027bad16769b02385aaa93ddb2da225d1c8
Wojtek Rzepecki <wojtek.rzepecki@mudita.com>
2020-12-18 12:24:53 +0100

    [EGD-4952] Added stetings for battery critical level

--- module-services/service-evtmgr/battery-level-check/BatteryLevelCheck.cpp
+++ module-services/service-evtmgr/battery-level-check/BatteryLevelCheck.cpp
@@ -7,24 +7,46 @@
 
 #include <Service/Bus.hpp>
 #include <common_data/EventStore.hpp>
+#include <service-db/service-db/Settings.hpp>
+#include <service-db/agents/settings/SystemSettings.hpp>
+#include <module-utils/Utils.hpp>
 
 namespace battery_level_check
 {
     namespace
     {
-        unsigned int batteryLevelCritical = 10;
+        unsigned int batteryLevelCritical = 0;
 
         sys::Service *parentService = nullptr;
+
+        std::unique_ptr<settings::Settings> settings;
 
         bool isBatteryLevelCritical(unsigned int level)
         {
             return level < batteryLevelCritical;
         }
+
+        void batteryLevelSettingsCallback(std::string setting)
+        {
+            if (!setting.empty()) {
+                batteryLevelCritical = utils::getNumericValue<unsigned int>(setting);
+            }
+        }
+
     } // namespace
 
     void init(sys::Service *service)
     {
         parentService = service;
+        settings      = std::make_unique<settings::Settings>(service);
+        settings->registerValueChange(settings::Battery::criticalLevel,
+                                      [&](std::string value) { batteryLevelSettingsCallback(value); });
+    }
+
+    void deinit()
+    {
+        settings->unregisterValueChange(settings::Battery::criticalLevel);
+        settings.reset();
     }
 
     void checkBatteryLevelCritical()
@@ -40,6 +62,7 @@ namespace battery_level_check
     void setBatteryCriticalLevel(unsigned int level)
     {
         batteryLevelCritical = level;
+        settings->setValue(settings::Battery::criticalLevel, std::to_string(batteryLevelCritical));
         checkBatteryLevelCritical();
     }
 
