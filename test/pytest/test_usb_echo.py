b92c40237d23c1025101d76ded492a5819d6f81c
Marek Niepieklo <marek.niepieklo@mudita.com>
2020-12-18 16:54:02 +0100

    [EGD-5004] Add USB-CDC echo mechanism
    Added echo mechanism to USB-CDC, update usb_stack submodule
    and added a test case to test harness

---
+++ test/pytest/test_usb_echo.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+# For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+import time
+import pytest
+
+from harness import log
+
+
+@pytest.mark.xfail
+@pytest.mark.usb_cdc_echo
+def test_echo_enable(harness, message_to_echo="Testing that echo mode on USB-CDC on Mudita Pure works!"):
+    harness.connection_echo_mode_on()
+
+    if not harness.is_echo_mode:
+        assert False, "Failed to enable Echo mode"
+
+    # Send the message
+    harness.connection.serial.write(message_to_echo.encode())
+
+    # Read back the message
+    result = harness.connection.serial.read(int(len(message_to_echo))).decode()
+
+    harness.connection_echo_mode_off()
+
+    assert message_to_echo == result, "Echo mode test failed"
