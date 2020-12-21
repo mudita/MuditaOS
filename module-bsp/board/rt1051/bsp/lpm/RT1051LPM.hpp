ae9a8ae4bc8d808ec18edde3f1c88ebf99fac7d5
Maciej-Mudita <maciej.gibowicz@mudita.com>
2020-12-21 16:15:43 +0100

    [EGD-4881] Add oscillator clock switching mechanisms
    Osc will change automatically at the lowest CPU frequency,
    which will save power

--- module-bsp/board/rt1051/bsp/lpm/RT1051LPM.hpp
+++ module-bsp/board/rt1051/bsp/lpm/RT1051LPM.hpp
@@ -7,9 +7,14 @@
 #include "bsp/lpm/bsp_lpm.hpp"
 #include "drivers/gpio/DriverGPIO.hpp"
 #include "CpuFreqLPM.hpp"
+#include <fsl_clock.h>
 
 namespace bsp
 {
+    inline constexpr uint8_t OscillatorReadyCounterValue{127};
+    inline constexpr uint8_t CCM_TupleShift{8};
+    inline constexpr uint8_t CCM_TupleMask{0x1F};
+    inline constexpr uint32_t ClockNeededRunWaitMode{3};
 
     class RT1051LPM : public LowPowerMode
     {
@@ -18,8 +23,11 @@ namespace bsp
         int32_t PowerOff() override final;
         int32_t Reboot() override final;
         void SetCpuFrequency(CpuFrequency freq) final;
+        void SwitchOscillatorSource(OscillatorSource source) final;
 
       private:
+        [[nodiscard]] bool IsClockEnabled(clock_ip_name_t name) const noexcept;
+
         std::shared_ptr<drivers::DriverGPIO> gpio;
         std::unique_ptr<bsp::CpuFreqLPM> CpuFreq;
     };
