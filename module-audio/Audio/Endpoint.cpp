40bf381eca9c7146f205a4b5dd3f369dd2b0e317
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2021-01-06 23:02:35 +0100

    [EGD-5086] Fix voice not starting when calling
    Due to a race condition between source and sink voice is not always
    starting when calling. Introduce audio stream connections to avoid
    race condition and improve handling of audio start and stop operations.
    
    Signed-off-by: Marcin Smoczyński <smoczynski.marcin@gmail.com>

--- module-audio/Audio/Endpoint.cpp
+++ module-audio/Audio/Endpoint.cpp
@@ -7,18 +7,21 @@
 
 using namespace audio;
 
-void Endpoint::setStream(Stream &stream)
+Endpoint::Endpoint(const Capabilities &caps) : _caps(caps)
+{}
+
+const Endpoint::Capabilities &Endpoint::getCapabilities() const noexcept
+{
+    return _caps;
+}
+
+void Endpoint::connectStream(Stream &stream)
 {
     assert(_stream == nullptr);
     _stream = &stream;
 }
 
-Stream *Endpoint::getStream() const noexcept
-{
-    return _stream;
-}
-
-void Endpoint::unsetStream()
+void Endpoint::disconnectStream()
 {
     assert(_stream != nullptr);
     _stream = nullptr;
@@ -29,16 +32,71 @@ bool Endpoint::isConnected() const noexcept
     return _stream != nullptr;
 }
 
-void Source::connect(Sink &sink, Stream &stream)
+StreamConnection::StreamConnection(Source *source, Sink *sink, Stream *stream)
+    : _sink(sink), _source(source), _stream(stream)
 {
-    connectedSink = &sink;
-    connectedSink->setStream(stream);
-    setStream(stream);
+    assert(_sink != nullptr);
+    assert(_source != nullptr);
+    assert(_stream != nullptr);
+
+    _sink->connectStream(*_stream);
+    _source->connectStream(*_stream);
 }
 
-void Source::disconnectStream()
+StreamConnection::~StreamConnection()
 {
-    unsetStream();
-    connectedSink->unsetStream();
-    connectedSink = nullptr;
+    destroy();
+}
+
+void StreamConnection::destroy()
+{
+    disable();
+    _sink->disconnectStream();
+    _source->disconnectStream();
+}
+
+void StreamConnection::enable()
+{
+    if (enabled) {
+        return;
+    }
+
+    _stream->reset();
+    _sink->enableOutput();
+    _source->enableInput();
+
+    enabled = true;
+}
+
+void StreamConnection::disable()
+{
+    if (!enabled) {
+        return;
+    }
+
+    _source->disableInput();
+    _sink->disableOutput();
+    _stream->reset();
+
+    enabled = false;
+}
+
+bool StreamConnection::isEnabled() const noexcept
+{
+    return enabled;
+}
+
+Source *StreamConnection::getSource() const noexcept
+{
+    return _source;
+}
+
+Sink *StreamConnection::getSink() const noexcept
+{
+    return _sink;
+}
+
+Stream *StreamConnection::getStream() const noexcept
+{
+    return _stream;
 }
