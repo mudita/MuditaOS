c2cd94a6f2aa3f54ba0559928a5f21e4fc042584
KacperLewandowski <themiddleearth1@gmail.com>
2021-01-11 00:12:46 +0100

    [EGD-4417] Fix operations in calculator
    1. Fix possibility to write illogical operations.
    2. Fix possibility to write multiple '0' when it is not needed.
    3. Add scientific notation when result do not fit on screen.

--- module-apps/application-calculator/data/CalculatorUtility.hpp
+++ module-apps/application-calculator/data/CalculatorUtility.hpp
@@ -4,6 +4,17 @@
 #pragma once
 #include <string>
 
+namespace CalculatorConstants
+{
+    inline constexpr auto veryLowPrecision = 4;
+    inline constexpr auto lowPrecision     = 5;
+    inline constexpr auto precision        = 6;
+    inline constexpr auto highPrecision    = 8;
+    inline constexpr auto expLength        = 1;
+    inline constexpr auto minusExpLength   = 2;
+    inline constexpr auto maxStringLength  = 7;
+} // namespace CalculatorConstants
+
 struct Result
 {
     std::string equation;
@@ -18,6 +29,10 @@ class Calculator
     /// @return: new string with replaced all occurrences of given string to the new one in whole input
     std::string replaceAllOccurrences(std::string input, const std::string &from, const std::string &to);
     std::string prepareEquationForParser(std::string input);
+    std::string getValueThatFitsOnScreen(double result);
+    long double getCoefficient(double result, uint32_t precision);
+    std::string convertToNumberWithPositiveExponent(double result, uint32_t exponent);
+    std::string convertToNumberWithNegativeExponent(double result, long long base);
 
   public:
     Result calculate(std::string source);
