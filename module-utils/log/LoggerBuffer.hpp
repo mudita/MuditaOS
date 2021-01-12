8ec631f9c4a44ed5b5c45f87d307457e9ed275d5
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-01-12 12:51:51 +0100

    [EGD-5181] Add Circular and Logger buffers
    Add RandomStringGenerator class.
    Implement Circular and Logger buffers.
    Add UTs for LoggerBuffer.

---
+++ module-utils/log/LoggerBuffer.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include "circular_buffer/StringCircularBuffer.hpp"
+
+class LoggerBuffer : public StringCircularBuffer
+{
+  public:
+    using StringCircularBuffer::StringCircularBuffer;
+
+    [[nodiscard]] std::pair<bool, std::string> get() override;
+    void put(const std::string &logMsg) override;
+    void put(std::string &&logMsg) override;
+
+    static constexpr auto lostBytesMessage = "bytes was lost.";
+
+  private:
+    void updateNumOfLostBytes();
+
+    size_t numOfLostBytes{0};
+};
