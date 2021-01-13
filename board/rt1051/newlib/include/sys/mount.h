f5c2cef9cd7656f4350ee7058866f72865f0a2eb
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2021-01-13 12:00:29 +0100

    [EGD-4498] Fix code after code review #1
    Fixed some small minor issus in the RO mode in the vfs

--- board/rt1051/newlib/include/sys/mount.h
+++ board/rt1051/newlib/include/sys/mount.h
@@ -8,15 +8,15 @@
    supported  */
 enum
 {
-  MS_RDONLY = 1,		/* Mount read-only.  */
-  MS_NOEXEC = 8,		/* Disallow program execution.  */
-  MS_SYNCHRONOUS = 16,		/* Writes are synced at once.  */
-  MS_REMOUNT = 32,		/* Alter flags of a mounted FS.  */
-  MS_MANDLOCK = 64,		/* Allow mandatory locks on an FS.  */
-  MS_DIRSYNC = 128,		/* Directory modifications are synchronous.  */
-  MS_NOATIME = 1024,		/* Do not update access times.  */
-  MS_NODIRATIME = 2048,		/* Do not update directory access times.  */
-  MS_BIND = 4096,		/* Bind directory at different place.  */
+  MS_RDONLY = 1,                /* Mount read-only.  */
+  MS_NOEXEC = 8,                /* Disallow program execution.  */
+  MS_SYNCHRONOUS = 16,          /* Writes are synced at once.  */
+  MS_REMOUNT = 32,              /* Alter flags of a mounted FS.  */
+  MS_MANDLOCK = 64,             /* Allow mandatory locks on an FS.  */
+  MS_DIRSYNC = 128,             /* Directory modifications are synchronous.  */
+  MS_NOATIME = 1024,            /* Do not update access times.  */
+  MS_NODIRATIME = 2048,         /* Do not update directory access times.  */
+  MS_BIND = 4096,               /* Bind directory at different place.  */
 };
 
 
