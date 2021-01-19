6c52e2d8312d0b467516f557de358b1bc01160d5
Wiktor S. Ovalle Correa <w.s.ovalle@mudita.com>
2021-01-19 13:17:59 +0100

    [EGD-5318] Fix coding style in board/
    Code style check was disabled for board/ subfolder.
    This commit fixes coding and enables style check
    for this folder.

--- board/linux/libiosyscalls/include/debug.hpp
+++ board/linux/libiosyscalls/include/debug.hpp
@@ -1,15 +1,20 @@
-// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
 
 #ifdef DEBUG_SHARED_LIBRARY_FS_LIB
-namespace vfsn::linux::internal {
-    void debug_trace_syscall(const char* fn, const char* format, ...);
+namespace vfsn::linux::internal
+{
+    void debug_trace_syscall(const char *fn, const char *format, ...);
 }
-#define TRACE_SYSCALL() vfsn::linux::internal::debug_trace_syscall(__PRETTY_FUNCTION__,"")
-#define TRACE_SYSCALLN(format,...) vfsn::linux::internal::debug_trace_syscall(__PRETTY_FUNCTION__,format,__VA_ARGS__)
+#define TRACE_SYSCALL()             vfsn::linux::internal::debug_trace_syscall(__PRETTY_FUNCTION__, "")
+#define TRACE_SYSCALLN(format, ...) vfsn::linux::internal::debug_trace_syscall(__PRETTY_FUNCTION__, format, __VA_ARGS__)
 #else
-#define TRACE_SYSCALL() do {} while(0)
-#define TRACE_SYSCALLN(format,...) do {} while(0)
+#define TRACE_SYSCALL()                                                                                                \
+    do {                                                                                                               \
+    } while (0)
+#define TRACE_SYSCALLN(format, ...)                                                                                    \
+    do {                                                                                                               \
+    } while (0)
 #endif
