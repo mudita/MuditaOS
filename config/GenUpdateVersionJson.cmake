da3a4382d8e52412d9fc1c41e68d247b4d3a6560
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2021-01-21 20:20:29 +0100

    [EGD-4529] on release upload packages to GitHub assets
    Uploading packages to github assets release is required for
    Mudita Center application to be able to update phones.

--- config/GenUpdateVersionJson.cmake
+++ config/GenUpdateVersionJson.cmake
@@ -8,7 +8,7 @@ include(Version)
 set(BOOTLOADER_INCLUDED "true")
 set(BOOTLOADER_FILENAME "ecoboot.bin")
 execute_process(
-    COMMAND grep "release:" "${CMAKE_BINARY_DIR}/update/download_info.txt"
+    COMMAND grep "release:" "${ECOBOOT_DOWNLOAD_LOG}"
     COMMAND awk "{print $2}"
     OUTPUT_VARIABLE BOOTLOADER_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -16,6 +16,6 @@ execute_process(
 
 configure_file(
     ${SRC_DIR}/config/version.json.cmake_template
-    ${CMAKE_BINARY_DIR}/update/version.json
+    ${CMAKE_BINARY_DIR}/version.json
     @ONLY
     )
