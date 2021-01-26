d896843d44450317cf2e6d89667793fe9b5310a0
Borys Jelenski <borys.jelenski@mudita.com>
2021-01-26 15:03:09 +0100

    [EGD-5415] Fix missing assets dep for lfstest.img
    Addionally, the genlfstestimg.sh script now accepts files to include
    in the image as arguments

--- module-vfs/tests/genlfstestimg.sh
+++ module-vfs/tests/genlfstestimg.sh
@@ -4,20 +4,22 @@
 
 usage() {
 cat << ==usage
-Usage: $(basename $0) [image_size] [image_file]
+Usage: $(basename $0) [image_size] [image_file] [files]...
 	image_size Target disk image size
 	image_file Target image name
+	files Files to include in the image
 ==usage
 }
 
 
-if [ $# -ne 2 ]; then
+if [ $# -lt 3 ]; then
 	echo "Error! Invalid argument count"
 	usage
 	exit -1
 fi
 IMAGE_SIZE="$1"
 IMAGE_FILE="$2"
+shift 2
 
 _REQ_CMDS="sfdisk truncate"
 for cmd in $_REQ_CMDS; do
@@ -39,5 +41,5 @@ unit: sectors
 
 /dev/sdz1 : start=$SECTOR_START, size=$SECTOR_END, type=9e
 ==sfdisk
-./genlittlefs --image $IMAGE_FILE --block_size=4096  --overwrite  --partition_num 1 -- assets/* .boot.json
+./genlittlefs --image $IMAGE_FILE --block_size=4096  --overwrite  --partition_num 1 -- "$@"
 
