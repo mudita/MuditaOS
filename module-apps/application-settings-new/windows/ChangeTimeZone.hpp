60b6b21516a5fdc92753d5c32eb89bae56d7a1db
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2021-02-11 15:41:56 +0100

    [EGD-5707] Add empty Time Zone selection window
    Add Empty window for time zone selection. Connection with DB
    will be don in separate PR.

---
+++ module-apps/application-settings-new/windows/ChangeTimeZone.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "BaseSettingsWindow.hpp"
+
+namespace gui
+{
+
+    class ChangeTimeZone : public BaseSettingsWindow
+    {
+      public:
+        explicit ChangeTimeZone(app::Application *app);
+        auto buildOptionsList() -> std::list<Option> override;
+    };
+} // namespace gui
