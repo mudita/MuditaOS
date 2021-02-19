89a6d300f7302e0dbe5a6e66d5f2d642bab4c4c5
Borys Jelenski <borys.jelenski@mudita.com>
2021-02-19 11:42:55 +0100

    [EGD-5503] Fix copyright
    Previous commit related to the same ticket introduced invalid copyright
    in some files. This commit fixes that.

--- module-bsp/board/rt1051/common/fsl_drivers/fsl_rtwdog.c
+++ module-bsp/board/rt1051/common/fsl_drivers/fsl_rtwdog.c
@@ -1,6 +1,3 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
-// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
-
 /*
  * Copyright (c) 2016, Freescale Semiconductor, Inc.
  * Copyright 2016-2019 NXP
--- module-bsp/board/rt1051/common/system_MIMXRT1051.c
+++ module-bsp/board/rt1051/common/system_MIMXRT1051.c
@@ -1,6 +1,3 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
-// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
-
 /*
 ** ###################################################################
 **     Processors:          MIMXRT1051CVJ5B
