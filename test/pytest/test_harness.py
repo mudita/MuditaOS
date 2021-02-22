324a40252d2488d2ec415d62b3ad145b4cbe2fec
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2021-02-22 13:53:40 +0100

    [EGD-5326] Fixed system messages logging crash
    with messages debug utility and error on memory there  was crash

--- test/pytest/test_harness.py
+++ test/pytest/test_harness.py
@@ -2,12 +2,14 @@
 # For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 import pytest
+from harness import log
 
 
-@pytest.mark.skip("not working ;/")
+# @pytest.mark.skip("not working ;/")
 @pytest.mark.rt1051
-@pytest.mark.usefixtures("phone_unlocked")
+# @pytest.mark.usefixtures("phone_unlocked")
 def test_send_AT(harness):
+    log.info("LOL")
     ret = harness.connection.send_at("AT")
-    print(ret)
+    log.info(ret)
     assert "OK" in ret
