980c0795054c3e8ad155ea12c9ca8128b5c4e12a
Piotr Tański <piotr.tanski@mudita.com>
2021-03-02 09:16:20 +0100

    [EGD-2653] Update list of call logs in chunks
    Automatically update the list of call logs in chunks.

--- module-db/queries/calllog/QueryCalllogGet.cpp
+++ module-db/queries/calllog/QueryCalllogGet.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "QueryCalllogGet.hpp"
@@ -10,15 +10,21 @@ using namespace db::query;
 CalllogGet::CalllogGet(std::size_t limit, std::size_t offset) : RecordQuery(limit, offset)
 {}
 
-CalllogGetResult::CalllogGetResult(const std::vector<CalllogRecord> &records) : RecordQueryResult(records)
-{}
-
 [[nodiscard]] auto CalllogGet::debugInfo() const -> std::string
 {
     return "CalllogGet";
 }
 
+CalllogGetResult::CalllogGetResult(std::vector<CalllogRecord> &&records, unsigned int dbRecordsCount)
+    : RecordQueryResult(std::move(records)), dbRecordsCount{dbRecordsCount}
+{}
+
 [[nodiscard]] auto CalllogGetResult::debugInfo() const -> std::string
 {
     return "CalllogGetResult";
+}
+
+auto CalllogGetResult::getTotalCount() const noexcept -> unsigned int
+{
+    return dbRecordsCount;
 }
