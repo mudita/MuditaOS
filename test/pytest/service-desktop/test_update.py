7164bbec8d1b3d8b9c654c57887c8c38ed1ad5f7
Roman Kubiak <roman.kubiak@mudita.com>
2021-03-10 16:33:44 +0100

    [EGD-6106] Fix update test on CI
    Update test is marked as rt1051 so CI ignores
    it during testing

--- test/pytest/service-desktop/test_update.py
+++ test/pytest/service-desktop/test_update.py
@@ -6,6 +6,7 @@ from harness.interface.defs import status
 
 @pytest.mark.service_desktop_test
 @pytest.mark.usefixtures("usb_unlocked")
+@pytest.mark.rt1051
 def test_update(harness):
     body = {}
     ret = harness.endpoint_request("update", "get", body)
