45574450ab475369ec87b0d483e266a201fc32e1
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-03-11 17:30:35 +0100

    [EGD-5932] Add the rest of queries to Quotes agent
    Implement add/read/write/delete quote queries

--- module-services/service-db/agents/quotes/QuotesAgent.cpp
+++ module-services/service-db/agents/quotes/QuotesAgent.cpp
@@ -30,6 +30,12 @@ namespace Quotes
                                std::bind(&QuotesAgent::handleEnableQuoteById, this, _1));
         parentService->connect(typeid(Messages::GetEnabledQuotesListRequest),
                                std::bind(&QuotesAgent::handleEnabledQuotesList, this, _1));
+        parentService->connect(typeid(Messages::AddQuoteRequest), std::bind(&QuotesAgent::handleAddQuote, this, _1));
+        parentService->connect(typeid(Messages::ReadQuoteRequest), std::bind(&QuotesAgent::handleReadQuote, this, _1));
+        parentService->connect(typeid(Messages::WriteQuoteRequest),
+                               std::bind(&QuotesAgent::handleWriteQuote, this, _1));
+        parentService->connect(typeid(Messages::DeleteQuoteRequest),
+                               std::bind(&QuotesAgent::handleDeleteQuote, this, _1));
     }
 
     auto QuotesAgent::getDbFilePath() -> const std::string
@@ -101,6 +107,47 @@ namespace Quotes
             auto query      = database->query(Queries::getEnabledQuotes);
             auto quotesList = getList<QuotesList, QuoteRecord>(std::move(msg->quotesList), std::move(query));
             return std::make_shared<Messages::GetEnabledQuotesListResponse>(std::move(quotesList));
+        }
+        return sys::msgNotHandled();
+    }
+
+    auto QuotesAgent::handleAddQuote(sys::Message *req) -> sys::MessagePointer
+    {
+        if (auto msg = dynamic_cast<Messages::AddQuoteRequest *>(req)) {
+            auto result = database->execute(
+                Queries::addQuote, msg->langId, msg->quote.c_str(), msg->author.c_str(), msg->enabled);
+            auto quoteId = database->getLastInsertRowId();
+            return std::make_shared<Messages::AddQuoteResponse>(result, quoteId);
+        }
+        return sys::msgNotHandled();
+    }
+
+    auto QuotesAgent::handleReadQuote(sys::Message *req) -> sys::MessagePointer
+    {
+        if (auto msg = dynamic_cast<Messages::ReadQuoteRequest *>(req)) {
+            auto result = database->query(Queries::readQuote, msg->quoteId);
+            QuoteRecord quoteRecord(result.get());
+            return std::make_shared<Messages::ReadQuoteResponse>(
+                quoteRecord.quote_id, quoteRecord.lang_id, quoteRecord.quote, quoteRecord.author, quoteRecord.enabled);
+        }
+        return sys::msgNotHandled();
+    }
+
+    auto QuotesAgent::handleWriteQuote(sys::Message *req) -> sys::MessagePointer
+    {
+        if (auto msg = dynamic_cast<Messages::WriteQuoteRequest *>(req)) {
+            auto result = database->execute(
+                Queries::writeQuote, msg->langId, msg->quote.c_str(), msg->author.c_str(), msg->enabled, msg->quoteId);
+            return std::make_shared<Messages::WriteQuoteResponse>(result);
+        }
+        return sys::msgNotHandled();
+    }
+
+    auto QuotesAgent::handleDeleteQuote(sys::Message *req) -> sys::MessagePointer
+    {
+        if (auto msg = dynamic_cast<Messages::DeleteQuoteRequest *>(req)) {
+            auto result = database->execute(Queries::deleteQuote, msg->quoteId);
+            return std::make_shared<Messages::DeleteQuoteResponse>(result);
         }
         return sys::msgNotHandled();
     }
--- module-services/service-db/test/test-service-db-quotes.hpp
+++ module-services/service-db/test/test-service-db-quotes.hpp
@@ -44,6 +44,33 @@ namespace Quotes
             return QuotesAgent::handleEnabledQuotesList(req);
         }
 
+        auto handleAddQuote(Messages::AddQuoteRequest *req) -> sys::MessagePointer
+        {
+            return QuotesAgent::handleAddQuote(req);
+        }
+
+        auto handleReadQuote(Messages::ReadQuoteRequest *req) -> sys::MessagePointer
+        {
+            return QuotesAgent::handleReadQuote(req);
+        }
+
+        auto handleWriteQuote(Messages::WriteQuoteRequest *req) -> sys::MessagePointer
+        {
+            return QuotesAgent::handleWriteQuote(req);
+        }
+
+        auto getAllQuotes(unsigned int limit = 0, unsigned int offset = 0)
+            -> std::shared_ptr<Messages::GetQuotesListResponse>
+        {
+            QuotesList quotesList;
+            quotesList.limit  = limit;
+            quotesList.offset = offset;
+
+            auto record  = std::make_unique<QuotesList>(quotesList);
+            auto request = std::make_shared<Messages::GetQuotesListRequest>(std::move(record));
+            return std::dynamic_pointer_cast<Messages::GetQuotesListResponse>(handleQuotesList(request.get()));
+        }
+
         auto getQuotesByCategoryId(unsigned int categoryId) -> std::vector<QuoteRecord>
         {
             QuotesList quotesList;
@@ -82,6 +109,34 @@ namespace Quotes
         {
             auto request = std::make_shared<Messages::EnableQuoteByIdRequest>(quoteId, enable);
             return std::dynamic_pointer_cast<Messages::EnableQuoteByIdResponse>(handleEnableQuoteById(request.get()));
+        }
+
+        auto addQuote(unsigned int langId, std::string quote, std::string author, bool enabled)
+            -> std::shared_ptr<Messages::AddQuoteResponse>
+        {
+            auto request =
+                std::make_shared<Messages::AddQuoteRequest>(langId, std::move(quote), std::move(author), enabled);
+            return std::dynamic_pointer_cast<Messages::AddQuoteResponse>(handleAddQuote(request.get()));
+        }
+
+        auto readQuote(unsigned int quoteId) -> std::shared_ptr<Messages::ReadQuoteResponse>
+        {
+            auto request = std::make_shared<Messages::ReadQuoteRequest>(quoteId);
+            return std::dynamic_pointer_cast<Messages::ReadQuoteResponse>(handleReadQuote(request.get()));
+        }
+
+        auto writeQuote(unsigned int quoteId, unsigned int langId, std::string quote, std::string author, bool enabled)
+            -> std::shared_ptr<Messages::WriteQuoteResponse>
+        {
+            auto request = std::make_shared<Messages::WriteQuoteRequest>(
+                quoteId, langId, std::move(quote), std::move(author), enabled);
+            return std::dynamic_pointer_cast<Messages::WriteQuoteResponse>(handleWriteQuote(request.get()));
+        }
+
+        auto deleteQuote(unsigned int quoteId) -> std::shared_ptr<Messages::DeleteQuoteResponse>
+        {
+            auto request = std::make_shared<Messages::DeleteQuoteRequest>(quoteId);
+            return std::dynamic_pointer_cast<Messages::DeleteQuoteResponse>(handleDeleteQuote(request.get()));
         }
     };
 } // namespace Quotes
