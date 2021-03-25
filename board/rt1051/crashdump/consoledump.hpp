bec9e7a3da2ed4e895eb2ff723e36754859cb145
Artur Śleszyński <artur.sleszynski@mudita.com>
2021-03-25 09:22:05 +0100

    [EGD-6156] Create crash dumps
    Integrate the CrashCatcher library in order to store phone state after
    a fault occurs. The dumps are stored in the CrashCatcher hex format and
    can be debugged using the CrashDebug stub built for x86_64 via the
    'debug_crash_dump.sh' script.

---
+++ board/rt1051/crashdump/consoledump.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <cstdint>
+
+// Exception Stack Frame
+typedef struct __attribute__((packed))
+{
+    uint32_t r0;
+    uint32_t r1;
+    uint32_t r2;
+    uint32_t r3;
+    uint32_t r12;
+    uint32_t lr;
+    uint32_t pc;
+    uint32_t psr;
+#if defined(__ARM_ARCH_7EM__)
+    uint32_t s[16];
+#endif
+} syslog_exception_stack_frame_t;
