93ea595dfe20eb1aaddb8e1b2b61844b0342ecf6
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2021-03-30 09:31:19 +0200

    [EGD-6113] Always run tests on linux release
    With original script was able to select build dir to run
    against this script always chooses "Release" version

--- test/pytest/test_dom_dump.py
+++ test/pytest/test_dom_dump.py
@@ -7,6 +7,6 @@ from harness import log
 def test_get_dom(harness):
     body = {"ui": True, "getWindow": True}
     result = harness.endpoint_request("developerMode", "get", body)
-    log.info("data {}".format(result))
+    #log.info("data {}".format(result))
     assert result['body']
     assert 'Window' in result['body']['dom']
