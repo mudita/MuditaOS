48c73d2e48786cfaeed72f382ae4aa370efd04bd
Marek Niepieklo <marek.niepieklo@mudita.com>
2021-04-01 12:44:15 +0200

    [EGD-6383] Add HW initialization for headset keys detection
    Replaced timer-based hander with IRQ handler to get
      headset status
    
    Added initialization for headset key press detection

--- module-bsp/bsp/headset/headset.hpp
+++ module-bsp/bsp/headset/headset.hpp
@@ -6,10 +6,15 @@
 
 namespace bsp {
  namespace headset {
- int32_t Init(xQueueHandle qHandle);
- 	bool Handler(uint8_t notification);
- 	 bool IsInserted();
- 	int32_t Deinit();
 
+    int32_t Init(xQueueHandle qHandle);
+
+    bool Handler(uint8_t notification);
+
+    bool IsInserted();
+
+    int32_t Deinit();
+
+    BaseType_t headset_IRQHandler();
  }
 }
