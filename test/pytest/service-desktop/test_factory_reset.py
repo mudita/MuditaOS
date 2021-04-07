90bfaa847d606711f4697a489cc7b9a901f1729d
Pawel.Paprocki <pawel.paprocki@mudita.com>
2021-04-07 17:21:09 +0200

    [EGD-6352] Add Factory reset mode
    Stop DB service
    Delete DB files
    Make reboot

--- test/pytest/service-desktop/test_factory_reset.py
+++ test/pytest/service-desktop/test_factory_reset.py
@@ -1,11 +1,11 @@
-# Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+# Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 # For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 import pytest
 from harness.interface.defs import status
 
 
 @pytest.mark.service_desktop_test
-@pytest.mark.skip(reason="This will make a factory reset")
+@pytest.mark.rt1051
 @pytest.mark.usefixtures("usb_unlocked")
 def test_factory_reset(harness):
     body = {"factoryRequest": True}
