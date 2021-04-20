9d41233686206b8f4d08dbd67bc31d1af08fec28
Pawel.Paprocki <pawel.paprocki@mudita.com>
2021-04-20 09:53:01 +0200

    [EGD-6313] Fix Display backlight settings are not presistent
    Add global scope to settings db function calls

--- image/user/db/settings_v2_002.sql
+++ image/user/db/settings_v2_002.sql
@@ -33,4 +33,7 @@ INSERT OR IGNORE INTO settings_tab (path, value) VALUES
     ('off_connection_frequency', '0'),
     ('off_notifications_when_locked', '0'),
     ('off_calls_from_favorites', '0'),
+    ('br_state', '0'),
+    ('br_auto_mode', '0'),
+    ('br_level', '50.0f'),
     ('keypad_light_state', '0');
