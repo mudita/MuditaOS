5d66bd9f14613e7f141269826d15c0c8e8183245
Lukasz Skrzypczak <lukasz.skrzypczak@mudita.com>
2021-04-22 00:30:38 -0700

    [EGD-6576] Create EEPROM low level driver
    Created low level driver for M24256 eeprom & dummy for linux.

--- module-bsp/targets/Target_Linux.cmake
+++ module-bsp/targets/Target_Linux.cmake
@@ -34,6 +34,8 @@ set(BOARD_SOURCES
 
         "${CMAKE_CURRENT_SOURCE_DIR}/board/linux/watchdog/watchdog.cpp"
 
+        "${CMAKE_CURRENT_SOURCE_DIR}/board/linux/eeprom/eeprom.cpp"
+
         CACHE INTERNAL "")
 
 set(BOARD_DIR_INCLUDES
