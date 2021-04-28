f6f28e8b532f401579780b62c2386b46258593cf
Lukasz Skrzypczak <lukasz.skrzypczak@mudita.com>
2021-04-28 13:40:19 +0200

    [EGD-6576] WiP I2C HAL API need to be modified
    WiP

--- module-bsp/board/rt1051/bsp/eeprom/M24256.hpp
+++ module-bsp/board/rt1051/bsp/eeprom/M24256.hpp
@@ -7,8 +7,8 @@
 
 namespace bsp::eeprom
 {
-    constexpr inline auto M24256_MEM_DEVICE_ADDR = 0xA0;
-    constexpr inline auto M24256_ID_DEVICE_ADDR  = 0xB0;
+    constexpr inline auto M24256_MEM_DEVICE_ADDR = (0xA0 >> 1);
+    constexpr inline auto M24256_ID_DEVICE_ADDR  = (0xB0 >> 1);
 
     constexpr inline auto M24256_PAGE_SIZE  = 64;
     constexpr inline auto M24256_TOTAL_SIZE = (32 * 1024); // bytes
