c42df2435d7c41c45f36f9da9f61442eb1326100
Pawel.Paprocki <pawel.paprocki@mudita.com>
2021-04-29 07:34:13 +0200

    [EGD-6646] Fix Contact Record db tests
    Clean  WAL file after db init

---
+++ module-db/tests/common.hpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <string>
+
+void RemoveDbFiles(const std::string &dbName);
