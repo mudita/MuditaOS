9d8b55e034dfcc16289756d195a5562a14c4aef1
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2021-05-11 15:31:52 +0200

    [EGD-6518] Add EEPROM block device driver for VFS
    Add block device driver for the factory EEPROM memory
    needed for factory settings (aka personalizations)
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>

--- module-vfs/targets/Target_RT1051.cmake
+++ module-vfs/targets/Target_RT1051.cmake
@@ -1,5 +1,6 @@
 set(BOARD_SOURCES ${BOARD_SOURCES}
         ${CMAKE_CURRENT_SOURCE_DIR}/board/rt1051/purefs/src/blkdev/disk_emmc.cpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/board/rt1051/purefs/src/blkdev/disk_eeprom.cpp
         ${CMAKE_CURRENT_SOURCE_DIR}/board/rt1051/purefs/src/fs/thread_local_cwd.cpp
         ${CMAKE_CURRENT_SOURCE_DIR}/board/rt1051/purefs/src/vfs_subsystem_internal.cpp
         ${CMAKE_CURRENT_SOURCE_DIR}/src/newlib/vfs_io_syscalls.cpp
