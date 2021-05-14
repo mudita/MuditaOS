3c725f89f80d3b05ca3fe25ee93bdbbf699fe382
Wojtek Rzepecki <wojtek.rzepecki@mudita.com>
2021-05-14 14:50:04 +0200

    [EGD-6742] Add autolock window settings
    Add connection to settings of autolock
    window contents

--- module-apps/application-settings-new/windows/AutolockWindow.hpp
+++ module-apps/application-settings-new/windows/AutolockWindow.hpp
@@ -1,9 +1,10 @@
-// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
 
 #include "BaseSettingsWindow.hpp"
+#include <module-apps/application-settings-new/ApplicationSettings.hpp>
 
 namespace gui
 {
@@ -11,11 +12,11 @@ namespace gui
     class AutolockWindow : public BaseSettingsWindow
     {
       public:
-        AutolockWindow(app::Application *app);
+        AutolockWindow(app::Application *app, app::settingsInterface::AutoLockSettings *autoLockSettings);
 
       private:
         auto buildOptionsList() -> std::list<Option> override;
 
-        std::string selectedTime;
+        app::settingsInterface::AutoLockSettings *autoLockSettings;
     };
 } // namespace gui
