798a2fb45afee614205ae8d8f7680dc678e251da
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-05-24 11:25:59 +0200

    [BH-361] Move date to separate directory
    Move date to separate directory

--- cmake/modules/PureCoverage.cmake
+++ cmake/modules/PureCoverage.cmake
@@ -28,7 +28,6 @@ if(COVERAGE_ENABLE)
         module-db/Database/sqlite3.c
         module-lwip/lib/.*
         module-os/.*
-        module-utils/date/include/date/date.h
         module-utils/gsl/.*
         module-utils/json/.*
         module-utils/libphonenumber/.*
