f059ceba05a0be4559fea8f6cfa739c480e5f8db
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2021-05-26 10:55:10 +0200

    [BH-353] Add minimal build target for BH
    * add BellHybridMain
    * fixing post review
    
    Git automatically converts BellHybird as move from source/main.cpp to
    prevent this and maintain "main.cpp" history adding new target as
    separate commit.

--- cmake/modules/DiskImage.cmake
+++ cmake/modules/DiskImage.cmake
@@ -1,7 +1,8 @@
 function(add_image SOURCE_TARGET)
     set(DISK_IMAGE_NAME ${SOURCE_TARGET}.img)
     set(DISK_IMAGE ${CMAKE_BINARY_DIR}/${DISK_IMAGE_NAME})
-    set(BIN_FILE ${SOURCE_TARGET}-boot.bin)
+    set(BIN_FILE_TARGET ${SOURCE_TARGET}-boot.bin)
+    get_target_property(BIN_FILE ${BIN_FILE_TARGET} BIN_FILE)
 
     if (${PROJECT_TARGET} STREQUAL "TARGET_RT1051")
         SET(TARGET_DEP ${BIN_FILE} pureflash)
@@ -11,17 +12,21 @@ function(add_image SOURCE_TARGET)
 
     add_custom_command(
         OUTPUT ${DISK_IMAGE_NAME}
-        DEPENDS
         DEPENDS genlittlefs
         DEPENDS assets
+        DEPENDS ${BIN_FILE_TARGET}
         DEPENDS ${TARGET_DEP}
-        DEPENDS ${BIN_FILE}
-        DEPENDS ${CMAKE_SOURCE_DIR}/generate_purephone_image.sh
-        COMMAND ${CMAKE_SOURCE_DIR}/generate_purephone_image.sh ${DISK_IMAGE} ${CMAKE_BINARY_DIR} ${BIN_FILE}
+        DEPENDS ${CMAKE_SOURCE_DIR}/generate_image.sh
+        COMMAND ${CMAKE_SOURCE_DIR}/generate_image.sh
+        ${DISK_IMAGE}
+        ${CMAKE_BINARY_DIR}
+        "$<$<STREQUAL:${PROJECT_TARGET},TARGET_RT1051>:${BIN_FILE}>"
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         COMMENT "Generate ${DISK_IMAGE_NAME}"
         )
+
     message("Adding disk image target: ${DISK_IMAGE_NAME}")
     add_custom_target(${SOURCE_TARGET}-disk-img
         DEPENDS ${DISK_IMAGE_NAME})
+
 endfunction()
--- cmake/modules/FetchBootloader.cmake
+++ cmake/modules/FetchBootloader.cmake
@@ -1,34 +1,22 @@
 # download the bootloader
+set(ECOBOOT_FILE ecoboot.bin)
+set(VERSION_JSON_FILE version.json)
 
-function(add_ecoboot)
-    set(ECOBOOT_FILE ecoboot.bin)
+function(fetch_ecoboot)
     set(ECOBOOT_DOWNLOAD_LOG ${CMAKE_BINARY_DIR}/download_info.txt)
-    if(DEFINED ENV{ASSETS_LOGIN} AND DEFINED ENV{ASSETS_TOKEN})
-        message("ecooboot download with tokens")
         add_custom_command(OUTPUT ${ECOBOOT_FILE}
             COMMAND ${CMAKE_SOURCE_DIR}/tools/download_asset.py
-            -l $ENV{ASSETS_LOGIN}
-            -t $ENV{ASSETS_TOKEN}
+            "$<$<BOOL:$ENV{ASSETS_LOGIN}>:-l$ENV{ASSETS_LOGIN}>"
+            "$<$<BOOL:$ENV{ASSETS_TOKEN}>:-t$ENV{ASSETS_TOKEN}>"
             -w ${CMAKE_BINARY_DIR} ecoboot download
             > ${ECOBOOT_DOWNLOAD_LOG}
             COMMENT "Downloading ${ECOBOOT_FILE}"
             BYPRODUCTS ${ECOBOOT_DOWNLOAD_LOG}
             )
-    else()
-        message("ecoboot download with git")
-        add_custom_command(OUTPUT ${ECOBOOT_FILE}
-            COMMAND ${CMAKE_SOURCE_DIR}/tools/download_asset.py
-            -w ${CMAKE_BINARY_DIR} ecoboot download
-            > ${ECOBOOT_DOWNLOAD_LOG}
-            COMMENT "Downloading ${ECOBOOT_FILE}"
-            BYPRODUCTS ${ECOBOOT_DOWNLOAD_LOG}
-            )
-    endif()
 
     add_custom_target(${ECOBOOT_FILE}-target DEPENDS ${ECOBOOT_FILE})
 
     # generate version.json file (runs CMake script at build time)
-    set(VERSION_JSON_FILE version.json)
     add_custom_command(OUTPUT ${VERSION_JSON_FILE}
         COMMAND ${CMAKE_COMMAND}
         -DSRC_DIR=${CMAKE_SOURCE_DIR}
