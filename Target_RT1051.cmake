8bc00dca1c4c5e426e1d04eaa5b59844e38303ec
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-05-28 15:03:21 +0200

    [BH-396] Move CrashDebug to a separate directory
    Move CrashDebug to a separate directory

--- Target_RT1051.cmake
+++ Target_RT1051.cmake
@@ -78,7 +78,6 @@ set(TARGET_SOURCES
         ${CMAKE_CURRENT_LIST_DIR}/board/rt1051/xip/evkbimxrt1050_flexspi_nor_config.c
         ${CMAKE_CURRENT_LIST_DIR}/board/rt1051/xip/evkbimxrt1050_sdram_ini_dcd.c
         ${CMAKE_CURRENT_LIST_DIR}/board/rt1051/xip/fsl_flexspi_nor_boot.c
-        ${CMAKE_SOURCE_DIR}/module-utils/CrashDebug/CrashCatcher/Core/src/CrashCatcher.c
         ${CMAKE_CURRENT_LIST_DIR}/board/rt1051/crashdump/CrashCatcher_armv7m.S
         ${CMAKE_CURRENT_LIST_DIR}/board/rt1051/crashdump/crashcatcher_impl.cpp
         ${CMAKE_CURRENT_LIST_DIR}/board/rt1051/crashdump/crashdumpwriter_vfs.cpp
@@ -93,8 +92,6 @@ set(TARGET_DIR_INCLUDES
 
 set(BOARD_DIR_INCLUDES
         ${CMAKE_CURRENT_LIST_DIR}/board/rt1051/newlib/include
-        ${CMAKE_SOURCE_DIR}/module-utils/CrashDebug/CrashCatcher/include
-        ${CMAKE_SOURCE_DIR}/module-utils/CrashDebug/CrashCatcher/Core/src
         ${CMAKE_SOURCE_DIR}/module-vfs/include/internal
 )
 
