7ee1c411ee7e5926534ea9088349701b6d10d6af
DariuszSabala <dariusz.sabala@mudita.com>
2021-05-30 08:14:43 +0200

    [BH-460] Moved TPLIB littlefs
    Moved littlefs library to dedicated third party directory,
    fixed include paths

--- module-vfs/drivers/include/purefs/fs/drivers/mount_point_littlefs.hpp
+++ module-vfs/drivers/include/purefs/fs/drivers/mount_point_littlefs.hpp
@@ -1,8 +1,8 @@
-// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 #pragma once
 #include <purefs/fs/mount_point.hpp>
-#include <littlefs/lfs.h>
+#include <lfs.h>
 
 namespace purefs::fs::drivers
 {
