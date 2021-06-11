0684b1271b8674e7416b5b815a29faa1843edd44
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-06-11 08:55:42 +0200

    [BH-515] Create main Bell app
    Create main Bell app

---
+++ module-apps/application-bell-main/ApplicationBellMain.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "ApplicationBellMain.hpp"
+#include "windows/BellMainWindow.hpp"
+
+namespace app
+{
+    ApplicationBellMain::ApplicationBellMain(std::string name,
+                                             std::string parent,
+                                             sys::phone_modes::PhoneMode mode,
+                                             StartInBackground startInBackground)
+        : Application(name, parent, mode, startInBackground)
+    {}
+
+    sys::ReturnCodes ApplicationBellMain::InitHandler()
+    {
+        auto ret = Application::InitHandler();
+        if (ret != sys::ReturnCodes::Success) {
+            return ret;
+        }
+
+        createUserInterface();
+
+        return sys::ReturnCodes::Success;
+    }
+
+    void ApplicationBellMain::createUserInterface()
+    {
+        windowsFactory.attach(gui::name::window::main_window, [](Application *app, const std::string &name) {
+            return std::make_unique<gui::BellMainWindow>(app);
+        });
+    }
+
+    sys::MessagePointer ApplicationBellMain::DataReceivedHandler(sys::DataMessage *msgl, sys::ResponseMessage *resp)
+    {
+        auto retMsg = Application::DataReceivedHandler(msgl);
+        if (dynamic_cast<sys::ResponseMessage *>(retMsg.get())->retCode == sys::ReturnCodes::Success) {
+            return retMsg;
+        }
+        return std::make_shared<sys::ResponseMessage>();
+    }
+} // namespace app
---
+++ module-apps/application-bell-main/windows/BellMainWindow.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "BellMainWindow.hpp"
+#include <i18n/i18n.hpp>
+
+namespace gui
+{
+    BellMainWindow::BellMainWindow(app::Application *app) : AppWindow(app, gui::name::window::main_window)
+    {
+        buildInterface();
+    }
+
+    void BellMainWindow::buildInterface()
+    {
+        AppWindow::buildInterface();
+
+        bottomBar->setActive(BottomBar::Side::CENTER, true);
+        bottomBar->setText(gui::BottomBar::Side::CENTER, utils::translate(style::strings::common::start));
+
+        setTitle("Bell Hybrid+");
+    }
+} // namespace gui
