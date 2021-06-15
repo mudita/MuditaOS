923add96871515c99e995b4051d0db9941797438
Wojtek Rzepecki <wojtek.rzepecki@mudita.com>
2021-06-15 10:16:16 +0200

    [EGD-2949] Fix fallback image
    Added fallback image in case
    of missing image asset

--- module-gui/gui/core/ImageManager.hpp
+++ module-gui/gui/core/ImageManager.hpp
@@ -1,25 +1,20 @@
 // Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
-
 #pragma once
 
+#include "ImageMap.hpp"
 #include <vector>
 #include <string>
 #include <cstdint>
-#include <cstring>
-
-#include "ImageMap.hpp"
 
 namespace gui
 {
-    class ImageNotFound : public std::runtime_error
-    {
-      public:
-        using std::runtime_error::runtime_error;
-    };
-
     class ImageManager
     {
+      private:
+        ImageMap *createFallbackImage();
+        std::uint32_t fallbackImageId{0};
+
       protected:
         std::string mapFolder;
         std::vector<ImageMap *> imageMaps;
@@ -28,6 +23,7 @@ namespace gui
 
         ImageMap *loadPixMap(std::string filename);
         ImageMap *loadVecMap(std::string filename);
+        void addFallbackImage();
         void loadImageMaps(std::string baseDirectory);
 
         ImageManager();
