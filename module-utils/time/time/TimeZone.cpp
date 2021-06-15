39a760bad6d6dd28300bad334c7902d7e46fa6e2
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2021-06-15 13:36:59 +0200

    [EGD-6702] Add time zone library
    The library is used to manually set the time zone.
    It is based on the IANA time zone database.

---
+++ module-utils/time/time/TimeZone.cpp
@@ -0,0 +1,97 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "TimeZone.hpp"
+#include <utz/utz.h>
+#include <utz/zones.h>
+#include <log/log.hpp>
+#include <ctime>
+
+namespace utils::time
+{
+    constexpr uint16_t maxZoneRuleLength{50};
+    constexpr uint8_t localTimeYearOffset{100};
+
+    [[nodiscard]] auto getAvailableTimeZonesToDisplay() -> std::vector<std::string>
+    {
+        std::vector<std::string> timeZonesNames;
+        auto zonePointer = reinterpret_cast<const char *>(zone_names);
+        uzone_t zoneOut;
+
+        for (uint16_t zone = 0; zone < NUM_ZONE_NAMES; zone++) {
+            unpack_zone(&zone_defns[get_next(&zonePointer)], zonePointer, &zoneOut);
+            timeZonesNames.push_back(zoneOut.name);
+            zonePointer++;
+        }
+
+        return timeZonesNames;
+    }
+
+    [[nodiscard]] auto getTimeZoneOffset(const std::string &zoneName) -> std::int8_t
+    {
+        uzone_t zoneOut;
+
+        if (get_zone_by_name(const_cast<char *>(zoneName.c_str()), &zoneOut)) {
+            LOG_ERROR("Zone %s not found", zoneName.c_str());
+            return 0;
+        }
+
+        return zoneOut.src->offset_inc_minutes;
+    }
+
+    void unpackDstRules(urule_packed_t *packedRules, char *dstRules, int8_t tzOffset, uint8_t currYear, bool isEndRule)
+    {
+        switch (packedRules->on_dayofmonth) {
+        case 0: // the last day of week in month
+            [[fallthrough]];
+        case 1: // first day of week in month
+            snprintf(dstRules,
+                     maxZoneRuleLength,
+                     ",M%d.%d.%d/%d",
+                     packedRules->in_month,
+                     packedRules->on_dayofmonth == 0 ? 5 : packedRules->on_dayofmonth,
+                     packedRules->on_dayofweek % 7,
+                     packedRules->at_hours + (isEndRule ? 1 : 0) + (packedRules->at_is_local_time ? 0 : tzOffset));
+            break;
+        default: // The Julian day n (1 <= n <= 365)
+            snprintf(dstRules,
+                     maxZoneRuleLength,
+                     ",J%d/%d",
+                     get_day_in_year(packedRules, currYear),
+                     packedRules->at_hours + (isEndRule ? 1 : 0) + (packedRules->at_is_local_time ? 0 : tzOffset));
+        }
+    }
+
+    [[nodiscard]] auto getTimeZoneRules(const std::string &zoneName) -> std::string
+    {
+        std::string zoneRules;
+        char offsetRules[maxZoneRuleLength];
+        urule_packed_t startRules, endRules;
+        uzone_t zoneOut;
+        time_t tm           = std::time(nullptr);
+        uint8_t currentYear = localtime(&tm)->tm_year - localTimeYearOffset;
+
+        if (get_zone_by_name(const_cast<char *>(zoneName.c_str()), &zoneOut)) {
+            LOG_ERROR("Zone %s not found", zoneName.c_str());
+            return "";
+        }
+
+        snprintf(offsetRules, maxZoneRuleLength, "UTC%d:%02d", -zoneOut.offset.hours, zoneOut.offset.minutes);
+
+        zoneRules = offsetRules;
+
+        if (!get_zone_rules(zoneOut.src, currentYear, &startRules, &endRules)) {
+            char dstRules[maxZoneRuleLength];
+
+            zoneRules.append("UTC");
+            unpackDstRules(&startRules, dstRules, zoneOut.offset.hours, currentYear, false);
+            zoneRules.append(dstRules);
+
+            unpackDstRules(&endRules, dstRules, zoneOut.offset.hours, currentYear, true);
+            zoneRules.append(dstRules);
+        }
+
+        return zoneRules;
+    }
+
+} // namespace utils::time
