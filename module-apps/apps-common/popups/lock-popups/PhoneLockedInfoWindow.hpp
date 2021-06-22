2abfbc74d69b9da99ee46f12d3a1395f0d928877
tomaszkrosnowski <tomaszkrosnowski@mudita.com>
2021-06-22 21:52:05 +0200

    [EGD-6041] Fix unlock windows behaviour
    Fixed behaviour so the window acts as in Miro requirements and has 2
    seconds timeout.

--- module-apps/apps-common/popups/lock-popups/PhoneLockedInfoWindow.hpp
+++ module-apps/apps-common/popups/lock-popups/PhoneLockedInfoWindow.hpp
@@ -3,22 +3,25 @@
 
 #pragma once
 
+#include "PhoneLockedInfoData.hpp"
+#include <module-apps/apps-common/popups/WindowWithTimer.hpp>
 #include <AppWindow.hpp>
 #include <Text.hpp>
 #include <gui/widgets/Icon.hpp>
 
 namespace gui
 {
-    class PhoneLockedInfoWindow : public AppWindow
+    class PhoneLockedInfoWindow : public WindowWithTimer
     {
-        Icon *infoIcon = nullptr;
-
       public:
         PhoneLockedInfoWindow(app::Application *app, const std::string &name);
         void onBeforeShow(ShowMode mode, SwitchData *data) override;
         bool onInput(const InputEvent &inputEvent) override;
-
         void buildInterface() override;
         status_bar::Configuration configureStatusBar(status_bar::Configuration appConfiguration) override;
+
+      private:
+        PhoneLockedInfoData::Stage mStage = PhoneLockedInfoData::Stage::Idle;
+        Icon *infoIcon                    = nullptr;
     };
 } /* namespace gui */
