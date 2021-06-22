80009597321ba89a1c31aa1b49684ae220b1d939
DariuszSabala <dariusz.sabala@mudita.com>
2021-06-22 15:15:25 +0200

    [BH-495] Created Bell Hybrid home screen
    Cleaned up PurePhone remainings, modified UI to comply with
    documentation, cleaned up include paths to comply Mudita guide

--- module-apps/application-bell-main/data/BellMainStyle.hpp
+++ module-apps/application-bell-main/data/BellMainStyle.hpp
@@ -5,6 +5,29 @@
 
 namespace bellMainStyle
 {
+    namespace mainWindow
+    {
+        namespace timeLabel
+        {
+            inline constexpr auto posX   = 0;
+            inline constexpr auto posY   = 200;
+            inline constexpr auto width  = 480;
+            inline constexpr auto height = 100;
+            inline constexpr auto font   = "gt_pressura_light_90";
+        } // namespace timeLabel
+
+        namespace temperatureLabel
+        {
+            inline constexpr auto posX   = 0;
+            inline constexpr auto posY   = 300;
+            inline constexpr auto width  = 480;
+            inline constexpr auto height = 50;
+            inline constexpr auto font   = "gt_pressura_regular_30";
+            inline constexpr auto degree = "\u00B0";
+        } // namespace temperatureLabel
+
+    } // namespace mainWindow
+
     namespace mainMenuWindow
     {
         inline constexpr gui::Length options_list_x     = 40;
--- module-apps/application-bell-main/windows/BellMainWindow.cpp
+++ module-apps/application-bell-main/windows/BellMainWindow.cpp
@@ -2,11 +2,15 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "BellMainWindow.hpp"
+
+#include <application-bell-main/ApplicationBellMain.hpp>
+#include <data/BellMainStyle.hpp>
+
+#include <gui/input/InputEvent.hpp>
 #include <i18n/i18n.hpp>
 #include <log/log.hpp>
 #include <service-appmgr/Controller.hpp>
-#include <gui/input/InputEvent.hpp>
-#include <application-bell-main/ApplicationBellMain.hpp>
+#include <time/time_conversion.hpp>
 
 namespace gui
 {
@@ -19,9 +23,26 @@ namespace gui
     {
         AppWindow::buildInterface();
 
-        bottomBar->setActive(BottomBar::Side::CENTER, true);
-        bottomBar->setText(gui::BottomBar::Side::CENTER, utils::translate(style::strings::common::start));
-        setTitle(utils::translate("app_bellmain_main_window_title"));
+        statusBar->setVisible(false);
+        header->setTitleVisibility(false);
+        bottomBar->setVisible(false);
+
+        namespace timeLabel = bellMainStyle::mainWindow::timeLabel;
+        time = new gui::Label(this, timeLabel::posX, timeLabel::posY, timeLabel::width, timeLabel::height);
+        time->setFilled(false);
+        time->setBorderColor(gui::ColorNoColor);
+        time->setFont(timeLabel::font);
+        time->setAlignment(Alignment(gui::Alignment::Horizontal::Center, gui::Alignment::Vertical::Center));
+
+        namespace temperatureLabel = bellMainStyle::mainWindow::temperatureLabel;
+        temperature                = new gui::Label(
+            this, temperatureLabel::posX, temperatureLabel::posY, temperatureLabel::width, temperatureLabel::height);
+        temperature->setFilled(false);
+        temperature->setBorderColor(gui::ColorNoColor);
+        temperature->setFont(temperatureLabel::font);
+        temperature->setAlignment(Alignment(gui::Alignment::Horizontal::Center, gui::Alignment::Vertical::Center));
+        std::string degree_sign(temperatureLabel::degree);
+        temperature->setText("12 " + degree_sign + "C");
     }
 
     bool BellMainWindow::onInput(const InputEvent &inputEvent)
@@ -41,6 +62,17 @@ namespace gui
 
         // check if any of the lower inheritance onInput methods catch the event
         return AppWindow::onInput(inputEvent);
+    }
+
+    bool BellMainWindow::updateTime()
+    {
+        if (time != nullptr) {
+            utils::time::Timestamp timestamp{std::time(nullptr)};
+            time->setText(timestamp.str("%H:%M"));
+            return true;
+        }
+
+        return false;
     }
 
 } // namespace gui
