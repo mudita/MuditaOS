641ccd8f1d97c48f503e2ecf0038b810132b64bc
Radoslaw Wicik <radoslaw.wicik@mudita.com>
2021-06-23 15:11:45 +0200

    [BH-591] Add Spinner Widget
    Adding spinner widget.

--- module-gui/gui/Common.hpp
+++ module-gui/gui/Common.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -139,7 +139,13 @@ namespace gui
         BottomLeft  = 0x40,
         BottomRight = 0x80,
     };
-
+    enum class Boundaries
+    {
+        Fixed,     ///< Fixed - will stop scrolling on first or last elements on appropriate top or bottom
+                   ///< directions.
+        Continuous ///< Continuous - will continue to beginning or end on first or last elements on
+                   ///< appropriate top or bottom directions.
+    };
 } // namespace gui
 
 inline const char *c_str(gui::RefreshModes refresh)
