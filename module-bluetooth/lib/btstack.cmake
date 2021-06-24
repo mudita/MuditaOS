65dd3aebccc13eaa0e59dc79d681c9f8c1b7efab
Bartosz Cichocki <bartosz.cichocki@mudita.com>
2021-06-24 21:31:03 +0200

    [EGD-6972] Update BT stack to 1.4.1 version
    This PR updates the BT stack to the newest version

--- module-bluetooth/lib/btstack.cmake
+++ module-bluetooth/lib/btstack.cmake
@@ -203,11 +203,11 @@ set(BOARD_DIR_SOURCES
 if(${PROJECT_TARGET} STREQUAL "TARGET_Linux")
 message("Linux specyfic sources")
 message("----------------------")
-# include(${CMAKE_CURRENT_SOURCE_DIR}/targets/Target_Linux.cmake)
+ include(${CMAKE_CURRENT_SOURCE_DIR}/targets/Target_Linux.cmake)
 list(APPEND BOARD_DIR_SOURCES
     # ${BOARD_SOURCES}
     ${BT_STACK_ROOT}/platform/posix/btstack_stdin_posix.c
-    ${BT_STACK_ROOT}/platform/posix/btstack_uart_block_posix.c
+    ${BT_STACK_ROOT}/platform/posix/btstack_uart_posix.c
     ${BT_STACK_ROOT}/platform/posix/btstack_run_loop_posix.c
     ${BT_STACK_ROOT}/platform/posix/btstack_tlv_posix.c
     )
