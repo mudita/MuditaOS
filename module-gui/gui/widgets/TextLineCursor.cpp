92c6b124e7b3817f4f5903a57c03947af4d60e01
Piotr Tański <piotr.tanski@mudita.com>
2021-06-29 17:26:56 +0200

    [EGD-7047] Fixed navigation down through input texts
    Fixed navigation down through input texts while adding a new contact.

--- module-gui/gui/widgets/TextLineCursor.cpp
+++ module-gui/gui/widgets/TextLineCursor.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "TextLineCursor.hpp"
@@ -219,6 +219,10 @@ namespace gui
         }
 
         if (direction == NavigationDirection::DOWN) {
+
+            if (checkNextLineDocumentEnd(selectedLineNumber)) {
+                return TextCursor::Move::End;
+            }
 
             handleDownNavigation(selectedLineNumber, selectedLineCursorPosition);
 
