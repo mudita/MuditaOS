149514e8364c0753c9eec29d48b227fb85fc4bda
Jakub Pyszczak <pyszczakjakub@gmail.com>
2021-07-19 10:17:02 +0200

    [EGD-7169] Changed genlfs path
    Changed search for genlfs path from the project path to the build one.

--- generate_image.sh
+++ generate_image.sh
@@ -49,8 +49,7 @@ if [ ! $MTOOLS_OK ]; then
 	exit -1
 fi
 
-SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-GENLFS=$(find $SDIR -type f -iname genlittlefs -executable -print -quit)
+GENLFS=$(find $BUILDDIR -type f -iname genlittlefs -executable -print -quit)
 if [ -z ${GENLFS} ]; then
     echo "Error: Unable to find genlilttlefs..."
     exit -1
