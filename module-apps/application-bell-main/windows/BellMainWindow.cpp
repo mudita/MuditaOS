a912db548997bf89d875be3494c8d130a5a48f12
Pawel.Paprocki <pawel.paprocki@mudita.com>
2021-07-22 15:14:27 +0200

    [BH-677] Add widget AlarmSetSpinner
    Add widget for Bell alarm display and edit

--- module-apps/application-bell-main/windows/BellMainWindow.cpp
+++ module-apps/application-bell-main/windows/BellMainWindow.cpp
@@ -2,15 +2,15 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "BellMainWindow.hpp"
-
 #include <application-bell-main/ApplicationBellMain.hpp>
 #include <data/BellMainStyle.hpp>
-
 #include <gui/input/InputEvent.hpp>
 #include <i18n/i18n.hpp>
 #include <log/log.hpp>
-#include <service-appmgr/Controller.hpp>
 #include <time/time_conversion.hpp>
+
+static constexpr uint32_t mockHour   = 10;
+static constexpr uint32_t mockMinute = 15;
 
 namespace gui
 {
@@ -26,6 +26,25 @@ namespace gui
         statusBar->setVisible(false);
         header->setTitleVisibility(false);
         bottomBar->setVisible(false);
+
+        auto vBox = new gui::VBox(this,
+                                  bellMainStyle::mainWindow::body::posX,
+                                  bellMainStyle::mainWindow::body::posY,
+                                  style::window::default_body_width,
+                                  style::window::default_body_height);
+        vBox->setEdges(gui::RectangleEdge::None);
+
+        alarmSetSpinner = new AlarmSetSpinner(this, 0, 0, style::alarm_set_spinner::w, style::alarm_set_spinner::h);
+        alarmSetSpinner->setMinimumSize(style::window::default_body_width, style::alarm_set_spinner::h);
+        // for test purposes only
+        alarmSetSpinner->setHour(mockHour);
+        alarmSetSpinner->setMinute(mockMinute);
+
+        alarmSetSpinner->setFont(style::window::font::largelight);
+        alarmSetSpinner->setEditMode(EditMode::Browse);
+        alarmStatus = BellAlarm::Status::DEACTIVATED;
+        alarmSetSpinner->setAlarmStatus(alarmStatus);
+        vBox->addWidget(alarmSetSpinner);
 
         namespace timeLabel = bellMainStyle::mainWindow::timeLabel;
         time = new gui::Label(this, timeLabel::posX, timeLabel::posY, timeLabel::width, timeLabel::height);
@@ -43,6 +62,8 @@ namespace gui
         temperature->setAlignment(Alignment(gui::Alignment::Horizontal::Center, gui::Alignment::Vertical::Center));
         std::string degree_sign(temperatureLabel::degree);
         temperature->setText("12 " + degree_sign + "C");
+
+        setFocusItem(time);
     }
 
     bool BellMainWindow::onInput(const InputEvent &inputEvent)
@@ -51,18 +72,49 @@ namespace gui
         if (inputEvent.isShortRelease()) {
             switch (inputEvent.getKeyCode()) {
             case KeyCode::KEY_ENTER:
-                LOG_INFO("Open MainMenu");
-                application->switchWindow(gui::window::name::bell_main_menu, nullptr);
-                return true;
-            case KeyCode::KEY_RF:
-                return true;
+                return handleEnterKey(inputEvent);
+            case KeyCode::KEY_UP:
+            case KeyCode::KEY_DOWN:
+                handleEditModeEntry();
+                break;
+
             default:
                 break;
             }
         }
-
         // check if any of the lower inheritance onInput methods catch the event
         return AppWindow::onInput(inputEvent);
+    }
+
+    bool BellMainWindow::handleEnterKey(const InputEvent &inputEvent)
+    {
+        if (alarmEditMode) {
+            auto ret = AppWindow::onInput(inputEvent);
+            if (ret == false) {
+                // alarm setting finished
+                alarmEditMode = false;
+                setFocusItem(nullptr);
+                alarmSetSpinner->setEditMode(EditMode::Browse);
+                alarmStatus = BellAlarm::Status::ACTIVATED;
+                alarmSetSpinner->setAlarmStatus(alarmStatus);
+                return true;
+            }
+            return ret;
+        }
+        else {
+            LOG_INFO("Open MainMenu");
+            application->switchWindow(gui::window::name::bell_main_menu, nullptr);
+            return true;
+        }
+    }
+
+    void BellMainWindow::handleEditModeEntry()
+    {
+        if (!alarmEditMode) {
+            alarmEditMode = true;
+            setFocusItem(alarmSetSpinner);
+            alarmSetSpinner->setEditMode(EditMode::Edit);
+        }
     }
 
     bool BellMainWindow::updateTime()
