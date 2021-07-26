62b8dc031ab85f369965c4b8d862981850b127e2
Przemyslaw Brudny <przemyslaw.brudny@mudita.com>
2021-07-26 16:07:22 +0200

    [EGD-7215] Connected Sim contact import with backends
    Connected Sim contact import with cellular contact read
    and DB contacts write.

--- module-apps/application-settings/presenter/network/SimContactsImportWindowPresenter.cpp
+++ module-apps/application-settings/presenter/network/SimContactsImportWindowPresenter.cpp
@@ -4,16 +4,71 @@
 #include "SimContactsImportWindowPresenter.hpp"
 
 SimContactsImportWindowPresenter::SimContactsImportWindowPresenter(
-    std::shared_ptr<SimContactsImportModel> simContactsProvider)
-    : simContactsProvider{std::move(simContactsProvider)}
-{}
+    app::Application *application, std::shared_ptr<SimContactsImportModel> simContactsProvider)
+    : application(application), simContactsProvider{std::move(simContactsProvider)}
+{
+    onSave = [&]() {
+        this->simContactsProvider->clearData();
+        getView()->contactsImported();
+        this->application->refreshWindow(gui::RefreshModes::GUI_REFRESH_DEEP);
+        requestCompleted = true;
+    };
+
+    onDuplicatesCheck = [&](bool duplicatesFound) {
+        if (duplicatesFound) {
+            duplicatesChecked = true;
+            this->simContactsProvider->clearData();
+            getView()->displayDuplicatesCount(this->simContactsProvider->getDuplicatesCount());
+            this->application->refreshWindow(gui::RefreshModes::GUI_REFRESH_DEEP);
+        }
+        else {
+            this->simContactsProvider->saveData(onSave);
+        }
+        requestCompleted = true;
+    };
+
+    onSimContactsReady = [&]() {
+        this->simContactsProvider->createSimImported();
+        getView()->contactsReady();
+        this->application->refreshWindow(gui::RefreshModes::GUI_REFRESH_DEEP);
+    };
+}
 
 std::shared_ptr<gui::ListItemProvider> SimContactsImportWindowPresenter::getSimContactsProvider() const
 {
     return simContactsProvider;
 }
 
-void SimContactsImportWindowPresenter::clearProviderData() const
+void SimContactsImportWindowPresenter::eraseProviderData() const
 {
-    simContactsProvider->clearData();
+    simContactsProvider->eraseData();
+}
+
+void SimContactsImportWindowPresenter::saveImportedContacts()
+{
+    this->application->refreshWindow(gui::RefreshModes::GUI_REFRESH_DEEP);
+    requestCompleted = false;
+    if (!duplicatesChecked) {
+        simContactsProvider->findDuplicates(onDuplicatesCheck);
+    }
+    else {
+        simContactsProvider->saveData(onSave);
+    }
+}
+
+bool SimContactsImportWindowPresenter::isRequestCompleted()
+{
+    return requestCompleted;
+}
+
+void SimContactsImportWindowPresenter::requestSimContacts()
+{
+    simContactsProvider->requestSimContacts(onSimContactsReady);
+}
+
+void SimContactsImportWindowPresenter::requestDuplicates()
+{
+    this->simContactsProvider->createDuplicates();
+    getView()->displayDuplicates();
+    this->application->refreshWindow(gui::RefreshModes::GUI_REFRESH_DEEP);
 }
