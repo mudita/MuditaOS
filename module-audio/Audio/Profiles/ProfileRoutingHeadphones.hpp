de3d40a4a03f84e9e194fbc22c76b4fa60f60f87
Jakub Pyszczak <jakub.pyszczak@mudita.com>
2021-08-04 11:03:49 +0200

    [EGD-7235] Audio quality enhancement
    Improved audio quality by enabling
    eq and adjusting it's parameters.
    Proper gains set up as well.

--- module-audio/Audio/Profiles/ProfileRoutingHeadphones.hpp
+++ module-audio/Audio/Profiles/ProfileRoutingHeadphones.hpp
@@ -22,7 +22,18 @@ namespace audio
                           .outputVolume = static_cast<float>(volume),
                           .inputGain    = static_cast<float>(gain),
                           .inputPath    = audio::codec::InputPath::Headphones,
-                          .outputPath   = audio::codec::OutputPath::Headphones},
+                          .outputPath   = audio::codec::OutputPath::Headphones,
+                          .filterCoefficients =
+                              {qfilter_CalculateCoeffs(
+                                   audio::equalizer::FilterType::FilterHighPass, 997.f, 44100, 0.701f, 0),
+                               qfilter_CalculateCoeffs(
+                                   audio::equalizer::FilterType::FilterLowPass, 4993.7f, 44100, 0.701f, 0),
+                               qfilter_CalculateCoeffs(
+                                   audio::equalizer::FilterType::FilterNone, 15975.7f, 44100, 0.701f, -10),
+                               qfilter_CalculateCoeffs(
+                                   audio::equalizer::FilterType::FilterNone, 200.4f, 44100, 0.701f, -10),
+                               qfilter_CalculateCoeffs(
+                                   audio::equalizer::FilterType::FilterNone, 1496.7f, 44100, 0.701f, -4)}},
                       AudioDevice::Type::Audiocodec)
         {}
     };
