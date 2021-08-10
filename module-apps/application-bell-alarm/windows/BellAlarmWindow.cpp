da198086f0363d403d701a190496300972b88c9f
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-08-10 15:56:05 +0200

    [BH-701] Use TimeSetFmtSpinner in Alarm app
    Use TimeSetFmtSpinner in Alarm app

--- module-apps/application-bell-alarm/windows/BellAlarmWindow.cpp
+++ module-apps/application-bell-alarm/windows/BellAlarmWindow.cpp
@@ -3,25 +3,48 @@
 
 #include "BellAlarmWindow.hpp"
 
+#include <apps-common/widgets/BellBaseLayout.hpp>
+#include <apps-common/widgets/TimeSetFmtSpinner.hpp>
+#include <data/BellAlarmStyle.hpp>
 #include <module-gui/gui/input/InputEvent.hpp>
 
 namespace gui
 {
-    BellAlarmWindow::BellAlarmWindow(app::Application *app, std::string name) : AppWindow(app, std::move(name))
+    BellAlarmWindow::BellAlarmWindow(
+        app::Application *app,
+        std::unique_ptr<app::bell_alarm::BellAlarmWindowContract::Presenter> &&windowPresenter,
+        std::string name)
+        : AppWindow(app, std::move(name)), presenter{std::move(windowPresenter)}
     {
+        presenter->attach(this);
         buildInterface();
     }
 
     void BellAlarmWindow::buildInterface()
     {
         AppWindow::buildInterface();
+
         statusBar->setVisible(false);
         header->setTitleVisibility(true);
         bottomBar->setVisible(false);
+
+        body              = new BellBaseLayout(this, 0, 0, style::window_width, style::window_height);
+        timeSetFmtSpinner = new TimeSetFmtSpinner(body->centerBox);
+        timeSetFmtSpinner->setFont(bell_alarm_style::time_set_fmt_spinner::font);
+
+        setFocusItem(body);
     }
 
     bool BellAlarmWindow::onInput(const InputEvent &inputEvent)
     {
+        if (body->onInput(inputEvent)) {
+            return true;
+        }
+        if (inputEvent.isShortRelease(KeyCode::KEY_ENTER)) {
+            presenter->saveData();
+            application->returnToPreviousWindow();
+            return true;
+        }
         return AppWindow::onInput(inputEvent);
     }
 
--- module-apps/application-bell-alarm/windows/BellAlarmWindow.hpp
+++ module-apps/application-bell-alarm/windows/BellAlarmWindow.hpp
@@ -4,18 +4,28 @@
 #pragma once
 
 #include <application-bell-alarm/ApplicationBellAlarm.hpp>
+#include <application-bell-alarm/presenter/BellAlarmWindowPresenter.hpp>
 
 #include <apps-common/windows/AppWindow.hpp>
 
 namespace gui
 {
-    class BellAlarmWindow : public AppWindow
+    class BellBaseLayout;
+    class TimeSetFmtSpinner;
+    class BellAlarmWindow : public AppWindow, public app::bell_alarm::BellAlarmWindowContract::View
     {
       public:
-        explicit BellAlarmWindow(app::Application *app, std::string name = window::name::bellAlarm);
+        explicit BellAlarmWindow(app::Application *app,
+                                 std::unique_ptr<app::bell_alarm::BellAlarmWindowContract::Presenter> &&windowPresenter,
+                                 std::string name = window::name::bellAlarm);
 
         void buildInterface() override;
         bool onInput(const InputEvent &inputEvent) override;
         void rebuild() override;
+
+      private:
+        BellBaseLayout *body{nullptr};
+        TimeSetFmtSpinner *timeSetFmtSpinner{nullptr};
+        std::unique_ptr<app::bell_alarm::BellAlarmWindowContract::Presenter> presenter;
     };
 } /* namespace gui */
