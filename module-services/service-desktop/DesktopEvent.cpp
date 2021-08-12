1b5b1751de059be8a3f53a537e85d280c6f82921
Artur Śleszyński <artur.sleszynski@mudita.com>
2021-08-12 11:40:53 +0200

    [CP-475] Make CenterPure devs owners of service-desktop
    Let CenterPure devs police service desktop to catch breaking changes
    regaring CP app communications.

--- module-services/service-desktop/DesktopEvent.cpp
+++ module-services/service-desktop/DesktopEvent.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
 #include "service-desktop/DesktopEvent.hpp"
 #include <service-desktop/parser/MessageHandler.hpp>
 
