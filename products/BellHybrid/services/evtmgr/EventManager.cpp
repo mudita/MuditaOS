952c5b0b66f9f55c2fa88faa33c107a4a7329a16
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2021-08-17 00:03:00 +0200

    [BH-774] Split event worker
    Split event worker to independetly customize events for products.
    
    Signed-off-by: Marcin Smoczyński <smoczynski.marcin@gmail.com>

--- products/BellHybrid/services/evtmgr/EventManager.cpp
+++ products/BellHybrid/services/evtmgr/EventManager.cpp
@@ -2,28 +2,3 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include <evtmgr/EventManager.hpp>
-
-#include <service-evtmgr/WorkerEvent.hpp>
-
-sys::ReturnCodes EventManager::InitHandler()
-{
-    EventManagerCommon::InitHandler();
-
-    using namespace std::string_literals;
-    std::list<sys::WorkerQueueInfo> list;
-    list.emplace_back("qIrq"s, sizeof(uint8_t), 10);
-    list.emplace_back("qHeadset"s, sizeof(uint8_t), 10);
-    list.emplace_back("qBattery"s, sizeof(uint8_t), 10);
-    list.emplace_back("qRTC"s, sizeof(uint8_t), 20);
-    list.emplace_back("qSIM"s, sizeof(uint8_t), 5);
-    list.emplace_back("qMagnetometer"s, sizeof(uint8_t), 5);
-    list.emplace_back(WorkerEvent::MagnetometerNotifyQueue, sizeof(uint8_t), 1);
-    list.emplace_back("qTorch"s, sizeof(uint8_t), 5);
-    list.emplace_back("qLightSensor"s, sizeof(uint8_t), 5);
-    list.emplace_back("qChargerDetect"s, sizeof(uint8_t), 5);
-
-    EventWorker->init(list, settings);
-    EventWorker->run();
-
-    return sys::ReturnCodes::Success;
-}
