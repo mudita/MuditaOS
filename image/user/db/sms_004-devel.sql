d3d207610dd90bedf848534f25adef0eb790b7a3
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2021-08-24 16:23:08 +0200

    [EGD-7395] Fix no templates
    Fix an inability to reject a call with a message because there were no
    templates available.
    
    Signed-off-by: Marcin Smoczyński <smoczynski.marcin@gmail.com>

--- image/user/db/sms_004-devel.sql
+++ image/user/db/sms_004-devel.sql
@@ -7,11 +7,7 @@ INSERT OR REPLACE INTO "sms" ("_id","thread_id","contact_id","date","error_code"
 INSERT OR REPLACE INTO "sms" ("_id","thread_id","contact_id","date","error_code","body","type") VALUES (4,1,1,1618907732,2,'I hope you''re feeling better now...',8);
 INSERT OR REPLACE INTO "sms" ("_id","thread_id","contact_id","date","error_code","body","type") VALUES (5,1,1,1618907733,2,'Thanks :) Today is better than yesterday',4);
 INSERT OR REPLACE INTO "sms" ("_id","thread_id","contact_id","date","error_code","body","type") VALUES (6,1,1,1618907734,2,'I''m happy to hear that :)',8);
-INSERT OR REPLACE INTO "templates" ("_id","text","lastUsageTimestamp") VALUES (1,'Thanks for reaching out. I can''t talk right now, I''ll call you later',4);
-INSERT OR REPLACE INTO "templates" ("_id","text","lastUsageTimestamp") VALUES (2,'I''ll call you later',3);
-INSERT OR REPLACE INTO "templates" ("_id","text","lastUsageTimestamp") VALUES (3,'I''ll be there in 15 minutes',2);
-INSERT OR REPLACE INTO "templates" ("_id","text","lastUsageTimestamp") VALUES (4,'Some test template number, which is too long to be displayed.',1);
-INSERT OR REPLACE INTO "templates" ("_id","text","lastUsageTimestamp") VALUES (5,'Give me 5 minutes',5);
+INSERT OR REPLACE INTO "templates" ("_id","text","lastUsageTimestamp") VALUES (5,'Some test template number, which is too long to be displayed.',1);
 INSERT OR REPLACE INTO "threads" ("_id","date","msg_count","read","contact_id","number_id","snippet","last_dir") VALUES (1,1618907734,3,1,1,1,'I''m happy to hear that :)',4);
 INSERT OR REPLACE INTO "threads" ("_id","date","msg_count","read","contact_id","number_id","snippet","last_dir") VALUES (2,1547492321,2,0,2,2,'It was great seeing you too :*',4);
 COMMIT;
