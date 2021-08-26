20f9c49cc414c1f437f978973b0d5fbd6acbd2fa
Przemyslaw Brudny <przemyslaw.brudny@mudita.com>
2021-08-26 12:10:13 +0200

    [BH-809] Split application builds per product
    PR fixes.

--- products/BellHybrid/AppSelector.cmake
+++ products/BellHybrid/AppSelector.cmake
@@ -1,4 +1,4 @@
-option(ENABLE_APP_BELL_ALARM "Enable application bell alarm" ON)
-option(ENABLE_APP_BELL_MAIN "Enable application bell main" ON)
-option(ENABLE_APP_BELL_SETTINGS "Enable application bell settings" ON)
-option(ENABLE_APP_SPECIAL_INPUT "Enable application special input" ON)
+set(ENABLE_APP_BELL_ALARM ON CACHE INTERNAL "Enable application bell alarm")
+set(ENABLE_APP_BELL_MAIN ON CACHE INTERNAL "Enable application bell main")
+set(ENABLE_APP_BELL_SETTINGS ON CACHE INTERNAL "Enable application bell settings")
+set(ENABLE_APP_SPECIAL_INPUT ON CACHE INTERNAL "Enable application special input")
