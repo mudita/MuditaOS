80f84ad5de1698db63174f28d3de525dece4abc0
Alek Rudnik <aleksander.rudnik@mudita.com>
2021-08-30 23:15:29 +0200

    [EGD-6836] Add multimedia indexer database
    Added multimedia indexer database.
    Decided to got with single table approach.
    User playlist will be implemented as seprate
    tables when needed.

---
+++ module-db/Databases/MultimediaFilesDB.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "MultimediaFilesDB.hpp"
+
+MultimediaFilesDB::MultimediaFilesDB(const char *name) : Database(name), files(this)
+{}
