5281f3d0c81d27dca99a0a01ef3e2c316c6c5f22
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-09-01 10:50:40 +0200

    [BH-836] Add missing items to Settings window
    Add missing items to Settings window

---
+++ products/BellHybrid/apps/application-bell-settings/windows/BellSettingsHomeViewWindow.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "BellSettingsHomeViewWindow.hpp"
+
+#include <application-bell-settings/ApplicationBellSettings.hpp>
+
+namespace gui
+{
+    BellSettingsHomeViewWindow::BellSettingsHomeViewWindow(app::Application *app, std::string name)
+        : AppWindow(app, std::move(name))
+    {
+        buildInterface();
+    }
+
+    void BellSettingsHomeViewWindow::rebuild()
+    {
+        erase();
+        buildInterface();
+    }
+
+    void BellSettingsHomeViewWindow::buildInterface()
+    {
+        AppWindow::buildInterface();
+        statusBar->setVisible(false);
+        header->setTitleVisibility(false);
+        bottomBar->setVisible(false);
+    }
+} /* namespace gui */
