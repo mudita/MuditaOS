40a0b83e1ae178ed682fa4892ae842e12e46dc48
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2021-09-03 11:11:00 +0200

    [EGD-7334] Add initial startup indexer
    Add initial startup indexer which index all music
    files when the phone is run first time.
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>

--- module-services/service-fileindexer/StartupIndexer.hpp
+++ module-services/service-fileindexer/StartupIndexer.hpp
@@ -5,51 +5,37 @@
 
 #include <Service/Service.hpp>
 #include <module-sys/Timers/TimerHandle.hpp>
+#include <filesystem>
 
-#include <list>
-
-namespace service::msg
-{
-    class FileChangeMessage;
-}
 namespace service::detail
 {
-    // RFC6838 IANA MIME types
-    enum class mimeType
-    {
-        _none_,
-        application,
-        audio,
-        example,
-        font,
-        image,
-        text,
-        video
-    };
     class StartupIndexer
     {
-        static constexpr auto timer_indexing_time = 100U;
 
       public:
         StartupIndexer()                       = default;
         ~StartupIndexer()                      = default;
         StartupIndexer(const StartupIndexer &) = delete;
         StartupIndexer &operator=(StartupIndexer) = delete;
-        auto start(std::shared_ptr<sys::Service> svc, std::string_view svc_name) -> void
-        {
-            collectStartupFiles();
-            setupTimers(svc, svc_name);
-        }
-        static auto getFileType(std::string_view path) -> mimeType;
+        auto start(std::shared_ptr<sys::Service> svc, std::string_view svc_name) -> void;
 
       private:
-        // Collect startup files when service starts
-        auto collectStartupFiles() -> void;
+        // Process single entry
+        static auto processEntry(std::shared_ptr<sys::Service> svc,
+                                 const std::filesystem::recursive_directory_iterator::value_type &entry) -> void;
         // Setup timers for notification
         auto setupTimers(std::shared_ptr<sys::Service> svc, std::string_view svc_name) -> void;
+        // On timer timeout
+        auto onTimerTimeout(std::shared_ptr<sys::Service> svc) -> void;
+        // Create lock file
+        static auto createLockFile() -> bool;
+        //  Check if lock file exists
+        static auto hasLockFile() -> bool;
 
       private:
-        std::list<std::shared_ptr<msg::FileChangeMessage>> mMsgs;
+        std::vector<std::string>::const_iterator mTopDirIterator;
+        std::filesystem::recursive_directory_iterator mSubDirIterator;
         sys::TimerHandle mIdxTimer;
+        bool mStarted{};
     };
 } // namespace service::detail
