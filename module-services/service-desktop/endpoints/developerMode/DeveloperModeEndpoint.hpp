9f99bd87ea1c0e05c21858e09d006f08cf6461e8
Jakub Pyszczak <jakub.pyszczak@mudita.com>
2021-09-06 15:23:33 +0200

    [EGD-7505] Developer mode endpoint extended
    Developer mode endpoint extender to support
    file renaming, removal and listing dirs.
    Harness requirements updated.

--- module-services/service-desktop/endpoints/developerMode/DeveloperModeEndpoint.hpp
+++ module-services/service-desktop/endpoints/developerMode/DeveloperModeEndpoint.hpp
@@ -5,6 +5,7 @@
 
 #include "DeveloperModeHelper.hpp"
 #include "Mode/UI_Helper.hpp"
+#include "fs/FS_Helper.hpp"
 #include <endpoints/Endpoint.hpp>
 #include <parser/ParserUtils.hpp>
 
@@ -27,11 +28,13 @@ class DeveloperModeEndpoint : public parserFSM::Endpoint
   private:
     const std::unique_ptr<parserFSM::DeveloperModeHelper> helper;
     const std::unique_ptr<parserFSM::UI_Helper> uiHelper;
+    const std::unique_ptr<parserFSM::FS_Helper> fsHelper;
 
   public:
     explicit DeveloperModeEndpoint(sys::Service *_ownerServicePtr)
         : Endpoint(_ownerServicePtr), helper(std::make_unique<parserFSM::DeveloperModeHelper>(ownerServicePtr)),
-          uiHelper(std::make_unique<parserFSM::UI_Helper>(ownerServicePtr))
+          uiHelper(std::make_unique<parserFSM::UI_Helper>(ownerServicePtr)),
+          fsHelper(std::make_unique<parserFSM::FS_Helper>(ownerServicePtr))
     {
         debugName = "DeveloperModeEndpoint";
     }
