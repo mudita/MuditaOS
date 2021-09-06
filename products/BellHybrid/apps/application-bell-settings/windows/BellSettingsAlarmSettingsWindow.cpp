0c073ac5b91c3150697da2cdc39230e3f532db38
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-09-06 16:03:16 +0200

    [BH-835] Add Alarm Settings menu window
    Add alarm settings options in `BellSettingsAlarmSettingsWindow`

--- products/BellHybrid/apps/application-bell-settings/windows/BellSettingsAlarmSettingsWindow.cpp
+++ products/BellHybrid/apps/application-bell-settings/windows/BellSettingsAlarmSettingsWindow.cpp
@@ -4,12 +4,16 @@
 #include "BellSettingsAlarmSettingsWindow.hpp"
 
 #include <application-bell-settings/ApplicationBellSettings.hpp>
+#include <apps-common/messages/DialogMetadataMessage.hpp>
+#include <apps-common/options/type/OptionBellMenu.hpp>
+#include <data/BellSettingsStyle.hpp>
 
 namespace gui
 {
     BellSettingsAlarmSettingsWindow::BellSettingsAlarmSettingsWindow(app::Application *app, std::string name)
         : OptionWindow(app, std::move(name))
     {
+        addOptions(alarmSettingsOptionsList());
         buildInterface();
     }
 
@@ -21,9 +25,47 @@ namespace gui
 
     void BellSettingsAlarmSettingsWindow::buildInterface()
     {
-        AppWindow::buildInterface();
+        header->setTitle(title);
+        header->setEdges(RectangleEdge::None);
         statusBar->setVisible(false);
-        header->setTitleVisibility(false);
         bottomBar->setVisible(false);
+
+        optionsList->setPosition(bell_settings_style::alarm_settings_window::options_list_margin_x,
+                                 bell_settings_style::alarm_settings_window::options_list_margin_y);
+        optionsList->setMaximumWidth(bell_settings_style::alarm_settings_window::default_body_width);
+        optionsList->setBoundaries(gui::Boundaries::Fixed);
+    }
+
+    std::list<Option> BellSettingsAlarmSettingsWindow::alarmSettingsOptionsList()
+    {
+        std::list<gui::Option> alarmSettingsOptionsList;
+        auto addAlarmSettingsOption = [&](const UTF8 &name, const std::string &window) {
+            alarmSettingsOptionsList.emplace_back(std::make_unique<gui::option::OptionBellMenu>(
+                name,
+                [=](gui::Item &item) {
+                    if (window.empty()) {
+                        return false;
+                    }
+                    application->switchWindow(
+                        window,
+                        gui::ShowMode::GUI_SHOW_INIT,
+                        std::make_unique<gui::DialogMetadataMessage>(gui::DialogMetadata{name, "search_big", " "}));
+                    return true;
+                },
+                [=](gui::Item &item) {
+                    // put focus change callback here
+                    return true;
+                },
+                this));
+        };
+
+        addAlarmSettingsOption(utils::translate("app_bell_settings_alarm_settings_prewake_up"),
+                               gui::window::name::bellSettingsAlarmSettingsPrewakeUp);
+        addAlarmSettingsOption(utils::translate("app_bell_settings_alarm_settings_alarm_tone_and_light"),
+                               gui::window::name::bellSettingsAlarmSettingsAlarmToneAndLight);
+        addAlarmSettingsOption(utils::translate("app_bell_settings_alarm_settings_snooze"),
+                               gui::window::name::bellSettingsAlarmSettingsSnooze);
+
+        return alarmSettingsOptionsList;
     }
 } /* namespace gui */
