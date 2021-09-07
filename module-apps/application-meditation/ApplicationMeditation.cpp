8abc69620829528d7b2bf7399fde1068cd8bbdda
Piotr Tański <piotr.tanski@mudita.com>
2021-09-07 12:50:36 +0200

    [EGD-7260] Fixed meditation timer options
    Fixed meditation timer options displaying.

--- module-apps/application-meditation/ApplicationMeditation.cpp
+++ module-apps/application-meditation/ApplicationMeditation.cpp
@@ -2,7 +2,7 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "ApplicationMeditation.hpp"
-#include "MeditationListViewWindows.hpp"
+#include "MeditationOptionsWindows.hpp"
 #include "MeditationTimerWindow.hpp"
 #include "MeditationWindow.hpp"
 #include "Names.hpp"
