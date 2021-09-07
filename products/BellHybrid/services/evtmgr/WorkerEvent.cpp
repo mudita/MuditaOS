6d1ec66cb4ffcc0f16c0791b5abdb8ceb0e6958f
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2021-09-07 21:19:41 +0200

    [BH-510] Knob driver
    Add knob driver for the rotary encoder.
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>

--- products/BellHybrid/services/evtmgr/WorkerEvent.cpp
+++ products/BellHybrid/services/evtmgr/WorkerEvent.cpp
@@ -6,6 +6,7 @@
 #include <bsp/eink_frontlight/eink_frontlight.hpp>
 #include <service-audio/AudioMessage.hpp>
 #include <service-evtmgr/EVMessages.hpp>
+#include <bsp/rotary_encoder/rotary_encoder.hpp>
 
 namespace bell
 {
@@ -14,21 +15,40 @@ namespace bell
     {}
 
     void WorkerEvent::addProductQueues(std::list<sys::WorkerQueueInfo> &queuesList)
-    {}
+    {
+        constexpr auto elementSize = sizeof(std::uint8_t);
+        queuesList.emplace_back(rotaryEncoderQueueName, elementSize, rotaryEncoderQueueSize);
+    }
 
     void WorkerEvent::initProductHardware()
     {
         bsp::eink_frontlight::init();
+        bsp::rotary_encoder::init(queues[static_cast<int32_t>(EventQueues::queueRotaryEncoder)]->GetQueueHandle());
     }
 
     bool WorkerEvent::handleMessage(std::uint32_t queueID)
     {
-
+        auto &queue = queues[queueID];
+        if (queueID == static_cast<uint32_t>(EventQueues::queueRotaryEncoder)) {
+            uint8_t notification;
+            if (!queue->Dequeue(&notification, 0)) {
+                return false;
+            }
+            handleRotaryEncoderEvent();
+        }
         return WorkerEventCommon::handleMessage(queueID);
     }
 
     void WorkerEvent::deinitProductHardware()
     {
         bsp::eink_frontlight::deinit();
+        bsp::rotary_encoder::deinit();
+    }
+
+    void WorkerEvent::handleRotaryEncoderEvent()
+    {
+        if (const auto &key = bsp::rotary_encoder::WorkerEventHandler(); key.has_value()) {
+            LOG_DEBUG("Rotary encoder handler %i", int(key.value()));
+        }
     }
 } // namespace bell
