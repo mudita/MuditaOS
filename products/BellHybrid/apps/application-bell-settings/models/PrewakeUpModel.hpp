be21cb06967afcb8eb3c9dfbeda7bda346ce5b7e
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-09-07 13:45:56 +0200

    [BH-823] Pre-wake up settings - part I
    Prepare MVP classes for `BellSettingsPrewakeUpWindow`

---
+++ products/BellHybrid/apps/application-bell-settings/models/PrewakeUpModel.hpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <apps-common/Application.hpp>
+#include <apps-common/InternalModel.hpp>
+
+namespace app::bell_settings
+{
+    class PrewakeUpModel : public app::InternalModel<gui::ListItem *>, public gui::ListItemProvider
+    {
+      public:
+        explicit PrewakeUpModel(app::Application *app);
+
+        ~PrewakeUpModel();
+
+        auto clearData() -> void;
+
+        auto saveData() -> void;
+
+        auto loadData() -> void;
+
+        auto createData() -> void;
+
+        void requestRecords(uint32_t offset, uint32_t limit) override;
+
+        [[nodiscard]] auto getItem(gui::Order order) -> gui::ListItem * override;
+
+        [[nodiscard]] auto requestRecordsCount() -> unsigned int override;
+
+        [[nodiscard]] auto getMinimalItemSpaceRequired() const -> unsigned int override;
+
+      private:
+        app::Application *application{};
+    };
+} // namespace app::bell_settings
