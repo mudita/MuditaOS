4f300e67a60129b2414a0059a9be227a3b5ee887
Mateusz Grzegorzek <mateusz.grzegorzek@mudita.com>
2021-09-15 17:32:12 +0200

    [BH-873] Activate alarm in backend on Alarm activation
    Activate alarm in backend on Alarm activation

--- products/BellHybrid/apps/common/src/popups/AlarmActivatedWindow.cpp
+++ products/BellHybrid/apps/common/src/popups/AlarmActivatedWindow.cpp
@@ -29,6 +29,15 @@ namespace gui
         Dialog::onBeforeShow(mode, msg.get());
     }
 
+    void AlarmActivatedWindow::activateAlarm(AlarmEventRecord alarmEvent)
+    {
+        alarmEvent.enabled = true;
+        auto task          = app::AsyncRequest::createFromMessage(
+            std::make_unique<alarms::AlarmUpdateRequestMessage>(alarmEvent), service::name::service_time);
+
+        task->execute(this->application, this);
+    }
+
     bool AlarmActivatedWindow::onAlarmResponseMessage(sys::ResponseMessage *response, ShowMode mode)
     {
         auto result = dynamic_cast<alarms::AlarmGetNextSingleEventsResponseMessage *>(response);
@@ -41,18 +50,30 @@ namespace gui
             LOG_WARN("There is no event!");
             return false;
         }
-        const auto &startDate = singleEvents.front().startDate;
+        const auto &singleEventRecord = singleEvents.front();
+        const auto &startDate         = singleEventRecord.startDate;
         LOG_DEBUG("Alarm time: %s", TimePointToString(startDate).c_str());
         const auto alarmTime = std::chrono::system_clock::to_time_t(startDate);
+        showAlarmTime(mode, alarmTime);
+        const auto alarmEventRecord = dynamic_cast<AlarmEventRecord *>(singleEventRecord.parent.get());
+        if (alarmEventRecord) {
+            activateAlarm(*alarmEventRecord);
+        }
+
+        statusBar->setVisible(false);
+        header->setTitleVisibility(false);
+        bottomBar->setActive(BottomBar::Side::RIGHT, false);
+
+        return true;
+    }
+
+    void AlarmActivatedWindow::showAlarmTime(ShowMode mode, time_t alarmTime)
+    {
         DialogMetadata metaData;
         metaData.icon = "bell_alarm_activated";
         metaData.text = utils::time::getBottomDescription(
             utils::time::calculateTimeDifference(alarmTime, utils::time::getCurrentTime()));
         auto msg = std::make_unique<DialogMetadataMessage>(std::move(metaData));
         Dialog::onBeforeShow(mode, msg.get());
-        statusBar->setVisible(false);
-        header->setTitleVisibility(false);
-        bottomBar->setActive(BottomBar::Side::RIGHT, false);
-        return true;
     }
 } /* namespace gui */
