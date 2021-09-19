3000350ccc5eb718953faceb9e1822168b2bcfbc
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2021-09-19 16:08:30 +0200

    [EGD-6630] Integration file indexer with music player
    The list of music tracks is retrieved from the database,
    where they are indexed beforehand,
    instead of being searched in real time in the file system.

--- module-apps/application-music-player/tests/unittest_songsmodel.cpp
+++ module-apps/application-music-player/tests/unittest_songsmodel.cpp
@@ -17,30 +17,19 @@ using ::testing::app::music_player::MockSongsRepository;
 TEST(SongsModel, Init)
 {
     auto mockRepo = std::make_shared<MockSongsRepository>();
-    auto model    = SongsModel(mockRepo);
+    auto model    = SongsModel(nullptr, mockRepo);
 
-    EXPECT_EQ(model.requestRecordsCount(), 0);
     EXPECT_FALSE(model.isSongPlaying());
 }
 
 TEST(SongsModel, EmptyContext)
 {
     auto mockRepo = std::make_shared<MockSongsRepository>();
-    auto model    = SongsModel(mockRepo);
+    auto model    = SongsModel(nullptr, mockRepo);
 
     auto ctx = model.getCurrentSongContext();
 
     EXPECT_EQ(ctx.currentFileToken, std::nullopt);
     EXPECT_TRUE(ctx.filePath.empty());
     EXPECT_EQ(ctx.currentSongState, app::music_player::SongState::NotPlaying);
-}
-
-TEST(SongsModel, createDataNoSongs)
-{
-    auto mockRepo = std::make_shared<MockSongsRepository>();
-    auto model    = SongsModel(mockRepo);
-
-    EXPECT_CALL(*mockRepo, scanMusicFilesList);
-    EXPECT_CALL(*mockRepo, getMusicFilesList).WillRepeatedly(Return(std::vector<tags::fetcher::Tags>()));
-    model.createData([](const std::string &) { return true; }, []() {}, [](const UTF8 &) {}, []() {});
 }
