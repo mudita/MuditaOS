028e23cc5d084b9958fc3e3c90a43215e7985eb2
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2021-09-20 08:34:32 +0200

    fix bell build

--- products/BellHybrid/services/evtmgr/EventManager.cpp
+++ products/BellHybrid/services/evtmgr/EventManager.cpp
@@ -6,7 +6,7 @@
 #include "WorkerEvent.hpp"
 #include <evtmgr/EventManager.hpp>
 #include <keymap/KeyMap.hpp>
-#include <module-bsp/hal/temperature_source/TemperatureSource.hpp>
+#include <hal/temperature_source/TemperatureSource.hpp>
 #include <module-sys/SystemManager/Constants.hpp>
 #include <screen-light-control/ScreenLightControl.hpp>
 #include <service-evtmgr/KbdMessage.hpp>
