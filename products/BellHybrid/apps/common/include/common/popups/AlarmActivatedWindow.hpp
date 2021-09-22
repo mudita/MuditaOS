5ea9f240c36dad4b6599556742133ad868e90270
Mateusz Piesta <mateusz.piesta@gmail.com>
2021-09-22 09:58:30 +0200

    [BH-891] Alarm popup fix
    Fixed bug and added proper handling
    of timer/input keys.

--- products/BellHybrid/apps/common/include/common/popups/AlarmActivatedWindow.hpp
+++ products/BellHybrid/apps/common/include/common/popups/AlarmActivatedWindow.hpp
@@ -3,22 +3,29 @@
 
 #pragma once
 
-#include <apps-common/windows/Dialog.hpp>
+#include <apps-common/popups/WindowWithTimer.hpp>
 #include <AsyncTask.hpp>
 
 struct AlarmEventRecord;
 
 namespace gui
 {
-    class AlarmActivatedWindow : public Dialog, public app::AsyncCallbackReceiver
+    class Icon;
+
+    class AlarmActivatedWindow : public WindowWithTimer, public app::AsyncCallbackReceiver
     {
       public:
         explicit AlarmActivatedWindow(app::ApplicationCommon *app);
-        void onBeforeShow(ShowMode mode, SwitchData *data) override;
 
       private:
-        void activateAlarm(AlarmEventRecord alarmEvent);
+        bool onInput(const InputEvent &inputEvent) override;
+        void buildInterface() override;
+        void onBeforeShow(ShowMode mode, SwitchData *data) override;
+        void activateAlarm(AlarmEventRecord &alarmEvent);
         bool onAlarmResponseMessage(sys::ResponseMessage *response, ShowMode mode);
         void showAlarmTime(ShowMode mode, time_t alarmTime);
+        void returnToPreviousWindow();
+
+        Icon *icon{};
     };
 } /* namespace gui */
