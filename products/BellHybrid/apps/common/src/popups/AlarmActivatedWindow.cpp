c8e65453c5b2ce03a7f87609ed1a729dddc470de
Mateusz Piesta <mateusz.piesta@gmail.com>
2021-09-22 09:58:30 +0200

    [BH-891] Alarm popup fix
    Fixed bug and added proper handling
    of timer/input keys.

--- products/BellHybrid/apps/common/src/popups/AlarmActivatedWindow.cpp
+++ products/BellHybrid/apps/common/src/popups/AlarmActivatedWindow.cpp
@@ -2,34 +2,39 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include <ApplicationCommon.hpp>
-#include <apps-common/messages/DialogMetadataMessage.hpp>
+#include <apps-common/popups/data/PopupRequestParams.hpp>
 #include <common/TimeUtils.hpp>
 #include <common/popups/AlarmActivatedWindow.hpp>
 #include <popups/Popups.hpp>
 #include <service-time/AlarmMessage.hpp>
 #include <service-time/Constants.hpp>
+#include <service-appmgr/Controller.hpp>
+#include <gui/input/InputEvent.hpp>
+#include <gui/widgets/Icon.hpp>
 
 namespace gui
 {
     AlarmActivatedWindow::AlarmActivatedWindow(app::ApplicationCommon *app)
-        : Dialog(app, popup::window::alarm_activated_window), app::AsyncCallbackReceiver{app}
-    {}
-
-    void AlarmActivatedWindow::onBeforeShow(ShowMode mode, [[maybe_unused]] SwitchData *data)
+        : WindowWithTimer(app, popup::window::alarm_activated_window), app::AsyncCallbackReceiver{app}
     {
+        buildInterface();
+        timerCallback = [this](Item &, sys::Timer &) {
+            returnToPreviousWindow();
+            return true;
+        };
+    }
+
+    void AlarmActivatedWindow::onBeforeShow(ShowMode mode, SwitchData *data)
+    {
+        WindowWithTimer::onBeforeShow(mode, data);
         auto task = app::AsyncRequest::createFromMessage(
             std::make_unique<alarms::AlarmGetNextSingleEventsRequestMessage>(), service::name::service_time);
 
         auto onResponseCallback = [this, mode](auto response) { return onAlarmResponseMessage(response, mode); };
         task->execute(this->application, this, onResponseCallback);
-
-        DialogMetadata metaData;
-        metaData.icon = "bell_alarm_activated";
-        auto msg      = std::make_unique<DialogMetadataMessage>(std::move(metaData));
-        Dialog::onBeforeShow(mode, msg.get());
     }
 
-    void AlarmActivatedWindow::activateAlarm(AlarmEventRecord alarmEvent)
+    void AlarmActivatedWindow::activateAlarm(AlarmEventRecord &alarmEvent)
     {
         alarmEvent.enabled = true;
         auto task          = app::AsyncRequest::createFromMessage(
@@ -56,24 +61,43 @@ namespace gui
         const auto alarmTime = std::chrono::system_clock::to_time_t(startDate);
         showAlarmTime(mode, alarmTime);
         const auto alarmEventRecord = dynamic_cast<AlarmEventRecord *>(singleEventRecord.parent.get());
-        if (alarmEventRecord) {
+        if (alarmEventRecord != nullptr) {
             activateAlarm(*alarmEventRecord);
         }
-
-        statusBar->setVisible(false);
-        header->setTitleVisibility(false);
-        bottomBar->setActive(BottomBar::Side::RIGHT, false);
 
         return true;
     }
 
-    void AlarmActivatedWindow::showAlarmTime(ShowMode mode, time_t alarmTime)
+    void AlarmActivatedWindow::showAlarmTime(ShowMode, time_t alarmTime)
     {
-        DialogMetadata metaData;
-        metaData.icon = "bell_alarm_activated";
-        metaData.text = utils::time::getBottomDescription(
-            utils::time::calculateTimeDifference(alarmTime, utils::time::getCurrentTime()));
-        auto msg = std::make_unique<DialogMetadataMessage>(std::move(metaData));
-        Dialog::onBeforeShow(mode, msg.get());
+        icon->text->setText(utils::time::getBottomDescription(
+            utils::time::calculateTimeDifference(alarmTime, utils::time::getCurrentTime())));
+    }
+    void AlarmActivatedWindow::buildInterface()
+    {
+        WindowWithTimer::buildInterface();
+
+        statusBar->setVisible(false);
+        header->setTitleVisibility(false);
+        bottomBar->setVisible(false);
+
+        icon = new Icon(this, 0, 0, style::window_width, style::window_height, "bell_alarm_activated", {});
+        icon->text->setFont(style::window::font::verybiglight);
+    }
+    void AlarmActivatedWindow::returnToPreviousWindow()
+    {
+        app::manager::Controller::sendAction(
+            application,
+            app::manager::actions::AbortPopup,
+            std::make_unique<gui::PopupRequestParams>(gui::popup::ID::AlarmDeactivated));
+        application->returnToPreviousWindow();
+    }
+    bool AlarmActivatedWindow::onInput(const InputEvent &inputEvent)
+    {
+        if (inputEvent.isShortRelease(KeyCode::KEY_ENTER) || inputEvent.isShortRelease(KeyCode::KEY_RF)) {
+            returnToPreviousWindow();
+            return true;
+        }
+        return false;
     }
 } /* namespace gui */
