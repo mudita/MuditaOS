f8dd719e1fb467ace1a3c8978a068c77d01ac964
Przemyslaw Brudny <przemyslaw.brudny@mudita.com>
2021-09-24 00:58:01 +0200

    [BH-702] Bell OnBoarding welcome window added
    Bell OnBoarding welcome window added.

---
+++ products/BellHybrid/apps/application-bell-onboarding/data/OnBoardingStyle.hpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <Style.hpp>
+
+namespace gui
+{
+    namespace window
+    {
+        namespace welcome
+        {
+            inline constexpr auto midline_correction    = 40U;
+            inline constexpr auto midline_w             = 480U;
+            inline constexpr auto midline_h             = 1U;
+            inline constexpr auto sun_logo_margin_top   = 10U;
+            inline constexpr auto sun_logo_margin_right = 25U;
+        } // namespace welcome
+    }     // namespace window
+} // namespace gui
