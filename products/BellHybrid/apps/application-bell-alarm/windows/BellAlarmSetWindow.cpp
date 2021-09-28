5e2f68daec7ac98159a6e30688d034db4c26c87c
Maciej Janicki <maciej.janicki@mudita.com>
2021-09-28 15:09:02 +0200

    [BH-921] Fix missing alarm set windows
    Fix missing alarm set windows
    Fix wrong alarm time description format
    Fix wrong alarm disable icon

---
+++ products/BellHybrid/apps/application-bell-alarm/windows/BellAlarmSetWindow.cpp
@@ -0,0 +1,71 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "BellAlarmSetWindow.hpp"
+#include "data/BellAlarmStyle.hpp"
+#include <application-bell-alarm/ApplicationBellAlarm.hpp>
+
+#include <gui/input/InputEvent.hpp>
+#include <gui/widgets/Icon.hpp>
+
+#include <common/TimeUtils.hpp>
+
+namespace gui
+{
+    BellAlarmSetWindow::BellAlarmSetWindow(app::ApplicationCommon *app,
+                                           std::shared_ptr<app::bell_alarm::BellAlarmSetContract::Presenter> presenter)
+        : WindowWithTimer(app, gui::window::name::bellAlarmSet), presenter{std::move(presenter)}
+    {
+        buildInterface();
+    }
+
+    void BellAlarmSetWindow::buildInterface()
+    {
+        AppWindow::buildInterface();
+        buildLayout();
+        registerCallbacks();
+    }
+
+    void BellAlarmSetWindow::buildLayout()
+    {
+        statusBar->setVisible(false);
+        header->setTitleVisibility(false);
+        bottomBar->setVisible(false);
+
+        icon = new Icon(this, 0, 0, style::window_width, style::window_height, {}, {});
+        icon->text->setFont(style::window::font::verybiglight);
+    }
+
+    void BellAlarmSetWindow::registerCallbacks()
+    {
+        timerCallback = [this](Item &, sys::Timer &timer) {
+            presenter->activate();
+            return true;
+        };
+    }
+
+    void BellAlarmSetWindow::onBeforeShow(ShowMode mode, SwitchData *data)
+    {
+        WindowWithTimer::onBeforeShow(mode, data);
+
+        if (presenter->isAlarmActive()) {
+            icon->image->set("big_alarm");
+            icon->text->setRichText(utils::time::getBottomDescription(
+                utils::time::calculateTimeDifference(presenter->getAlarmTime(), utils::time::getCurrentTime())));
+        }
+        else {
+            icon->image->set("big_no-alarm");
+            icon->text->setRichText(utils::translate("app_bell_alarm_set_not_active"));
+        }
+    }
+
+    bool BellAlarmSetWindow::onInput(const InputEvent &inputEvent)
+    {
+        if (inputEvent.isShortRelease(KeyCode::KEY_ENTER) || inputEvent.isShortRelease(KeyCode::KEY_RF)) {
+            detachTimerIfExists();
+            presenter->activate();
+            return true;
+        }
+        return false;
+    }
+} // namespace gui
