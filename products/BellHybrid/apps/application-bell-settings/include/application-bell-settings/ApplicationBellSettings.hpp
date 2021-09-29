37ee2ae4c967b07f56979ff6e9e7debab776b551
Przemyslaw Brudny <przemyslaw.brudny@mudita.com>
2021-09-29 19:54:26 +0200

    [BH-852] Bell settings language selector
    Bell settings language selector.

--- products/BellHybrid/apps/application-bell-settings/include/application-bell-settings/ApplicationBellSettings.hpp
+++ products/BellHybrid/apps/application-bell-settings/include/application-bell-settings/ApplicationBellSettings.hpp
@@ -8,15 +8,16 @@
 
 namespace gui::window::name
 {
-    inline constexpr auto bellSettings                               = "BellSettings";
-    inline constexpr auto bellSettingsAdvanced                       = "BellSettingsAdvanced";
-    inline constexpr auto bellSettingsTimeUnits                      = "BellSettingsTimeUnits";
-    inline constexpr auto bellSettingsDialog                         = "BellSettingsDialog";
-    inline constexpr auto bellSettingsFinished                       = "BellSettingsFinished";
-    inline constexpr auto bellSettingsFrontlight                     = "BellSettingsFrontlight";
-    inline constexpr auto bellSettingsHomeView                       = "BellSettingsHomeView";
-    inline constexpr auto bellSettingsBedtimeTone                    = "BellSettingsBedtimeTone";
-    inline constexpr auto bellSettingsTurnOff                        = "BellSettingsTurnOff";
+    inline constexpr auto bellSettings            = "BellSettings";
+    inline constexpr auto bellSettingsAdvanced    = "BellSettingsAdvanced";
+    inline constexpr auto bellSettingsTimeUnits   = "BellSettingsTimeUnits";
+    inline constexpr auto bellSettingsDialog      = "BellSettingsDialog";
+    inline constexpr auto bellSettingsFinished    = "BellSettingsFinished";
+    inline constexpr auto bellSettingsFrontlight  = "BellSettingsFrontlight";
+    inline constexpr auto bellSettingsHomeView    = "BellSettingsHomeView";
+    inline constexpr auto bellSettingsLanguage    = "BellSettingsLanguage";
+    inline constexpr auto bellSettingsBedtimeTone = "BellSettingsBedtimeTone";
+    inline constexpr auto bellSettingsTurnOff     = "BellSettingsTurnOff";
 } // namespace gui::window::name
 
 namespace app
