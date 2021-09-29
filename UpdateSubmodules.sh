99304c4c37bc60ea312266ff2d61d57438222d3d
GravisZro <noreply+github@adaptivetime.com>
2021-09-29 15:12:20 -0400

    [EGD-7254] Fix submodule update script
    Fixes the long broken UpdateSubmodules.sh shell script.  The script now
    consists of a single git command.

--- UpdateSubmodules.sh
+++ UpdateSubmodules.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+# Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 # For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
-echo "Building all submodules while checking out from MASTER branch."
-
-git submodule update 
-git submodule foreach git checkout master --recursive
-git submodule foreach git pull origin master --recursive
+echo "Updating all submodules."
+git submodule update --init --recursive
