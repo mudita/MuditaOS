30f2a11681a9c2e2f7493998e0243d23fbb64dad
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2021-09-30 09:35:30 +0200

    [BH-802] Adapt audio subsytem for Bell
    Hook audio device factory for Bell to the audio subsystem.
    
    Signed-off-by: Marcin Smoczyński <smoczynski.marcin@gmail.com>

---
+++ module-audio/board/rt1051/bellpx/BellPxAudioDeviceFactory.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "BellPxAudioDeviceFactory.hpp"
+
+#include <Audio/Profiles/Profile.hpp>
+
+#include <log/log.hpp>
+
+#include <stdexcept>
+
+using audio::AudioDevice;
+using audio::BellPxAudioDeviceFactory;
+
+std::shared_ptr<AudioDevice> BellPxAudioDeviceFactory::getDevice(const audio::Profile &profile)
+{
+    std::shared_ptr<AudioDevice> device;
+
+    switch (profile.GetAudioDeviceType()) {
+    case AudioDevice::Type::Audiocodec: {
+        LOG_ERROR("Bell audio codec support is not yet implemented.");
+    } break;
+
+    default:
+        break;
+    };
+
+    return device;
+}
+
+std::shared_ptr<AudioDevice> BellPxAudioDeviceFactory::createCellularAudioDevice()
+{
+    throw std::runtime_error("Cellular audio device not valid for Bell platform.");
+}
--- module-audio/board/rt1051/puretx/PureTxAudioDeviceFactory.cpp
+++ module-audio/board/rt1051/puretx/PureTxAudioDeviceFactory.cpp
@@ -1,24 +1,24 @@
 // Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
-#include "RT1051DeviceFactory.hpp"
-#include "board/rt1051/RT1051AudioCodec.hpp"
-#include "board/rt1051/RT1051CellularAudio.hpp"
+#include "PureTxAudioDeviceFactory.hpp"
+#include "PureTxAudioCodec.hpp"
+#include "RT1051CellularAudio.hpp"
 #include "audio/BluetoothAudioDevice.hpp"
 
 #include <Audio/Profiles/Profile.hpp>
 
 using audio::AudioDevice;
-using audio::RT1051AudioCodec;
+using audio::PureTxAudioCodec;
+using audio::PureTxAudioDeviceFactory;
 using audio::RT1051CellularAudio;
-using audio::RT1051DeviceFactory;
 
-std::shared_ptr<AudioDevice> RT1051DeviceFactory::getDevice(const audio::Profile &profile)
+std::shared_ptr<AudioDevice> PureTxAudioDeviceFactory::getDevice(const audio::Profile &profile)
 {
     std::shared_ptr<AudioDevice> device;
     switch (profile.GetAudioDeviceType()) {
     case AudioDevice::Type::Audiocodec: {
-        device = std::make_shared<RT1051AudioCodec>(profile.GetAudioConfiguration());
+        device = std::make_shared<PureTxAudioCodec>(profile.GetAudioConfiguration());
     } break;
 
     case AudioDevice::Type::BluetoothA2DP: {
@@ -44,7 +44,7 @@ std::shared_ptr<AudioDevice> RT1051DeviceFactory::getDevice(const audio::Profile
     return device;
 }
 
-std::shared_ptr<AudioDevice> RT1051DeviceFactory::createCellularAudioDevice()
+std::shared_ptr<AudioDevice> PureTxAudioDeviceFactory::createCellularAudioDevice()
 {
     return std::make_shared<RT1051CellularAudio>();
 }
