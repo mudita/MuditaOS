3e46e6418fad3bab7159ec03aadda1681564d07f
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2021-10-01 08:43:31 +0200

    [EGD-7674] Add separate list of music files currently being played
    In addition to the list of music tracks displayed to the user,
    a separate list of currently played tracks has been implemented.

--- module-apps/application-music-player/models/SongsRepository.hpp
+++ module-apps/application-music-player/models/SongsRepository.hpp
@@ -16,6 +16,13 @@
 
 namespace app::music_player
 {
+    struct FilesCache
+    {
+        std::vector<db::multimedia_files::MultimediaFilesRecord> records{};
+        std::uint32_t recordsOffset{0};
+        std::uint32_t recordsCount{0};
+    };
+
     class AbstractTagsFetcher
     {
       public:
@@ -43,12 +50,13 @@ namespace app::music_player
 
         virtual ~AbstractSongsRepository() noexcept = default;
 
+        virtual void initCache()                                                    = 0;
         virtual void getMusicFilesList(std::uint32_t offset,
                                        std::uint32_t limit,
                                        const OnGetMusicFilesListCallback &callback) = 0;
-        virtual std::size_t getFileIndex(const std::string &filePath) const        = 0;
         virtual std::string getNextFilePath(const std::string &filePath) const     = 0;
         virtual std::string getPreviousFilePath(const std::string &filePath) const = 0;
+        virtual void updateRepository(const std::string &filePath)                  = 0;
     };
 
     class SongsRepository : public AbstractSongsRepository, public app::AsyncCallbackReceiver
@@ -56,17 +64,55 @@ namespace app::music_player
       public:
         explicit SongsRepository(ApplicationCommon *application, std::unique_ptr<AbstractTagsFetcher> tagsFetcher);
 
+        void initCache();
         void getMusicFilesList(std::uint32_t offset,
                                std::uint32_t limit,
                                const OnGetMusicFilesListCallback &callback) override;
-        std::size_t getFileIndex(const std::string &filePath) const override;
         std::string getNextFilePath(const std::string &filePath) const override;
         std::string getPreviousFilePath(const std::string &filePath) const override;
 
+        /// If the currently playing track is not included in the musicFilesCache then we request new data of the size
+        /// of the cache otherwise if the index number of the currently playing track exceeds the upper or lower
+        /// cacheThreshold, then we request the next records. See doc/SongsRepository.md for more information.
+        void updateRepository(const std::string &filePath) override;
+
       private:
+        using OnUpdateMusicFilesListCallback = std::function<bool(
+            const std::vector<db::multimedia_files::MultimediaFilesRecord> &, unsigned int, std::uint32_t)>;
+
         ApplicationCommon *application;
 
         std::unique_ptr<AbstractTagsFetcher> tagsFetcher;
-        std::vector<db::multimedia_files::MultimediaFilesRecord> musicFiles;
+
+        /// collection of music files displayed in the list view
+        FilesCache musicFilesViewCache;
+        /// collection of music files currently playing
+        FilesCache musicFilesModelCache;
+
+        std::size_t getCachedFileIndex(const std::string &filePath) const;
+        std::uint32_t calculateOffset();
+
+        /// overwrite the musicFiles view cache with new data from the given index
+        void updateMusicFilesList(std::uint32_t offset,
+                                  std::uint32_t limit,
+                                  const OnUpdateMusicFilesListCallback &callback);
+
+        /// overwrite musicFiles model cache with new data from zero index
+        bool initCacheCallback(const std::vector<db::multimedia_files::MultimediaFilesRecord> &records,
+                               unsigned int repoRecordsCount);
+        /// overwrite the musicFiles model cache with new data from the given index
+        bool newDataCallback(const std::vector<db::multimedia_files::MultimediaFilesRecord> &records,
+                             unsigned int repoRecordsCount,
+                             std::uint32_t offset);
+        /// adding new data to the musicFiles model cache at the end of the list, with the first records being deleted
+        /// so that the cache size is not changed
+        bool newBackDataCallback(const std::vector<db::multimedia_files::MultimediaFilesRecord> &records,
+                                 unsigned int repoRecordsCount,
+                                 std::uint32_t offset);
+        /// adding new data to the musicFiles model cache to the top of the list, but the last records are removed so
+        /// that the cache size is not changed
+        bool newFrontDataCallback(const std::vector<db::multimedia_files::MultimediaFilesRecord> &records,
+                                  unsigned int repoRecordsCount,
+                                  std::uint32_t offset);
     };
 } // namespace app::music_player
