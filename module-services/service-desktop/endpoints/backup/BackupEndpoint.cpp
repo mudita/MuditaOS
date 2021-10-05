4eb00eba99142c5918e87e25dea7cb5b873dd23c
Marek Niepieklo <marek.niepieklo@mudita.com>
2021-10-05 15:22:58 +0200

    [CP-648] Handle updated API requests
    Added BackupHelper - fails build

--- module-services/service-desktop/endpoints/backup/BackupEndpoint.cpp
+++ module-services/service-desktop/endpoints/backup/BackupEndpoint.cpp
@@ -2,7 +2,6 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include <endpoints/backup/BackupEndpoint.hpp>
-
 #include <endpoints/JsonKeyNames.hpp>
 #include <endpoints/message/Sender.hpp>
 #include <service-desktop/DesktopMessages.hpp>
@@ -16,82 +15,27 @@
 
 namespace sdesktop::endpoints
 {
-    using sender::putToSendQueue;
-
     auto BackupEndpoint::handle(Context &context) -> void
     {
-        switch (context.getMethod()) {
-        case http::Method::get:
-            checkState(context);
-            break;
-        case http::Method::post:
-            executeRequest(context);
-            break;
-        case http::Method::put:
-        case http::Method::del:
-            context.setResponseStatus(http::Code::BadRequest);
-            putToSendQueue(context.createSimpleResponse());
-            break;
+        auto [sent, response] = helper->process(context.getMethod(), context);
+
+        if (sent == sent::delayed) {
+            LOG_DEBUG("There is no proper delayed serving mechanism - depend on invisible context caching");
         }
-    }
-
-    auto BackupEndpoint::executeRequest(Context &context) -> sys::ReturnCodes
-    {
-        json11::Json responseBodyJson;
-        auto owner = static_cast<ServiceDesktop *>(ownerServicePtr);
-
-        if (owner->getBackupRestoreStatus().state == BackupRestore::OperationState::Running) {
-            LOG_DEBUG("Backup already running");
-            // a backup is already running, don't start a second task
-            context.setResponseStatus(http::Code::NotAcceptable);
-        }
-        else {
-            LOG_DEBUG("Starting backup");
-            // initialize new backup information
-            owner->prepareBackupData();
-
-            // start the backup process in the background
-            ownerServicePtr->bus.sendUnicast(std::make_shared<sdesktop::BackupMessage>(),
-                                             service::name::service_desktop);
-
-            // return new generated backup info
-            context.setResponseBody(owner->getBackupRestoreStatus());
-        }
-
-        putToSendQueue(context.createSimpleResponse());
-
-        return sys::ReturnCodes::Success;
-    }
-
-    auto BackupEndpoint::checkState(Context &context) -> sys::ReturnCodes
-    {
-        json11::Json responseBodyJson;
-        auto owner = static_cast<ServiceDesktop *>(ownerServicePtr);
-
-        if (context.getBody()[json::taskId].is_string()) {
-            if (owner->getBackupRestoreStatus().taskId == context.getBody()[json::taskId].string_value()) {
-                if (owner->getBackupRestoreStatus().state == BackupRestore::OperationState::Finished) {
-                    context.setResponseStatus(http::Code::SeeOther);
-                }
-                else {
-                    context.setResponseStatus(http::Code::OK);
-                }
-
-                context.setResponseBody(owner->getBackupRestoreStatus());
+        if (sent == sent::no) {
+            if (not response) {
+                LOG_ERROR("Response not sent & response not created : respond with error");
+                context.setResponseStatus(http::Code::NotAcceptable);
             }
             else {
-                context.setResponseStatus(http::Code::NotFound);
+                context.setResponse(response.value());
             }
-        }
-        else {
-            LOG_DEBUG("Backup task not found");
-            context.setResponseStatus(http::Code::BadRequest);
-        }
 
-        LOG_DEBUG("Responding");
-        putToSendQueue(context.createSimpleResponse());
-
-        return sys::ReturnCodes::Success;
+            sender::putToSendQueue(context.createSimpleResponse());
+        }
+        if (sent == sent::yes and response) {
+            LOG_ERROR("Response set when we already handled response in handler");
+        }
     }
 
 } // namespace sdesktop::endpoints
--- module-services/service-desktop/endpoints/include/endpoints/backup/BackupEndpoint.hpp
+++ module-services/service-desktop/endpoints/include/endpoints/backup/BackupEndpoint.hpp
@@ -4,30 +4,23 @@
 #pragma once
 
 #include <endpoints/Endpoint.hpp>
-
-#include <system/Common.hpp>
-
-#include <string>
-
-namespace sys
-{
-    class Service;
-} // namespace sys
+#include "BackupHelper.hpp"
 
 namespace sdesktop::endpoints
 {
     class BackupEndpoint : public Endpoint
     {
       public:
-        BackupEndpoint(sys::Service *ownerServicePtr) : Endpoint(ownerServicePtr)
+        BackupEndpoint(sys::Service *ownerServicePtr)
+            : Endpoint(ownerServicePtr), helper(std::make_unique<BackupHelper>(ownerServicePtr))
         {
             debugName = "BackupEndpoint";
         }
+
         auto handle(Context &context) -> void override;
 
       private:
-        auto executeRequest(Context &context) -> sys::ReturnCodes;
-        auto checkState(Context &context) -> sys::ReturnCodes;
+        const std::unique_ptr<BackupHelper> helper;
     };
 
 } // namespace sdesktop::endpoints
