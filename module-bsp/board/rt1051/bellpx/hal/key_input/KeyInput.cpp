34c4e3a2fc8da603206d519f92c26f3c12dcc747
Wojtek Rzepecki <wojtek.rzepecki@mudita.com>
2021-10-07 10:28:30 +0200

    [BH-872] Fix encoder hadnling
    interrupt driven encoder handling

--- module-bsp/board/rt1051/bellpx/hal/key_input/KeyInput.cpp
+++ module-bsp/board/rt1051/bellpx/hal/key_input/KeyInput.cpp
@@ -2,9 +2,11 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "KeyInput.hpp"
+#include <bsp/KeyInputCommon.hpp>
 
 #include <hal/GenericFactory.hpp>
 #include <bsp/switches/switches.hpp>
+#include <bsp/rotary_encoder/rotary_encoder.hpp>
 #include <board/BoardDefinitions.hpp>
 
 namespace hal::key_input
@@ -17,21 +19,35 @@ namespace hal::key_input
     void KeyInput::init(xQueueHandle queueHandle)
     {
         bsp::bell_switches::init(queueHandle);
+        bsp::rotary_encoder::init(queueHandle);
     }
 
     void KeyInput::deinit()
     {
         bsp::bell_switches::deinit();
+        bsp::rotary_encoder::deinit();
     }
 
     std::vector<bsp::KeyEvent> KeyInput::getKeyEvents(KeyNotificationSource notification)
     {
-        return bsp::bell_switches::getKeyEvents(notification);
+        auto keyNotification = static_cast<bsp::NotificationSource>(notification);
+        if (keyNotification == bsp::NotificationSource::rotaryEncoder) {
+            return bsp::rotary_encoder::getKeyEvents();
+        }
+        else {
+            return bsp::bell_switches::getKeyEvents(keyNotification);
+        }
     }
 
     BaseType_t generalIRQHandler(std::uint32_t irqMask)
     {
-        return bsp::bell_switches::IRQHandler(irqMask);
+        constexpr std::uint32_t encoderMask{99};
+        if (irqMask == encoderMask) {
+            return bsp::rotary_encoder::IRQHandler(irqMask);
+        }
+        else {
+            return bsp::bell_switches::IRQHandler(irqMask);
+        }
     }
 
     BaseType_t wakeupIRQHandler()
