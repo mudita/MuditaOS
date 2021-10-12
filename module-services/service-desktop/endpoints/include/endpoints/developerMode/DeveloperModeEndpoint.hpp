c9caad5c11a99b436f950728b2ef7be1a735b680
Marek Niepieklo <marek.niepieklo@mudita.com>
2021-10-12 10:39:20 +0200

    [CP-610] Add removeFile operation to Filesystem EP
    Moved Rename/Remove/ListDir from DeveloperMode to Filesystem EP API

--- module-services/service-desktop/endpoints/include/endpoints/developerMode/DeveloperModeEndpoint.hpp
+++ module-services/service-desktop/endpoints/include/endpoints/developerMode/DeveloperModeEndpoint.hpp
@@ -6,7 +6,6 @@
 #include <endpoints/Endpoint.hpp>
 #include "DeveloperModeHelper.hpp"
 #include "Mode/UI_Helper.hpp"
-#include "fs/FS_Helper.hpp"
 
 #include <memory>
 
@@ -18,13 +17,11 @@ namespace sdesktop::endpoints
       private:
         const std::unique_ptr<DeveloperModeHelper> helper;
         const std::unique_ptr<UI_Helper> uiHelper;
-        const std::unique_ptr<FS_Helper> fsHelper;
 
       public:
         explicit DeveloperModeEndpoint(sys::Service *_ownerServicePtr)
             : Endpoint(_ownerServicePtr), helper(std::make_unique<DeveloperModeHelper>(ownerServicePtr)),
-              uiHelper(std::make_unique<UI_Helper>(ownerServicePtr)),
-              fsHelper(std::make_unique<FS_Helper>(ownerServicePtr))
+              uiHelper(std::make_unique<UI_Helper>(ownerServicePtr))
         {
             debugName = "DeveloperModeEndpoint";
         }
---
+++ module-services/service-desktop/endpoints/include/endpoints/filesystem/FS_Helper.hpp
@@ -0,0 +1,55 @@
+// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <endpoints/BaseHelper.hpp>
+#include "FileOperations.hpp"
+
+namespace sdesktop::endpoints
+{
+    namespace json::fs
+    {
+        inline constexpr auto removeFile   = "removeFile";
+        inline constexpr auto renameFile   = "renameFile";
+        inline constexpr auto destFilename = "destFilename";
+        inline constexpr auto listDir      = "listDir";
+        inline constexpr auto path         = "path";
+        inline constexpr auto fileName     = "fileName";
+        inline constexpr auto fileSize     = "fileSize";
+        inline constexpr auto fileCrc32    = "fileCrc32";
+        inline constexpr auto chunkSize    = "chunkSize";
+        inline constexpr auto chunkNo      = "chunkNo";
+        inline constexpr auto data         = "data";
+        inline constexpr auto rxID         = "rxID";
+        inline constexpr auto txID         = "txID";
+
+        inline constexpr auto fileDoesNotExist = "file does not exist";
+    } // namespace json::fs
+
+    class FS_Helper : public BaseHelper
+    {
+      public:
+        explicit FS_Helper(sys::Service *p, FileOperations &fileOps) : BaseHelper(p), fileOps(fileOps)
+        {}
+
+        auto processGet(Context &context) -> ProcessResult final;
+        auto processPut(Context &context) -> ProcessResult final;
+        auto processDelete(Context &context) -> ProcessResult final;
+
+      private:
+        auto startGetFile(Context &context) const -> ResponseContext;
+        auto getFileChunk(Context &context) const -> ResponseContext;
+
+        auto startSendFile(Context &context) const -> ResponseContext;
+        auto sendFileChunk(Context &context) const -> ResponseContext;
+
+        auto requestFileRemoval(const std::string &fileName) -> bool;
+        auto requestFileRename(const std::string &fileName, const std::string &destFileName) noexcept -> bool;
+        auto requestListDir(const std::string &directory) -> ResponseContext;
+
+        auto requestLogsFlush() const -> void;
+
+        FileOperations &fileOps;
+    };
+} // namespace sdesktop::endpoints
