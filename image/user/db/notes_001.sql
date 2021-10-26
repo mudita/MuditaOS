e3abca068e81a6f33f1cc7ffcace327b0c3075f4
Maciej Janicki <maciej.janicki@mudita.com>
2021-10-26 12:58:53 +0200

    [BH-1073] Remove Pure specific assets
    Remove pure specific assets

--- image/user/db/notes_001.sql
+++
@@ -1,8 +0,0 @@
--- Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
--- For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
-
-CREATE TABLE IF NOT EXISTS notes(
-    _id INTEGER PRIMARY KEY,
-    date INTEGER,
-    snippet TEXT DEFAULT ''
-);
--- image/user/db/notifications_001.sql
+++
@@ -1,9 +0,0 @@
--- Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
--- For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
-
-CREATE TABLE IF NOT EXISTS notifications(
-    _id INTEGER PRIMARY KEY,
-    key INTEGER UNIQUE DEFAULT 0,
-    value INTEGER DEFAULT 0,
-    contact_id INTEGER DEFAULT 0
-);
