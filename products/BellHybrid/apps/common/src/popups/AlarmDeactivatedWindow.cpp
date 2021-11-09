d7dc17cf715f0afad31cd5c4c72da170ecf78d87
Tigran Soghbatyan <tigran.soghbatyan@digica.com>
2021-11-09 15:33:37 +0100

    [BH-1149] Fix popup layout
    Make popup layout according to the design

--- products/BellHybrid/apps/common/src/popups/AlarmDeactivatedWindow.cpp
+++ products/BellHybrid/apps/common/src/popups/AlarmDeactivatedWindow.cpp
@@ -5,6 +5,7 @@
 #include <apps-common/messages/DialogMetadataMessage.hpp>
 #include <apps-common/popups/data/PopupRequestParams.hpp>
 #include <apps-common/popups/Popups.hpp>
+#include <common/data/StyleCommon.hpp>
 #include <common/popups/AlarmDeactivatedWindow.hpp>
 #include <gui/input/InputEvent.hpp>
 #include <gui/widgets/Icon.hpp>
@@ -56,6 +57,8 @@ namespace gui
                         style::window_height,
                         "big_no-alarm_W_G",
                         utils::translate("app_bell_alarm_deactivated"));
+        icon->setAlignment(gui::Alignment(gui::Alignment::Horizontal::Center, gui::Alignment::Vertical::Top));
+        icon->image->setMargins({0, bell_style::popup_icon_top_margin, 0, bell_style::popup_icon_bottom_margin});
         icon->text->setFont(style::window::font::verybiglight);
     }
 
