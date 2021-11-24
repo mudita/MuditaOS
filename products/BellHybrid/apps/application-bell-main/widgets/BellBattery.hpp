8439e6a488a1a2992080b9e261aabb9a66c904cc
Paweł Joński <pawel.jonski@mudita.com>
2021-11-24 12:19:20 +0100

    [BH-1254] Charging battery UI fix
    Charging battery UI fix:
    - bigger font
    - center widget

--- products/BellHybrid/apps/application-bell-main/widgets/BellBattery.hpp
+++ products/BellHybrid/apps/application-bell-main/widgets/BellBattery.hpp
@@ -16,7 +16,7 @@ namespace gui
         constexpr auto battery_low        = "bell_battery_lvl1";
         constexpr auto battery_critical   = "bell_battery_empty";
         constexpr auto battery_charging   = "bell_battery_charging";
-        constexpr auto font_small         = style::window::font::verybiglight;
+        constexpr auto font_small         = style::window::font::largelight;
         constexpr auto image_h            = 64;
         constexpr auto image_w            = 64U;
         constexpr auto image_right_margin = 10U;
