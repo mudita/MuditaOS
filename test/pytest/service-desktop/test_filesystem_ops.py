9f8af2bf380baa8424de55f67ad1a25779972139
Marek Niepieklo <marek.niepieklo@mudita.com>
2021-11-29 15:40:23 +0100

    [CP-832] Merge get/send file tests, add remove file test
    Added new file operations test for get, send, remove file.
    Updated lorem-ipsum.txt file.

--- test/pytest/service-desktop/test_filesystem_ops.py
+++ test/pytest/service-desktop/test_filesystem_ops.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 # For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 import pytest
-from harness.interface.defs import status
+import filecmp
+
 from harness.api.developermode import PhoneModeLock
-from harness.api.filesystem import get_file, FsInitGet, FsGetChunk
+from harness.api.filesystem import get_file, put_file, FsInitGet, FsGetChunk, FsRemoveFile, FsRenameFile
 from harness import log
 from harness.request import TransactionError
 from harness.interface.defs import Status
@@ -11,6 +12,66 @@ from harness.interface.defs import Status
 
 def setPasscode(harness, flag):
     PhoneModeLock(flag).run(harness)
+
+
+testFileName = "lorem-ipsum.txt"
+osLogFileName = "MuditaOS.log"
+
+sysUserPath = "/sys/user"
+sysUserLogsPath = sysUserPath + "/logs"
+
+@pytest.mark.service_desktop_test
+@pytest.mark.usefixtures("phone_unlocked")
+@pytest.mark.rt1051
+def test_send_file(harness):
+    """
+    Attempt requesting and sending file to Pure
+    """
+    put_file(harness, testFileName, sysUserPath)
+
+
+@pytest.mark.service_desktop_test
+@pytest.mark.usefixtures("phone_unlocked")
+@pytest.mark.rt1051
+def test_get_file_back(harness):
+    """
+    Attempt requesting and transfering file data
+    """
+    loadedFile = "./lorem-ipsum-2.txt"
+
+    get_file(harness, testFileName, "./", sysUserPath, loadedFile)
+
+    assert filecmp.cmp(testFileName, loadedFile, shallow=True)
+
+@pytest.mark.service_desktop_test
+@pytest.mark.usefixtures("phone_unlocked")
+@pytest.mark.rt1051
+def test_rename_file(harness):
+    """
+    Attempt renaming a file and confirm by getting it back and comparing with original
+    """
+    sourceFile = "/sys/user/" + testFileName
+    renamedFile = "renamed_" + testFileName
+    destFile = "/sys/user/" + renamedFile
+
+    FsRenameFile(sourceFile, destFile).run(harness)
+
+    get_file(harness, renamedFile, "./", sysUserPath)
+
+    assert filecmp.cmp(testFileName, renamedFile, shallow=True)
+
+@pytest.mark.service_desktop_test
+@pytest.mark.usefixtures("phone_unlocked")
+@pytest.mark.rt1051
+def test_delete_files(harness):
+    """
+    Attempt deleting a file and confirm removal by attempting to get it back
+    """
+    fileToRemove = sysUserPath + "/" + "renamed_" +  testFileName
+    FsRemoveFile(fileToRemove).run(harness)
+
+    with pytest.raises(TransactionError, match=r".*" + str(Status.NotFound.value) + ".*"):
+        get_file(harness, testFileName, "./", sysUserPath)
 
 
 @pytest.mark.service_desktop_test
@@ -25,11 +86,10 @@ def test_get_not_existing_file(harness):
     Getting a large file may hit screen auto lock.
     We need to disable pass code for duration of test
     """
-    setPasscode(harness, False)
 
-    fileName = "Unknown.file"
+    testFileName = "Unknown.file"
     with pytest.raises(TransactionError, match=r".*" + str(Status.NotFound.value) + ".*"):
-        get_file(harness, fileName, "./", "/sys/user/logs/")
+        get_file(harness, testFileName, "./", sysUserLogsPath)
 
 
 @pytest.mark.service_desktop_test
@@ -44,8 +104,7 @@ def test_get_invalid_chunks(harness):
     requesting data with invalid rxID:
        - rxID != current rxID transfer
     """
-    fileName = "MuditaOS.log"
-    ret = FsInitGet("/sys/user/logs/", fileName).run(harness)
+    ret = FsInitGet(sysUserLogsPath, osLogFileName).run(harness)
 
     assert ret.fileSize != 0
 
@@ -72,5 +131,4 @@ def test_get_file(harness):
     """
     Get file MuditaOS.log file - whole transfer
     """
-
-    get_file(harness, "MuditaOS.log", "./", "/sys/user/logs/")
+    get_file(harness, osLogFileName, "./", sysUserLogsPath)
