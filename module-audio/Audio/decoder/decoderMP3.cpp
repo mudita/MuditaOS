28e9e50d3e930b0214df3186edd352563e6adc24
Kuba <jakub.kleczkowski@mudita.com>
2021-12-14 12:38:11 +0100

    [EGD-8094] Change mp3 decoder replaced with dr_mp3
    Mp3 decoder replaced with dr_mp3.

--- module-audio/Audio/decoder/decoderMP3.cpp
+++ module-audio/Audio/decoder/decoderMP3.cpp
@@ -1,9 +1,8 @@
 // Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
-#define MINIMP3_ONLY_MP3
-#define MINIMP3_NO_SIMD
-#define MINIMP3_IMPLEMENTATION
+#define DR_MP3_IMPLEMENTATION
+#define DR_MP3_NO_STDIO
 
 #include "decoderMP3.hpp"
 
@@ -20,241 +19,57 @@ namespace audio
             return;
         }
 
-        mp3d = std::make_unique<mp3dec_t>();
+        mp3 = std::make_unique<drmp3>();
 
-        mp3dec_init(mp3d.get());
-
-        if (!find_first_valid_frame()) {
+        drmp3_init(mp3.get(), this->drmp3_read, this->drmp3_seek, this, NULL);
+        if (mp3 == NULL) {
+            LOG_ERROR("********************************** Unable to init mp3 decoder");
             return;
+            // Failed to open file
         }
 
+        chanNumber = mp3->channels;
+        sampleRate = mp3->sampleRate;
         // NOTE: Always convert to S16LE as internal format
         bitsPerSample = 16;
         isInitialized = true;
     }
 
+    decoderMP3::~decoderMP3()
+    {
+        drmp3_uninit(mp3.get());
+    }
     void decoderMP3::setPosition(float pos)
     {
-        decoderNotFirstRun = false;
-
-        std::fseek(fd, (pos * fileSize) + firstValidFrameFileOffset, SEEK_SET);
-
-        // TODO: M.P Currently calculating MP3 position is unsupported, in general seeking is supported though.
-        // position += (float) ((float) (samplesToReadChann / chanNumber) / (float) sampleRate);
-    }
-
-    bool decoderMP3::find_first_valid_frame()
-    {
-
-        mp3dec_frame_info_t info = {};
-
-        int32_t bytesAvailable = DECODER_BUFFER_SIZE;
-        uint32_t bufferIndex   = 0;
-
-        auto decBuffer = std::make_unique<uint8_t[]>(DECODER_BUFFER_SIZE);
-
-        std::rewind(fd);
-
-        if (std::fread(decBuffer.get(), 1, DECODER_BUFFER_SIZE, fd) == 0) {
-            return false;
-        }
-
-        for (;;) {
-            // refill buffer if necessary(only if over 87,5% of bytes are consumed)
-            if (bufferIndex > (DECODER_BUFFER_SIZE - (DECODER_BUFFER_SIZE / 8))) {
-                memcpy(&decBuffer[0], &decBuffer[bufferIndex], bytesAvailable);
-                uint32_t bytesRead =
-                    std::fread(&decBuffer[bytesAvailable], 1, DECODER_BUFFER_SIZE - bytesAvailable, fd);
-
-                if (bytesRead == 0) {
-                    return false;
-                }
-
-                bytesAvailable += bytesRead;
-                bufferIndex = 0;
-            }
-
-            for (;;) {
-                uint32_t smpl =
-                    mp3dec_decode_frame(mp3d.get(), &decBuffer[bufferIndex], bytesAvailable, nullptr, &info);
-                bufferIndex += info.frame_bytes;
-                bytesAvailable -= info.frame_bytes;
-
-                // Valid frame
-                if (smpl && info.frame_bytes) {
-
-                    // Fill necessary parameters
-                    samplesPerFrame           = smpl;
-                    sampleRate                = info.hz;
-                    chanNumber                = info.channels;
-                    firstValidFrameByteSize   = (144 * info.bitrate_kbps * 1000 / info.hz);
-                    firstValidFrameFileOffset = std::ftell(fd) - bytesAvailable - firstValidFrameByteSize;
-
-                    std::rewind(fd);
-
-                    return true;
-                }
-                // Decoder skipped ID3 or invalid data
-                else if ((smpl == 0) && info.frame_bytes) {
-                }
-                // insufficient data
-                else if ((info.frame_bytes == 0) && (smpl == 0)) {
-                    break;
-                }
-            }
-        }
-    }
-
-    uint32_t decoderMP3::get_frames_count()
-    {
-
-        int32_t bytesAvailable   = DECODER_BUFFER_SIZE;
-        uint32_t frames_count    = 0;
-        mp3dec_frame_info_t info = {};
-        bool last_refill         = false;
-
-        uint32_t bufferIndex = 0;
-        auto decBuffer       = std::make_unique<uint8_t[]>(DECODER_BUFFER_SIZE);
-
-        // Jump to the file beginning
-        std::rewind(fd);
-
-        /* Fill decBuffer */
-        if (std::fread(decBuffer.get(), 1, DECODER_BUFFER_SIZE, fd) == 0) {
-            return 0;
-        }
-
-    refill:
-
-        // refill buffer if necessary(only if over 87,5% of bytes are consumed)
-        if (bufferIndex > (DECODER_BUFFER_SIZE - (DECODER_BUFFER_SIZE / 8))) {
-            memcpy(&decBuffer[0], &decBuffer[bufferIndex], bytesAvailable);
-            uint32_t bytesRead =
-                std::fread(decBuffer.get() + bytesAvailable, 1, DECODER_BUFFER_SIZE - bytesAvailable, fd);
-
-            if (bytesRead != (DECODER_BUFFER_SIZE - bytesAvailable)) {
-                last_refill = true;
-            }
-
-            bytesAvailable += bytesRead;
-            bufferIndex = 0;
-        }
-
-        while (1) {
-
-            uint32_t smpl = mp3dec_decode_frame(mp3d.get(), &decBuffer[bufferIndex], bytesAvailable, nullptr, &info);
-            bufferIndex += info.frame_bytes;
-            bytesAvailable -= info.frame_bytes;
-
-            // Valid frame
-            if (smpl && info.frame_bytes) {
-                frames_count++;
-            }
-
-            // Decoder skipped ID3 or invalid data
-            if ((smpl == 0) && info.frame_bytes) {
-                frames_count++;
-            }
-
-            // Last frame
-            if (last_refill && (info.frame_bytes == 0) && (smpl == 0)) {
-                return frames_count;
-            }
-
-            // insufficient data
-            if ((info.frame_bytes == 0) && (smpl == 0)) {
-                goto refill;
-            }
-        }
+        auto totalFramesCount = drmp3_get_pcm_frame_count(mp3.get());
+        drmp3_seek_to_pcm_frame(mp3.get(), totalFramesCount * pos);
+        position = float(totalFramesCount) * pos / float(sampleRate);
     }
 
     uint32_t decoderMP3::decode(uint32_t samplesToRead, int16_t *pcmData)
     {
-        mp3dec_frame_info_t info = {0, 0, 0, 0, 0, 0};
 
-        if (!decoderNotFirstRun) {
-            decoderBuffer    = std::make_unique<uint8_t[]>(DECODER_BUFFER_SIZE);
-            pcmsamplesbuffer = std::make_unique<uint16_t[]>(pcmsamplesbuffer_size);
+        uint32_t samplesRead = 0;
+        samplesRead =
+            drmp3_read_pcm_frames_s16(mp3.get(), samplesToRead / chanNumber, reinterpret_cast<drmp3_int16 *>(pcmData));
 
-            // Fill decoderBuffer
-            uint32_t bytesRead = std::fread(decoderBuffer.get(), 1, DECODER_BUFFER_SIZE, fd);
-
-            if (bytesRead == 0) {
-                return 0;
-            }
-
-            bytesAvailable   = DECODER_BUFFER_SIZE;
-            decoderBufferIdx = 0;
-
-            decoderNotFirstRun = true;
-        }
-        else if (!lastRefill) {
-            bytesAvailable = DECODER_BUFFER_SIZE - decoderBufferIdx;
+        if (samplesRead) {
+            position += float(samplesRead) / float(sampleRate);
         }
 
-        uint32_t samplesFetched = pcmsamplesbuffer_idx;
+        return samplesRead * chanNumber;
+    }
 
-    refill:
+    size_t decoderMP3::drmp3_read(void *pUserData, void *pBufferOut, size_t bytesToRead)
+    {
+        auto *userdata = reinterpret_cast<decoderMP3 *>(pUserData);
+        return std::fread(pBufferOut, 1, bytesToRead, userdata->fd);
+    }
 
-        // refill buffer if necessary(only if over 87,5% of bytes consumed)
-        if (!lastRefill && (decoderBufferIdx > (DECODER_BUFFER_SIZE - (DECODER_BUFFER_SIZE / 8)))) {
-            memcpy(&decoderBuffer[0], &decoderBuffer[decoderBufferIdx], bytesAvailable);
-            uint32_t bytesRead =
-                std::fread(&decoderBuffer[bytesAvailable], 1, DECODER_BUFFER_SIZE - bytesAvailable, fd);
-
-            if (bytesRead != (DECODER_BUFFER_SIZE - bytesAvailable)) {
-                lastRefill = true;
-            }
-
-            bytesAvailable += bytesRead;
-            decoderBufferIdx = 0;
-        }
-
-        uint32_t samplesToReadChann = samplesToRead;
-
-        while (1) {
-            uint32_t smpl = 0;
-            if (samplesFetched < samplesToReadChann) {
-                smpl = mp3dec_decode_frame(mp3d.get(),
-                                           &decoderBuffer[decoderBufferIdx],
-                                           bytesAvailable,
-                                           (short *)&pcmsamplesbuffer[samplesFetched],
-                                           &info);
-            }
-
-            bytesAvailable -= info.frame_bytes;
-            decoderBufferIdx += info.frame_bytes;
-
-            // Valid frame
-            if (smpl && info.frame_bytes) {
-                samplesFetched += smpl * info.channels;
-            }
-
-            if (samplesFetched >= samplesToReadChann) {
-                break;
-            }
-
-            // Insufficient data
-            if (!lastRefill && (info.frame_bytes == 0) && (smpl == 0)) {
-                goto refill;
-            }
-
-            if (lastRefill && (info.frame_bytes == 0) && (smpl == 0)) {
-                return 0;
-            }
-        }
-
-        // Copy decoded samples to dest buffer
-        memcpy(pcmData, &pcmsamplesbuffer[0], samplesToReadChann * sizeof(int16_t));
-
-        // Manage samples in internal buffer
-        pcmsamplesbuffer_idx = samplesFetched - samplesToReadChann;
-        memmove(&pcmsamplesbuffer[0], &pcmsamplesbuffer[samplesToReadChann], pcmsamplesbuffer_idx * sizeof(int16_t));
-
-        /* Calculate frame duration in seconds */
-        position += (float)((float)(chanNumber == 2 ? samplesToRead / chanNumber : samplesToRead) / (float)sampleRate);
-
-        return samplesToRead;
+    drmp3_bool32 decoderMP3::drmp3_seek(void *pUserData, int offset, drmp3_seek_origin origin)
+    {
+        auto *userdata = reinterpret_cast<decoderMP3 *>(pUserData);
+        return !std::fseek(userdata->fd, offset, origin == drmp3_seek_origin_start ? SEEK_SET : SEEK_CUR);
     }
 
 } // namespace audio
