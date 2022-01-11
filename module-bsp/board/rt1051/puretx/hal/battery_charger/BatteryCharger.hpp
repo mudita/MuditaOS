12dff0764f3560dbb5fc745975026092ec90fb89
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2022-01-11 20:30:34 +0100

    [EGD-8141] Fix brownout handling while charging
    When the voltage dropped below the brownout threshold,
    the system did not detect the battery charging
    and displayed a window with the low battery icon all the time.

--- module-bsp/board/rt1051/puretx/hal/battery_charger/BatteryCharger.hpp
+++ module-bsp/board/rt1051/puretx/hal/battery_charger/BatteryCharger.hpp
@@ -1,9 +1,10 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
 
 #include <hal/battery_charger/AbstractBatteryCharger.hpp>
+#include <timers.h>
 
 namespace hal::battery
 {
@@ -21,6 +22,7 @@ namespace hal::battery
         void checkBatteryChargerInterrupts();
 
         AbstractBatteryCharger::BatteryChargerEvents &eventsHandler;
+        TimerHandle_t timerHandle;
     };
 
     BaseType_t INTBHandlerIRQ();
