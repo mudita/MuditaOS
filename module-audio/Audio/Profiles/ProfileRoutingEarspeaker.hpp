afbc30b3fdb5d4f0b1d6f4629ba0a02b7efc546f
Bartosz Cichocki <bartosz.cichocki@mudita.com>
2022-01-12 11:44:28 +0100

    [EGD-8159] Change earspeaker EQ
    Changed the earspeaker EQ according to the poll

--- module-audio/Audio/Profiles/ProfileRoutingEarspeaker.hpp
+++ module-audio/Audio/Profiles/ProfileRoutingEarspeaker.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 #pragma once
 
@@ -25,15 +25,15 @@ namespace audio
                           .outputPath   = audio::codec::OutputPath::Earspeaker,
                           .filterCoefficients =
                               {qfilter_CalculateCoeffs(
-                                   audio::equalizer::FilterType::FilterHighPass, 997.f, 44100, 0.701f, 0),
+                                   audio::equalizer::FilterType::FilterHighPass, 700.f, 44100, 0.701f, 10),
                                qfilter_CalculateCoeffs(
-                                   audio::equalizer::FilterType::FilterLowPass, 4993.7f, 44100, 0.701f, 0),
+                                   audio::equalizer::FilterType::FilterLowPass, 4993.7f, 44100, 0.701f, -5),
                                qfilter_CalculateCoeffs(
-                                   audio::equalizer::FilterType::FilterNone, 15975.7f, 44100, 0.701f, -10),
+                                   audio::equalizer::FilterType::FilterLowPass, 6000.7f, 44100, 0.701f, 10),
                                qfilter_CalculateCoeffs(
-                                   audio::equalizer::FilterType::FilterNone, 200.4f, 44100, 0.701f, -10),
+                                   audio::equalizer::FilterType::FilterHighPass, 100.4f, 44100, 0.701f, 10),
                                qfilter_CalculateCoeffs(
-                                   audio::equalizer::FilterType::FilterNone, 1496.7f, 44100, 0.701f, -4)}},
+                                   audio::equalizer::FilterType::FilterNotch, 1500.7f, 44100, 0.701f, -3)}},
                       AudioDevice::Type::Audiocodec)
         {}
     };
