5678f39948804d17f5d56d939cc4bee878284565
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2022-01-24 11:53:37 +0100

    [EGD-8170] Crash fix on PhoneWindow settings
    Missed when fixing others in bulk

--- module-apps/application-settings/windows/apps/PhoneWindow.hpp
+++ module-apps/application-settings/windows/apps/PhoneWindow.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -44,5 +44,7 @@ namespace gui
 
         /// Audio settings model
         std::unique_ptr<audio_settings::AbstractAudioSettingsModel> mAudioModel;
+
+        OptionWindowDestroyer rai_destroyer = OptionWindowDestroyer(*this);
     };
 } // namespace gui
