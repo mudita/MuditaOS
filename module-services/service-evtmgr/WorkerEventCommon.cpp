5f416baf9d18c7830c6bdce7c606fbb44029f4f7
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2022-02-01 13:58:11 +0100

    [EGD-8192] Deinit of hardware owned by Event manager
    Hardware de-initialization at the pre-close shutdown stage
    to turn off all hardware beforehand.

--- module-services/service-evtmgr/WorkerEventCommon.cpp
+++ module-services/service-evtmgr/WorkerEventCommon.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+﻿// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "service-evtmgr/EVMessages.hpp"
@@ -169,8 +169,6 @@ void WorkerEventCommon::init(std::shared_ptr<settings::Settings> settings)
 bool WorkerEventCommon::deinit(void)
 {
     Worker::deinit();
-
-    deinitProductHardware();
 
     keyInput->deinit();
     batteryController->deinit();
