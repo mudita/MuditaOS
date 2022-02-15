d3ead5759a4fdeee708d3c0158bfdd6620903bd1
wojtekrzepecki <wojtek.rzepecki@mudita.com>
2022-02-15 10:45:28 +0100

    [MOS-224] Fix phone lock window timeout
    extened timeout for better readbility

--- module-apps/apps-common/popups/lock-popups/PhoneLockedInfoWindow.cpp
+++ module-apps/apps-common/popups/lock-popups/PhoneLockedInfoWindow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "PhoneLockedInfoWindow.hpp"
@@ -11,7 +11,7 @@
 using namespace gui;
 
 PhoneLockedInfoWindow::PhoneLockedInfoWindow(app::ApplicationCommon *app, const std::string &name)
-    : WindowWithTimer(app, name, std::chrono::seconds(2))
+    : WindowWithTimer(app, name, std::chrono::seconds{4})
 {
     buildInterface();
 }
