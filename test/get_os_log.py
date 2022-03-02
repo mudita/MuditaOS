25922ba886d0b595513e43eccf9d151f0d5a1e29
Marek Niepieklo <marek.niepieklo@mudita.com>
2022-03-02 14:59:33 +0100

    [MOS-273] Update Harness to detect Mudita Bell devices
    Added detection of Mudita Harmony

--- test/get_os_log.py
+++ test/get_os_log.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+# Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 # For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 import sys
