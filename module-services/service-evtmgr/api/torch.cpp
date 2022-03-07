daccf6f77207300f3b7add4a9c8ac6b5d4fcdd4d
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2022-03-07 15:56:40 +0100

    [MOS-256] Turn off torch driver when not in use
    We save on power consumption ~0.7 mA.

--- module-services/service-evtmgr/api/torch.cpp
+++ module-services/service-evtmgr/api/torch.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "service-evtmgr/torch.hpp"
@@ -10,8 +10,7 @@ namespace event::service::api
 {
     bool isTorchOn()
     {
-        auto [works, isOn] = bsp::torch::getState();
-        assert(works);
+        auto isOn = bsp::torch::getState();
         return isOn == bsp::torch::State::on;
     }
 } // namespace event::service::api
