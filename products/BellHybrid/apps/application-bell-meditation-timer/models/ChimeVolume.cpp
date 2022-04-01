3cb9bf495dc7199c7856382b5c03458e8aec8db2
Tomasz Rybarski <tomasz.rybarski@digica.com>
2022-04-01 15:34:24 +0200

    [BH-1433] Chime Volume Model Fix
    Fixed saving of volume in Chime Model

--- products/BellHybrid/apps/application-bell-meditation-timer/models/ChimeVolume.cpp
+++ products/BellHybrid/apps/application-bell-meditation-timer/models/ChimeVolume.cpp
@@ -8,11 +8,12 @@ namespace app::meditation::models
 
     void ChimeVolume::setValue(std::uint8_t value)
     {
+        defaultValue = value;
         audioModel.setVolume(value, AbstractAudioModel::PlaybackType::Meditation, {});
     }
     std::uint8_t ChimeVolume::getValue() const
     {
-        return defaultValue;
+        return audioModel.getVolume(AbstractAudioModel::PlaybackType::Meditation).value_or(0);
     }
 
     void ChimeVolume::restoreDefault()
