2ddeaaf9876b6c27ab66b5d5ac0ecc3d62fa988e
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2022-04-07 15:30:11 +0200

    [MOS-380] Sentinels for each added that removes bad data
    As in topic

---
+++ module-sys/SystemManager/GovernorSentinelOperations.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#include "GovernorSentinelOperations.hpp"
+
+namespace sys
+{
+
+    void for_each_governed_sentinel(GovernorSentinelsVector &sentinels, governed_callback &foo)
+    {
+        for (auto sentinel = sentinels.rbegin(); sentinels.rend() != sentinel; ++sentinel) {
+            // remove dangling unique ptrs
+            if (not *sentinel) {
+                sentinels.erase(sentinel.base());
+                continue;
+            }
+            auto sentinelWeakPointer = (*sentinel)->GetSentinel();
+            // remove expired ptrs
+            if (sentinelWeakPointer.expired()) {
+                sentinels.erase(sentinel.base());
+                continue;
+            }
+            // call foo on sentinel
+            if (foo(**sentinel)) {
+                return;
+            };
+        }
+    };
+
+    void for_each_sentinel(GovernorSentinelsVector &sentinels, sentinel_foo &foo)
+    {
+        governed_callback cb = [&](const GovernorSentinel &sentinel) {
+            std::shared_ptr<CpuSentinel> sharedResource = sentinel.GetSentinel().lock();
+            if (sharedResource) {
+                if (foo(sharedResource)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        for_each_governed_sentinel(sentinels, cb);
+    };
+
+    void clean(GovernorSentinelsVector &sentinels)
+    {
+        governed_callback empty_governed_callback = [&](const GovernorSentinel &) { return false; };
+        for_each_governed_sentinel(sentinels, empty_governed_callback);
+    }
+} // namespace sys
