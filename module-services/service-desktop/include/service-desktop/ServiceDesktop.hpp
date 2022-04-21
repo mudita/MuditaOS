f3c7418ad3557a9d5d894f8586f4bf10d8d9b820
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2022-04-22 01:38:24 +0200

    [CP-1231] Refactor handlers in service desktop
    Refactored handlers of ses::messages and
    some cleanups in service desktop.

--- module-services/service-desktop/include/service-desktop/ServiceDesktop.hpp
+++ module-services/service-desktop/include/service-desktop/ServiceDesktop.hpp
@@ -3,22 +3,19 @@
 
 #pragma once
 
-#include <memory> // for allocator, unique_ptr
-
-#include "system/Common.hpp"   // for ReturnCodes, ServicePowerMode
-#include "Service/Message.hpp" // for MessagePointer, DataMessage (ptr only), ResponseMessage (ptr only)
-#include "Service/Service.hpp" // for Service
-#include "Timers/TimerHandle.hpp"
+#include <bsp/usb/usb.hpp>
 #include "Constants.hpp"
 #include "USBSecurityModel.hpp"
-#include "system/SystemReturnCodes.hpp"
+#include "DeveloperModeMessage.hpp"
+#include "DesktopMessages.hpp"
+#include <locks/data/PhoneLockMessages.hpp>
+#include <service-bluetooth/messages/Status.hpp>
+#include <service-bluetooth/messages/BondedDevices.hpp>
+#include <service-bluetooth/messages/ResponseVisibleDevices.hpp>
+#include <service-db/DBServiceName.hpp>
 #include <service-desktop/BackupRestore.hpp>
 #include <service-desktop/OutboxNotifications.hpp>
-#include <service-db/DBServiceName.hpp>
-
-#include <bsp/usb/usb.hpp>
-
-#include <filesystem>
+#include <service-evtmgr/BatteryMessages.hpp>
 
 namespace settings
 {
@@ -26,7 +23,6 @@ namespace settings
 }
 
 class WorkerDesktop;
-class UpdateMuditaOS;
 
 namespace sdesktop
 {
@@ -35,7 +31,6 @@ namespace sdesktop
     inline constexpr auto cdc_queue_len             = 1024;
     inline constexpr auto cdc_queue_object_size     = 1024;
     inline constexpr auto irq_queue_object_size     = sizeof(bsp::USBDeviceStatus);
-    inline constexpr auto file_transfer_timeout     = 5000;
     constexpr auto connectionActiveTimerName        = "connectionActiveTimer";
     constexpr auto connectionActiveTimerDelayMs     = std::chrono::milliseconds{1000 * 20};
     inline constexpr auto RECEIVE_QUEUE_BUFFER_NAME = "receiveQueueBuffer";
@@ -43,6 +38,8 @@ namespace sdesktop
     inline constexpr auto IRQ_QUEUE_BUFFER_NAME     = "irqQueueBuffer";
     inline constexpr auto DeviceUniqueIdLength      = 32;
     inline constexpr auto DeviceUniqueIdName        = "sd_device_unique_id";
+    constexpr auto pathFactoryDataSerial            = "factory_data/serial";
+    constexpr auto pathFactoryDataCaseColour        = "factory_data/case_colour";
 
 }; // namespace sdesktop
 
@@ -57,14 +54,13 @@ class ServiceDesktop : public sys::Service
     ServiceDesktop();
     ~ServiceDesktop() override;
 
+    std::unique_ptr<WorkerDesktop> desktopWorker;
     BackupRestore::OperationStatus backupRestoreStatus;
 
     sys::ReturnCodes InitHandler() override;
     sys::ReturnCodes DeinitHandler() override;
-    sys::ReturnCodes SwitchPowerModeHandler(const sys::ServicePowerMode mode) override;
+    sys::ReturnCodes SwitchPowerModeHandler(sys::ServicePowerMode mode) override;
     sys::MessagePointer DataReceivedHandler(sys::DataMessage *msg, sys::ResponseMessage *resp) override;
-
-    std::unique_ptr<WorkerDesktop> desktopWorker;
 
     std::string prepareBackupFilename();
     void prepareBackupData();
@@ -88,23 +84,44 @@ class ServiceDesktop : public sys::Service
     void removeNotificationEntries(const std::vector<uint32_t> &);
 
   private:
-    auto getDeviceUniqueId() const -> std::string;
-
-    void generateDeviceUniqueId();
-    void setDeviceUniqueId(const std::string &token);
-    auto usbWorkerInit() -> sys::ReturnCodes;
-    auto usbWorkerDeinit() -> sys::ReturnCodes;
-
     std::unique_ptr<sdesktop::USBSecurityModel> usbSecurityModel;
     std::unique_ptr<settings::Settings> settings;
     std::unique_ptr<sdesktop::bluetooth::BluetoothMessagesHandler> btMsgHandler;
     OutboxNotifications outboxNotifications;
     sys::TimerHandle connectionActiveTimer;
-    void restartConnectionActiveTimer();
-    void checkChargingCondition();
-
     static constexpr unsigned int DefaultLogFlushTimeoutInMs = 1000U;
     bool initialized                                         = false;
+
+    void generateDeviceUniqueId();
+    auto getDeviceUniqueId() const -> std::string;
+    void setDeviceUniqueId(const std::string &token);
+
+    auto usbWorkerInit() -> sys::ReturnCodes;
+    auto usbWorkerDeinit() -> sys::ReturnCodes;
+
+    void restartConnectionActiveTimer();
+
+    void checkChargingCondition();
+
+    template <typename T> auto connectHandler() -> bool
+    {
+        return connect(typeid(T), [&](sys::Message *msg) { return handle(static_cast<T *>(msg)); });
+    }
+
+    [[nodiscard]] auto handle(db::NotificationMessage *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(locks::UnlockedPhone *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(locks::LockedPhone *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(locks::NextPhoneUnlockAttemptLockTime *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(message::bluetooth::ResponseStatus *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(message::bluetooth::ResponseBondedDevices *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(message::bluetooth::ResponseVisibleDevices *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(sdesktop::developerMode::DeveloperModeRequest *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(sdesktop::BackupMessage *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(sdesktop::RestoreMessage *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(sdesktop::FactoryMessage *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(sdesktop::usb::USBConfigured *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(sdesktop::usb::USBDisconnected *msg) -> std::shared_ptr<sys::Message>;
+    [[nodiscard]] auto handle(sevm::BatteryStatusChangeMessage *msg) -> std::shared_ptr<sys::Message>;
 };
 
 namespace sys
