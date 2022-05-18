abe053e696fea4161af0def973598c00ed0f05d6
Lefucjusz <marcin.lyda@mudita.com>
2022-05-18 11:47:08 +0200

    [MOS-363] Call app texts not fully shown fix
    Fixes for the cut texts in call app problem.
    Problematic texts have been shortened
    and additionally icons have been slightly
    widened.

--- module-apps/application-call/widgets/StateIcon.hpp
+++ module-apps/application-call/widgets/StateIcon.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -16,9 +16,9 @@ namespace gui
 {
     namespace icon
     {
-        constexpr uint32_t w = 100;
-        constexpr uint32_t h = 100;
-        constexpr uint32_t defaultSideMargin = 20;
+        constexpr uint32_t w                 = 150;
+        constexpr uint32_t h                 = 100;
+        constexpr uint32_t defaultSideMargin = 5;
     }     // namespace icon
 
     /// @brief Icon widget with custom predefined images and strings
