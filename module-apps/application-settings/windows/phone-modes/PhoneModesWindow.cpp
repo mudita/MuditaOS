10a22314bd7c0ca70d6ea8d4d866f3788abee7a4
Adam Wulkiewicz <adam.wulkiewicz@mudita.com>
2022-07-05 23:27:54 +0200

    [MOS-586] Crash of simulator in Settings->Phone Modes
    Fix out of bounds memory access when selected phone mode is
    Uninitialized

--- module-apps/application-settings/windows/phone-modes/PhoneModesWindow.cpp
+++ module-apps/application-settings/windows/phone-modes/PhoneModesWindow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "PhoneModesWindow.hpp"
@@ -24,10 +24,14 @@ namespace gui
         auto selectedPhoneModeIndex = static_cast<unsigned int>(phoneModeSettings->getCurrentPhoneMode());
 
         modifiedModesStrings = initialModesStrings;
-        modifiedModesStrings[selectedPhoneModeIndex] =
-            "<text><b>" + modifiedModesStrings[selectedPhoneModeIndex] + " </b></text>";
-
-        refreshOptions(modesOptList(), selectedPhoneModeIndex);
+        if (selectedPhoneModeIndex < modifiedModesStrings.size()) {
+            modifiedModesStrings[selectedPhoneModeIndex] =
+                "<text><b>" + modifiedModesStrings[selectedPhoneModeIndex] + " </b></text>";
+            refreshOptions(modesOptList(), selectedPhoneModeIndex);
+        }
+        else {
+            refreshOptions(modesOptList());
+        }
     }
 
     auto PhoneModesWindow::modesOptList() -> std::list<gui::Option>
