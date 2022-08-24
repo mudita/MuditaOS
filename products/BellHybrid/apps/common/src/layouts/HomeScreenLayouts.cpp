22819c5f6749fcce7d0df95bc63172a13a0e8f61
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-08-24 06:45:37 +0200

    [BH-1558] Temperature layouts conf
    Enable/disable temperature layouts based on
    the global configuration.

--- products/BellHybrid/apps/common/src/layouts/HomeScreenLayouts.cpp
+++ products/BellHybrid/apps/common/src/layouts/HomeScreenLayouts.cpp
@@ -6,30 +6,53 @@
 #include <common/layouts/HomeScreenLayoutClassic.hpp>
 #include <common/layouts/HomeScreenLayoutClassicWithAmPm.hpp>
 #include <common/layouts/HomeScreenLayoutClassicWithBattery.hpp>
+#if CONFIG_ENABLE_TEMP == 1
 #include <common/layouts/HomeScreenLayoutClassicWithTemp.hpp>
-#include <common/layouts/HomeScreenLayoutVertical.hpp>
+#endif
 #include <common/layouts/HomeScreenLayoutVerticalSimple.hpp>
 #include <common/layouts/HomeScreenLayoutVerticalWithAmPm.hpp>
+#include <ProductConfig.hpp>
 
 namespace gui::factory
 {
     std::map<std::string, LayoutGenerator> getLayoutsFormat24h()
     {
-        return {{"Classic", []() { return new HomeScreenLayoutClassic("Classic"); }},
-                {"ClassicWithTemp", []() { return new HomeScreenLayoutClassicWithTemp("ClassicWithTemp"); }},
+        return
+        {
+            {"Classic", []() { return new HomeScreenLayoutClassic("Classic"); }},
                 {"ClassicWithBattery", []() { return new HomeScreenLayoutClassicWithBattery("ClassicWithBattery"); }},
-                {"VerticalSimple", []() { return new HomeScreenLayoutVerticalSimple("VerticalSimple"); }}};
-    };
+            {
+                "VerticalSimple", []() { return new HomeScreenLayoutVerticalSimple("VerticalSimple"); }
+            }
+#if CONFIG_ENABLE_TEMP == 1
+            ,
+            {
+                "ClassicWithTemp", []() { return new HomeScreenLayoutClassicWithTemp("ClassicWithTemp"); }
+            }
+#endif
+        };
+    }
 
     std::map<std::string, LayoutGenerator> getLayoutsFormat12h()
     {
-        return {{"Classic", []() { return new HomeScreenLayoutClassic("Classic"); }},
-                {"ClassicWithTemp", []() { return new HomeScreenLayoutClassicWithTemp("ClassicWithTemp"); }},
+        return
+        {
+            {"Classic", []() { return new HomeScreenLayoutClassic("Classic"); }},
+
                 {"ClassicWithAmPm", []() { return new HomeScreenLayoutClassicWithAmPm("ClassicWithAmPm"); }},
                 {"ClassicWithBattery", []() { return new HomeScreenLayoutClassicWithBattery("ClassicWithBattery"); }},
                 {"VerticalSimple", []() { return new HomeScreenLayoutVerticalSimple("VerticalSimple"); }},
-                {"VerticalWithAmPm", []() { return new HomeScreenLayoutVerticalWithAmPm("VerticalWithAmPm"); }}};
-    };
+            {
+                "VerticalWithAmPm", []() { return new HomeScreenLayoutVerticalWithAmPm("VerticalWithAmPm"); }
+            }
+#if CONFIG_ENABLE_TEMP == 1
+            ,
+            {
+                "ClassicWithTemp", []() { return new HomeScreenLayoutClassicWithTemp("ClassicWithTemp"); }
+            }
+#endif
+        };
+    }
 
     std::map<std::string, LayoutGenerator> getAllLayouts()
     {
