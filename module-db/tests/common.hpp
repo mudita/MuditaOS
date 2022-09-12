330900432d85f68aec1a5807dec601233f99deda
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-09-12 10:34:04 +0200

    [CP-1560] New user directory structure
    * New user directory implemented.
    * Generate user directory structure at compile
    time instead of at runtime(Pure/Harmony).
    * Changed MTP root path to /storage
    * FileIndexer: Minor refactor
    * FileIndexer: Fixed handling incorrect/non-existing
    scan directories.
    * Updated Repository module to correctly handle
    many assets paths.
    * MultimediaDB: Minor unit tests refactor and fixed
    some issues when using simulator.
    * MultimediaDB: Added new queries and unit tests
    * Harmony/Relaxation: Updated to correctly
    use audio assets from more than one source.
    * Harmony/Relaxation: Updated model and list items
    provider.
    * Harmony/Relaxation: Fixed stack overflow in audio service

--- module-db/tests/common.hpp
+++ module-db/tests/common.hpp
@@ -3,6 +3,43 @@
 
 #pragma once
 
+#include "Database/Database.hpp"
+#include <catch2/catch.hpp>
 #include <string>
+#include <filesystem>
 
 void RemoveDbFiles(const std::string &dbName);
+
+template <typename Db>
+class DatabaseUnderTest
+{
+  public:
+    explicit DatabaseUnderTest(std::filesystem::path name)
+    {
+        Database::initialize();
+
+        if (std::filesystem::exists(name)) {
+            std::filesystem::remove(name);
+        }
+
+        db = std::make_unique<Db>(name.c_str());
+
+        if (not db->isInitialized()) {
+            throw std::runtime_error("Could not initialize database");
+        }
+    }
+
+    ~DatabaseUnderTest()
+    {
+        Database::deinitialize();
+    }
+
+    Db &get()
+    {
+        return *db;
+    }
+
+  private:
+    std::filesystem::path name;
+    std::unique_ptr<Db> db;
+};
--- module-vfs/paths/filesystem_paths.cpp
+++ module-vfs/paths/filesystem_paths.cpp
@@ -5,18 +5,20 @@
 
 namespace
 {
-    constexpr inline auto PATH_SYS         = "/sys";
-    constexpr inline auto PATH_CONF        = "/mfgconf";
-    constexpr inline auto PATH_USER        = "user";
-    constexpr inline auto PATH_CURRENT     = "current";
-    constexpr inline auto PATH_PREVIOUS    = "previous";
-    constexpr inline auto PATH_UPDATES     = "updates";
-    constexpr inline auto PATH_TMP         = "tmp";
-    constexpr inline auto PATH_BACKUP      = "backup";
-    constexpr inline auto PATH_FACTORY     = "factory";
-    constexpr inline auto PATH_LOGS        = "logs";
-    constexpr inline auto PATH_CRASH_DUMPS = "crash_dumps";
-    constexpr inline auto eMMC_disk        = PATH_SYS;
+    constexpr auto PATH_SYS          = "/sys";
+    constexpr auto PATH_CONF         = "/mfgconf";
+    constexpr auto PATH_USER         = "user";
+    constexpr auto PATH_CURRENT      = "current";
+    constexpr auto PATH_PREVIOUS     = "previous";
+    constexpr auto PATH_UPDATES      = "updates";
+    constexpr auto PATH_TMP          = "tmp";
+    constexpr auto PATH_BACKUP       = "backup";
+    constexpr auto PATH_FACTORY      = "factory";
+    constexpr auto PATH_LOGS         = "logs";
+    constexpr auto PATH_CRASH_DUMPS  = "crash_dumps";
+    constexpr auto PATH_USER_AUDIO   = "audio";
+    constexpr auto PATH_USER_STORAGE = "storage";
+    constexpr auto eMMC_disk         = PATH_SYS;
 } // namespace
 
 namespace purefs
@@ -81,6 +83,14 @@ namespace purefs
         std::filesystem::path getCrashDumpsPath() noexcept
         {
             return getUserDiskPath() / PATH_CRASH_DUMPS;
+        }
+        std::filesystem::path getUserAudioPath() noexcept
+        {
+            return getUserDiskPath() / PATH_USER_AUDIO;
+        }
+        std::filesystem::path getUserStoragePath() noexcept
+        {
+            return getUserDiskPath() / PATH_USER_STORAGE;
         }
     } // namespace dir
 } // namespace purefs
