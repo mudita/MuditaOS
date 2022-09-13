cf6cca223085847e33992624dccc77a0f3ce61a2
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2022-09-13 20:16:35 +0200

    [CP-1484] Add new mechanism for data synchronization between MOS and MC
    This mechanism is similar to the current backup implementation,
    but is stripped of irrelevant data that is not required by the
    MC to perform data synchronization.

--- module-services/service-db/include/service-db/DBServiceMessage.hpp
+++ module-services/service-db/include/service-db/DBServiceMessage.hpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+﻿// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -15,6 +15,13 @@ class DBServiceMessageBackup : public DBMessage
   public:
     DBServiceMessageBackup(MessageType messageType, std::string backupPath);
     std::string backupPath;
+};
+
+class DBServiceMessageSyncPackage : public DBMessage
+{
+  public:
+    DBServiceMessageSyncPackage(MessageType messageType, const std::string &syncPackagePath);
+    std::string syncPackagePath;
 };
 
 class DBServiceResponseMessage : public DBResponseMessage
--- module-services/service-db/messages/DBServiceMessage.cpp
+++ module-services/service-db/messages/DBServiceMessage.cpp
@@ -1,10 +1,14 @@
-﻿// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+﻿// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include <service-db/DBServiceMessage.hpp>
 
 DBServiceMessageBackup::DBServiceMessageBackup(MessageType messageType, std::string backupPath)
     : DBMessage(messageType), backupPath(backupPath)
+{}
+
+DBServiceMessageSyncPackage::DBServiceMessageSyncPackage(MessageType messageType, const std::string &syncPackagePath)
+    : DBMessage(messageType), syncPackagePath(syncPackagePath)
 {}
 
 DBServiceResponseMessage::DBServiceResponseMessage(uint32_t retCode, uint32_t count, MessageType respTo)
