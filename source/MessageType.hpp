19895c78a23d61babfbd53b15805f659f4f3c283
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2022-09-13 20:16:35 +0200

    [CP-1484] Add new mechanism for data synchronization between MOS and MC
    This mechanism is similar to the current backup implementation,
    but is stripped of irrelevant data that is not required by the
    MC to perform data synchronization.

--- source/MessageType.hpp
+++ source/MessageType.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -17,6 +17,7 @@ enum class MessageType
 
     DBServiceNotification, ///< Common service-db notification message.
     DBServiceBackup,
+    DBSyncPackage,
 
     DBSettingsGet,    ///< get current settings from database
     DBSettingsUpdate, ///< update settings
@@ -116,6 +117,7 @@ enum class MessageType
     // service-desktop  messages
     UpdateOS,
     Backup,
+    Sync,
     Restore,
     Factory,
     DeveloperModeRequest,
