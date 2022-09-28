55a7edc208dabd87b67c7848bbee674b25a6abe9
Lefucjusz <marcin.lyda@mudita.com>
2022-09-28 13:46:38 +0200

    [MOS-743] Workaround for audio config paths
    Workaround for the issue that updater
    places audio config JSON files in
    wrong location. This should be
    removed after the updater is fixed.
    Additionally update of loudspeaker
    routing config.

--- module-audio/Audio/Profiles/ProfilePlaybackHeadphones.hpp
+++ module-audio/Audio/Profiles/ProfilePlaybackHeadphones.hpp
@@ -14,7 +14,7 @@ namespace audio
             : Profile(
                   "Playback Headphones",
                   Type::PlaybackHeadphones,
-                  purefs::dir::getUserDiskPath() / "data/equalizer/headphones_playback.json",
+                  purefs::dir::getCurrentOSPath() / "user/data/equalizer/headphones_playback.json",
                   audio::codec::Configuration{
                       .sampleRate_Hz = 0,
                       .bitWidth      = 16,
