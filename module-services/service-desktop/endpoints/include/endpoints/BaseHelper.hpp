bb5be3dca1655a450523fd58494596d4c6615a57
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2022-10-21 19:19:51 +0200

    [CP-1427] Cleanup leftovers after old backup restore implementation
    The synchronization mechanism has been freed from the constraints
    of the old backup and restore implementation.

--- module-services/service-desktop/endpoints/include/endpoints/BaseHelper.hpp
+++ module-services/service-desktop/endpoints/include/endpoints/BaseHelper.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -56,6 +56,7 @@ namespace sdesktop::endpoints
       public:
         explicit BaseHelper(sys::Service *p) : owner(p)
         {}
+        virtual ~BaseHelper() = default;
 
         /// generall processing function
         ///
