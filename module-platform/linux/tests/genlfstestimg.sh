f8ef3220e25cfdf9d1f39220052f5c108cca068e
Lefucjusz <marcin.lyda@mudita.com>
2022-11-21 14:16:57 +0100

    [MOS-783] Fixed Pure unit tests
    Fixed (most of the) hardcoded
    paths in Pure's unit tests.

--- module-platform/linux/tests/genlfstestimg.sh
+++ module-platform/linux/tests/genlfstestimg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-#Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+#Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 #For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 usage() {
@@ -15,23 +15,28 @@ Usage: $(basename $0) [image_size] [image_file] [sysroot] [files]...
 if [ $# -lt 2 ]; then
 	echo "Error! Invalid argument count"
 	usage
-	exit -1
+	exit 1
 fi
 IMAGE_SIZE="$1"
 IMAGE_FILE="$2"
 SYSROOT="$3"
 shift 3
 
-if [ ! -d "${SYSROOT}/sys" ]; then
-	echo "Invalid sysroot: ${SYSROOT}"
-	exit -1
+# Validate image source folder
+if [ ! -d "${SYSROOT}/system_a" ]; then
+	echo "Fatal! Image folder 'system_a' missing in build. Check build system."
+	exit 1
+fi
+if [ ! -d "${SYSROOT}/user" ]; then
+	echo "Fatal! Image folder 'user' missing in build. Check build system."
+	exit 1
 fi
 
 _REQ_CMDS="sfdisk truncate"
 for cmd in $_REQ_CMDS; do
 	if [ ! $(command -v $cmd) ]; then
 		echo "Error! $cmd is not installed, please use 'sudo apt install' for install required tool"
-		exit -1
+		exit 1
 	fi
 done
 truncate -s $IMAGE_SIZE $IMAGE_FILE
@@ -46,4 +51,4 @@ unit: sectors
 /dev/sdz1 : start=$SECTOR_START, size=$SECTOR_END, type=9e
 ==sfdisk
 pwd
-./genlittlefs --image $IMAGE_FILE --block_size=32768  --overwrite  --partition_num 1 -- ${SYSROOT}/sys/.boot.json ${SYSROOT}/sys/current/assets/* module-platform/test_dir/*
+./genlittlefs --image $IMAGE_FILE --block_size=32768  --overwrite  --partition_num 1 -- ${SYSROOT}/user/boot.json ${SYSROOT}/system_a/assets/* module-platform/test_dir/*
