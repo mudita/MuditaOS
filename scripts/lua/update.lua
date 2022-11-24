c8930ac85f8ba0ebdc57280dccc593b9e90e079e
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-11-24 10:28:37 +0100

    [MOS-806] Prepare scripts dependencies
    * Added scripts dependencies
    * Added script to generate scripts package
    automatically
    * Added integration with PureOS CMake
    * [MOS-804] Created factory reset script

---
+++ scripts/lua/update.lua
@@ -0,0 +1,11 @@
+local update = {}
+
+update.script_name = "update.lua"
+update.img_in_progress = "assets/gui_image_update_in_progress.bin"
+update.img_success = "assets/gui_image_update_success.bin"
+update.img_failure = "assets/gui_image_update_failed.bin"
+
+function update.execute()
+end
+
+return update
