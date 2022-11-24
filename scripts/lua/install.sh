04c7a4277d1b0fbcdd317ff19efd0fee3e9e155c
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-11-24 10:28:37 +0100

    [MOS-806] Prepare scripts dependencies
    * Added scripts dependencies
    * Added script to generate scripts package
    automatically
    * Added integration with PureOS CMake
    * [MOS-804] Created factory reset script

---
+++ scripts/lua/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+# For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+function validate_product_selection() {
+    case ${PRODUCT,,} in
+        pure | purephone)
+            PRODUCT_SHORTNAME="pure"
+            PRODUCT="PurePhone"
+            return 0 ;;
+        bell | bellhybrid)
+            PRODUCT_SHORTNAME="bell"
+            PRODUCT="BellHybrid"
+            return 0 ;;
+        *)
+            echo "Wrong product: \"${PRODUCT}\""
+    esac
+}
+
+PRODUCT=$1
+DESTINATION=$2
+
+if validate_product_selection; then
+    if [ -d ${DESTINATION} ]; then
+        rm -Rf ${DESTINATION}
+    fi
+    parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+    cd "$parent_path"
+
+    mkdir -p ${DESTINATION}
+    cp -r assets/${PRODUCT} ${DESTINATION}/assets/
+    cp -r share ${DESTINATION}/share
+    cp -r migration/migration.lua ${DESTINATION}/share
+    cp *.lua ${DESTINATION}
+    cd -
+fi
