902a3a315eb8fd14bc8cadc827300f90f43a5152
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-11-24 10:28:37 +0100

    [MOS-806] Prepare scripts dependencies
    * Added scripts dependencies
    * Added script to generate scripts package
    automatically
    * Added integration with PureOS CMake
    * [MOS-804] Created factory reset script

---
+++ cmake/modules/AddScripts.cmake
@@ -0,0 +1,18 @@
+function(add_scripts_target)
+    cmake_parse_arguments(
+            _ARG
+            ""
+            "TARGET;DEST_DIR;PRODUCT"
+            "DEPENDS"
+            ${ARGN}
+    )
+
+    add_custom_target(
+            ${_ARG_TARGET}
+            DEPENDS ${_ARG_DEPENDS}
+
+            COMMAND  ${PROJECT_SOURCE_DIR}/scripts/lua/install.sh ${_ARG_PRODUCT} ${_ARG_DEST_DIR}
+            COMMENT
+            "Installing scripts for ${_ARG_PRODUCT} to ${_ARG_DEST_DIR} directory"
+    )
+endfunction()
