bc855f94b05c7fa3ddef2cbab44c6633235f32ae
Bartosz <sp2fet@gmail.com>
2022-11-24 14:14:36 +0100

    [MOS-000] Rebase fixes
    Fixes for master -> UDM rebase

---
+++ products/BellHybrid/alarms/include/AlarmSoundPaths.hpp
@@ -0,0 +1,22 @@
+
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <filesystem>
+#include <vector>
+
+namespace alarms::paths
+{
+    std::filesystem::path getAlarmDir() noexcept;
+    std::filesystem::path getMusicDir() noexcept;
+    std::filesystem::path getPreWakeUpChimesDir() noexcept;
+    std::filesystem::path getSnoozeChimesDir() noexcept;
+    std::filesystem::path getBedtimeReminderChimesDir() noexcept;
+    std::filesystem::path getBackgroundSoundsDir() noexcept;
+    std::filesystem::path getMeditationSoundsDir() noexcept;
+
+    /// Check if system paths exist. In case of error, returns vector of missing entries.
+    std::vector<std::filesystem::path> validate() noexcept;
+} // namespace alarms::paths
