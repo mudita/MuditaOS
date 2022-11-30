adc57be17fdbc8a0fb5a0fb9c6c534981f566639
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-11-30 16:44:34 +0100

    [MOS-835] Per product layout of database migration scripts
    Implemented DB init migration and version.json update

---
+++ module-db/databases/migration/events/0/up.sql
@@ -0,0 +1,23 @@
+-- Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+-- For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+CREATE TABLE IF NOT EXISTS events(
+                                     _id INTEGER PRIMARY KEY,
+                                     name TEXT DEFAULT '',
+                                     start_date DATETIME,
+                                     end_date DATETIME,
+                                     duration INTEGER,
+                                     is_all_day BOOLEAN,
+                                     rrule TEXT DEFAULT ''
+);
+
+CREATE TABLE IF NOT EXISTS alarms(
+                                     _id INTEGER PRIMARY KEY,
+                                     hour INTEGER,
+                                     minute INTEGER,
+                                     music_tone TEXT,
+                                     enabled BOOLEAN,
+                                     snooze_duration INTEGER,
+                                     rrule TEXT DEFAULT ''
+);
+
--- products/PurePhone/services/db/databases/scripts/sms_002.sql
+++
@@ -1,4 +0,0 @@
--- Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
--- For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
-
-INSERT OR IGNORE INTO threads_count ( _id, count ) VALUES (1,0);
