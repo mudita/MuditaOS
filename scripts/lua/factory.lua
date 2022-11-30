a100cd21b2d0a2b10fe904870a43b0e9f4d355cb
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-11-30 09:00:00 +0100

    [MOS-803] Add backup/restore scripts
    * Added test directory layout
    * Added test assets
    * Added backup/restore scripts

--- scripts/lua/factory.lua
+++ scripts/lua/factory.lua
@@ -1,12 +1,7 @@
 local helpers = require('helpers')
-local recovery = require('recovery')
+local paths = require('paths')
 
 local factory = {}
-local user_dir = recovery.sys.user()
-local system_dir = recovery.sys.source_slot()
-local db_dir = system_dir .. "/db"
-local db_factory_dir = system_dir .. "/db/factory"
-local file_indexer_cache = system_dir .. "/data/.directory_is_indexed"
 
 factory.script_name = "factory.lua"
 factory.img_in_progress = "assets/gui_image_factory_reset_in_progress.bin"
@@ -14,19 +9,19 @@ factory.img_success = "assets/gui_image_factory_reset_success.bin"
 factory.img_failure = "assets/gui_image_factory_reset_failed.bin"
 
 local function remove_old_databases()
-    print(string.format("Removing old databases from '%s'", db_dir))
-    helpers.rm_files_from_dir(db_dir)
+    print(string.format("Removing old databases from '%s'", paths.db_dir))
+    helpers.rm_files_from_dir(paths.db_dir)
 end
 
 local function copy_clean_databases()
-    print(string.format("Copying clean databases from '%s' to '%s'", db_factory_dir, db_dir))
-    helpers.copy_dir(db_factory_dir, db_dir)
+    print(string.format("Copying clean databases from '%s' to '%s'", paths.db_factory_dir, paths.db_dir))
+    helpers.copy_dir(paths.db_factory_dir, paths.db_dir)
 end
 
 local function remove_cache()
-    if helpers.exists(file_indexer_cache) then
-        print(string.format("Removing cache file '%s'", file_indexer_cache))
-        assert(os.remove(file_indexer_cache))
+    if helpers.exists(paths.file_indexer_cache) then
+        print(string.format("Removing cache file '%s'", paths.file_indexer_cache))
+        assert(os.remove(paths.file_indexer_cache))
     end
 end
 
---
+++ scripts/lua/share/consts.lua
@@ -0,0 +1,5 @@
+local consts = {}
+
+consts.version_file = "version.json"
+
+return consts
