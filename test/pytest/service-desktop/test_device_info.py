ab175bf2b8986a183c5398e75cd0d23c0cad9ca2
Lefucjusz <marcin.lyda@mudita.com>
2022-12-05 13:08:04 +0100

    [CP-1421] Fix backup, restore and device info tests
    Fix of the aforementioned tests after changes
    implemented in PureOS.

--- test/pytest/service-desktop/test_device_info.py
+++ test/pytest/service-desktop/test_device_info.py
@@ -18,16 +18,19 @@ def test_get_device_information(harness):
     assert ret.diag_info["accessTechnology"] in ['0', '2', '3', '4', '5', '6', '7', '255']
     assert 0 <= int(ret.diag_info["networkStatus"]) < 7
     assert ret.diag_info["networkOperatorName"] is not None
-    assert int(ret.diag_info["deviceSpaceTotal"]) == 14945
-    assert int(ret.diag_info["systemReservedSpace"]) == 2042
     assert 0 < int(ret.diag_info["usedUserSpace"]) <= 12903
+    assert int(ret.diag_info["systemReservedSpace"]) == 1024
+    assert int(ret.diag_info["deviceSpaceTotal"]) == (12903 + 1024)  # User + system partitions sizes
     assert re.match(r"^(\d|[a-z]){8,40}$", ret.diag_info["gitRevision"])
     assert ret.diag_info["gitBranch"] is not None
     assert int(ret.diag_info["currentRTCTime"]) > 1641991996
     assert re.match(r"^\d\.\d\.\d", ret.diag_info["version"])
     assert re.match(r"^\d{3,14}$", ret.diag_info["serialNumber"])
     assert ret.diag_info["caseColour"] in ["gray", "black"]
-    assert ret.diag_info["backupLocation"] == "/sys/user/backup"
+    assert ret.diag_info["recoveryStatusFilePath"] == "/user/temp/recovery_status.json"
+    assert ret.diag_info["updateFilePath"] == "/user/temp/update.tar"
+    assert ret.diag_info["backupFilePath"] == "/user/temp/backup.tar"
+    assert ret.diag_info["syncFilePath"] == "/user/temp/sync.tar"
     assert re.match(r"^(\d|[a-zA-Z]){32}$", ret.diag_info["deviceToken"])
 
 
@@ -35,11 +38,11 @@ def test_get_device_information(harness):
 @pytest.mark.usefixtures("phone_unlocked")
 def test_get_list_of_log_files(harness):
     ret = GetDiagnosticFilesList(DiagnosticsFileList.LOGS).run(harness)
-    assert "/sys/user/logs/MuditaOS.log" in ret.files
+    assert "/system/log/MuditaOS.log" in ret.files
 
 @pytest.mark.skip(reason = "Deactivated until we have possibility to generate crash dumps on demand [CP-690]")
 @pytest.mark.service_desktop_test
 @pytest.mark.usefixtures("phone_unlocked")
 def test_get_list_of_crash_dump_files(harness):
     ret = GetDiagnosticFilesList(DiagnosticsFileList.CRASH_DUMPS).run(harness)
-    assert "/sys/user/crash_dumps/crashdump.hex" in ret.files
+    assert "/system/crash_dumps/crashdump.hex" in ret.files
