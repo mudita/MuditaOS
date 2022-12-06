48a3961d3fff903e88fa14e8f5f5ddb99d9e6e00
Bartosz <sp2fet@gmail.com>
2022-12-06 15:38:16 +0100

    [MOS-845] Create pre-patch release for UDM
    This release has some changes needed to bootstrap UDM release

--- products/PurePhone/BinaryAssetsVersions.cmake
+++ products/PurePhone/BinaryAssetsVersions.cmake
@@ -1,9 +1,11 @@
 # This file sets versions of downloaded binaries for release packaging purposes
 
 if( NOT DEFINED ECOBOOT_BIN_VERSION)
-    set(ECOBOOT_BIN_VERSION 1.1.2 CACHE STRING "bootloader binary version to download from bootloader release page")
+    set(ECOBOOT_BIN_VERSION 1.2.2 CACHE STRING "bootloader binary version to download from bootloader release page")
 endif()
 
 if (NOT DEFINED UPDATER_BIN_VERSION)
     set(UPDATER_BIN_VERSION 1.5.4 CACHE STRING "updater binary version to download from updater release page")
 endif()
+
+
