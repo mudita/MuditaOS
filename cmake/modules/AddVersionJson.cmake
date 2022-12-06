960248e07e02ddb48a019917ea8471a186f439d4
Bartosz <sp2fet@gmail.com>
2022-12-06 09:26:16 +0100

    [MOS-835] Fix version.json entry
    Renamed boot to os

--- cmake/modules/AddVersionJson.cmake
+++ cmake/modules/AddVersionJson.cmake
@@ -6,9 +6,9 @@ function(add_version_rt1051_json SOURCE_TARGET)
             -DDST_FILE=${CMAKE_BINARY_DIR}/${SOURCE_TARGET}-version.json
             -DBOOTLOADER_FILENAME=ecoboot.bin
             -DBOOTLOADER_FILE=${SYSROOT_BIN_PATH}/ecoboot.bin
-            -DBOOT_FILENAME=os.bin
-            -DBOOT_FILE=$<TARGET_PROPERTY:${SOURCE_TARGET},BIN_FILE>
-            -DBOOT_VERSION=${PROJECT_VERSION}
+            -DOS_FILENAME=os.bin
+            -DOS_FILE=$<TARGET_PROPERTY:${SOURCE_TARGET},BIN_FILE>
+            -DOS_VERSION=${PROJECT_VERSION}
             -DRECOVERY_FILENAME=recovery.bin
             -DRECOVERY_FILE=${SYSROOT_BIN_PATH}/recovery.bin
             -DOS_VERSION_MAJOR=${OS_VERSION_MAJOR}
