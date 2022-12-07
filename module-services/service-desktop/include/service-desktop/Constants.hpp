3e31af9d49beda94a55875c8a9563e98997c8b65
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-12-07 10:54:50 +0100

    [MOS-807] Unpacking update package
    * Added unpacking and validating of
    update package
    * Removed 'utils-bootconfig'
    * Added 'tar' module
    * Minor compilation issues fixed

--- module-services/service-desktop/include/service-desktop/Constants.hpp
+++ module-services/service-desktop/include/service-desktop/Constants.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -8,4 +8,12 @@
 namespace service::name
 {
     inline constexpr auto service_desktop = "ServiceDesktop";
-};
+}
+namespace sdesktop::paths
+{
+    inline constexpr auto updateFilename         = "update.tar";
+    inline constexpr auto syncFilename           = "sync.tar";
+    inline constexpr auto backupFilename         = "backup.tar";
+    inline constexpr auto recoveryStatusFilename = "recovery_status.json";
+
+} // namespace sdesktop::paths
