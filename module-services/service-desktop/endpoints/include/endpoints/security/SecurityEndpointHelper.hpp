783ba3cfd45c09588a354c65499383e40901cf77
Mateusz Piesta <mateusz.piesta@gmail.com>
2022-12-07 10:54:50 +0100

    [MOS-807] Unpacking update package
    * Added unpacking and validating of
    update package
    * Removed 'utils-bootconfig'
    * Added 'tar' module
    * Minor compilation issues fixed

--- module-services/service-desktop/endpoints/include/endpoints/security/SecurityEndpointHelper.hpp
+++ module-services/service-desktop/endpoints/include/endpoints/security/SecurityEndpointHelper.hpp
@@ -1,9 +1,11 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
 
 #include <endpoints/BaseHelper.hpp>
+
+#include <ctime>
 
 namespace sdesktop::endpoints
 {
