03379186f9d094b1fc29bbc19be0e742ccbbd314
Bartosz <sp2fet@gmail.com>
2022-12-14 18:45:57 +0100

    [MOS-000] Fixed tests for update
    Fixed tests for performing update, now it does not require
    developer endpoint

--- test/pytest/test_updater.py
+++ test/pytest/test_updater.py
@@ -3,54 +3,73 @@
 
 import os
 import pytest
-import logging
-from harness import log
+import shutil
+from harness import log, utils
 from harness.interface.defs import Method, Endpoint
-from harness.request import Transaction, Request, TransactionError
 from harness.rt_harness_discovery import get_harness_automatic
 from harness.harness import Harness
 from harness.api.filesystem import put_file, get_file
-from harness.api.developermode import PhoneModeLock
 from harness.api.update import PhoneReboot, Reboot
+from harness.api.device_info import GetDeviceInfo
+
+old_version = None
+old_sha = None
 
 
 def get_version(harness: Harness):
     r = harness.request(Endpoint.DEVICEINFO, Method.GET, {}).response
     version = r.body["version"]
     sha = r.body["gitRevision"]
-    return f"version: {version} sha: {sha}"
+    return version, sha
 
 
-def disable_some_logs(harness: Harness):
-    from harness.interface.defs import PureLogLevel
-    for val in ["SysMgrService", "ServiceDesktop_w2", "CellularMux"]:
-        ret = harness.request(Endpoint.DEVELOPERMODE, Method.POST, {
-                              "log": True, "service": val, "level": PureLogLevel.LOGERROR.value})
-        log.info(f"{ret.response}")
-
-
-@pytest.mark.usefixtures("phone_unlocked")
+@pytest.mark.usefixtures("phone_security_unlocked")
 @pytest.mark.rt1051
-def test_update(harness: Harness):
-    filename = "update.tar"
+@pytest.mark.order("first")
+def test_perform_update(harness: Harness, update_file_path):
+    filename = os.path.basename(update_file_path)
+    path = os.path.dirname(update_file_path)
 
-    log.info(get_version(harness))
-    PhoneModeLock(False).run(harness)
-    put_file(harness, filename, "/sys/user/temp")
+    if filename != "update.tar":
+        filename = "update.tar"
+        try:
+            os.remove(os.path.join(path, filename))
+        except OSError:
+            pass
+        shutil.copyfile(update_file_path, os.path.join(path, filename))
+        update_file_path = os.path.join(path, filename)
+
+    global old_version
+    global old_sha
+    (old_version, old_sha) = get_version(harness)
+
+    put_file(harness, update_file_path, "/sys/user/temp")
     PhoneReboot(Reboot.UPDATE).run(harness)
     assert harness.connection.watch_port_reboot(300)
 
-    harness = get_harness_automatic(300)
-    import time
-    time.sleep(15)
-    harness.unlock_phone()
-    PhoneModeLock(False).run(harness)
 
-    log.info(get_version(harness))
-    get_file(harness, "updater.log", "./")
-    with open("updater.log") as f:
+@pytest.mark.usefixtures("phone_security_unlocked")
+@pytest.mark.rt1051
+@pytest.mark.order("second")
+def test_verify_update():
+    global old_version
+    global old_sha
+
+    harness = get_harness_automatic(300)
+
+    ret = GetDeviceInfo().run(harness)
+    recovery_status_path = ret.response.body["recoveryStatusFilePath"]
+    path = os.path.dirname(recovery_status_path)
+    filename = os.path.basename(recovery_status_path)
+
+    (new_version, new_sha) = get_version(harness)
+    assert new_version != old_version
+    assert new_sha != old_sha
+
+    get_file(harness, file_pure=filename, path_local="./", path_pure=path)
+    with open(filename) as f:
         line = f.readline()
-        assert "OK" in line
-    PhoneModeLock(True).run(harness)
+        print(line)
+        assert "successful" in line
 
     log.info("update done!")
