bf58d68c15efec52f29fa0b1834a9f076b3f488d
Lefucjusz <marcin.lyda@mudita.com>
2022-12-19 16:08:18 +0100

    [MOS-852] Fix autolock not working in UDM version
    Fix of the issue that in some cases phone
    wouldn't lock automatically after given
    timeout. This is not a proper fix,
    rather a workaround, the whole autolock
    mechanism should get refactored to
    support testing via harness.

--- products/PurePhone/services/appmgr/ApplicationManager.cpp
+++ products/PurePhone/services/appmgr/ApplicationManager.cpp
@@ -156,7 +156,7 @@ namespace app::manager
 
         connect(typeid(PreventBlockingRequest), [this]([[maybe_unused]] sys::Message *msg) {
             if (!phoneLockHandler.isPhoneLocked()) {
-                autoLockTimer.stop();
+                autoLockTimer.start();
             }
             return std::make_shared<sys::ResponseMessage>();
         });
@@ -521,8 +521,9 @@ namespace app::manager
             autoLockTimer.stop();
             return;
         }
-        if (auto focusedApp = getFocusedApplication(); focusedApp == nullptr || focusedApp->preventsAutoLocking()) {
-            autoLockTimer.stop();
+        if (const auto focusedApp = getFocusedApplication();
+            (focusedApp == nullptr) || (focusedApp->preventsAutoLocking())) {
+            autoLockTimer.start();
             return;
         }
         if (phoneModeObserver->isTetheringOn()) {
