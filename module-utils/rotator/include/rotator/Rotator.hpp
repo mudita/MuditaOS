91fb46f9593673ba18b6be117b42bf3a38186187
Lukasz Mastalerz <lukasz.mastalerz@mudita.com>
2023-01-12 14:13:38 +0100

    [CP-1512] Add crashdumps indexing
    Adding indexing to crashdumps to to ensure resistance to the device date
    retraction.
    When max crashdump count reach the file with higher index number will be
    remove.

--- module-utils/rotator/include/rotator/Rotator.hpp
+++ module-utils/rotator/include/rotator/Rotator.hpp
@@ -1,10 +1,13 @@
-// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
 
+#include <Utils.hpp>
+
 #include <filesystem>
 #include <string>
+#include <set>
 
 namespace utils
 {
@@ -39,6 +42,32 @@ namespace utils
             auto path = source;
             path.replace_extension(getRotatedFileExtension(rotationCount));
             return path;
+        }
+
+        uint getFileNumber(const std::string &filename) const
+        {
+            const auto position = filename.rfind(".");
+            if (position == std::string::npos) {
+                return 0;
+            }
+            const auto indexStartPosition = position + 1;
+            auto numberLength             = filename.length() - indexStartPosition;
+            return utils::toNumeric(filename.substr(indexStartPosition, numberLength));
+        }
+
+        std::filesystem::path incrementFileNumber(const std::filesystem::path &crashdumpPath) const
+        {
+            auto newCrashdumpName = crashdumpPath.string();
+            auto position         = newCrashdumpName.rfind(".");
+            if (position == std::string::npos) {
+                return crashdumpPath;
+            }
+            const auto indexStartPosition = position + 1;
+            const auto numberLength       = newCrashdumpName.length() - indexStartPosition;
+            auto fileNumber               = utils::toNumeric(newCrashdumpName.substr(indexStartPosition, numberLength));
+            auto incrementedFileNumber    = fileNumber ? std::to_string(++fileNumber) : "hex.0";
+            newCrashdumpName.replace(indexStartPosition, numberLength, incrementedFileNumber);
+            return std::filesystem::path(newCrashdumpName);
         }
 
       public:
@@ -77,6 +106,32 @@ namespace utils
             }
             auto rotatedLogPath = getRotatedFilePath(path, 1);
             std::filesystem::rename(path, rotatedLogPath, ec);
+            return (ec) ? false : true;
+        }
+
+        bool rotateFiles(const std::filesystem::path &path) const
+        {
+            std::error_code ec;
+            std::set<std::filesystem::path> crashdumps{};
+
+            for (const auto &entry : std::filesystem::directory_iterator(path)) {
+                crashdumps.insert(entry.path());
+            }
+
+            for (const auto &crashdump : crashdumps) {
+                if (getFileNumber(crashdump.string()) >= maxRotationFilesCount) {
+                    std::filesystem::remove(crashdump, ec);
+                    if (ec) {
+                        return false;
+                    }
+                    continue;
+                }
+                std::filesystem::rename(crashdump, incrementFileNumber(crashdump), ec);
+                if (ec) {
+                    return false;
+                }
+            }
+
             return (ec) ? false : true;
         }
     };
--- module-utils/rotator/tests/test_Rotator.cpp
+++ module-utils/rotator/tests/test_Rotator.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include <catch2/catch.hpp>
@@ -22,6 +22,11 @@ namespace
         std::filesystem::path rotatedFilePath(const std::filesystem::path &source, int rotationCount)
         {
             return this->getRotatedFilePath(source, rotationCount);
+        }
+
+        std::filesystem::path incrementFileIndex(const std::filesystem::path &source)
+        {
+            return this->incrementFileNumber(source);
         }
     };
 } // namespace
@@ -91,4 +96,83 @@ TEST_CASE("Rotation of hex dump files")
             REQUIRE(std::filesystem::remove(rotatedFileName));
         }
     }
+}
+
+TEST_CASE("Rotate files with serial_number and timestamp")
+{
+    HexRotator rotator;
+    std::string path("./rotator_tests");
+
+    SECTION("Increment filename index")
+    {
+        std::string filename          = "123_12345_crashdump.hex.1";
+        std::string expected_filename = "123_12345_crashdump.hex.2";
+        auto result                   = rotator.incrementFileIndex(std::filesystem::path(filename));
+        REQUIRE(result == expected_filename);
+    }
+
+    SECTION("Rotate files")
+    {
+
+        std::vector<std::string> test_filenames = {
+            "/123_12345_crashdump.hex.1", "/123_12346_crashdump.hex.2", "/123_12347_crashdump.hex.3"};
+        std::vector<std::string> expected_test_filenames = {
+            "/123_12345_crashdump.hex.2", "/123_12346_crashdump.hex.3", "/123_12347_crashdump.hex.4"};
+
+        std::filesystem::create_directory(path);
+
+        for (const auto &tf : test_filenames) {
+            std::ofstream(std::string(path + tf).c_str());
+        }
+        REQUIRE(rotator.rotateFiles(std::filesystem::path(path)));
+        for (const auto &etf : expected_test_filenames) {
+            REQUIRE(std::filesystem::exists(std::string(path + etf)));
+        }
+
+        std::filesystem::remove_all(path);
+    }
+
+    SECTION("Rotate files - max number reach")
+    {
+
+        std::vector<std::string> test_filenames = {"/123_12345_crashdump.hex.1",
+                                                   "/123_12346_crashdump.hex.2",
+                                                   "/123_12347_crashdump.hex.3",
+                                                   "/123_12349_crashdump.hex.4",
+                                                   "/123_12351_crashdump.hex.5"};
+
+        std::vector<std::string> expected_test_filenames = {"/123_12345_crashdump.hex.2",
+                                                            "/123_12346_crashdump.hex.3",
+                                                            "/123_12347_crashdump.hex.4",
+                                                            "/123_12349_crashdump.hex.5"};
+
+        std::string file_expected_to_not_exist = "/123_12351_crashdump.hex.6";
+
+        std::filesystem::create_directory(path);
+
+        for (const auto &tf : test_filenames) {
+            std::ofstream(std::string(path + tf).c_str());
+        }
+        REQUIRE(rotator.rotateFiles(std::filesystem::path(path)));
+        for (const auto &tf : test_filenames) {
+            REQUIRE_FALSE(std::filesystem::exists(std::string(path + tf)));
+        }
+        for (const auto &etf : expected_test_filenames) {
+            REQUIRE(std::filesystem::exists(std::string(path + etf)));
+        }
+        REQUIRE_FALSE(std::filesystem::exists(std::string(path + file_expected_to_not_exist)));
+
+        std::filesystem::remove_all(path);
+    }
+
+    SECTION("Increment filename - no number at the end")
+    {
+        std::string filename          = "123_12345_crashdump.hex";
+        std::string expected_filename = "123_12345_crashdump.hex.0";
+        auto result                   = rotator.incrementFileIndex(std::filesystem::path(filename));
+        REQUIRE(result == expected_filename);
+    }
+
+    // in case of fail in earlier tests
+    std::filesystem::remove_all(path);
 }
