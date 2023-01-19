ec46e63e59a5a198fda86cf7351adc6b01a5cdde
Bartosz <sp2fet@gmail.com>
2023-01-19 16:03:08 +0100

    [MOS-000] Rebase fixes
    Rebase fixes

--- products/PurePhone/PurePhoneMain.cpp
+++ products/PurePhone/PurePhoneMain.cpp
@@ -174,19 +174,11 @@ int main()
     systemServices.emplace_back(sys::CreatorFor<ServiceCellular>());
 #endif
 #endif
-
 #ifdef ENABLE_SERVICE_AUDIO
     systemServices.emplace_back(sys::CreatorFor<ServiceAudio>());
 #endif
 #ifdef ENABLE_SERVICE_BLUETOOTH
     systemServices.emplace_back(sys::CreatorFor<ServiceBluetooth>());
-#endif
-#ifdef ENABLE_SERVICE_DESKTOP
-    /// Due to the problem with USB MTP not supporting hierarchical folders structure, we cannot use
-    /// 'purefs::dir::getUserMediaPath()'. Instead, we can only pass a specific app folder which is very limiting.
-    /// Hopefully, support for hierarchical folders will be added in the future and such a case won't be relevant
-    /// anymore.
-    systemServices.emplace_back(sys::CreatorFor<ServiceDesktop>(purefs::dir::getUserMediaPath() / "app/music_player"));
 #endif
 #ifdef ENABLE_SERVICE_TIME
     systemServices.emplace_back(sys::CreatorFor<stm::ServiceTime>(std::make_shared<alarms::AlarmOperationsFactory>()));
@@ -199,7 +191,11 @@ int main()
         sys::CreatorFor<service::gui::ServiceGUI>(gui::Size{BOARD_EINK_DISPLAY_RES_X, BOARD_EINK_DISPLAY_RES_Y}));
 #endif
 #ifdef ENABLE_SERVICE_DESKTOP
-    systemServices.emplace_back(sys::CreatorFor<ServiceDesktop>());
+    /// Due to the problem with USB MTP not supporting hierarchical folders structure, we cannot use
+    /// 'purefs::dir::getUserMediaPath()'. Instead, we can only pass a specific app folder which is very limiting.
+    /// Hopefully, support for hierarchical folders will be added in the future and such a case won't be relevant
+    /// anymore.
+    systemServices.emplace_back(sys::CreatorFor<ServiceDesktop>(purefs::dir::getUserMediaPath() / "app/music_player"));
 #endif
 #if ENABLE_SERVICE_TEST
     systemServices.emplace_back(sys::CreatorFor<service::test::ServiceTest>());
