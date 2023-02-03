25a8f781ee7f852ea75d2c6a4fc382f62881ecdd
Lefucjusz <marcin.lyda@mudita.com>
2023-02-03 16:43:49 +0100

    [MOS-199] Change unknown char glyph to match with design
    Fix of the issue that glyph displayed as
    a substitute for unsupported character
    was different than the design suggested.
    
    Additionally minor code cleanup.

--- module-gui/gui/core/RawFont.hpp
+++ module-gui/gui/core/RawFont.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -11,8 +11,8 @@
 #include <map>
 #include <memory>
 #include <string>
-#include "FontGlyph.hpp"   // for FontGlyph
-#include "FontKerning.hpp" // for FontKerning
+#include "FontGlyph.hpp"
+#include "FontKerning.hpp"
 
 namespace gui
 {
@@ -27,25 +27,25 @@ namespace gui
 
         gui::Status load(uint8_t *data);
 
-        static const uint32_t none_char_id = std::numeric_limits<const uint32_t>().max();
+        static const std::uint32_t none_char_id = std::numeric_limits<const std::uint32_t>().max();
         // structure holding detailed information about font
         FontInfo info;
-        // number of glyphs in the fontno
-        uint32_t glyph_count;
+        // number of glyphs in the font
+        std::uint32_t glyph_count;
         // offset to the beginning of the glyph data
-        uint32_t glyph_data_offset;
+        std::uint32_t glyph_data_offset;
         // number of kerning pairs
-        uint32_t kern_count;
+        std::uint32_t kern_count;
         // array of glyphs structures
-        uint32_t kern_data_offset;
+        std::uint32_t kern_data_offset;
         // offset to the beginning of the image data
-        uint32_t image_data_offset;
-        // id of the font asigned by the font manager
-        uint32_t id;
+        std::uint32_t image_data_offset;
+        // id of the font assigned by the font manager
+        std::uint32_t id;
 
         /// return glyph for selected code
         /// if code is not found in font, it is searched in the fallback font, if not found - unsupportedGlyph returned
-        FontGlyph *getGlyph(uint32_t id) const;
+        FontGlyph *getGlyph(std::uint32_t id) const;
 
         /**
          * @brief Returns kerning value for pair of the two characters.
@@ -53,14 +53,14 @@ namespace gui
          * @param id2 Code of the second character - if none_char_id then return 0
          * @return Value of the kerning or 0 if pair was not found.
          */
-        int32_t getKerning(uint32_t id1, uint32_t id2) const;
+        int32_t getKerning(std::uint32_t id1, std::uint32_t id2) const;
         /**
          * @brief Method calculates how many chars will fit specified width using current font.
          * @param str UTF8 string that will be used to calculate how many chars can fit provided space.
          * @param space Number of pixels in width availabale to calculate how many chars will fit.
          * @return number of chars that can fit provided space;
          */
-        uint32_t getCharCountInSpace(const UTF8 &str, const uint32_t space) const;
+        std::uint32_t getCharCountInSpace(const UTF8 &str, const std::uint32_t space) const;
         /**
          * @brief Calculates how many pixels will occupy selected part of the string.
          * @param str String used as a source of text.
@@ -68,23 +68,23 @@ namespace gui
          * @param count Number of characters that should be used during calculating pixels width.
          * @return Number of pixels in width occupied by selected part of the text.
          */
-        uint32_t getPixelWidth(const UTF8 &str, const uint32_t start, const uint32_t count) const;
+        std::uint32_t getPixelWidth(const UTF8 &str, const std::uint32_t start, const std::uint32_t count) const;
         /**
          * @brief Calculates how many pixels will occupy string.
          * @param str String used as a source of text.
          * @return Number of pixels in width occupied by string.
          */
-        uint32_t getPixelWidth(const UTF8 &str) const;
+        std::uint32_t getPixelWidth(const UTF8 &str) const;
         /**
          * @brief returns number of pixels occupied by character horizontally.
          *
          * if previous char is set - then tries to append kerning
          */
-        uint32_t getCharPixelWidth(uint32_t charCode, uint32_t previousChar = none_char_id) const;
+        std::uint32_t getCharPixelWidth(std::uint32_t charCode, std::uint32_t previousChar = none_char_id) const;
         /**
          * @brief Returns number of pixels occupied by the character vertically.
          */
-        uint32_t getCharPixelHeight(uint32_t charCode);
+        std::uint32_t getCharPixelHeight(std::uint32_t charCode);
         const std::string getName()
         {
             return info.face;
@@ -92,8 +92,8 @@ namespace gui
         void setFallbackFont(RawFont *font);
 
       private:
-        std::map<uint32_t, std::unique_ptr<FontGlyph>> glyphs;
-        std::map<uint32_t, std::map<uint32_t, std::unique_ptr<FontKerning>>> kerning;
+        std::map<std::uint32_t, std::unique_ptr<FontGlyph>> glyphs;
+        std::map<std::uint32_t, std::map<std::uint32_t, std::unique_ptr<FontKerning>>> kerning;
         /// if the fallback font is set it is used in case of a glyph being unsupported in the primary font
         RawFont *fallback_font = nullptr;
         /// the glyph used when requested glyph is unsupported in the font (and the fallback font if one is set)
@@ -103,9 +103,9 @@ namespace gui
 
         /// return glyph for selected code
         /// if code is not found - nullptr is returned
-        FontGlyph *findGlyph(uint32_t id) const;
+        FontGlyph *findGlyph(std::uint32_t id) const;
         /// return glyph for selected code
         /// if code is not found - nullptr is returned
-        FontGlyph *findGlyphFallback(uint32_t id) const;
+        FontGlyph *findGlyphFallback(std::uint32_t id) const;
     };
 } /* namespace gui */
