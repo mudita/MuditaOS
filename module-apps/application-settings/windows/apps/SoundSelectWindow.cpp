131289c4af3b6efc2e51b23c4f32c155ad9d3445
Lefucjusz <marcin.lyda@mudita.com>
2023-02-14 23:08:04 +0100

    [MOS-28] Change alarm sounds and ringtones preview behavior
    Change of the behavior of alarm sounds
    and ringtones preview, so that it matches
    with the design.
    
    Additionally fixed lack of ringtones list
    looping.

--- module-apps/application-settings/windows/apps/SoundSelectWindow.cpp
+++ module-apps/application-settings/windows/apps/SoundSelectWindow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "SoundSelectWindow.hpp"
@@ -39,6 +39,7 @@ namespace gui
                                             style::nav_bar::height + style::margins::small,
                                         mSoundsModel,
                                         listview::ScrollBarType::Proportional);
+        mSoundsList->setBoundaries(Boundaries::Continuous);
 
         setFocusItem(mSoundsList);
     }
