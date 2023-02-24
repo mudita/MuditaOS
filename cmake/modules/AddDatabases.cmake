d0e3f4527104a73802bc6b4af5a3b30f9623d148
Mateusz Piesta <mateusz.piesta@mudita.com>
2023-02-24 15:47:48 +0100

    [MOS-922] Add database migration docs
    * Added database migration docs
    * Updated database migration tool

--- cmake/modules/AddDatabases.cmake
+++ cmake/modules/AddDatabases.cmake
@@ -2,31 +2,39 @@
 Example of use:
 add_databases_target(
         TARGET <target_name>
+        DB_SET <database set dir>
         SOURCE_DIR <source_dir>
         DEST_DIR <destination_dir>
         DEVEL <true/false>
         DEPENDS <dependencies list>
 )
-]]#
+]]
+#
 function(add_databases_target)
     cmake_parse_arguments(
             _ARG
             ""
-            "TARGET;COMMON_DIR;PRODUCT_DIR;DEST_DIR;DEVEL;"
+            "TARGET;COMMON_DIR;PRODUCT_DIR;DB_SET;DEST_DIR;DEVEL;"
             "DEPENDS"
             ${ARGN}
     )
 
     if (${_ARG_DEVEL} STREQUAL "ON" OR ${_ARG_DEVEL} EQUAL 1)
-        set (DEVEL --development True)
-    endif()
+        set(DEVEL --devel True)
+    endif ()
 
     add_custom_target(
             ${_ARG_TARGET}
             DEPENDS ${_ARG_DEPENDS}
 
-            COMMAND python3 ${PROJECT_SOURCE_DIR}/tools/init_databases.py --common_path ${_ARG_COMMON_DIR} --product_path ${_ARG_PRODUCT_DIR} --output_path ${_ARG_DEST_DIR} ${DEVEL}
-            COMMENT
-            "Creating databases using schemas from: ${_ARG_COMMON_DIR} and ${_ARG_PRODUCT_DIR}, and storing them under: ${_ARG_DEST_DIR}"
+            COMMAND python3 ${PROJECT_SOURCE_DIR}/tools/db_migration.py init
+            --env ${CMAKE_BINARY_DIR}
+            --dbset ${_ARG_DB_SET}
+            --out ${_ARG_DEST_DIR}
+            --dirs ${_ARG_PRODUCT_DIR}
+            --dirs ${_ARG_COMMON_DIR}
+
+            COMMAND python3 ${PROJECT_SOURCE_DIR}/tools/db_migration.py install
+            --env ${CMAKE_BINARY_DIR} ${DEVEL}
     )
 endfunction()
