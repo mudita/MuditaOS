4378d6172fc5ab5ec1f29e613df67203dd36343c
Adam Wulkiewicz <adam.wulkiewicz@mudita.com>
2023-02-27 20:12:56 +0100

    [BH-1624] Update unit test of DrawCommandsQueue
    The interface was modified and synchronization mechanism was removed.
    The thread no longer waits in dequeue().

--- module-services/service-gui/tests/test-DrawCommandsQueue.cpp
+++ module-services/service-gui/tests/test-DrawCommandsQueue.cpp
@@ -1,10 +1,9 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include <catch2/catch.hpp>
 
 #include "DrawCommandsQueue.hpp"
-#include "MockedSynchronizationMechanism.hpp"
 
 #include <chrono>
 #include <thread>
@@ -14,7 +13,7 @@ using namespace service::gui;
 TEST_CASE("DrawCommandsQueueTests")
 {
     constexpr auto ExpectedQueueSize = 4U;
-    DrawCommandsQueue queue{ExpectedQueueSize, std::make_unique<MockedSynchronizationMechanism>()};
+    DrawCommandsQueue queue{ExpectedQueueSize};
 
     SECTION("Enqueue")
     {
@@ -28,25 +27,25 @@ TEST_CASE("DrawCommandsQueueTests")
         REQUIRE(queue.size() == 1U);
 
         const auto item = queue.dequeue();
-        REQUIRE(item.commands.empty());
-        REQUIRE(item.refreshMode == ::gui::RefreshModes::GUI_REFRESH_FAST);
+        REQUIRE(item->commands.empty());
+        REQUIRE(item->refreshMode == ::gui::RefreshModes::GUI_REFRESH_FAST);
         REQUIRE(queue.size() == 0);
     }
 
     SECTION("Multi-thread dequeue")
     {
         std::thread thr{[&queue]() {
-            std::this_thread::sleep_for(std::chrono::milliseconds{100});
             queue.enqueue(DrawCommandsQueue::QueueItem{});
         }};
 
+        std::this_thread::sleep_for(std::chrono::milliseconds{100});
         const auto item = queue.dequeue();
         if (thr.joinable()) {
             thr.join();
         }
 
-        REQUIRE(item.commands.empty());
-        REQUIRE(item.refreshMode == ::gui::RefreshModes::GUI_REFRESH_FAST);
+        REQUIRE(item->commands.empty());
+        REQUIRE(item->refreshMode == ::gui::RefreshModes::GUI_REFRESH_FAST);
         REQUIRE(queue.size() == 0U);
     }
 
