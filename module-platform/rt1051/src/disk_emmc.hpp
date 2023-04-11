bb10c090474682e6c81d3ffc40d36d41998f9ca3
Lefucjusz <marcin.lyda@mudita.com>
2023-04-11 18:05:17 +0200

    [BH-1672] Add logging of eMMC parameters
    Added logging of eMMC storage card
    parameters so that it's easy to
    determine what chip is installed
    in the device the logs are from.

--- module-platform/rt1051/src/disk_emmc.hpp
+++ module-platform/rt1051/src/disk_emmc.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -8,6 +8,7 @@
 #include <mutex.hpp>
 #include <memory>
 #include <atomic>
+#include <map>
 
 #if !defined(TARGET_RT1051)
 static_assert(false, "Unsupported target.");
@@ -20,6 +21,20 @@ namespace purefs::blkdev
     class disk_emmc final : public disk
     {
       public:
+        struct disk_emmc_info
+        {
+            std::string manufacturer;
+            std::uint32_t blocksCount;
+            std::uint32_t blockSize;
+            std::uint64_t capacity;
+            std::string name;
+            std::uint8_t versionMajor;
+            std::uint8_t versionMinor;
+            std::uint32_t serialNumber;
+            std::uint8_t productionMonth;
+            std::uint16_t productionYear;
+        };
+
         static constexpr auto statusBlkDevSuccess = 0;
         static constexpr auto statusBlkDevFail    = -1;
 
@@ -35,11 +50,13 @@ namespace purefs::blkdev
         auto get_info(info_type what, hwpart_t hwpart) const -> scount_t override;
         auto pm_control(pm_state target_state) -> int override;
         auto pm_read(pm_state &current_state) -> int override;
+        auto get_emmc_info() const -> disk_emmc_info;
+        auto get_emmc_info_str() const -> std::string;
 
       private:
         auto switch_partition(hwpart_t newpart) -> int;
+        auto get_emmc_manufacturer() const -> std::string;
 
-      private:
         int initStatus;
         pm_state pmState{pm_state::active};
         mutable cpp_freertos::MutexRecursive mutex;
@@ -47,5 +64,9 @@ namespace purefs::blkdev
 
         std::unique_ptr<_mmc_card> mmcCard;
         std::shared_ptr<drivers::DriverUSDHC> driverUSDHC;
+
+        /* Read from datasheets */
+        const std::map<std::uint8_t, std::string> mmcManufacturersMap{
+            {0x15, "Samsung"}, {0x70, "Kingston"}, {0xF4, "Biwin"}};
     };
 } // namespace purefs::blkdev
