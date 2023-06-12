a2cef32dc4d0dd498b76a5ff244b4e3a52eeda06
Lukasz Mastalerz <lukasz.mastalerz@mudita.com>
2023-06-12 15:06:58 +0200

    [BH-1532] Changing alarm's trigger time when alarm is snoozed causes several problems
    Fixed alarm problems when it was re-set while snooze was still active

--- products/BellHybrid/apps/common/src/AlarmModel.cpp
+++ products/BellHybrid/apps/common/src/AlarmModel.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "models/AlarmModel.hpp"
@@ -56,6 +56,12 @@ namespace app
         if (!alarmEventPtr) {
             return;
         }
+
+        if (alarmStatus == alarms::AlarmStatus::Snoozed) {
+            disableSnooze(*alarmEventPtr);
+            alarmStatus = alarms::AlarmStatus::Activated;
+        }
+
         auto clockTime = std::localtime(&time);
         alarmEventPtr->alarmTime =
             AlarmTime{std::chrono::hours{clockTime->tm_hour}, std::chrono::minutes{clockTime->tm_min}};
