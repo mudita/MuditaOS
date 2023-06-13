5f19f7df87f13ab7b104c83d4c3c790754573452
rrandomsky <rafal.radomski@mudita.com>
2023-06-13 10:28:23 +0200

    [MOS-946] Revert: USSD contacts
    Revert: Fixed adding USSD codes to phonebook.

--- module-utils/utility/Utils.hpp
+++ module-utils/utility/Utils.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -99,7 +99,7 @@ namespace utils
 
     static inline bool is_phone_number(const std::string &s)
     {
-        return s.find_first_not_of(" +-.*#()0123456789") == std::string::npos;
+        return s.find_first_not_of(" +-.#()0123456789") == std::string::npos;
     }
 
     static inline std::string ltrim(const std::string &s)
