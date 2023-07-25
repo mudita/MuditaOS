932b9fb096362a7ef5c19a0c58606a933bf57559
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2023-07-25 10:52:01 +0200

    [BH-1729] Changed the "Turn off Harmony" popup display time to 10 seconds
    When the user activates the prompt to turn off Harmony, it has been
    displayed for more than 3 minutes.

--- products/BellHybrid/apps/common/include/common/popups/BellTurnOffOptionWindow.hpp
+++ products/BellHybrid/apps/common/include/common/popups/BellTurnOffOptionWindow.hpp
@@ -1,9 +1,10 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
 
 #include <common/options/BellShortOptionWindow.hpp>
+#include <Timers/TimerHandle.hpp>
 
 namespace gui
 {
@@ -12,10 +13,12 @@ namespace gui
       public:
         static constexpr auto defaultName = "BellTurnOffOptionWindow";
         explicit BellTurnOffOptionWindow(app::ApplicationCommon *app, const char *name = defaultName);
+        void onBeforeShow(ShowMode mode, SwitchData *data) override;
+        void onClose(CloseReason reason) override;
 
       private:
         std::list<Option> settingsOptionsList();
-
+        sys::TimerHandle popupTimer;
         const UTF8 yesStr;
         const UTF8 noStr;
     };
