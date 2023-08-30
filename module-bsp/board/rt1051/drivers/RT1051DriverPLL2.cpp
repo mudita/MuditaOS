aafabdde06afb33b724213517b862d65bfc1a451
Lefucjusz <marcin.lyda@mudita.com>
2023-08-30 15:26:19 +0200

    Critical section on bandgap

--- module-bsp/board/rt1051/drivers/RT1051DriverPLL2.cpp
+++ module-bsp/board/rt1051/drivers/RT1051DriverPLL2.cpp
@@ -14,16 +14,16 @@ namespace drivers
     {
         if (!IsPLL2Enabled()) {
             // Switch DCDC to CCM mode to improve stability
-            //            DCDC_BootIntoCCM(DCDC);
+            DCDC_BootIntoCCM(DCDC);
 
             // Set VDD_SOC_IN to 1.15V required to safely start PLL2
-            //            DCDC_AdjustTargetVoltage(DCDC, bsp::VDDRun_1150_mV, bsp::VDDStandby_925_mV);
+            DCDC_AdjustTargetVoltage(DCDC, bsp::VDDRun_1150_mV, bsp::VDDStandby_925_mV);
 
             // Switch to regular bandgap
-            //            bsp::bandgap::SwitchToRegularMode();
+            bsp::bandgap::SwitchToRegularMode();
 
             // Switch to regular LDO
-            //            bsp::ldo::SwitchToRegularMode();
+            bsp::ldo::SwitchToRegularMode();
 
             // Enable PLL2
             clkPLL2setup(CLK_ENABLE);
@@ -32,26 +32,26 @@ namespace drivers
 
     RT1051DriverPLL2::~RT1051DriverPLL2() noexcept
     {
-        //        if ((CLOCK_GetMux(kCLOCK_SemcMux) == SemcMuxPeripheralClock) && !bsp::IsClockEnabled(kCLOCK_Lpspi1) &&
-        //            !bsp::IsClockEnabled(kCLOCK_Lpspi2) && !bsp::IsClockEnabled(kCLOCK_Lpspi3) &&
-        //            !bsp::IsClockEnabled(kCLOCK_Lpspi4) && !bsp::IsClockEnabled(kCLOCK_Usdhc1) &&
-        //            !bsp::IsClockEnabled(kCLOCK_Usdhc2)) {
-        //
-        //            // Disable PLL2
-        //            clkPLL2setup(CLK_DISABLE);
-        //
-        //            // After turning off PLL2 and with CPU @4MHZ VDD_SOC_IN can be set to 950mV
-        //            DCDC_AdjustTargetVoltage(DCDC, bsp::VDDRun_950_mV, bsp::VDDStandby_925_mV);
-        //
-        //            // Switch DCDC to DCM mode to reduce current consumption
-        //            DCDC_BootIntoDCM(DCDC);
-        //
-        //            // Switch to low power LDO
-        //            bsp::ldo::SwitchToLowPowerMode();
-        //
-        //            // Switch to low power bandgap
-        //            bsp::bandgap::SwitchToLowPowerMode();
-        //        }
+        if ((CLOCK_GetMux(kCLOCK_SemcMux) == SemcMuxPeripheralClock) && !bsp::IsClockEnabled(kCLOCK_Lpspi1) &&
+            !bsp::IsClockEnabled(kCLOCK_Lpspi2) && !bsp::IsClockEnabled(kCLOCK_Lpspi3) &&
+            !bsp::IsClockEnabled(kCLOCK_Lpspi4) && !bsp::IsClockEnabled(kCLOCK_Usdhc1) &&
+            !bsp::IsClockEnabled(kCLOCK_Usdhc2)) {
+
+            // Disable PLL2
+            clkPLL2setup(CLK_DISABLE);
+
+            // After turning off PLL2 and with CPU @4MHZ VDD_SOC_IN can be set to 950mV
+            DCDC_AdjustTargetVoltage(DCDC, bsp::VDDRun_950_mV, bsp::VDDStandby_925_mV);
+
+            // Switch DCDC to DCM mode to reduce current consumption
+            DCDC_BootIntoDCM(DCDC);
+
+            // Switch to low power LDO
+            bsp::ldo::SwitchToLowPowerMode();
+
+            // Switch to low power bandgap
+            bsp::bandgap::SwitchToLowPowerMode();
+        }
     }
 
     bool RT1051DriverPLL2::IsPLL2Enabled() const noexcept
