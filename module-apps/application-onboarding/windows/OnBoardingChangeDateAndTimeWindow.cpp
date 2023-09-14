13217cce3449be74735cf0d8fbacef8aee462df0
Lefucjusz <marcin.lyda@mudita.com>
2023-09-14 14:15:23 +0200

    [MOS-1032] Fix double return to previous window in onboarding
    Fix of the regression created by previous
    commit - saving manually set time resulted
    in switching two windows back instead of
    just one when done in onboarding.

--- module-apps/application-onboarding/windows/OnBoardingChangeDateAndTimeWindow.cpp
+++ module-apps/application-onboarding/windows/OnBoardingChangeDateAndTimeWindow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "OnBoardingChangeDateAndTimeWindow.hpp"
@@ -13,13 +13,6 @@ namespace app::onBoarding
 
     bool OnBoardingChangeDateAndTimeWindow::onInput(const gui::InputEvent &inputEvent)
     {
-        if (inputEvent.isKeyRelease(gui::KeyCode::KEY_ENTER)) {
-            auto ret = ChangeDateAndTimeWindow::onInput(inputEvent);
-            application->returnToPreviousWindow();
-            return ret;
-        }
-
         return ChangeDateAndTimeWindow::onInput(inputEvent);
     }
-
 } // namespace app::onBoarding
