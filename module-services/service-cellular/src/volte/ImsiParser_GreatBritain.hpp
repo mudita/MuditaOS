3cf0abc0d0c5e4b7e34d673d20304595a6a621ae
Lefucjusz <marcin.lyda@mudita.com>
2023-11-02 15:11:57 +0100

    [MOS-1051] Updated HNI codes for VoLTE support
    Reviewed and updated HNI codes lists
    for all countries with VoLTE support.

--- module-services/service-cellular/src/volte/ImsiParser_GreatBritain.hpp
+++ module-services/service-cellular/src/volte/ImsiParser_GreatBritain.hpp
@@ -10,26 +10,50 @@ namespace cellular::service
     struct ImsiParserGB : ImsiParser
     {
         ImsiParserGB()
-            : ImsiParser(std::vector<std::string>{"23401",
-                                                  "23410",
-                                                  "23411",
-                                                  "23403",
-                                                  "23415",
-                                                  "23420",
-                                                  "23430",
-                                                  "23433",
-                                                  "23434",
-                                                  "23450",
-                                                  "23480",
-                                                  "365010",
-                                                  "348170",
-                                                  "348770",
-                                                  "34605",
-                                                  "346001",
-                                                  "346140",
-                                                  "750001",
-                                                  "26601"},
-                         SupportStatus::Beta)
+            : ImsiParser(
+                  std::vector<std::string>{
+                      /* 3 Mobile */
+                      "23420",
+                      "23494",
+                      /* JT */
+                      "23450",
+                      /* O2 */
+                      "23402",
+                      "23410",
+                      "23411",
+                      /* Orange */
+                      "23433",
+                      "23434",
+                      /* Telet */
+                      "23480",
+                      /* T-Mobile */
+                      "23430",
+                      "23431",
+                      "23432",
+                      /* Vectone Mobile */
+                      "23401",
+                      /* Vodafone */
+                      "23407",
+                      "23415",
+                      "23477",
+                      "23489",
+                      "23491",
+                      "23492",
+                      /* Anguilla */
+                      "365010", // Weblinks Limited
+                      /* Virgin Islands */
+                      "348170", // Flow
+                      "348770", // Digicell
+                      /* Cayman Islands */
+                      "34605",  // Flow
+                      "346001", // Logic
+                      "346140", // Digicel
+                      /* Falkland Islands */
+                      "750001", // Sure
+                      /* Gibraltar */
+                      "26601" // GibTel
+                  },
+                  SupportStatus::Beta)
         {}
     };
 } // namespace cellular::service
--- module-services/service-cellular/src/volte/ImsiParser_Poland.hpp
+++ module-services/service-cellular/src/volte/ImsiParser_Poland.hpp
@@ -10,23 +10,35 @@ namespace cellular::service
     struct ImsiParserPL : ImsiParser
     {
         ImsiParserPL()
-            : ImsiParser(std::vector<std::string>{"26001",
-                                                  "26011",
-                                                  "26002",
-                                                  "26010",
-                                                  "26034",
-                                                  "26003",
-                                                  "26015",
-                                                  "26016",
-                                                  "26017",
-                                                  "26006",
-                                                  "26045",
-                                                  "26098",
-                                                  "26008",
-                                                  "26009",
-                                                  "26012",
-                                                  "26013"},
-                         SupportStatus::Beta)
+            : ImsiParser(
+                  std::vector<std::string>{/* Polkomtel */
+                                           "26001",
+                                           "26004",
+                                           "26011",
+                                           "26015",
+                                           "26016",
+                                           "26017",
+                                           /* T-Mobile */
+                                           "26002",
+                                           "26010",
+                                           "26031",
+                                           "26034",
+                                           /* Orange */
+                                           "26003",
+                                           "26005",
+                                           /* P4 */
+                                           "26006",
+                                           "26045",
+                                           "26098",
+                                           /* EXATEL */
+                                           "26008",
+                                           /* Lycamobile */
+                                           "26009",
+                                           /* Cyfrowy Polsat */
+                                           "26012",
+                                           /* Move Telecom */
+                                           "26013"},
+                  SupportStatus::Beta)
         {}
     };
 } // namespace cellular::service
