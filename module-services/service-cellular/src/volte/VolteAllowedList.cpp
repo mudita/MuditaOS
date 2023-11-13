3056cd6f01358c1a77f142fe981666284b245b32
Lefucjusz <marcin.lyda@mudita.com>
2023-11-13 13:02:16 +0100

    [MOS-1058] Add VoLTE support for Germany
    Add VoLTE support only for Germany,
    disable for the rest of yet
    unreleased countries.

--- module-services/service-cellular/src/volte/VolteAllowedList.cpp
+++ module-services/service-cellular/src/volte/VolteAllowedList.cpp
@@ -4,12 +4,7 @@
 #include "VolteAllowedList.hpp"
 #include "ImsiParser_Poland.hpp"
 #include "ImsiParser_UnitedStates.hpp"
-#include "ImsiParser_Netherlands.hpp"
 #include "ImsiParser_Germany.hpp"
-#include "ImsiParser_Denmark.hpp"
-#include "ImsiParser_GreatBritain.hpp"
-#include "ImsiParser_Canada.hpp"
-#include "ImsiParser_Austria.hpp"
 
 #include <log/log.hpp>
 #include <magic_enum.hpp>
@@ -54,14 +49,6 @@ namespace cellular::service
 
     void VolteAllowedList::buildList()
     {
-        pushBack(allowedList,
-                 ImsiParserPL(),
-                 ImsiParserUS(),
-                 ImsiParserDK(),
-                 ImsiParserDE(),
-                 ImsiParserNL(),
-                 ImsiParserGB(),
-                 ImsiParserCA(),
-                 ImsiParserAT());
+        pushBack(allowedList, ImsiParserPL(), ImsiParserUS(), ImsiParserDE());
     }
 } // namespace cellular::service
