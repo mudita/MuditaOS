89ab781d55cc87b84470ea6f2b329be0154ee6a3
Dawid Wojtas <dawid.wojtas@mudita.com>
2023-11-16 15:58:38 +0100

    Change USDHC2 clock speed
    Now ~82MHz, before ~109MHz.

--- module-bsp/board/rt1051/puretx/clock_config.cpp
+++ module-bsp/board/rt1051/puretx/clock_config.cpp
@@ -168,7 +168,7 @@ void BOARD_BootClockRUN(void)
     CLOCK_SetMux(kCLOCK_Usdhc1Mux, 0); // CSCMR1  (16) 0 - PLL2_PFD2, 1 - PLL2_PFD0
 
     /* Set USDHC2_PODF. */
-    CLOCK_SetDiv(kCLOCK_Usdhc2Div, 2); // CSCDR1
+    CLOCK_SetDiv(kCLOCK_Usdhc2Div, 3); // CSCDR1 Div by 3 to get ~82MHz clock
     /* Set Usdhc2 clock source. */
     /* PLL2_PFD2/3 = 396MHz/3 = 132MHz */
     CLOCK_SetMux(kCLOCK_Usdhc2Mux, 0); // CSCMR1  (17) 0 - PLL2_PFD2, 1 - PLL2_PFD0
