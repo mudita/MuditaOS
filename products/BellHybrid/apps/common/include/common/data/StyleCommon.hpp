7f543f7a806d2e8ab4ebe1cb1fe0de8e2c751257
Lukasz Mastalerz <lukasz.mastalerz@mudita.com>
2023-12-15 14:12:25 +0100

    [BH-1807] Increase clock font
    Increase clock font in Relaxation, Meditation, Power nap mode

--- products/BellHybrid/apps/common/include/common/data/StyleCommon.hpp
+++ products/BellHybrid/apps/common/include/common/data/StyleCommon.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -11,7 +11,7 @@ namespace gui::bell_style
     inline constexpr auto font            = ::style::window::font::supersizemelight;
     inline constexpr auto font_center     = ::style::window::font::largelight;
     inline constexpr auto font_top        = ::style::window::font::largelight;
-    inline constexpr auto statusClockFont = style::window::font::verybiglight;
+    inline constexpr auto statusClockFont = style::window::font::large;
 
     inline constexpr auto popup_icon_top_margin    = 120;
     inline constexpr auto popup_icon_bottom_margin = 30;
