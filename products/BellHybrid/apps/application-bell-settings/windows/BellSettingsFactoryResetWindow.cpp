5e2d33e39530fd9f80db46ff38b33d3c76fe3694
Lefucjusz <marcin.lyda@mudita.com>
2023-12-19 12:49:40 +0100

    [BH-1731] Fix factory reset confirmation window layout
    * Fixed factory reset confirmation window
    layout to match the corresponding design.
    * Added missing translations.

--- products/BellHybrid/apps/application-bell-settings/windows/BellSettingsFactoryResetWindow.cpp
+++ products/BellHybrid/apps/application-bell-settings/windows/BellSettingsFactoryResetWindow.cpp
@@ -16,9 +16,7 @@
 namespace gui
 {
     BellSettingsFactoryResetWindow::BellSettingsFactoryResetWindow(app::ApplicationCommon *app, const std::string &name)
-        : BellOptionWithDescriptionWindow(app, name), resetStr{utils::translate(
-                                                          "app_bell_settings_factory_reset_option_reset")},
-          backStr{utils::translate("app_bell_settings_factory_reset_option_back")}
+        : BellOptionWithDescriptionWindow(app, name)
     {
         addOptions(settingsOptionsList());
         setListTitle(utils::translate("app_bell_settings_factory_reset"));
@@ -28,21 +26,24 @@ namespace gui
     std::list<Option> BellSettingsFactoryResetWindow::settingsOptionsList()
     {
         std::list<gui::Option> settingsOptionList;
+
         auto addWinSettings = [&](const UTF8 &name, std::function<bool(Item &)> activatedCallback) {
             settingsOptionList.emplace_back(std::make_unique<gui::option::OptionBellMenu>(
-                name, activatedCallback, [=](gui::Item &) { return true; }, this));
+                name, activatedCallback, []([[maybe_unused]] gui::Item &item) { return true; }, this));
         };
 
-        addWinSettings(backStr, [this](auto &) {
-            application->returnToPreviousWindow();
-            return true;
-        });
-        addWinSettings(resetStr, [this](auto &) {
-            auto switchRequest = std::make_unique<app::manager::SwitchRequest>(
-                service::name::appmgr, app::applicationBellName, gui::BellFactoryReset::name, nullptr);
-            application->bus.sendUnicast(std::move(switchRequest), service::name::appmgr);
-            return true;
-        });
+        addWinSettings(utils::translate("app_bell_settings_factory_reset_option_back"),
+                       [this]([[maybe_unused]] Item &item) {
+                           application->returnToPreviousWindow();
+                           return true;
+                       });
+        addWinSettings(utils::translate("app_bell_settings_factory_reset_option_reset"),
+                       [this]([[maybe_unused]] Item &item) {
+                           auto switchRequest = std::make_unique<app::manager::SwitchRequest>(
+                               service::name::appmgr, app::applicationBellName, gui::BellFactoryReset::name, nullptr);
+                           application->bus.sendUnicast(std::move(switchRequest), service::name::appmgr);
+                           return true;
+                       });
 
         return settingsOptionList;
     }
