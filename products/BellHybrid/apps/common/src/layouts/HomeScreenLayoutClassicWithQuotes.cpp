17d2ed12498650aa71fd2e160db0d8b3f9a0022b
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2024-02-13 10:57:20 +0100

    am/pm on home screen

--- products/BellHybrid/apps/common/src/layouts/HomeScreenLayoutClassicWithQuotes.cpp
+++ products/BellHybrid/apps/common/src/layouts/HomeScreenLayoutClassicWithQuotes.cpp
@@ -35,19 +35,29 @@ namespace gui
         time->setFont(style::window::font::huge);
         time->setMargins({mainWindow::ampm::time_margin_w, 0, 0, 0});
 
+        ampm = new TextFixedSize(timeHBox);
+        ampm->setMinimumSize(mainWindow::ampm::layout_w, style::bell_base_layout::center_layout_h);
+        ampm->setMaximumSize(mainWindow::ampm::layout_w, style::bell_base_layout::center_layout_h);
+        ampm->setMargins({mainWindow::ampm::margin_w, 0, 0, 0});
+        ampm->setFont(mainWindow::time::font_small);
+        ampm->setAlignment(Alignment(Alignment::Horizontal::Left, Alignment::Vertical::Top));
+        ampm->setEdges(RectangleEdge::None);
+        ampm->activeItem = false;
+        ampm->drawUnderline(false);
+
         resizeItems();
     }
 
     void HomeScreenLayoutClassicWithQuotes::setTime(std::time_t newTime)
     {
         HomeScreenLayoutClassic::setTime(newTime);
-        // const auto t = std::localtime(&newTime);
+        const auto t = std::localtime(&newTime);
 
-        // if (ampm->visible) {
-        //     const auto hours = std::chrono::hours{t->tm_hour};
-        //     const auto isPM  = date::is_pm(hours);
-        //     ampm->setText(isPM ? utils::time::Locale::getPM() : utils::time::Locale::getAM());
-        // }
+        if (ampm->visible) {
+            const auto hours = std::chrono::hours{t->tm_hour};
+            const auto isPM  = date::is_pm(hours);
+            ampm->setText(isPM ? utils::time::Locale::getPM() : utils::time::Locale::getAM());
+        }
     }
 
     void HomeScreenLayoutClassicWithQuotes::setTimeFormat(utils::time::Locale::TimeFormat fmt)
@@ -55,11 +65,11 @@ namespace gui
         HomeScreenLayoutClassic::setTimeFormat(fmt);
         if (fmt == utils::time::Locale::TimeFormat::FormatTime12H) {
             time->setMargins({bellMainStyle::mainWindow::ampm::time_margin_w, 0, 0, 0});
-            // ampm->setVisible(true);
+            ampm->setVisible(true);
         }
         else {
             time->setMargins({0, 0, 0, 0});
-            // ampm->setVisible(false);
+            ampm->setVisible(false);
         }
     }
 
