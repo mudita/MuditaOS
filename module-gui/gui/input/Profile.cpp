d8293b82546777f2cfc02126868ef2ba92cb9c9e
Lefucjusz <marcin.lyda@mudita.com>
2024-02-21 17:23:56 +0100

    Revert "[MOS-1064] Fix no input language selected for French/Spanish"
    This reverts commit 9ef454085e4cca8a1f6c3c270cf460f899c9ee23.

--- module-gui/gui/input/Profile.cpp
+++ module-gui/gui/input/Profile.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include <log/log.hpp>
@@ -9,31 +9,33 @@
 
 namespace gui
 {
+
     Profile::Profile(const std::filesystem::path &filepath)
     {
-        profileJson = createJson(filepath);
+        name       = filepath.stem();
+        inputChars = createJson(filepath);
     }
 
     const std::string &Profile::getName() noexcept
     {
-        return profileJson[nameKey].string_value();
+        return name;
     }
 
     const json11::Json &Profile::getJson() const noexcept
     {
-        return profileJson;
+        return inputChars;
     }
 
     const json11::Json Profile::createJson(const std::string &filepath)
     {
-        const auto fd = std::fopen(filepath.c_str(), "r");
+        auto fd = std::fopen(filepath.c_str(), "r");
 
         if (fd == nullptr) {
             LOG_FATAL("Error during opening file %s", filepath.c_str());
             return json11::Json();
         }
 
-        const auto fsize = std::filesystem::file_size(filepath);
+        uint32_t fsize = std::filesystem::file_size(filepath);
 
         auto stream = std::make_unique<char[]>(fsize + 1);
 
@@ -42,7 +44,7 @@ namespace gui
         std::fread(stream.get(), 1, fsize, fd);
 
         std::string err;
-        const auto parsedJson = json11::Json::parse(stream.get(), err);
+        json11::Json parsedJson = json11::Json::parse(stream.get(), err);
 
         auto _ = gsl::finally([fd] { std::fclose(fd); });
 
@@ -53,13 +55,14 @@ namespace gui
         return parsedJson;
     }
 
-    std::uint32_t Profile::getCharKey(bsp::KeyCodes code, std::uint32_t times)
+    uint32_t Profile::getCharKey(bsp::KeyCodes code, uint32_t times)
     {
-        const auto ts  = profileJson[utils::to_string(static_cast<int>(code))].string_value();
-        const auto utf = UTF8(ts);
+        std::string ts = inputChars[utils::to_string(static_cast<int>(code))].string_value();
+        UTF8 utf       = UTF8(ts);
         if (ts.size() > 0) {
             return utf[times % utf.length()];
         }
         return utf[0];
     }
-} // namespace gui
+
+} /* namespace gui */
