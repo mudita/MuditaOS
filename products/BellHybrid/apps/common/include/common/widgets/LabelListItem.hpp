99b0b485386dd059a99ee2c7fb590495b8fc0581
Dawid Wojtas <dawid.wojtas@mudita.com>
2024-03-05 12:48:39 +0100

    Move part relaxation code to common section

---
+++ products/BellHybrid/apps/common/include/common/widgets/LabelListItem.hpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+//#include "data/RelaxationCommon.hpp"
+#include <common/data/RelaxationCommon.hpp>
+#include "ListItem.hpp"
+#include "widgets/TextWithIconsWidget.hpp"
+
+namespace gui
+{
+    class RelaxationItem : public ListItem
+    {
+      private:
+        app::relaxation::MusicType musicType{};
+
+      public:
+        explicit RelaxationItem(app::relaxation::MusicType musicType);
+        virtual ~RelaxationItem() = default;
+        app::relaxation::MusicType getMusicType();
+    };
+
+    class RelaxationMarkerItem : public ListItem
+    {
+      public:
+        explicit RelaxationMarkerItem(const UTF8 &labelText);
+        virtual ~RelaxationMarkerItem() = default;
+    };
+} // namespace gui
