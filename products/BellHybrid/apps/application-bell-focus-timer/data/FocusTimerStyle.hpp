7ecb6fc96897d4044a6425b7d454f87c46605392
rrandomsky <rafal.radomski@mudita.com>
2024-04-05 22:08:53 +0200

    [BH-1899] Focus timer basic
    Added a basic version of the Focuse timer function.
    This basic version allows the user to run the focus timer with
    only the default parameters. Focus time is set at 25 minutes,
    short break at 5 minutes for 10 focus intervals.

---
+++ products/BellHybrid/apps/application-bell-focus-timer/data/FocusTimerStyle.hpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <Style.hpp>
+#include "widgets/BellBaseLayout.hpp"
+
+namespace app::focusTimerStyle
+{
+    namespace runningStyle
+    {
+        namespace progress
+        {
+            inline constexpr auto radius                   = 192u;
+            inline constexpr auto penWidth                 = 3u;
+            inline constexpr auto verticalDeviationDegrees = 38u;
+        } // namespace progress
+
+        namespace timer
+        {
+            inline constexpr auto font      = style::window::font::supersizeme;
+            inline constexpr auto maxSizeX  = 340u;
+            inline constexpr auto maxSizeY  = 198u;
+            inline constexpr auto marginTop = 19u;
+        } // namespace timer
+
+        namespace pauseIcon
+        {
+            inline constexpr auto image     = "big_pause";
+            inline constexpr auto maxSizeX  = 203u;
+            inline constexpr auto maxSizeY  = 203u;
+            inline constexpr auto marginTop = timer::marginTop - (maxSizeY - timer::maxSizeY);
+        } // namespace pauseIcon
+
+        namespace ringIcon
+        {
+            inline constexpr auto image     = "big_bell_ringing";
+            inline constexpr auto maxSizeX  = 220u;
+            inline constexpr auto maxSizeY  = 203u;
+            inline constexpr auto marginTop = timer::marginTop - (maxSizeY - timer::maxSizeY);
+        } // namespace ringIcon
+
+        namespace bottomDescription
+        {
+            inline constexpr auto marginTop = 38u;
+            inline constexpr auto maxSizeX  = style::bell_base_layout::w;
+            inline constexpr auto maxSizeY  = 42u;
+            inline constexpr auto font      = style::window::font::verybig;
+        } // namespace bottomDescription
+
+        namespace clock
+        {
+            inline constexpr auto marginTop = 17u;
+            inline constexpr auto maxSizeX  = 340u;
+            inline constexpr auto maxSizeY  = 84u;
+        } // namespace clock
+    }     // namespace runningStyle
+
+    namespace timerStyle
+    {
+        namespace text
+        {
+            inline constexpr auto font = style::window::font::supersizemelight;
+        }
+
+        namespace minute
+        {
+            inline constexpr auto font = style::window::font::largelight;
+        }
+    } // namespace timerStyle
+} // namespace app::focusTimerStyle
--- products/BellHybrid/apps/application-bell-focus-timer/presenter/FocusTimerPresenter.cpp
+++ products/BellHybrid/apps/application-bell-focus-timer/presenter/FocusTimerPresenter.cpp
@@ -3,13 +3,184 @@
 
 #include "FocusTimerPresenter.hpp"
 
+#include "FocusCommon.hpp"
+#include "Application.hpp"
+#include "widgets/ProgressTimer.hpp"
+
+#include <common/LanguageUtils.hpp>
+#include <common/models/TimeModel.hpp>
+#include <common/windows/BellFinishedWindow.hpp>
+#include <service-db/Settings.hpp>
+
+namespace
+{
+    static constexpr std::chrono::milliseconds betweenSessionDelayTime{5000};
+} // namespace
+
 namespace app::focus
 {
+
     FocusTimerPresenter::FocusTimerPresenter(app::ApplicationCommon *app,
                                              settings::Settings *settings,
+                                             std::unique_ptr<AbstractTimeModel> timeModel,
                                              AbstractBatteryModel &batteryModel,
                                              AbstractLowBatteryInfoModel &lowBatteryInfoModel)
-        : app{app}, settings{settings}, batteryModel{batteryModel}, lowBatteryInfoModel{lowBatteryInfoModel}
-    {}
+        : app{app}, settings{settings}, batteryModel{batteryModel},
+          lowBatteryInfoModel{lowBatteryInfoModel}, timeModel{std::move(timeModel)}
+    {
+        //        focusSessionDuration = std::chrono::minutes{
+        //                utils::getNumericValue<int>(settings->getValue(focusDBRecordName,
+        //                settings::SettingsScope::AppLocal))};
+        //        shortBreakDuration = std::chrono::minutes{
+        //                utils::getNumericValue<int>(settings->getValue(focusDBRecordName,
+        //                settings::SettingsScope::AppLocal))};
+        //        focusSessionsLeft =
+        //                utils::getNumericValue<int>(settings->getValue(focusDBRecordName,
+        //                settings::SettingsScope::AppLocal));
+
+        focusSessionDuration = std::chrono::minutes{25};
+        shortBreakDuration   = std::chrono::minutes{5};
+
+        allFocusSessionsCount = 10;
+        focusSessionsLeft     = allFocusSessionsCount;
+
+        betweenSessionTimer = sys::TimerFactory::createSingleShotTimer(
+            app, "betweenSessionTimer", betweenSessionDelayTime, [this](sys::Timer &) { executeNextStep(); });
+    }
+
+    void FocusTimerPresenter::setTimer(std::unique_ptr<app::TimerWithCallbacks> &&_timer)
+    {
+        timer = std::move(_timer);
+        timer->registerOnFinishedCallback([this]() { executeNextStep(); });
+    }
+
+    void FocusTimerPresenter::handleUpdateTimeEvent()
+    {
+        getView()->setTime(timeModel->getCurrentTime());
+    }
+
+    void FocusTimerPresenter::start()
+    {
+        static_cast<app::Application *>(app)->suspendIdleTimer();
+        startTime();
+    }
+
+    void FocusTimerPresenter::stop()
+    {
+        finish();
+    }
+
+    bool FocusTimerPresenter::isTimerStopped()
+    {
+        return timer->isStopped();
+    }
+
+    bool FocusTimerPresenter::isAllSessionsFinished()
+    {
+        return currentTimerPhase == FocusTimerPhase::AllFocusSessionsEnded;
+    }
+
+    void FocusTimerPresenter::pause()
+    {
+        betweenSessionTimer.stop();
+        timer->stop();
+        getView()->pause();
+    }
+
+    void FocusTimerPresenter::resume()
+    {
+        isMiddleTimeBetweenBreakAndFocus() ? executeNextStep() : timer->start();
+        getView()->resume();
+    }
+
+    void FocusTimerPresenter::abandon()
+    {
+        finish();
+    }
+
+    void FocusTimerPresenter::finish()
+    {
+        timer->stop();
+        betweenSessionTimer.stop();
+        app->switchWindow(focus::window::name::main);
+    }
+
+    void FocusTimerPresenter::executeNextStep()
+    {
+        switch (currentTimerPhase) {
+        case FocusTimerPhase::FocusTime:
+            focusSessionsLeft--;
+            if (focusSessionsLeft == 0) {
+                currentTimerPhase = FocusTimerPhase::AllFocusSessionsEnded;
+                getView()->onAllFocusSessionsFinished();
+                startTime();
+            }
+            else {
+                currentTimerPhase = FocusTimerPhase::FocusTimeEnded;
+                getView()->onFocusSessionFinished();
+                startTime();
+            }
+            break;
+
+        case FocusTimerPhase::FocusTimeEnded:
+            currentTimerPhase = FocusTimerPhase::ShortBreakTime;
+            getView()->onShortBreakStarted();
+            startTime();
+            break;
+
+        case FocusTimerPhase::ShortBreakTime:
+            currentTimerPhase = FocusTimerPhase::ShortBreakTimeEnded;
+            getView()->onShortBreakFinished();
+            startTime();
+            break;
+
+        case FocusTimerPhase::ShortBreakTimeEnded:
+            currentTimerPhase = FocusTimerPhase::FocusTime;
+            getView()->onFocusSessionStarted();
+            startTime();
+            break;
+
+        case FocusTimerPhase::AllFocusSessionsEnded:
+            finish();
+            break;
+        }
+    }
+
+    void FocusTimerPresenter::onBeforeShow()
+    {
+        getView()->setTimeFormat(timeModel->getTimeFormat());
+    }
+
+    void FocusTimerPresenter::startTime()
+    {
+        switch (currentTimerPhase) {
+        case FocusTimerPhase::FocusTime:
+            timer->reset(std::chrono::minutes(focusSessionDuration));
+            timer->start();
+            break;
+
+        case FocusTimerPhase::ShortBreakTime:
+            timer->reset(std::chrono::minutes(shortBreakDuration));
+            timer->start();
+            break;
+
+        case FocusTimerPhase::FocusTimeEnded:
+        case FocusTimerPhase::ShortBreakTimeEnded:
+        case FocusTimerPhase::AllFocusSessionsEnded:
+            betweenSessionTimer.start();
+            break;
+        }
+    }
+
+    bool FocusTimerPresenter::isMiddleTimeBetweenBreakAndFocus()
+    {
+        switch (currentTimerPhase) {
+        case FocusTimerPhase::FocusTimeEnded:
+        case FocusTimerPhase::ShortBreakTimeEnded:
+            return true;
+        default:
+            return false;
+        }
+    }
 
 } // namespace app::focus
