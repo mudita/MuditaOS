1eaf2e35bf41a1b69ce14b256e52a87a50fba791
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2024-05-28 11:58:12 +0200

    [BH-1946] Add update instructions in settings
    User will see instructions on how to update Harmony operating system at
    any time in settings.

---
+++ products/BellHybrid/apps/application-bell-settings/presenter/UpdateInstructionWindowPresenter.hpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <common/windows/UpdateInstructionWindowContract.hpp>
+#include <apps-common/ApplicationCommon.hpp>
+
+#include <vector>
+#include <Item.hpp>
+
+namespace app::bell_settings
+{
+    class UpdateInstructionWindowPresenter : public gui::UpdateInstructionWindowContract::Presenter
+    {
+      private:
+        app::ApplicationCommon *app;
+        std::vector<gui::Item *> layoutOptions;
+        void initLayoutOptions();
+
+      public:
+        explicit UpdateInstructionWindowPresenter(app::ApplicationCommon *app);
+
+        std::vector<gui::Item *> getLayouts() const override;
+        bool isLastLayout(const gui::Item *layout) const override;
+        gui::Item *getFirstLayout() const override;
+        bool onInput(const gui::InputEvent &inputEvent, const gui::Item *currentLayout) override;
+    };
+} // namespace app::bell_settings
