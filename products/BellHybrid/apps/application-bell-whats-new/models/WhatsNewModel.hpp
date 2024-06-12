611a242f7e27115f7d0f2a0db8be49acdaefb254
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2024-06-12 10:45:35 +0200

    [BH-1989] Add mechanism for downloading What's New entries
    Created a parser that downloads all the necessary
    entries from the database to be displayed in
    What's New application.

--- products/BellHybrid/apps/application-bell-whats-new/models/WhatsNewModel.hpp
+++ products/BellHybrid/apps/application-bell-whats-new/models/WhatsNewModel.hpp
@@ -3,11 +3,29 @@
 
 #pragma once
 
+#include <string>
+#include <vector>
+
+namespace app
+{
+    class ApplicationCommon;
+}
+
 namespace app::whatsNew::models
 {
+    struct Feature
+    {
+        const std::string description;
+        const std::string iconName;
+    };
+
     class WhatsNewModel
     {
       public:
-        WhatsNewModel();
+        explicit WhatsNewModel(app::ApplicationCommon *app);
+
+      private:
+        app::ApplicationCommon *app{nullptr};
+        std::vector<Feature> features;
     };
 } // namespace app::whatsNew::models
