88b5721b85b5e34381c2ea919d524d434fee5518
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2024-06-12 10:45:35 +0200

    [BH-1989] Add mechanism for downloading What's New entries
    Created a parser that downloads all the necessary
    entries from the database to be displayed in
    What's New application.

---
+++ products/BellHybrid/services/db/include/db/WhatsNewMessages.hpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <MessageType.hpp>
+#include <Service/Message.hpp>
+#include <vector>
+
+namespace service::db::whatsNew
+{
+    struct Record
+    {
+        std::string description;
+        std::string iconName;
+    };
+
+    struct VersionNumber
+    {
+        std::uint16_t major;
+        std::uint16_t minor;
+        std::uint16_t patch;
+    };
+
+    namespace messages
+    {
+        struct GetByVersion : public sys::DataMessage
+        {
+            explicit GetByVersion(VersionNumber version) : version{version}
+            {}
+            VersionNumber version;
+        };
+
+        struct Response : public sys::ResponseMessage
+        {
+            Response() = default;
+            explicit Response(const std::vector<Record> &records) : records{records}
+            {}
+
+            std::vector<Record> records{};
+        };
+
+    } // namespace messages
+} // namespace service::db::whatsNew
