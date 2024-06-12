56301b0c18674e8288700de93ae26d43481d3504
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2024-06-12 10:45:35 +0200

    [BH-1989] Add mechanism for downloading What's New entries
    Created a parser that downloads all the necessary
    entries from the database to be displayed in
    What's New application.

---
+++ products/BellHybrid/services/db/agents/WhatsNewAgent.hpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
+// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+#pragma once
+
+#include <service-db/DatabaseAgent.hpp>
+#include <module-services/service-db/include/service-db/SettingsMessages.hpp>
+#include <module-sys/Service/include/Service/Message.hpp>
+
+#include <string>
+
+namespace sys
+{
+    class Service;
+} // namespace sys
+
+namespace service::db::agents
+{
+    class WhatsNew : public DatabaseAgent
+    {
+      public:
+        WhatsNew(sys::Service *parentService, std::string dbName);
+
+        void registerMessages() override;
+        void unRegisterMessages() override;
+        auto getAgentName() -> const std::string override;
+
+      private:
+        sys::MessagePointer handleGetRecordsByVersion(const sys::Message *req);
+
+        std::string dbName;
+        Database db;
+    };
+} // namespace service::db::agents
