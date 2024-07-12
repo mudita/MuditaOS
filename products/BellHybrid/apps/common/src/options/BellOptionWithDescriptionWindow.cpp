mudita/MuditaOS

commit c6192fd9120effd28a9ac5b2391e7bfec7964e47

[BH-1989][BH-1990] Core functionality of What's New app
Implemented core functionality of What's New
application.

products/BellHybrid/apps/common/src/options/BellOptionWithDescriptionWindow.cpp
@@ -14,8 +14,7 @@ namespace gui
     {
         constexpr auto one_option_height      = style::bell_options::h + 2U * style::bell_options::option_margin;
         constexpr auto option_layout_height   = 2U * one_option_height;
-        constexpr auto description_height     = 175U;
-        constexpr auto max_description_height = description_height + 16U;
+        constexpr auto max_to_min_difference  = 16U;
         constexpr auto top_center_margin      = -14;
     } // namespace
 
@@ -62,12 +61,12 @@ namespace gui
         setFocusItem(optionsList);
     }
 
-    void BellOptionWithDescriptionWindow::setListTitle(const std::string &title)
+    void BellOptionWithDescriptionWindow::setListTitle(const std::string &title, const std::string &font)
     {
         auto titleBody = new TextFixedSize(body->firstBox);
         titleBody->drawUnderline(false);
         titleBody->setAlignment(Alignment(Alignment::Horizontal::Center, Alignment::Vertical::Top));
-        titleBody->setFont(style::window::font::largelight);
+        titleBody->setFont(font);
         titleBody->setMinimumWidth(style::bell_base_layout::outer_layouts_w);
         titleBody->setEdges(RectangleEdge::None);
         titleBody->setEditMode(EditMode::Browse);
@@ -80,19 +79,27 @@ namespace gui
         body->resizeItems();
     }
 
-    void BellOptionWithDescriptionWindow::setListDescription(const std::string &title)
+    void BellOptionWithDescriptionWindow::setListDescription(const std::string &title,
+                                                             TokenMap tokenMap,
+                                                             const std::string &font,
+                                                             const unsigned height)
     {
         auto descriptionBody = new TextFixedSize(body->centerBox);
         descriptionBody->drawUnderline(false);
         descriptionBody->setAlignment(Alignment(Alignment::Horizontal::Center, Alignment::Vertical::Center));
-        descriptionBody->setFont(style::window::font::verybiglight);
-        descriptionBody->setMinimumSize(style::window_width, description_height);
+        descriptionBody->setFont(font);
+        descriptionBody->setMinimumSize(style::window_width, height);
         descriptionBody->setEditMode(EditMode::Browse);
-        descriptionBody->setRichText(title);
+        if (tokenMap.has_value()) {
+            descriptionBody->setRichText(title, std::move(tokenMap.value()));
+        }
+        else {
+            descriptionBody->setText(title);
+        }
 
         body->centerBox->setMargins(gui::Margins{0, top_center_margin, 0, 0});
-        body->centerBox->setMaximumHeight(max_description_height);
-        body->centerBox->setMinimumHeight(description_height);
+        body->centerBox->setMaximumHeight(height + max_to_min_difference);
+        body->centerBox->setMinimumHeight(height);
         body->centerBox->resizeItems();
         body->resizeItems();
     }

module-apps/apps-common/widgets/BellSideListItem.hpp
@@ -17,7 +17,7 @@ namespace gui
         void setBottomDescriptionText(const std::string &description);
 
       protected:
-        BellSideListItem(BellBaseLayout::LayoutType type = BellBaseLayout::LayoutType::WithArrows);
+        explicit BellSideListItem(BellBaseLayout::LayoutType type = BellBaseLayout::LayoutType::WithArrows);
         void setupBottomTextBox(const std::string &description);
         void setupTopTextBox(const std::string &description);
 

module-db/Common/Types.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once

products/BellHybrid/apps/application-bell-meditation-timer/CMakeLists.txt
@@ -27,7 +27,6 @@ target_sources(application-bell-meditation-timer
         presenter/MeditationProgressPresenter.cpp
         presenter/MeditationTimerPresenter.cpp
         presenter/ReadyGoingPresenter.cpp
-        presenter/SessionEndedPresenter.cpp
         presenter/SettingsPresenter.cpp
         presenter/StatisticsPresenter.cpp
         windows/MeditationMainWindow.cpp

products/BellHybrid/apps/application-bell-meditation-timer/presenter/SessionEndedPresenter.cpp
@@ -1,17 +1,0 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
-// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
-
-#include "SessionEndedPresenter.hpp"
-
-#include <service-appmgr/Controller.hpp>
-
-namespace app::meditation
-{
-    SessionEndedPresenter::SessionEndedPresenter(app::ApplicationCommon *app) : app{app}
-    {}
-
-    void SessionEndedPresenter::activate()
-    {
-        app::manager::Controller::sendAction(app, app::manager::actions::Home);
-    }
-} // namespace app::meditation

products/BellHybrid/apps/application-bell-meditation-timer/presenter/SessionEndedPresenter.hpp
@@ -1,39 +1,0 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
-// For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
-
-#pragma once
-
-#include <Application.hpp>
-#include <apps-common/BasePresenter.hpp>
-
-namespace app
-{
-    class ApplicationCommon;
-}
-
-namespace app::meditation
-{
-    class SessionEndedPresenterContract
-    {
-      public:
-        class View
-        {
-          public:
-            virtual ~View() = default;
-        };
-        class Presenter : public BasePresenter<SessionEndedPresenterContract::View>
-        {
-          public:
-            virtual void activate() = 0;
-        };
-    };
-
-    class SessionEndedPresenter : public SessionEndedPresenterContract::Presenter
-    {
-        app::ApplicationCommon *app{};
-        void activate() override;
-
-      public:
-        explicit SessionEndedPresenter(app::ApplicationCommon *app);
-    };
-} // namespace app::meditation

products/BellHybrid/apps/application-bell-onboarding/ApplicationBellOnBoarding.cpp
@@ -97,7 +97,6 @@ namespace app
 
     void ApplicationBellOnBoarding::createUserInterface()
     {
-
         windowsFactory.attach(gui::name::window::main_window, [this](ApplicationCommon *app, const std::string &name) {
             auto powerOffPresenter = std::make_unique<gui::BellPowerOffPresenter>(app);
             return std::make_unique<gui::OnBoardingOnOffWindow>(app, std::move(powerOffPresenter), name);

products/BellHybrid/apps/application-bell-powernap/data/PowerNapListItem.hpp
@@ -1,7 +1,8 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
+
 #include <apps-common/widgets/BellSideListItem.hpp>
 #include <apps-common/widgets/spinners/Spinners.hpp>
 

commit 9000735abd122dd8a71acdc5ad582ef29a88d718

[BH-2037] Fix text parsing on factory reset screen
Fix of a regression introduced by c6192fd9,
resulting in BellOptionWithDescriptionWindow
not parsing rich text in case no token map
is provided.

products/BellHybrid/apps/common/src/options/BellOptionWithDescriptionWindow.cpp
@@ -94,7 +94,7 @@ namespace gui
             descriptionBody->setRichText(title, std::move(tokenMap.value()));
         }
         else {
-            descriptionBody->setText(title);
+            descriptionBody->setRichText(title);
         }
 
         body->centerBox->setMargins(gui::Margins{0, top_center_margin, 0, 0});