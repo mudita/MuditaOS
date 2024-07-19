d10ccbe4f7a40513d93381f5e01a7cc8dcc61897
Lefucjusz <marcin.lyda@mudita.com>
2024-07-19 11:44:59 +0200

    [BH-2048] Major Eink driver refactor
    * Cleaned up BSP layer of Eink driver.
    * Removed changing TCON config with
    different refresh modes, used one set
    of values for all modes instead.
    * Moved coordinates transformation
    routines to separate file.
    * Minor fixes in 2bpp transformation
    routine.
    * Cleanups in lots of various
    places.

--- module-services/service-eink/ServiceEink.cpp
+++ module-services/service-eink/ServiceEink.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2017-2023, Mudita Sp. z.o.o. All rights reserved.
+﻿// Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include <board.h>
@@ -461,7 +461,7 @@ namespace service::eink
     {
         const auto waveformUpdateMsg = static_cast<service::eink::PrepareDisplayEarlyRequest *>(message);
         display->prepareEarlyRequest(translateToEinkRefreshMode(waveformUpdateMsg->getRefreshMode()),
-                                     hal::eink::WaveformTemperature::MEASURE_NEW);
+                                     hal::eink::WaveformTemperature::MeasureNew);
         return sys::MessageNone{};
     }
 
