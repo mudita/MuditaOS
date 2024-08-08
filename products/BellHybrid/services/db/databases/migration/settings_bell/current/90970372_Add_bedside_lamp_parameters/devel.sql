22e538033ac10b8679125315ecee1bf3f03a310a
Lefucjusz <marcin.lyda@mudita.com>
2024-08-08 14:02:03 +0200

    add database migration

---
+++ products/BellHybrid/services/db/databases/migration/settings_bell/current/90970372_Add_bedside_lamp_parameters/devel.sql
@@ -0,0 +1,6 @@
+-- Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
+-- For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+-- Message: Add bedside lamp parameters
+-- Revision: 90970372-6151-4de3-91dd-f1c8684bc6f4
+-- Create Date: 2024-08-08 13:56:09
