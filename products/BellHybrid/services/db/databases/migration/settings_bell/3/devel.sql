46589fde4d572f981a232c079021fd425f8b56a4
Lefucjusz <marcin.lyda@mudita.com>
2024-09-24 18:01:13 +0200

    [BH-2070] Commit database migrations for 2.9.0
    Commit all database migrations before
    2.9.0 release.

--- products/BellHybrid/services/db/databases/migration/settings_bell/3/devel.sql
+++ products/BellHybrid/services/db/databases/migration/settings_bell/3/devel.sql
@@ -1,5 +1,9 @@
 -- Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
--- For licensing, see https://github.com/mudita/MuditaOS/blob/master/LICENSE.md
+-- For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+
+-- Message: Add bedside lamp parameters
+-- Revision: 90970372-6151-4de3-91dd-f1c8684bc6f4
+-- Create Date: 2024-08-08 13:56:09
 
 -- Message: Enable relaxation fade effect by default
 -- Revision: 39114449-29a6-42a3-a239-57b8c726501c
@@ -7,4 +11,3 @@
 
 -- Insert SQL here
 
-DELETE FROM settings_tab WHERE path = 'relaxation_fade_active';
