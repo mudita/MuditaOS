6e0117695a2796fee39ae52c8d6eb85227ace95a
Lefucjusz <marcin.lyda@mudita.com>
2024-09-24 18:01:13 +0200

    [BH-2070] Commit database migrations for 2.9.0
    Commit all database migrations before
    2.9.0 release.

--- products/BellHybrid/services/db/databases/migration/settings_bell/current/39114449_Enable_relaxation_fade_effect_by_default/devel.sql
+++
@@ -1,8 +0,0 @@
--- Copyright (c) 2017-2024, Mudita Sp. z.o.o. All rights reserved.
--- For licensing, see https://github.com/mudita/MuditaOS/blob/master/LICENSE.md
-
--- Message: Enable relaxation fade effect by default
--- Revision: 39114449-29a6-42a3-a239-57b8c726501c
--- Create Date: 2024-09-05 11:37:44
-
--- Insert SQL here
