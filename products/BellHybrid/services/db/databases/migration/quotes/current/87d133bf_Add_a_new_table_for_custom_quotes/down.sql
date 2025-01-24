001e70aded5dba8cdf19f09cacd063a49bed0b7a
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2025-01-24 08:17:34 +0100

    [BH-2098] Add new table with custom quotes to the database
    A new table has been added to the quotes database for user-added quotes.

---
+++ products/BellHybrid/services/db/databases/migration/quotes/current/87d133bf_Add_a_new_table_for_custom_quotes/down.sql
@@ -0,0 +1,9 @@
+-- Copyright (c) 2017-2025, Mudita Sp. z.o.o. All rights reserved.
+-- For licensing, see https://github.com/mudita/MuditaOS/blob/master/LICENSE.md
+
+-- Message: Add a new table for custom quotes
+-- Revision: 87d133bf-f98c-429d-9f15-6963fd72f7e1
+-- Create Date: 2025-01-23 15:32:56
+
+-- Insert SQL here
+DROP TABLE custom_quote_table;
