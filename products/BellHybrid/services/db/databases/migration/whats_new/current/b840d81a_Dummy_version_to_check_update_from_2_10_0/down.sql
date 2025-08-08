d9fcb9306a4f6b2830cad94a188cca34e664eb96
Lefucjusz <marcin.lyda@mudita.com>
2025-08-08 13:46:37 +0200

    Dummy migration to test update from 2.10.0

---
+++ products/BellHybrid/services/db/databases/migration/whats_new/current/b840d81a_Dummy_version_to_check_update_from_2_10_0/down.sql
@@ -0,0 +1,11 @@
+-- Copyright (c) 2017-2025, Mudita Sp. z.o.o. All rights reserved.
+-- For licensing, see https://github.com/mudita/MuditaOS/blob/master/LICENSE.md
+
+-- Message: Dummy version to check update from 2 10 0
+-- Revision: b840d81a-76ed-4a4b-a47b-73b205585165
+-- Create Date: 2025-08-08 13:45:15
+
+-- Insert SQL here
+
+DELETE FROM whats_new WHERE Major = 2 AND Minor = 10 AND Patch = 1 AND English_title = 'Dummy test 1';
+DELETE FROM whats_new WHERE Major = 2 AND Minor = 10 AND Patch = 1 AND English_title = 'Dummy test 2';
---
+++ products/BellHybrid/services/db/databases/migration/whats_new/current/b840d81a_Dummy_version_to_check_update_from_2_10_0/up.sql
@@ -0,0 +1,12 @@
+-- Copyright (c) 2017-2025, Mudita Sp. z.o.o. All rights reserved.
+-- For licensing, see https://github.com/mudita/MuditaOS/blob/master/LICENSE.md
+
+-- Message: Dummy version to check update from 2 10 0
+-- Revision: b840d81a-76ed-4a4b-a47b-73b205585165
+-- Create Date: 2025-08-08 13:45:15
+
+-- Insert SQL here
+
+INSERT OR REPLACE INTO whats_new (Major, Minor, Patch, English_title, Deutsch_title, Español_title, Polski_title, Français_title, Icon, English_desc, Deutsch_desc, Español_desc, Polski_desc, Français_desc) VALUES
+    (2, 10, 1, 'Dummy test 1', 'Test 1 German', 'Test 1 Spanish', 'Test 1 Polski', 'Test 1 French', 'whatsnew_info_W_M', 'Test message 1 in English.', 'Test message 1 in German.', 'Test message 1 in Spanish.', 'Wiadomość testowa 1 po polsku.', 'Test message 1 in French.'),
+    (2, 10, 1, 'Dummy test 2', 'Test 2 German', 'Test 2 Spanish', 'Test 2 Polski', 'Test 2 French', 'whatsnew_quote_W_M', 'Test message 2 in English.', 'Test message 2 in German.', 'Test message 2 in Spanish.', 'Wiadomość testowa 2 po polsku.', 'Test message 2 in French.');
